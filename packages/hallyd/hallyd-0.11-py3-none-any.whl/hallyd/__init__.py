26017e952dc01604e3ae9ab17280d26c6354207d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:47 +0000

    Add package hallyd/0.11/hallyd-0.11-py3-none-any.whl

---
+++ packages/hallyd/hallyd-0.11-py3-none-any.whl/hallyd/__init__.py
@@ -0,0 +1,24 @@
+#  SPDX-FileCopyrightText: © 2021 Josef Hahn
+#  SPDX-License-Identifier: AGPL-3.0-only
+"""
+Hallyd.
+
+See submodules.
+"""
+import hallyd.bindle
+import hallyd.cleanup
+import hallyd.coding
+import hallyd.disk
+import hallyd.fs
+import hallyd.fs_monitor
+import hallyd.io
+import hallyd.ipc
+import hallyd.ipc_hub
+import hallyd.lang
+import hallyd.net
+import hallyd.py_module
+import hallyd.services
+import hallyd.subprocess
+import hallyd.terminal
+import hallyd.text
+import hallyd.typing
