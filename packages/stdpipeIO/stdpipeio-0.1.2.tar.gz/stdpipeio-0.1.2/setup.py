35676d7e1868693cda27c325d8fc2081c414f03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:03:30 +0000

    Add package stdpipeIO/0.1.2/stdpipeio-0.1.2.tar.gz

---
+++ packages/stdpipeIO/stdpipeio-0.1.2.tar.gz/stdpipeio-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stdpipeIO',
+    version='0.1.2',
+    author='Greendog',
+    author_email='x6reend0g@foxmail.com',
+    description='A simple decorator for receiving and sending data via the standard pipe',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    test_suite="tests",
+)
