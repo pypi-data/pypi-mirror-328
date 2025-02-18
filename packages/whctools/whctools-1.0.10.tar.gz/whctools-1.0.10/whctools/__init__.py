a2b988a7c706ea275b6301795af7c45674ba71d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:49 +0000

    Add package whctools/1.0.10/whctools-1.0.10.tar.gz

---
+++ packages/whctools/whctools-1.0.10.tar.gz/whctools-1.0.10/whctools/__init__.py
@@ -0,0 +1,7 @@
+"""whctools plugin app for Alliance Auth."""
+
+# pylint: disable = invalid-name
+default_app_config = "whctools.apps.WhctoolsConfig"
+
+__version__ = "1.0.10"
+__title__ = "WHC Tools"
