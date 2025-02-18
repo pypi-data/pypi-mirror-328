23c0ab8a7b6c760b775a937f36855a95a8f8c961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:47 +0000

    Add package chia-blockchain/2.5.2rc1/chia_blockchain-2.5.2rc1.tar.gz

---
+++ packages/chia-blockchain/chia_blockchain-2.5.2rc1.tar.gz/chia_blockchain-2.5.2rc1/chia/types/generator_types.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from chia.types.blockchain_format.serialized_program import SerializedProgram
+from chia.util.streamable import Streamable, streamable
+
+
+@streamable
+@dataclass(frozen=True)
+class BlockGenerator(Streamable):
+    program: SerializedProgram
+    generator_refs: list[bytes]
