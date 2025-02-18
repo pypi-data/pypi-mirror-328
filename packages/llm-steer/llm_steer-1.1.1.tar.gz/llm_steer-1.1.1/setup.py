5c18d05f47fbc0a58be160c8989c2c238f20c365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:25:18 +0000

    Add package llm-steer/1.1.1/llm_steer-1.1.1.tar.gz

---
+++ packages/llm-steer/llm_steer-1.1.1.tar.gz/llm_steer-1.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='llm_steer',
+    version='1.1.1',
+    description='Steer LLM outputs towards a certain topic/subject and enhance response capabilities using activation engineering by adding steer vectors',
+    author='Mihai Chirculescu',
+    author_email='apropodemine@gmail.com',
+    py_modules=['llm_steer'],
+    url="https://github.com/Mihaiii/llm_steer",
+    install_requires=[
+        'transformers'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
