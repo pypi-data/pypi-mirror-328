f430abeb2de282ede5ad9f7830414b2b395f6c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:25:29 +0000

    Add package idade3x-yan/0.1.1/idade3x_yan-0.1.1-py3-none-any.whl

---
+++ packages/idade3x-yan/idade3x_yan-0.1.1-py3-none-any.whl/bagulho/__init__.py
@@ -0,0 +1,6 @@
+def bagulho_yan():
+    idade = input("Qual sua idade? ")
+    idade3x = int(idade) * 3
+    print(f"Se você viver mais o dobro da sua idade você viverá até os {idade3x} anos de idade!")
+
+bagulho_yan()
