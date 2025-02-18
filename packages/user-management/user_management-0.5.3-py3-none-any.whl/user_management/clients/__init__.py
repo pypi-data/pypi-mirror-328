39170aaa2076528efb2a13512763b2cbd7db6349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:27:49 +0000

    Add package user-management/0.5.3/user_management-0.5.3-py3-none-any.whl

---
+++ packages/user-management/user_management-0.5.3-py3-none-any.whl/user_management/clients/__init__.py
@@ -0,0 +1,8 @@
+from .db_clients import UserClient, SubscriptionClient, ProductClient, CustomerClient
+
+__all__ = [
+    'UserClient',
+    'SubscriptionClient',
+    'ProductClient',
+    'CustomerClient'
+] 
---
+++ packages/user-management/user_management-0.5.3-py3-none-any.whl/user_management/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .customer import Customer
+from .subscription import Subscription
+from .product import Product
+
+__all__ = ['User', 'Customer', 'Subscription', 'Product'] 
