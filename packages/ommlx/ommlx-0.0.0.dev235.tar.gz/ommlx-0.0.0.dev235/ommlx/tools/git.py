da0a03462dc92ee94376bcd0a95804451c2c310c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:50 +0000

    Add package ommlx/0.0.0.dev235/ommlx-0.0.0.dev235.tar.gz

---
+++ packages/ommlx/ommlx-0.0.0.dev235.tar.gz/ommlx-0.0.0.dev235/ommlx/tools/git.py
@@ -0,0 +1,21 @@
+from omdev.tools.git.messages import GitMessageGenerator
+from omdev.tools.git.messages import GitMessageGeneratorManifest
+
+
+##
+
+
+class AiGitMessageGenerator(GitMessageGenerator):
+    def generate_commit_message(self, args: GitMessageGenerator.GenerateCommitMessageArgs) -> str:
+        raise NotImplementedError
+
+
+#
+
+
+# @omlish-manifest
+_AI_GIT_MESSAGE_GENERATOR_MANIFEST = GitMessageGeneratorManifest(
+    mod_name=__name__,
+    attr_name='AiGitMessageGenerator',
+    name='ai',
+)
