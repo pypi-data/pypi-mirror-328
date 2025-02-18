566751ce4ac6e82bedaa724f9dc3861f875fe9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:03:31 +0000

    Add package based-random/0.1.0/based_random-0.1.0.tar.gz

---
+++ packages/based-random/based_random-0.1.0.tar.gz/based_random-0.1.0/based_random/internal.py
@@ -0,0 +1,82 @@
+from enum import Enum
+import re
+from typing import Generator, Iterable, List, TypeVar
+import requests
+from bs4 import BeautifulSoup, NavigableString
+from datetime import datetime
+import concurrent.futures
+
+_pool = concurrent.futures.ThreadPoolExecutor(max_workers=64)
+
+_base_url = 'https://based.cooking'
+
+def _get(path: str = "", **kwargs) -> requests.Response:
+    return requests.get(f'{_base_url}/{path}', **kwargs)
+
+def _time() -> int:
+    now = datetime.now()
+    return int(now.timestamp() * 10**6) 
+
+def _iter_time() -> Generator[int]:
+    while True:
+        ts = _time()
+        bit = 1
+        while ts >= bit:
+            yield 1 if ts & bit else 0
+            bit = bit << 1
+
+def _categories(soup: BeautifulSoup):
+    l = soup.find(id='tagcloud')
+    assert l is not None 
+    assert not isinstance(l, NavigableString)
+    return l.find_all('li')
+
+
+_T = TypeVar('_T')
+def _time_filter(iter: Iterable[_T]) -> List[_T]:
+    bits = _iter_time()
+    return [a for a, b in zip(iter, bits) if b == 1]
+
+def _select_recipie(selected_cat):
+    href = selected_cat.find('a')['href']
+    catlist = requests.get(href)
+    soup = BeautifulSoup(catlist.text, 'html.parser')
+    recipies = soup.find_all('a')
+
+    assert recipies is not None 
+    assert not isinstance(recipies, NavigableString)
+
+    return _time_filter(recipies)
+
+def _ingridients(recipie):
+    r = _get(recipie['href'])
+    soup = BeautifulSoup(r.text, 'html.parser')
+    ingr = soup.find(id='ingredients')
+    if ingr is None:
+        return []
+
+    ingrsul = ingr.find_next_sibling('ul')
+    if ingrsul is None:
+        return []
+
+    assert not isinstance(ingrsul, NavigableString)
+
+    ingrs = ingrsul.find_all('li')
+    return [i.text for i in ingrs]
+
+class _UnitType(Enum):
+    Freedome = 0
+    HolySI = 1
+
+def _parse_unit_type(s: str) -> _UnitType:
+    holy_si_indicators = [ # open for more details
+        r'\d+\s?g',
+        r'\d+\s?ml',
+        r'\d+\s?grams'
+    ]
+
+    for si_indicator in holy_si_indicators:
+        if re.search(si_indicator, s):
+            return _UnitType.HolySI
+
+    return _UnitType.Freedome
---
+++ packages/based-random/based_random-0.1.0.tar.gz/based_random-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='based-random',
+    version='0.1.0',    
+    description='Generates a random number based on the unit-type of the based.cooking website',
+    url='https://github.com/Giftzwerg02/based-random',
+    author='Giftzwerg02',
+    license='MIT',
+    packages=find_packages(exclude=['based_random.internal']),
+    py_modules=['based_random'],
+    install_requires=['requests',
+                      'beautifulsoup4',                     
+                      ],
+
+    classifiers=[
+    ],
+)
+
