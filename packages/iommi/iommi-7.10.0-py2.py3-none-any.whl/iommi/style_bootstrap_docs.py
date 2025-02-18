032439fbcbdac4980a9e14f2a7879223bcafa36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:04:07 +0000

    Add package iommi/7.10.0/iommi-7.10.0-py2.py3-none-any.whl

---
+++ packages/iommi/iommi-7.10.0-py2.py3-none-any.whl/iommi/style_bootstrap_docs.py
@@ -0,0 +1,22 @@
+from iommi import (
+    Asset,
+    Style,
+)
+from iommi.style_bootstrap5 import bootstrap5
+
+bootstrap_docs = Style(
+    bootstrap5,
+    root__assets__doc_style=Asset.css(attrs__href='https://docs.iommi.rocks/en/latest/_static/iframe_custom.css'),
+    root__assets__iommi_js=Asset.js(attrs=dict(src='https://docs.iommi.rocks/en/latest/_static/iommi.js')),
+    internal=True,
+    Container=dict(
+        attrs__class={
+            'container': False,
+            'mt-5': False,
+            'pt-5': False,
+        },
+        attrs__style__padding='1rem',
+    ),
+    Admin__parts__table__h_tag__attrs__style={'margin-top': '3rem'},
+
+)
