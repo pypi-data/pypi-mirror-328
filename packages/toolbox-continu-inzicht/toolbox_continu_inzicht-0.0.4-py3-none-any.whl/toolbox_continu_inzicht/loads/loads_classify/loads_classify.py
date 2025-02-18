909b35378dcf251749f36223a5df722b9bbf0611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:19 +0000

    Add package toolbox-continu-inzicht/0.0.4/toolbox_continu_inzicht-0.0.4-py3-none-any.whl

---
+++ packages/toolbox-continu-inzicht/toolbox_continu_inzicht-0.0.4-py3-none-any.whl/toolbox_continu_inzicht/loads/loads_classify/loads_classify.py
@@ -0,0 +1,107 @@
+from pydantic.dataclasses import dataclass
+from toolbox_continu_inzicht.base.data_adapter import DataAdapter
+import pandas as pd
+from typing import Optional
+
+
+@dataclass(config={"arbitrary_types_allowed": True})
+class LoadsClassify:
+    """
+    Met deze functie worden de waterstanden met opgegeven grenzen geclassificeerd.
+
+    """
+
+    data_adapter: DataAdapter
+
+    df_in_thresholds: Optional[pd.DataFrame] | None = None
+    df_in_loads: Optional[pd.DataFrame] | None = None
+    df_out: Optional[pd.DataFrame] | None = None
+
+    # Lijst met drempelwaarden per meetlocatie
+    input_schema_thresholds = {
+        "measurement_location_id": "int64",
+        "lower_boundary": "float64",
+        "upper_boundary": "float64",
+        "color": "object",
+        "label": "object",
+        "unit": "object",
+    }
+
+    # belasting per moment per meetlocaties
+    input_schema_loads = {
+        "measurement_location_id": "int64",
+        "parameter_id": "int64",
+        "unit": "object",
+        "date_time": "object",
+        "value": "float64",
+        "value_type": "object",
+        "hours": "int64",
+    }
+
+    def run(self, input: list[str], output: str) -> None:
+        """
+        De runner van de Loads Classify.
+
+        Args:
+            input List(str): [0] lijst met drempelwaarden per meetlocatie
+                             [1] belasting per moment per meetlocaties
+            output (str):    uitvoer sectie van het yaml-bestand:
+                             koppeling van de maatgevende meetlocaties per dijkvak
+
+        Returns:
+            Dataframe: Pandas dataframe met geclassificeerde waterstanden voor opgegeven momenten.
+        """
+
+        if not len(input) == 2:
+            raise UserWarning("Input variabele moet 2 string waarden bevatten.")
+
+        # drempelwaarden per meetlocatie
+        # TODO we kunnen er toch niet vanuit gaan dat de eerste (nulde) input string de verwijzing naar de klassegrenzen is...?
+        self.df_in_thresholds = self.data_adapter.input(
+            input[0], self.input_schema_thresholds
+        )
+
+        if len(self.df_in_thresholds) == 0:
+            raise UserWarning(
+                "Er zijn geen klassegrenzen beschikbaar voor de gegeven belastingen!"
+            )
+
+        # belasting per moment per meetlocaties
+        self.df_in_loads = self.data_adapter.input(input[1], self.input_schema_loads)
+
+        df_thresholds = self.df_in_thresholds.copy()
+        df_thresholds["lower_boundary"] = df_thresholds["lower_boundary"].fillna(
+            -999900
+        )
+        df_thresholds["upper_boundary"] = df_thresholds["upper_boundary"].fillna(
+            +999900
+        )
+
+        # waterstanden in centimeters
+        df_loads = self.df_in_loads.copy()
+
+        df_loads.set_index("measurement_location_id")
+        df_thresholds.set_index("measurement_location_id")
+
+        self.df_out = df_loads.merge(
+            df_thresholds, on="measurement_location_id", how="outer"
+        )
+        self.df_out = self.df_out[
+            [
+                "measurement_location_id",
+                "date_time",
+                "value",
+                "lower_boundary",
+                "upper_boundary",
+                "color",
+                "label",
+                "hours",
+            ]
+        ]
+
+        self.df_out = self.df_out[
+            (self.df_out["value"] <= self.df_out["upper_boundary"])
+            & (self.df_out["value"] > self.df_out["lower_boundary"])
+        ]
+
+        self.data_adapter.output(output=output, df=self.df_out)
---
+++ packages/toolbox-continu-inzicht/toolbox_continu_inzicht-0.0.4-py3-none-any.whl/toolbox_continu_inzicht/loads/loads_fews/loads_fews.py
@@ -0,0 +1,232 @@
+from datetime import datetime, timedelta
+from pydantic.dataclasses import dataclass
+from toolbox_continu_inzicht.base.data_adapter import DataAdapter
+import pandas as pd
+from typing import Optional, List
+from toolbox_continu_inzicht.utils.datetime_functions import (
+    datetime_from_string,
+)
+from toolbox_continu_inzicht.utils.fetch_functions import fetch_data_get
+
+
+@dataclass(config={"arbitrary_types_allowed": True})
+class LoadsFews:
+    """
+    Met deze functie worden gegevens uit de opgegeven FEWS omgeving opgehaald via REST.
+    """
+
+    data_adapter: DataAdapter
+
+    df_in: Optional[pd.DataFrame] | None = None
+    df_out: Optional[pd.DataFrame] | None = None
+
+    # Kolommen schema van de invoer data meetlocaties
+    input_schema = {
+        "measurement_location_id": "int64",
+        "measurement_location_code": "object",
+        "measurement_location_description": "object",
+    }
+
+    def run(self, input: str, output: str) -> None:
+        """
+        De runner van de Loads Fews.
+
+        Args:
+
+        Returns:
+            None
+        """
+
+        self.df_in = self.data_adapter.input(input, self.input_schema)
+
+        global_variables = self.data_adapter.config.global_variables
+        calc_time = global_variables["calc_time"]
+
+        if "LoadsFews" not in global_variables:
+            raise UserWarning("LoadsFews niet in ")
+
+        options = global_variables["LoadsFews"]
+
+        # TODO: add user mapping for aquo equivalent
+        # if "aquo_equivalent" in options:
+        #     user_aquo_mapping = dict(
+        #         zip(options["parameters"], options["aquo_equivalent"])
+        #     )
+        # else:
+        #     user_aquo_mapping = dict(zip(options["parameters"], options["parameters"]))
+
+        # moments eventueel toevoegen aan options
+        if "moments" not in options and "moments" in global_variables:
+            options["moments"] = global_variables["moments"]
+        elif "moments" not in options:
+            options["moments"] = [-24, 0, 24, 48]
+
+        # missing value controleren
+        if "MISSING_VALUE" not in options:
+            if "MISSING_VALUE" in self.data_adapter.config.global_variables:
+                options["MISSING_VALUE"] = self.data_adapter.config.global_variables[
+                    "MISSING_VALUE"
+                ]
+            else:
+                options["MISSING_VALUE"] = -999
+
+        url = self.create_url(options=options)
+        parameters = self.create_params(
+            calc_time=calc_time,
+            options=options,
+            moments=options["moments"],
+            locations=self.df_in,
+        )
+        status, json_data = fetch_data_get(
+            url=url, params=parameters, mime_type="json", path_certificate=None
+        )
+
+        if status is None and json_data is not None:
+            self.df_out = self.create_dataframe(
+                options=options,
+                calc_time=calc_time,
+                json_data=json_data,
+                locations=self.df_in,
+            )
+
+            self.data_adapter.output(output=output, df=self.df_out)
+
+        return self.df_out
+
+    def create_url(self, options: dict) -> str:
+        """
+        Maak een rest url voor FEWS
+
+        Args:
+            options (_type_): Options uit de invoer yaml
+
+        Returns:
+            str: URL
+        """
+        host = options["host"]
+        port = options["port"]
+        region = options["region"]
+
+        return f"{host}:{port}/FewsWebServices/rest/{region}/v1/timeseries"
+
+    def create_params(
+        self, calc_time: datetime, options: dict, moments: List, locations: pd.DataFrame
+    ) -> dict:
+        """
+        Maak een lijst van FEWS parameters om mee te sturen bij het ophalen van data.
+
+        Args:
+            calc_time (datetime): T0 in UTC
+            options (_type_): options uit de invoer yaml
+
+        Returns:
+            dict: lijst met parameters
+        """
+        params = {}
+
+        if len(moments) > 0:
+            n_moments = len(moments) - 1
+            starttime = calc_time + timedelta(hours=int(moments[0]))
+            endtime = calc_time + timedelta(hours=int(moments[n_moments]))
+
+            params["filterId"] = options["filter"]
+            params["useDisplayUnits"] = False
+            params["showThresholds"] = True
+            params["omitMissing"] = True
+            params["onlyHeaders"] = False
+            params["showEnsembleMemberIds"] = False
+            params["documentVersion"] = options["version"]
+            params["documentFormat"] = "PI_JSON"
+            params["forecastCount"] = 1
+            params["parameterIds"] = options["parameters"]
+            params["locationIds"] = locations["measurement_location_code"].tolist()
+            params["startTime"] = starttime.strftime("%Y-%m-%dT%H:%M:%SZ")
+            params["endTime"] = endtime.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        return params
+
+    def create_dataframe(
+        self,
+        options: dict,
+        calc_time: datetime,
+        json_data: str,
+        locations: pd.DataFrame,
+    ) -> pd.DataFrame:
+        """Maak een pandas dataframe
+
+        Args:
+            json_data (str): JSON data
+
+        Returns:
+            Dataframe: Pandas dataframe geschikt voor uitvoer
+            definition:
+                - Meetlocatie id (measurement_location_id)
+                - Meetlocatie code (measurement_location_code)
+                - Meetlocatie omschrijving/naam (measurement_location_description)
+                - Parameter id overeenkomstig Aquo-standaard: '4724' (parameter_id)
+                - Parameter code overeenkomstig Aquo-standaard: 'WATHTE' (parameter_code)
+                - Parameter omschrijving overeenkomstig Aquo-standaard: 'Waterhoogte' (parameter_description)
+                - Eenheid (unit)
+                - Datum en tijd (date_time)
+                - Waarde (value)
+                - Type waarde: meting of verwachting (value_type)
+        """
+
+        dataframe = pd.DataFrame()
+        if "timeSeries" in json_data:
+            records = []
+            for serie in json_data["timeSeries"]:
+                parameter_id = 4724
+                parameter_code = serie["header"]["parameterId"]
+                parameter_description = serie["header"]["parameterId"]
+                measurement_location_code = serie["header"]["locationId"]
+                # measurement_location_description = serie["header"]["stationName"]
+                unit = serie["header"]["units"]
+
+                measuringstation = locations[
+                    locations["measurement_location_code"] == measurement_location_code
+                ]
+
+                if len(measuringstation) > 0:
+                    measurement_location_id = int(
+                        measuringstation["measurement_location_id"].iloc[0]
+                    )
+                    measurement_location_description = str(
+                        measuringstation["measurement_location_description"].iloc[0]
+                    )
+
+                    if parameter_code in options["parameters"]:
+                        if "events" in serie:
+                            for event in serie["events"]:
+                                datestr = f"{event['date']}T{event['time']}Z"
+                                utc_dt = datetime_from_string(
+                                    datestr, "%Y-%m-%dT%H:%M:%SZ"
+                                )
+                                value_type = "meting"
+
+                                if utc_dt > calc_time:
+                                    value_type = "verwachting"
+
+                                if event["value"]:
+                                    value = float(event["value"])
+                                else:
+                                    value = options["MISSING_VALUE"]
+
+                                record = {
+                                    "measurement_location_id": measurement_location_id,
+                                    "measurement_location_code": measurement_location_code,
+                                    "measurement_location_description": measurement_location_description,
+                                    "parameter_id": parameter_id,
+                                    "parameter_code": parameter_code,
+                                    "parameter_description": parameter_description,
+                                    "unit": unit,
+                                    "date_time": utc_dt,
+                                    "value": value,
+                                    "value_type": value_type,
+                                }
+
+                                records.append(record)
+
+            dataframe = pd.DataFrame.from_records(records)
+
+        return dataframe
