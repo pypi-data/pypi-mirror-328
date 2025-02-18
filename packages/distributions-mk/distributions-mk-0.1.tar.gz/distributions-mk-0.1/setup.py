525d0e6fb7d1e63e7844c4e2403206d7b00e545e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:13:46 +0000

    Add package distributions-mk/0.1/distributions-mk-0.1.tar.gz

---
+++ packages/distributions-mk/distributions-mk-0.1.tar.gz/distributions-mk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-mk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-mk'],
+      zip_safe=False)
