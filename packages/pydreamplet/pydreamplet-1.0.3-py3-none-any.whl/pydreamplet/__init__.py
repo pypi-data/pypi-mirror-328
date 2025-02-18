c27dfdc437cfc38a5651966174a35a394792e62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:03:29 +0000

    Add package pydreamplet/1.0.3/pydreamplet-1.0.3-py3-none-any.whl

---
+++ packages/pydreamplet/pydreamplet-1.0.3-py3-none-any.whl/pydreamplet/__init__.py
@@ -0,0 +1,29 @@
+from pydreamplet.core import (
+    SVG,
+    Animate,
+    Circle,
+    Ellipse,
+    G,
+    Line,
+    Path,
+    Rect,
+    SvgElement,
+    Text,
+    TextOnPath,
+)
+from pydreamplet.math import Vector
+
+__all__ = [
+    "SVG",
+    "Animate",
+    "Circle",
+    "Ellipse",
+    "G",
+    "Line",
+    "Path",
+    "Rect",
+    "SvgElement",
+    "Text",
+    "TextOnPath",
+    "Vector",
+]
