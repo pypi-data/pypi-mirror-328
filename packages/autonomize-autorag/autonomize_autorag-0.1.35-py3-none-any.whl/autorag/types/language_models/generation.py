6ae19a98e6534bbf5de22896201a8638fe219161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:03 +0000

    Add package autonomize-autorag/0.1.35/autonomize_autorag-0.1.35-py3-none-any.whl

---
+++ packages/autonomize-autorag/autonomize_autorag-0.1.35-py3-none-any.whl/autorag/types/language_models/generation.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-class-docstring, missing-module-docstring
+
+from typing import Literal, Optional
+
+from pydantic import BaseModel
+
+
+class ToolCall(BaseModel):
+    name: Optional[str] = None
+    arguments: Optional[str] = None
+
+
+class Usage(BaseModel):
+    completion_tokens: Optional[int] = None
+    prompt_tokens: Optional[int] = None
+    total_tokens: Optional[int] = None
+
+
+class Generation(BaseModel):
+    content: Optional[str] = None
+    finish_reason: Optional[
+        Literal["stop", "length", "tool_calls", "content_filter", "function_call"]
+    ] = None
+    role: Optional[str] = None
+    tool_call: Optional[ToolCall] = None
+    usage: Optional[Usage] = None
