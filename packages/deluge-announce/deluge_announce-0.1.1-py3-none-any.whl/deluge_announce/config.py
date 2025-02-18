6fa2867bb80f7782bc7b9d6bbae9bf1f1267f812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:25:21 +0000

    Add package deluge-announce/0.1.1/deluge_announce-0.1.1-py3-none-any.whl

---
+++ packages/deluge-announce/deluge_announce-0.1.1-py3-none-any.whl/deluge_announce/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+class Config:
+    in_docker = True if os.environ.get("IN_DOCKER") == "true" else False
+
+    if in_docker:
+        app_data = Path("/app_data")
+    else:
+        load_dotenv()  # pyright: ignore [reportUnusedCallResult]
+        app_data = Path(Path.cwd() / "app_data")
+
+    log_path: Path = Path(app_data / "logs")
+
+    app_data.mkdir(exist_ok=True, parents=True)
+    log_path.mkdir(exist_ok=True, parents=True)
+
+    web_url: str = os.environ.get("WEB_URL", "")
+    password: str = os.environ.get("PASSWORD", "")
+    cron_schedule: str | None = os.environ.get("CRON_SCHEDULE", "0 * * * *")
