c181998ab14d268c1cd4b14b569861067e3cf113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:25:23 +0000

    Add package idade3x-yan/0.1.4/idade3x_yan-0.1.4-py3-none-any.whl

---
+++ packages/idade3x-yan/idade3x_yan-0.1.4-py3-none-any.whl/idade3x_yan/utils.py
@@ -0,0 +1,5 @@
+def yan_idade():
+    idade = input('Digite sua idade: ')
+    idade3x = 3*int(idade)
+    print(f'Se voce vivesse mais o dobre que já viveu, você irá viver ate {idade3x} anos!')
+   
