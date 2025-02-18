4c86079ca494a414c322d95c0a2489269a63bcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:29:04 +0000

    Add package odoo-toolkit/0.1.42/odoo_toolkit-0.1.42.tar.gz

---
+++ packages/odoo-toolkit/odoo_toolkit-0.1.42.tar.gz/odoo_toolkit-0.1.42/odoo_toolkit/mv/__init__.py
@@ -0,0 +1,17 @@
+from typer import Typer
+
+from .reset import app as reset_app
+from .setup import app as setup_app
+
+app = Typer(no_args_is_help=True)
+app.add_typer(setup_app)
+app.add_typer(reset_app)
+
+
+@app.callback()
+def callback() -> None:
+    """Work with an :ringed_planet: Odoo Multiverse environment.
+
+    The following commands allow you to set up and Odoo Multiverse environment and perform several useful actions inside
+    the environment.
+    """
