fbc8ee0173c2d1a6b8ac8b2891a725545e6ad0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:27:51 +0000

    Add package withpi/0.1.0a56/withpi-0.1.0a56.tar.gz

---
+++ packages/withpi/withpi-0.1.0a56.tar.gz/withpi-0.1.0a56/src/withpi/types/data_generation_status.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["DataGenerationStatus"]
+
+
+class DataGenerationStatus(BaseModel):
+    detailed_status: List[str]
+    """Detailed status of the job"""
+
+    job_id: str
+    """The job id"""
+
+    state: Literal["QUEUED", "RUNNING", "DONE", "ERROR"]
+    """Current state of the job"""
+
+    data: Optional[List[str]] = None
+    """The generated data. Absent unless state is done"""
