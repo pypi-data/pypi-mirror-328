8764c0a5090a71dbb8f52e8c05ee28f8d88c44f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:17 +0000

    Add package FastRAMQPI/10.1.0/fastramqpi-10.1.0.tar.gz

---
+++ packages/FastRAMQPI/fastramqpi-10.1.0.tar.gz/fastramqpi-10.1.0/fastramqpi/pytest_util.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from typing import Any
+
+import tenacity
+from tenacity import stop_after_delay
+from tenacity import wait_fixed
+
+
+def retry(
+    stop: Any = stop_after_delay(20),
+    wait: Any = wait_fixed(2),
+) -> Any:
+    """Tenacity retry decorator, with defaults useful for testing.
+
+    Args:
+        stop: Stop after 10 seconds so the test doesn't run forever.
+            The default duration is long since some integrations need multiple rounds
+            of AMQP messages to complete, and therefore need a long time to get in a
+            consistent state.
+        wait: Wait two seconds between assertion attempts.
+
+    Returns:
+        Function decorated with retrying.
+    """
+    # reraise=True so a false assertion fails the test
+    return tenacity.retry(stop=stop, wait=wait, reraise=True)
