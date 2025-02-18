8c63ad66d349cb2c1f97e3168f0fade8c21ca23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:21 +0000

    Add package aerolattice/0.2.0/aerolattice-0.2.0.tar.gz

---
+++ packages/aerolattice/aerolattice-0.2.0.tar.gz/aerolattice-0.2.0/src/vortex.rs
@@ -0,0 +1,10 @@
+//! Vortex panel implementation.
+
+use pyo3::prelude::*;
+
+// /// A vortex panel.
+// pub struct VortexPanel {
+//     #[pyo3(get, set)]
+//     /// Center of the vortex panel.
+
+// }
