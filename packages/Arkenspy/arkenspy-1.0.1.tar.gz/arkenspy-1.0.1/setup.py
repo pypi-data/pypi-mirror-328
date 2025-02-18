14a3ec2b2085e1be7081fed723dffdf7318e2e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:03 +0000

    Add package Arkenspy/1.0.1/arkenspy-1.0.1.tar.gz

---
+++ packages/Arkenspy/arkenspy-1.0.1.tar.gz/arkenspy-1.0.1/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup,find_packages
+from pathlib import Path
+
+setup(
+    name="Arkenspy",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "github"
+        "aiohttp==3.9.0b0"
+    ],
+    author="as2rofy",
+    author_email="a28092452@gmail.com",
+    description="Arkenspy is a token manager library",
+    long_description= "README.md",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    project_urls={
+        "Source Repository" : "https://github.com/anigamer101/Arkens"
+    }
+)
