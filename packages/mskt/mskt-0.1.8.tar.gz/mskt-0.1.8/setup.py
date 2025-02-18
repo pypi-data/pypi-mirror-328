19f2234f4cf3daa7c2c96a12a8551479c071e494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:14:05 +0000

    Add package mskt/0.1.8/mskt-0.1.8.tar.gz

---
+++ packages/mskt/mskt-0.1.8.tar.gz/mskt-0.1.8/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import Extension, setup
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+
+print(dir_path)
+
+try:
+    from Cython.Build import cythonize
+
+    cython_function_path = "pymskt/cython/cython_functions.pyx"
+    print(cython_function_path)
+    ext_modules = cythonize(
+        [Extension(name="pymskt.cython_functions", sources=[cython_function_path])], annotate=True
+    )
+except ImportError:
+    print("Import Error - Not building cython function!")
+    ext_modules = None
+
+setup(ext_modules=ext_modules, zip_safe=False)
