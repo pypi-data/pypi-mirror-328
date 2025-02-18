b26891adcdffb6616900a3a7ba3d9197045d52c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:14:01 +0000

    Add package orionis/0.1.0/orionis-0.1.0.tar.gz

---
+++ packages/orionis/orionis-0.1.0.tar.gz/orionis-0.1.0/orionis/framework.py
@@ -0,0 +1,45 @@
+#--------------------------------------------------------------------------
+# Name of the project or framework
+#--------------------------------------------------------------------------
+NAME = "orionis"
+
+#--------------------------------------------------------------------------
+# Current version of the project or framework
+#--------------------------------------------------------------------------
+VERSION = "0.1.0"
+
+#--------------------------------------------------------------------------
+# Full name of the author or maintainer of the project
+#--------------------------------------------------------------------------
+AUTHOR = "Raul Mauricio Uñate Castro"
+
+#--------------------------------------------------------------------------
+# Email address of the author or maintainer for contact purposes
+#--------------------------------------------------------------------------
+AUTHOR_EMAIL = "raulmauriciounate@gmail.com"
+
+#--------------------------------------------------------------------------
+# Short description of the project or framework
+#--------------------------------------------------------------------------
+DESCRIPTION = "Orionis Framework – Elegant, Fast, and Powerful."
+
+#--------------------------------------------------------------------------
+# URL to the project's skeleton or template repository
+# This is typically used for initial project setup
+#--------------------------------------------------------------------------
+SKELETON = "https://github.com/orionis-framework/skeleton"
+
+#--------------------------------------------------------------------------
+# URL to the project's framework repository
+#--------------------------------------------------------------------------
+FRAMEWORK = "https://github.com/orionis-framework/framework"
+
+#--------------------------------------------------------------------------
+# URL to the project's documentation
+#--------------------------------------------------------------------------
+DOCS = "https://github.com/orionis-framework/docs"
+
+#--------------------------------------------------------------------------
+# Minimum Python version required to run the project
+#--------------------------------------------------------------------------
+PYTHON_REQUIRES = ">=3.12"
