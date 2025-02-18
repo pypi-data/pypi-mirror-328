ddc4b5c3c49382f3c0f8ac3571a78753d84f1c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:47 +0000

    Add package saplings/5.0.7/saplings-5.0.7.tar.gz

---
+++ packages/saplings/saplings-5.0.7.tar.gz/saplings-5.0.7/saplings/abstract/Tool.py
@@ -0,0 +1,42 @@
+# Standard library
+from typing import List
+from abc import ABC, abstractmethod
+
+# Local
+try:
+    from saplings.dtos import Message
+except ImportError:
+    from dtos import Message
+
+
+class Tool(ABC):
+    @abstractmethod
+    def __init__(self, **kwargs):
+        self.name: str
+        self.description: str
+        self.parameters: dict
+        self.is_terminal: bool
+
+    @abstractmethod
+    async def run(self, **kwargs) -> any:
+        return None
+
+    def update_definition(self, trajectory: List[Message] = [], **kwargs):
+        pass
+
+    def format_output(self, output: any) -> str:
+        return str(output)
+
+    def get_schema(self):
+        return {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description,
+                "strict": True,
+                "parameters": self.parameters,
+            },
+        }
+
+
+# TODO: Implement a `from_langchain_tool` static method?
