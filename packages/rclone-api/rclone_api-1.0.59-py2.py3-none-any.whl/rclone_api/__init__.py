5be307d7d1ecfa6967c663c2696dcd115d109527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:13:53 +0000

    Add package rclone-api/1.0.59/rclone_api-1.0.59-py2.py3-none-any.whl

---
+++ packages/rclone-api/rclone_api-1.0.59-py2.py3-none-any.whl/rclone_api/__init__.py
@@ -0,0 +1,27 @@
+from .completed_process import CompletedProcess
+from .config import Config
+from .diff import DiffItem, DiffType
+from .dir import Dir
+from .dir_listing import DirListing
+from .file import File
+from .filelist import FileList
+from .process import Process
+from .rclone import Rclone, rclone_verbose
+from .remote import Remote
+from .rpath import RPath
+
+__all__ = [
+    "Rclone",
+    "File",
+    "Config",
+    "Remote",
+    "Dir",
+    "RPath",
+    "DirListing",
+    "FileList",
+    "Process",
+    "DiffItem",
+    "DiffType",
+    "rclone_verbose",
+    "CompletedProcess",
+]
