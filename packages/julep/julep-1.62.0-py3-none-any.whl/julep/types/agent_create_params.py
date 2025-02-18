6de2e08d767ef2226e2fb730283f7d21ec6038d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:50 +0000

    Add package julep/1.62.0/julep-1.62.0-py3-none-any.whl

---
+++ packages/julep/julep-1.62.0-py3-none-any.whl/julep/types/agent_create_params.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Union, Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["AgentCreateParams"]
+
+
+class AgentCreateParams(TypedDict, total=False):
+    name: Required[str]
+
+    about: str
+
+    canonical_name: Optional[str]
+
+    default_settings: Optional[object]
+
+    instructions: Union[str, List[str]]
+
+    metadata: Optional[object]
+
+    model: str
---
+++ packages/julep/julep-1.62.0-py3-none-any.whl/julep/types/agent_update_params.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Union, Optional
+from typing_extensions import TypedDict
+
+__all__ = ["AgentUpdateParams"]
+
+
+class AgentUpdateParams(TypedDict, total=False):
+    about: str
+
+    canonical_name: Optional[str]
+
+    default_settings: Optional[object]
+
+    instructions: Union[str, List[str]]
+
+    metadata: Optional[object]
+
+    model: str
+
+    name: Optional[str]
