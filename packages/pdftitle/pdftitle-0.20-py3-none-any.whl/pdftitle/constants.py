7eeeaaef5667c67ea29482b460abf5ee3d98867f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:14:18 +0000

    Add package pdftitle/0.20/pdftitle-0.20-py3-none-any.whl

---
+++ packages/pdftitle/pdftitle-0.20-py3-none-any.whl/pdftitle/constants.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024 Mete Balci
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""pdftitle constants"""
+
+ALGO_ORIGINAL = "original"
+ALGO_MAX2 = "max2"
+ALGO_ELIOT = "eliot"
+ALGO_OPENAI = "openai"
---
+++ packages/pdftitle/pdftitle-0.20-py3-none-any.whl/pdftitle/exceptions.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Mete Balci
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""pdftitle exceptions"""
+
+
+class PDFTitleException(Exception):
+    """base class for all pdftitle exceptions"""
