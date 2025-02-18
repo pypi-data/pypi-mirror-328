dc8e51a7fb5de4805a56ca290535c88f4a9ac56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:04:05 +0000

    Add package dowse/0.1.1/dowse-0.1.1-py3-none-any.whl

---
+++ packages/dowse/dowse-0.1.1-py3-none-any.whl/dowse/impls/basic/llms/preprocessor/__init__.py
@@ -0,0 +1,23 @@
+import json
+from typing import Callable
+
+from pydantic import BaseModel, Field
+
+from dowse.interfaces.processor.base import Processor
+from dowse.models import Tweet
+from dowse.tools import convert_dollar_amount_to_eth, get_token_address_tool
+
+
+class FormattedCommand(BaseModel):
+    content: str
+    caller: str = Field(description="The Twitter handle of the caller")
+
+
+class ProcessTokens(Processor[Tweet, FormattedCommand]):
+    tools: list[Callable] = [
+        get_token_address_tool,
+        convert_dollar_amount_to_eth,
+    ]
+
+    async def to_string(self, command: Tweet) -> str:
+        return json.dumps({"caller": command.creator_name, "content": command.content})
---
+++ packages/dowse/dowse-0.1.1-py3-none-any.whl/dowse/models/commands/crypto/transfer.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from pydantic import BaseModel
+from typing_extensions import Doc
+
+
+class TransferArgs(BaseModel):
+    token_symbol: Annotated[str | None, Doc("The symbol of the token to transfer")]
+    token_address: Annotated[str, Doc("The address of the token to transfer")]
+    amount: Annotated[int, Doc("The amount of the token to transfer")]
+
+    recipient: Annotated[
+        str, Doc("The twitter handle or address of the recipient of the transfer")
+    ]
+    sender: Annotated[
+        str, Doc("The twitter handle or address of the sender of the transfer")
+    ]
