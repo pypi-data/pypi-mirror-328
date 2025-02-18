c6fbd9cbfec05d7539f59c1d4db48ac854595019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:13:36 +0000

    Add package sibi-dst/0.3.44/sibi_dst-0.3.44.tar.gz

---
+++ packages/sibi-dst/sibi_dst-0.3.44.tar.gz/sibi_dst-0.3.44/sibi_dst/__init__.py
@@ -0,0 +1,9 @@
+try:
+    import importlib.metadata as version_reader
+except ImportError:
+    import importlib_metadata as version_reader
+
+try:
+    __version__ = version_reader.version("sibi-dst")
+except version_reader.PackageNotFoundError:
+    __version__ = "unknown"
