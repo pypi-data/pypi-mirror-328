00a0f17eadf9871fdd680ea6fc9e5c5ed1c06a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:14:19 +0000

    Add package himap/1.0.0/himap-1.0.0.tar.gz

---
+++ packages/himap/himap-1.0.0.tar.gz/himap-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import glob
+
+
+# Setting up
+setup(
+    name="himap",
+    packages=find_packages(include=["himap", "himap.*", "himap/cython_build"]),
+    ext_modules=cythonize(glob.glob("himap/cython_build/fwd_bwd.pyx"), compiler_directives={'boundscheck': False})
+)
