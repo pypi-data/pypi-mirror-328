1a946160cc6d70b8004f91dafafbccf94ed6a7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:19 +0000

    Add package hellocn/1.2.0/hellocn-1.2.0.tar.gz

---
+++ packages/hellocn/hellocn-1.2.0.tar.gz/hellocn-1.2.0/README.md
@@ -0,0 +1,2 @@
+# hellocn
+Hello, World!
---
+++ packages/hellocn/hellocn-1.2.0.tar.gz/hellocn-1.2.0/src/hellocn/hello.py
@@ -0,0 +1,12 @@
+##!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 作者：Yang Qing
+# 邮箱：yangqing@gitc.cc
+# 描述：Hello, World!（您好，世界！）
+
+def main():
+    print("Hello, World!")
+    input("\t\tyangqing")
+
+if __name__ == "__main__":
+    main()
