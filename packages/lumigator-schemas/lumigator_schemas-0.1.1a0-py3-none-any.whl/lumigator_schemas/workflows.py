bbafa287bbc694c445fa7cbf0ec460093c8526f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:13:47 +0000

    Add package lumigator-schemas/0.1.1a0/lumigator_schemas-0.1.1a0-py3-none-any.whl

---
+++ packages/lumigator-schemas/lumigator_schemas-0.1.1a0-py3-none-any.whl/lumigator_schemas/workflows.py
@@ -0,0 +1,48 @@
+import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from lumigator_schemas.jobs import (
+    JobResults,
+    LowercaseEnum,
+)
+
+
+class WorkflowStatus(LowercaseEnum):
+    CREATED = "created"
+    RUNNING = "running"
+    FAILED = "failed"
+    SUCCEEDED = "succeeded"
+
+
+class WorkflowCreateRequest(BaseModel):
+    name: str
+    description: str = ""
+    experiment_id: str | None = None
+    model: str
+    dataset: UUID
+    max_samples: int = -1  # set to all samples by default
+    model_url: str | None = None
+    system_prompt: str | None = None
+    inference_output_field: str = "predictions"
+    config_template: str | None = None
+
+
+class WorkflowResponse(BaseModel, from_attributes=True):
+    id: str
+    experiment_id: str
+    model: str
+    name: str
+    description: str
+    status: WorkflowStatus
+    created_at: datetime.datetime
+    updated_at: datetime.datetime | None = None
+
+
+# This schema extends workflow response and adds a few more fields
+class WorkflowDetailsResponse(WorkflowResponse):
+    jobs: list[JobResults] | None = None
+    metrics: dict | None = None
+    parameters: dict | None = None
+    artifacts_download_url: str | None = None
