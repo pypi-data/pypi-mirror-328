9ad288d9b4d59e9e0afe1a13b479101eb96827ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:02 +0000

    Add package dualcodec/0.3.3/dualcodec-0.3.3.tar.gz

---
+++ packages/dualcodec/dualcodec-0.3.3.tar.gz/dualcodec-0.3.3/dualcodec/__init__.py
@@ -0,0 +1,5 @@
+from .codec_model.get_model import get_model, model_id_to_fname, model_id_to_cfgname
+from .codec_model.inference_with_semantic import Inference
+
+from .utils.base_trainer import BaseTrainer
+from .codec_model import DualCodec
