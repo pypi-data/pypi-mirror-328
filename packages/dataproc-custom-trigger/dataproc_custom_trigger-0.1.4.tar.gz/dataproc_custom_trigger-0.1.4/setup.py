693752a1bae16443d0015b5d3e2221b63d1b3dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:03:39 +0000

    Add package dataproc-custom-trigger/0.1.4/dataproc_custom_trigger-0.1.4.tar.gz

---
+++ packages/dataproc-custom-trigger/dataproc_custom_trigger-0.1.4.tar.gz/dataproc_custom_trigger-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataproc_custom_trigger",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "apache-airflow>=2.2",  # ajusta según tus necesidades
+        "apache-airflow-providers-google>=6.0.0",  # ajusta la versión necesaria
+    ],
+)
