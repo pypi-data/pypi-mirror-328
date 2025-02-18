fad114c57bfe4c77a70daf6148867f4cf8e0fb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:14:01 +0000

    Add package da-template-lib/0.0.20/da_template_lib-0.0.20.tar.gz

---
+++ packages/da-template-lib/da_template_lib-0.0.20.tar.gz/da_template_lib-0.0.20/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(
+    name='da-template-lib',
+    version='0.0.20',
+    license='MIT License',
+    author='Kaue Matheus Santana',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='kaue.santana@equifax.com',
+    keywords='Template Inovação em Dados',
+    description='Template squad Inovação em Dados',
+    packages=find_packages(),  # Isso inclui automaticamente todos os pacotes
+    install_requires=[
+        'google-cloud-storage',
+        'google-cloud-bigquery',
+        'unicode',
+        'pandas'
+        ]
+    )
