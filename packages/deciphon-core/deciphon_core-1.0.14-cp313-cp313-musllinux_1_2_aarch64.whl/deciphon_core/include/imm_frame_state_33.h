3147060d260f63410092de2b0ec349796a8fb345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:28:08 +0000

    Add package deciphon-core/1.0.14/deciphon_core-1.0.14-cp313-cp313-musllinux_1_2_aarch64.whl

---
+++ packages/deciphon-core/deciphon_core-1.0.14-cp313-cp313-musllinux_1_2_aarch64.whl/deciphon_core/include/imm_frame_state_33.h
@@ -0,0 +1,9 @@
+#ifndef IMM_FRAME_STATE_33_H
+#define IMM_FRAME_STATE_33_H
+
+struct imm_frame_state;
+struct imm_seq;
+
+float imm_joint_n3_33(struct imm_frame_state const *, struct imm_seq const *);
+
+#endif
