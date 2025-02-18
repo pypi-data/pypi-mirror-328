a8e96da3f6ad233823e21b496cccdf267febc59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:03:30 +0000

    Add package lpitabformer/1.0.2/lpitabformer-1.0.2.tar.gz

---
+++ packages/lpitabformer/lpitabformer-1.0.2.tar.gz/lpitabformer-1.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lpitabformer",
+    version="1.0.2",
+    author="linqin",
+    url='https://github.com/Ci-TJ/LPItabformer',
+    author_email="1911449@tongji.edu.cn",
+    description="Lpitabformer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
