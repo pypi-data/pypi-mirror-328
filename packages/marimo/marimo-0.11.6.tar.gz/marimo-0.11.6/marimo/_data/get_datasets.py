318012b3e199536880cfafab2e8e39ba61b9766a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 05:04:09 +0000

    Add package marimo/0.11.6/marimo-0.11.6.tar.gz

---
+++ packages/marimo/marimo-0.11.6.tar.gz/marimo-0.11.6/marimo/_data/get_datasets.py
@@ -0,0 +1,215 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Optional, cast
+
+from marimo import _loggers
+from marimo._data.models import DataTable, DataTableColumn, DataType
+from marimo._plugins.ui._impl.tables.utils import get_table_manager_or_none
+
+LOGGER = _loggers.marimo_logger()
+
+if TYPE_CHECKING:
+    import duckdb
+
+
+def get_datasets_from_variables(
+    variables: List[tuple[str, object]],
+) -> List[DataTable]:
+    tables: List[DataTable] = []
+    for variable_name, value in variables:
+        table = _get_data_table(value, variable_name)
+        if table is not None:
+            tables.append(table)
+
+    return tables
+
+
+def _get_data_table(value: object, variable_name: str) -> Optional[DataTable]:
+    try:
+        table = get_table_manager_or_none(value)
+        if table is None:
+            return None
+
+        columns = [
+            DataTableColumn(
+                name=column_name,
+                type=column_type[0],
+                external_type=column_type[1],
+                sample_values=table.get_sample_values(column_name),
+            )
+            for column_name, column_type in table.get_field_types()
+        ]
+        return DataTable(
+            name=variable_name,
+            variable_name=variable_name,
+            num_rows=table.get_num_rows(force=False),
+            num_columns=table.get_num_columns(),
+            source_type="local",
+            source="memory",
+            columns=columns,
+        )
+    except Exception as e:
+        LOGGER.error(
+            "Failed to get table data for variable %s",
+            variable_name,
+            exc_info=e,
+        )
+        return None
+
+
+def has_updates_to_datasource(query: str) -> bool:
+    import duckdb  # type: ignore[import-not-found,import-untyped,unused-ignore] # noqa: E501
+
+    try:
+        statements = duckdb.extract_statements(query.strip())
+    except Exception:
+        # May not be valid SQL
+        return False
+
+    return any(
+        statement.type == duckdb.StatementType.ATTACH
+        or statement.type == duckdb.StatementType.DETACH
+        or statement.type == duckdb.StatementType.ALTER
+        # This may catch some false positives for other CREATE statements
+        or statement.type == duckdb.StatementType.CREATE
+        for statement in statements
+    )
+
+
+def get_datasets_from_duckdb(
+    connection: Optional[duckdb.DuckDBPyConnection],
+    engine_name: Optional[str] = None,
+) -> List[DataTable]:
+    try:
+        return _get_datasets_from_duckdb_internal(connection, engine_name)
+    except Exception as e:
+        LOGGER.error(e)
+        return []
+
+
+def _get_datasets_from_duckdb_internal(
+    connection: Optional[duckdb.DuckDBPyConnection],
+    engine_name: Optional[str] = None,
+) -> List[DataTable]:
+    # Columns
+    # 0:"database"
+    # 1:"schema"
+    # 2:"name"
+    # 3:"column_names"
+    # 4:"column_types"
+    # 5:"temporary"
+    if connection is None:
+        import duckdb
+
+        databases = duckdb.execute("SHOW ALL TABLES").fetchall()
+    else:
+        databases = connection.execute("SHOW ALL TABLES").fetchall()
+    if not len(databases):
+        # No tables
+        return []
+
+    tables: list[DataTable] = []
+
+    for (
+        database,
+        schema,
+        name,
+        column_names,
+        column_types,
+        *_rest,
+    ) in databases:
+        assert len(column_names) == len(column_types)
+        assert isinstance(column_names, list)
+        assert isinstance(column_types, list)
+
+        columns = [
+            DataTableColumn(
+                name=column_name,
+                type=_db_type_to_data_type(column_type),
+                external_type=column_type,
+                sample_values=[],
+            )
+            for column_name, column_type in zip(
+                cast(list[str], column_names),
+                cast(list[str], column_types),
+            )
+        ]
+
+        tables.append(
+            DataTable(
+                source_type="duckdb" if engine_name is None else "connection",
+                source=database,
+                name=f"{database}.{schema}.{name}",
+                num_rows=None,
+                num_columns=len(columns),
+                variable_name=None,
+                columns=columns,
+                engine=engine_name,
+            )
+        )
+
+    return tables
+
+
+def _db_type_to_data_type(db_type: str) -> DataType:
+    db_type = db_type.lower()
+    # Numeric types
+    if db_type in [
+        "tinyint",
+        "smallint",
+        "integer",
+        "bigint",
+        "hugeint",
+        "utinyint",
+        "usmallint",
+        "uinteger",
+        "ubigint",
+        "uhugeint",
+    ]:
+        return "integer"
+    if db_type in [
+        "float",
+        "real",
+        "double",
+        "decimal",
+        "numeric",
+    ] or db_type.startswith("decimal"):
+        return "number"
+    # Boolean type
+    if db_type == "boolean":
+        return "boolean"
+    # String types
+    if db_type in [
+        "varchar",
+        "char",
+        "bpchar",
+        "text",
+        "string",
+        "blob",
+        "uuid",
+    ]:
+        return "string"
+    # Date and Time types
+    if db_type == "date":
+        return "date"
+    if db_type == "time":
+        return "time"
+    if db_type in [
+        "timestamp",
+        "timestamp with time zone",
+        "timestamptz",
+        "datetime",
+        "interval",
+    ]:
+        return "datetime"
+    # Nested types
+    if db_type in ["array", "list", "struct", "map", "union"]:
+        return "unknown"
+    # Special types
+    if db_type == "bit":
+        return "string"  # Representing bit as string
+    if db_type == "enum":
+        return "string"  # Representing enum as string
+    # Unknown type
+    return "unknown"
---
+++ packages/marimo/marimo-0.11.6.tar.gz/marimo-0.11.6/marimo/_data/models.py
@@ -0,0 +1,115 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import date, datetime, time, timedelta  # noqa: TCH003
+from decimal import Decimal
+from typing import Any, List, Literal, Optional, Union
+
+DataType = Literal[
+    "string",
+    "boolean",
+    "integer",
+    "number",
+    "date",
+    "datetime",
+    "time",
+    "unknown",
+]
+# This is the data type based on the source library
+# e.g. polars, pandas, numpy, etc.
+ExternalDataType = str
+
+
+@dataclass
+class DataTableColumn:
+    """
+    Represents a column in a data table.
+
+    Attributes:
+        name (str): The name of the column.
+        type (DataType): The data type of the column.
+    """
+
+    name: str
+    type: DataType
+    external_type: ExternalDataType
+    sample_values: List[Any]
+
+
+DataTableSource = Literal["local", "duckdb", "connection"]
+
+VariableName = str
+
+
+@dataclass
+class DataTable:
+    """
+    Represents a data table.
+
+    Attributes:
+        source (str): The source of the data table.
+        name (str): The name of the data table.
+        num_rows (Optional[int]): The number of rows in the data table.
+        num_columns (Optional[int]): The number of columns in the data table.
+        variable_name (Optional[VariableName]): The variable name associated with
+        the data table.
+        columns (List[DataTableColumn]): The list of columns in the data table.
+        engine (Optional[VariableName]): The engine associated with the data table.
+    """
+
+    source_type: DataTableSource
+    source: str
+    name: str
+    num_rows: Optional[int]
+    num_columns: Optional[int]
+    variable_name: Optional[VariableName]
+    columns: List[DataTableColumn]
+    engine: Optional[VariableName] = None
+
+
+NumericLiteral = Union[int, float, Decimal]
+TemporalLiteral = Union[date, time, datetime, timedelta]
+NonNestedLiteral = Union[NumericLiteral, TemporalLiteral, str, bool, bytes]
+
+
+@dataclass
+class ColumnSummary:
+    """
+    Represents a summary of a column in a data table.
+
+    """
+
+    total: Optional[int] = None
+    nulls: Optional[int] = None
+    unique: Optional[int] = None
+    min: Optional[NonNestedLiteral] = None
+    max: Optional[NonNestedLiteral] = None
+    mean: Optional[NonNestedLiteral] = None
+    median: Optional[NonNestedLiteral] = None
+    std: Optional[NonNestedLiteral] = None
+    true: Optional[int] = None
+    false: Optional[int] = None
+    p5: Optional[NonNestedLiteral] = None
+    p25: Optional[NonNestedLiteral] = None
+    # p50 is the median
+    p75: Optional[NonNestedLiteral] = None
+    p95: Optional[NonNestedLiteral] = None
+
+
+@dataclass
+class DataSourceConnection:
+    """
+    Represents a data source connection.
+
+    Attributes:
+        source (str): The source of the data source connection. E.g 'postgres'.
+        dialect (str): The dialect of the data source connection. E.g 'postgresql'.
+        name (str): The name of the data source connection. E.g 'engine'.
+        display_name (str): The display name of the data source connection. E.g 'PostgresQL (engine)'.
+    """
+
+    source: str
+    dialect: str
+    name: str
+    display_name: str
