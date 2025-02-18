e4deee2ffd57cd3b9ff9bcef55be3f7c44a43982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 03:27:50 +0000

    Add package airflow-provider-datarobot/0.1.0/airflow_provider_datarobot-0.1.0.tar.gz

---
+++ packages/airflow-provider-datarobot/airflow_provider_datarobot-0.1.0.tar.gz/airflow_provider_datarobot-0.1.0/datarobot_provider/operators/model_insights.py
@@ -0,0 +1,154 @@
+# Copyright 2022 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+from collections.abc import Sequence
+from typing import Any
+from typing import Optional
+
+import datarobot as dr
+from airflow.utils.context import Context
+
+from datarobot_provider.operators.base_datarobot_operator import BaseDatarobotOperator
+
+
+class ComputeFeatureImpactOperator(BaseDatarobotOperator):
+    """
+    Creates Feature Impact job in DataRobot.
+    :param project_id: DataRobot project ID
+    :type project_id: str
+    :param model_id: DataRobot model ID
+    :type model_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    :return: Feature Impact job ID
+    :rtype: str
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Sequence[str] = [
+        "project_id",
+        "model_id",
+    ]
+
+    def __init__(
+        self,
+        *,
+        project_id: str,
+        model_id: str,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.model_id = model_id
+
+    def validate(self):
+        if not self.project_id:
+            raise ValueError("project_id is required to compute Feature Impact.")
+
+        if not self.model_id:
+            raise ValueError("model_id is required to compute Feature Impact.")
+
+    def execute(self, context: Context) -> str:
+        model = dr.models.Model.get(self.project_id, self.model_id)
+
+        job = model.request_feature_impact()
+
+        self.log.info(f"Feature Impact Job submitted, job_id={job.id}")
+
+        return job.id
+
+
+class ComputeFeatureEffectsOperator(BaseDatarobotOperator):
+    """
+    Submit request to compute Feature Effects for the model.
+    :param project_id: DataRobot project ID
+    :type project_id: str
+    :param model_id: DataRobot model ID
+    :type model_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    :return: Feature Effects job ID
+    :rtype: str
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Sequence[str] = [
+        "project_id",
+        "model_id",
+    ]
+
+    def __init__(
+        self,
+        *,
+        project_id: str,
+        model_id: str,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.model_id = model_id
+
+    def validate(self):
+        if not self.project_id:
+            raise ValueError("project_id is required to compute Feature Effects.")
+
+        if not self.model_id:
+            raise ValueError("model_id is required to compute Feature Effects.")
+
+    def execute(self, context: Context) -> str:
+        model = dr.models.Model.get(self.project_id, self.model_id)
+
+        job = model.request_feature_effect()
+
+        self.log.info(f"Feature Effects Job submitted, job_id={job.id}")
+
+        return job.id
+
+
+class ComputeShapOperator(BaseDatarobotOperator):
+    """
+    Creates SHAP impact job in DataRobot.
+    :param project_id: DataRobot project ID
+    :type project_id: str
+    :param model_id: DataRobot model ID
+    :type model_id: str
+    :param datarobot_conn_id: Connection ID, defaults to `datarobot_default`
+    :type datarobot_conn_id: str, optional
+    :return: SHAP impact job ID
+    :rtype: str
+    """
+
+    # Specify the arguments that are allowed to parse with jinja templating
+    template_fields: Sequence[str] = [
+        "project_id",
+        "model_id",
+    ]
+
+    def __init__(
+        self,
+        *,
+        project_id: Optional[str] = None,
+        model_id: Optional[str] = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.model_id = model_id
+
+    def validate(self):
+        if self.project_id is None:
+            raise ValueError("project_id is required to compute SHAP impact.")
+
+        if self.model_id is None:
+            raise ValueError("model_id is required to compute SHAP impact.")
+
+    def execute(self, context: Context) -> str:
+        shap_impact_job = dr.ShapImpact.create(project_id=self.project_id, model_id=self.model_id)
+
+        self.log.info(f"Compute SHAP impact Job submitted, job_id={shap_impact_job.id}")
+
+        return shap_impact_job.id
