58f7dbe68324ef2609464d85f7bc6e62f664871d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 06:03:45 +0000

    Add package clipspy/1.0.5/clipspy-1.0.5.tar.gz

---
+++ packages/clipspy/clipspy-1.0.5.tar.gz/clipspy-1.0.5/setup.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2016-2025, Matteo Cafasso
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import fileinput
+from setuptools import find_packages, setup
+
+
+CWD = os.path.dirname(__file__)
+
+
+def package_version():
+    module_path = os.path.join(CWD, 'clips', '__init__.py')
+    for line in fileinput.input(module_path):
+        if line.startswith('__version__'):
+            return line.split('=')[-1].strip().replace('\'', '')
+
+
+setup(
+    name="clipspy",
+    version=package_version(),
+    author="Matteo Cafasso",
+    author_email="noxdafox@gmail.com",
+    description=("CLIPS Python bindings"),
+    license="BSD",
+    long_description=open(os.path.join(CWD, 'README.rst')).read(),
+    packages=find_packages(),
+    ext_package="clips",
+    setup_requires=["cffi>=1.0.0"],
+    install_requires=["cffi>=1.0.0"],
+    cffi_modules=["clips/clips_build.py:ffibuilder"],
+    include_dirs=["/usr/include/clips", "/usr/local/include/clips"],
+    data_files=[('lib', ['lib/clips.cdef'])],
+    keywords="clips python cffi expert-system",
+    url="https://github.com/noxdafox/clipspy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
