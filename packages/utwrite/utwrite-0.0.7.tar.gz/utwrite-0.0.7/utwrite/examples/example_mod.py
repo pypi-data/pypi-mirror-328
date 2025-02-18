f6329defb7f5498df42e6c461933a033b5dbb180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-18 04:03:42 +0000

    Add package utwrite/0.0.7/utwrite-0.0.7.tar.gz

---
+++ packages/utwrite/utwrite-0.0.7.tar.gz/utwrite-0.0.7/utwrite/examples/example_mod.py
@@ -0,0 +1,116 @@
+r"""
+
+Example module to auto generate unittests from docstring
+
+:Unittest decorator:
+@unittest.skipUnless(sys.version_info.major == 3, "Lets say it requires Pyhon3 only")
+"""
+def default_func():
+    r"""
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+        ex.default_func()
+        # Result: 1 #
+    """
+    return 1
+
+def list_func():
+    r"""
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+        ex.list_func()
+        # Result: [1,2,3] #
+    """
+    return [1,2,3]
+
+def almost_equal_func():
+    r"""
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+
+        # python 3 ...
+        ex.almost_equal_func()
+        # Result: [0.5] @self.assertListAlmostEqual#
+    """
+    return [1/2]
+
+def __dunder_func():
+    r"""This should not generate unittest.
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+        ex.__dunder_func()
+        # Result: None #
+    """
+    return
+
+def __dunder_test_tag_func():
+    r"""This should generate unittest.
+
+    :Tags:
+        test
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+        getattr(ex, '__dunder_test_tag_func')()
+        # Result: None #
+    """
+    return
+
+def notest_tag_func():
+    r"""This should not generate unittest.
+
+    :Tags:
+        notest
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+        ex.notest_tag_func()
+        # Result: None #
+    """
+    return
+
+def missing_test_crash_func():
+    r"""Funcion with no examples section (bad).
+
+    By default functions without an example section are tagged with
+    @MISSINGTEST, which means it will raise an error.
+
+    This is so the developer dont't forget to create unittest, or tag as
+    `notest`.
+    """
+
+def np_explicit_assert_func(n):
+    r"""External assertion
+
+    Examples::
+
+        import numpy as np
+        import utwrite.examples.example_mod as ex
+
+        ex.np_explicit_assert_func(3)
+        # Result: np.array([0, 1, 2]) @np.testing.assert_array_equal#
+    """
+    import numpy as np
+
+    return np.arange(n)
+
+def escaped_assertion_token_func():
+    r"""Escaped assertion token
+
+    Examples::
+
+        import utwrite.examples.example_mod as ex
+        ex.escaped_assertion_token_func()
+        # Result: '\@' #
+    """
+    return '@'
