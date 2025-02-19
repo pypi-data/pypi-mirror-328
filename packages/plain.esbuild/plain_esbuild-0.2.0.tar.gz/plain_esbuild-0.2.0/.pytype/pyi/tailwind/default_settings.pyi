8075d13d370bc8dbbfa355827c2602feaa2a74e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:49 +0000

    Add package plain.esbuild/0.2.0/plain_esbuild-0.2.0.tar.gz

---
+++ packages/plain.esbuild/plain_esbuild-0.2.0.tar.gz/plain_esbuild-0.2.0/.pytype/pyi/tailwind/default_settings.pyi
@@ -0,0 +1,7 @@
+# (generated with --quick)
+
+from typing import Any
+
+APP_ASSETS_DIR: Any
+TAILWIND_DIST_PATH: Any
+TAILWIND_SRC_PATH: Any
---
+++ packages/plain.esbuild/plain_esbuild-0.2.0.tar.gz/plain_esbuild-0.2.0/.pytype/pyi/tailwind/entrypoints.pyi
@@ -0,0 +1,8 @@
+# (generated with --quick)
+
+from typing import Any
+
+compile: Any
+
+def run_compile() -> None: ...
+def run_dev_compile() -> None: ...
