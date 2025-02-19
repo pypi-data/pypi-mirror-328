8b7681fb96d811adbdb473cba105a71c525ff145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:27:37 +0000

    Add package circuit-breaker-box/0.2.1/circuit_breaker_box-0.2.1-py3-none-any.whl

---
+++ packages/circuit-breaker-box/circuit_breaker_box-0.2.1-py3-none-any.whl/circuit_breaker_box/retryers.py
@@ -0,0 +1,62 @@
+import dataclasses
+import logging
+import typing
+
+import tenacity
+
+from circuit_breaker_box import BaseCircuitBreaker, BaseRetrier, ResponseType
+
+
+logger = logging.getLogger(__name__)
+
+P = typing.ParamSpec("P")
+
+
+@dataclasses.dataclass(kw_only=True)
+class Retrier(BaseRetrier[ResponseType]):
+    async def retry(  # type: ignore[return]
+        self,
+        coroutine: typing.Callable[P, typing.Awaitable[ResponseType]],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> ResponseType:
+        for attempt in tenacity.Retrying(  # noqa: RET503
+            stop=self.stop,
+            wait=self.wait_strategy,
+            retry=self.retry_cause,
+            reraise=self.reraise,
+            before=self._log_attempts,
+        ):
+            with attempt:
+                return await coroutine(*args, **kwargs)
+
+
+@dataclasses.dataclass(kw_only=True)
+class RetrierCircuitBreaker(BaseRetrier[ResponseType]):
+    circuit_breaker: BaseCircuitBreaker
+
+    async def retry(  # type: ignore[return]
+        self,
+        coroutine: typing.Callable[P, typing.Awaitable[ResponseType]],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> ResponseType:
+        if not (host := str(kwargs.get("host", ""))):
+            msg = "'host' argument should be defined"
+            raise ValueError(msg)
+
+        for attempt in tenacity.Retrying(  # noqa: RET503
+            stop=self.stop,
+            wait=self.wait_strategy,
+            retry=self.retry_cause,
+            reraise=self.reraise,
+            before=self._log_attempts,
+        ):
+            with attempt:
+                if not await self.circuit_breaker.is_host_available(host):
+                    await self.circuit_breaker.raise_host_unavailable_error(host)
+
+                if attempt.retry_state.attempt_number > 1:
+                    await self.circuit_breaker.increment_failures_count(host)
+
+                return await coroutine(*args, **kwargs)
