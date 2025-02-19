039e8878b7d2ea33f91c5e46558d667fd48c27d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:16:20 +0000

    Add package getplayercard/0.4.3/GetPlayerCard-0.4.3-cp39-abi3-win32.whl

---
+++ packages/getplayercard/GetPlayerCard-0.4.3-cp39-abi3-win32.whl/getpc/__init__.py
@@ -0,0 +1,5 @@
+from ._core import CharacterCard, load_template
+from .excel import export_to_excel
+from .typing import RuleSet
+
+__all__ = ['CharacterCard', 'export_to_excel', 'load_template', 'RuleSet']
