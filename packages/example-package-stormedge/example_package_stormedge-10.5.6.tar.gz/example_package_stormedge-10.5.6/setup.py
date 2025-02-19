17d69735c1f44767ae0f3f20f4dbd1e793089e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:06:39 +0000

    Add package example-package-stormedge/10.5.6/example_package_stormedge-10.5.6.tar.gz

---
+++ packages/example-package-stormedge/example_package_stormedge-10.5.6.tar.gz/example_package_stormedge-10.5.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example_package_stormedge",
+    version="10.5.6",
+    author="ZZL",
+    url='http://www.baidu.com',
+    author_email="sdfds@qq.com",
+    description="xxx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
