76e8ea6603c457a328a7790bef9efc13df9dd84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 11:06:02 +0000

    Add package UnextPythonServiceUtils/0.0.6/UnextPythonServiceUtils-0.0.6-py3-none-any.whl

---
+++ packages/UnextPythonServiceUtils/UnextPythonServiceUtils-0.0.6-py3-none-any.whl/UnextPythonServiceUtils/clients/bedrock_client/call_bedrock.py
@@ -0,0 +1,74 @@
+import boto3
+import json
+from typing import Dict, Union
+from decouple import Config, RepositoryEnv
+
+config = Config(RepositoryEnv(".env"))
+
+
+class BedrockClient_Sync:
+
+    def __init__(self) -> None:
+        self.client = boto3.client(
+            "bedrock-runtime",
+            region_name=str(config("BEDROCK_AWS_REGION_NAME")),
+            aws_access_key_id=str(config("AWS_ACCESS_KEY_ID")),
+            aws_secret_access_key=str(config("AWS_SECRET_ACCESS_KEY")),
+        )
+
+    def no_stream_bedrock_response(
+        self, payload: dict, modelId: str
+    ) -> Dict[str, Union[str, int]]:
+        try:
+            response = self.client.invoke_model(
+                body=json.dumps(payload),
+                modelId=modelId,
+                accept="application/json",
+                contentType="application/json",
+            )
+            body = response.get("body").read().decode("utf-8")
+            response_body = json.loads(body)
+            result = (
+                response_body.get("generation")
+                or response_body.get("outputs")[0]["text"]
+            )
+            tokens_count = response_body.get("generation_token_count") or None
+            if tokens_count:
+                return dict(llm_response=result, tokens=tokens_count)
+            return dict(llm_response=result)
+        except (
+            self.client.exceptions.AccessDeniedException,
+            self.client.exceptions.ResourceNotFoundException,
+            self.client.exceptions.ThrottlingException,
+            self.client.exceptions.ModelTimeoutException,
+            self.client.exceptions.InternalServerException,
+            self.client.exceptions.ModelStreamErrorException,
+            self.client.exceptions.ValidationException,
+            self.client.exceptions.ModelNotReadyException,
+            self.client.exceptions.ServiceQuotaExceededException,
+            self.client.exceptions.ModelErrorException,
+        ) as e:
+            raise e
+
+    def stream_bedrock_response(self, payload=None, modelId=None):
+        try:
+            response_stream = self.client.invoke_model_with_response_stream(
+                body=json.dumps(payload),
+                modelId=modelId,
+                accept="application/json",
+                contentType="application/json",
+            )
+            return response_stream
+        except (
+            self.client.exceptions.AccessDeniedException,
+            self.client.exceptions.ResourceNotFoundException,
+            self.client.exceptions.ThrottlingException,
+            self.client.exceptions.ModelTimeoutException,
+            self.client.exceptions.InternalServerException,
+            self.client.exceptions.ModelStreamErrorException,
+            self.client.exceptions.ValidationException,
+            self.client.exceptions.ModelNotReadyException,
+            self.client.exceptions.ServiceQuotaExceededException,
+            self.client.exceptions.ModelErrorException,
+        ) as e:
+            raise e
