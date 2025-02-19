0e26891aab4450b2f2a31d0a10c8ae47686dbbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:49 +0000

    Add package plain.code/0.4.0/plain_code-0.4.0.tar.gz

---
+++ packages/plain.code/plain_code-0.4.0.tar.gz/plain_code-0.4.0/.pytype/pyi/code/__init__.pyi
@@ -0,0 +1,6 @@
+# (generated with --quick)
+
+from typing import Any
+
+__all__: list[str]
+cli: Any
---
+++ packages/plain.code/plain_code-0.4.0.tar.gz/plain_code-0.4.0/.pytype/pyi/code/cli.pyi
@@ -0,0 +1,17 @@
+# (generated with --quick)
+
+import click
+import pathlib
+import subprocess
+import sys
+import tomllib
+from typing import Any
+
+DEFAULT_RUFF_CONFIG: pathlib.Path
+Path: type[pathlib.Path]
+check: Any
+cli: Any
+fix: Any
+print_event: Any
+
+def get_code_config() -> Any: ...
