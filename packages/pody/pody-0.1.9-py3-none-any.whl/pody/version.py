e3f124b4d7a97a76c5e7eba11947fd82adc22dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:18:28 +0000

    Add package pody/0.1.9/pody-0.1.9-py3-none-any.whl

---
+++ packages/pody/pody-0.1.9-py3-none-any.whl/pody/version.py
@@ -0,0 +1,8 @@
+
+VERSION_HISTORY = {
+    "0.1.9": [
+        "Improve help route, format help output as table", 
+    ]
+}
+
+VERSION = tuple([int(x) for x in list(VERSION_HISTORY.keys())[0].split('.')])
