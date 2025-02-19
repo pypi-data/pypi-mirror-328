71d675964d75386e6a817ed41cad0bfab90e4f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:04:22 +0000

    Add package gf-api-client/0.1.2/gf_api_client-0.1.2.tar.gz

---
+++ packages/gf-api-client/gf_api_client-0.1.2.tar.gz/gf_api_client-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gf-api-client",
+    version="0.1.2",
+    author="GF",
+    author_email="tanzhixiong@gf.com.cn",
+    description="广发证券开放平台 API 客户端",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.31.0",
+        "pandas>=2.0.0",
+    ],
+)
