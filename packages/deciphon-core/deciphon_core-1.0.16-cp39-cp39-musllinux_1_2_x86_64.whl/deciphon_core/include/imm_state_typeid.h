aa8309ea87f12c693f8184a42a369415409b211f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:14 +0000

    Add package deciphon-core/1.0.16/deciphon_core-1.0.16-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/deciphon-core/deciphon_core-1.0.16-cp39-cp39-musllinux_1_2_x86_64.whl/deciphon_core/include/imm_state_typeid.h
@@ -0,0 +1,14 @@
+#ifndef IMM_STATE_TYPEID_H
+#define IMM_STATE_TYPEID_H
+
+enum imm_state_typeid
+{
+  IMM_NULL_STATE = 0,
+  IMM_MUTE_STATE = 1,
+  IMM_NORMAL_STATE = 2,
+  IMM_CODON_STATE = 3,
+  IMM_FRAME_STATE = 4,
+  IMM_TABLE_STATE = 4,
+};
+
+#endif
