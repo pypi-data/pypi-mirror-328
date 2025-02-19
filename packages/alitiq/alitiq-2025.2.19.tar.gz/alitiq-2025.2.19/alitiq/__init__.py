29ed93bde941ace0eef2d5e6d71238a87d8adbb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:16:53 +0000

    Add package alitiq/2025.2.19/alitiq-2025.2.19.tar.gz

---
+++ packages/alitiq/alitiq-2025.2.19.tar.gz/alitiq-2025.2.19/alitiq/__init__.py
@@ -0,0 +1,7 @@
+"""easy access"""
+
+from alitiq.load_forecast import alitiqLoadAPI  # noqa
+from alitiq.models.load_forecast import LoadMeasurementForm  # noqa
+from alitiq.models.solar_power_forecast import PvMeasurementForm  # noqa
+from alitiq.models.solar_power_forecast import SolarPowerPlantModel  # noqa
+from alitiq.solar_power_forecast import alitiqSolarAPI  # noqa
