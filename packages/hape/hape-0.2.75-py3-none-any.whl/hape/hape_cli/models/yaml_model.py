d9d1400c70960dbb3c26cff06570ca2046229463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:17:20 +0000

    Add package hape/0.2.75/hape-0.2.75-py3-none-any.whl

---
+++ packages/hape/hape-0.2.75-py3-none-any.whl/hape/hape_cli/models/yaml_model.py
@@ -0,0 +1,29 @@
+import yaml
+from hape.logging import Logging
+from hape.hape_cli.models.crud_model import Crud
+from hape.hape_cli.interfaces.format_model import FormatModel
+
+class Yaml(FormatModel):
+    
+    def __init__(self, model_schema: bool):
+        self.logger = Logging.get_logger('hape.hape_cli.models.yaml_model')
+        self.schema = None
+        self.model_schema = model_schema
+    
+    def load(self, schema: str):
+        self.schema = yaml.safe_load(schema)
+    
+    def get(self):
+        self.logger.debug(f"Getting YAML {{'self.model_schema': {self.model_schema}'}}")
+        self.generate()
+            
+    def generate(self):
+        self.logger.debug(f"Generating YAML {{'self.model_schema': {self.model_schema}'}}")
+        if self.model_schema:
+            print(Crud._model_schema)
+        else:
+            self.logger.error("Nothing to generate.")
+            exit(1)
+    
+    
+
