8443bdcd611dd424c674ad06ae4fc5e019bf9448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:05:40 +0000

    Add package llama-cpp-cffi/0.4.25/llama_cpp_cffi-0.4.25-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/llama-cpp-cffi/llama_cpp_cffi-0.4.25-cp312-cp312-manylinux_2_34_x86_64.whl/llama/context.py
@@ -0,0 +1,35 @@
+__all__ = [
+    'context_init',
+    'context_free',
+]
+
+from .llama_cpp import lib, ffi, lock, llama_model_p, llama_context_p, llama_context_params
+from .options import ModelOptions
+
+
+def context_init(model: llama_model_p, model_options: ModelOptions) -> llama_context_p:
+    ctx_params: llama_context_params = lib.llama_context_default_params()
+    ctx_params.n_ctx = model_options.n_ctx
+    ctx_params.n_batch = model_options.n_batch
+    ctx_params.n_ubatch = model_options.n_ubatch
+    ctx_params.n_threads = model_options.n_threads
+
+    if model_options.n_threads_batch is None:
+        ctx_params.n_threads_batch = model_options.n_threads
+    else:
+        ctx_params.n_threads_batch = model_options.n_threads_batch
+
+    ctx_params.type_k = model_options.type_k.value
+    ctx_params.type_v = model_options.type_v.value
+    ctx_params.flash_attn = model_options.flash_attn
+
+    with lock:
+        context: llama_context_p = lib.llama_new_context_with_model(model, ctx_params)
+
+    assert context != ffi.NULL
+    return context
+
+
+def context_free(context: llama_context_p):
+    with lock:
+        lib.llama_free(context)
