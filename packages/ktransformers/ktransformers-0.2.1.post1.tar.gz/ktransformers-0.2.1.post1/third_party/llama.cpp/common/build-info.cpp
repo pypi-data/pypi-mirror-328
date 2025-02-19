03238f29136f93a01682a2c710f00eca0d896c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:06:46 +0000

    Add package ktransformers/0.2.1.post1/ktransformers-0.2.1.post1.tar.gz

---
+++ packages/ktransformers/ktransformers-0.2.1.post1.tar.gz/ktransformers-0.2.1.post1/third_party/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3173;
+char const *LLAMA_COMMIT = "a94e6ff8";
+char const *LLAMA_COMPILER = "cc (Ubuntu 9.4.0-1ubuntu1~20.04.3) 9.4.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
