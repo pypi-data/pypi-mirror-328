2a374df3d80415c4adf50faf2b874553a9119f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:15:56 +0000

    Add package bionty/1.1.0/bionty-1.1.0.tar.gz

---
+++ packages/bionty/bionty-1.1.0.tar.gz/bionty-1.1.0/bionty/base/entities/_cellmarker.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from bionty.base._public_ontology import PublicOntology
+
+from ._shared_docstrings import _doc_params, doc_entites
+
+
+@_doc_params(doc_entities=doc_entites)
+class CellMarker(PublicOntology):
+    """Cell markers.
+
+    1. Cell Marker Ontology
+    http://bio-bigdata.hrbmu.edu.cn/CellMarker/
+
+    Args:
+        {doc_entities}
+    """
+
+    def __init__(
+        self,
+        organism: Literal["human", "mouse"] | None = None,
+        source: Literal["cellmarker"] | None = None,
+        version: Literal["2.0"] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(source=source, version=version, organism=organism, **kwargs)
---
+++ packages/bionty/bionty-1.1.0.tar.gz/bionty-1.1.0/bionty/base/entities/_pathway.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from bionty.base._public_ontology import PublicOntology
+
+from ._shared_docstrings import _doc_params, doc_entites
+
+
+@_doc_params(doc_entities=doc_entites)
+class Pathway(PublicOntology):
+    """Pathway.
+
+    1. Gene Ontology
+    https://bioportal.bioontology.org/ontologies/GO/?p=summary
+
+    2. Pathway Ontology
+    https://bioportal.bioontology.org/ontologies/PW/?p=summary
+
+    Args:
+        {doc_entities}
+    """
+
+    def __init__(
+        self,
+        organism: Literal["all"] | None = None,
+        source: Literal["go", "pw"] | None = None,
+        version: Literal["2023-05-10", "2024-06-17"] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(source=source, version=version, organism=organism, **kwargs)
