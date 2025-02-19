a658ecf872cd82dce8051e116c5b053a9d4fe57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:27:36 +0000

    Add package simpleCan/1.7.3/simpleCan-1.7.3-py3-none-any.whl

---
+++ packages/simpleCan/simpleCan-1.7.3-py3-none-any.whl/simpleCan/__init__.py
@@ -0,0 +1,70 @@
+import logging
+from simpleCan.util import dataStructure as ds, can_func
+from simpleCan.util.task import SendMessageTask, RecvMessageTask
+from simpleCan.util.messageList import MessageList
+
+__all__ = ['SimpleCan']
+
+
+class SimpleCan:
+
+    def __init__(self):
+        # create a list to store all messages sending to DDU
+        self.tasklist = []
+        self.messageList = MessageList()
+        can_func.setup()
+
+    def env_setup(self, duration=360):
+        self.messageList.clearMessageList()
+        self.messageList.load_default_messageList()
+        messageList = self.messageList.get_messageList()
+        self.clearTaskList()
+        for i in range(len(messageList)):
+            self.tasklist.append(SendMessageTask(message_id=messageList[i].id,
+                                                 data=messageList[i].data,
+                                                 period=messageList[i].period,
+                                                 duration=duration))
+
+    def env_run(self):
+        for task in self.tasklist:
+            task.task_run()
+
+    def sendMessage(self, message_id, data, period, duration=30):
+        task = SendMessageTask(message_id=message_id,
+                               data=data,
+                               period=period,
+                               duration=duration)
+        self.tasklist.append(task)
+        task.task_run()
+
+    def recvMessage(self, message_id, offset = 0, duration=10) -> ds.ReceivedCanMessage:
+        return RecvMessageTask.recvMessage(message_id=message_id, offset = offset, duration = duration)
+
+    def modifyMessage(self, message_id, data):
+        try:
+            for task in self.tasklist:
+                if task.get_messageID() == message_id:
+                    logging.critical(task.get_messageID())
+                    task.task_modifyData(newData=data)
+
+        except Exception as e:
+            logging.error(e)
+
+    def stopMessage(self, message_id):
+        try:
+            for task in self.tasklist:
+                if task.get_messageID() == message_id:
+                    task.task_stop()
+        except Exception as e:
+            logging.error(e)
+
+    def clearTaskList(self):
+        self.tasklist = []
+
+    def endAllTasks(self):
+        for task in self.tasklist:
+            task.task_stop()
+
+    def __del__(self):
+        self.endAllTasks()
+        can_func.teardown()
