4e02cf6e716d7cda656de49e00ef24fa01167651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:06:03 +0000

    Add package roofai/6.236.1/roofai-6.236.1-py3-none-any.whl

---
+++ packages/roofai/roofai-6.236.1-py3-none-any.whl/roofai/help/functions.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from blue_options.terminal import show_usage, xtra
+from abcli.help.generic import help_functions as generic_help_functions
+
+from roofai import ALIAS
+from roofai.help.dataset import help_functions as help_dataset
+from roofai.help.google_maps import help_functions as help_google_maps
+from roofai.help.roboflow import help_functions as help_roboflow
+from roofai.help.semseg import help_functions as help_semseg
+
+
+help_functions = generic_help_functions(plugin_name=ALIAS)
+
+help_functions.update(
+    {
+        "dataset": help_dataset,
+        "google_maps": help_google_maps,
+        "roboflow": help_roboflow,
+        "semseg": help_semseg,
+    }
+)
