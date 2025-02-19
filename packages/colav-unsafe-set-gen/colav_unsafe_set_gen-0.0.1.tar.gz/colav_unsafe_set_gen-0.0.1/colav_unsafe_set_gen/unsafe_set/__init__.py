3803695e592d6f26140c721e1ce17b55be79adfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:05:59 +0000

    Add package colav-unsafe-set-gen/0.0.1/colav_unsafe_set_gen-0.0.1.tar.gz

---
+++ packages/colav-unsafe-set-gen/colav_unsafe_set_gen-0.0.1.tar.gz/colav_unsafe_set_gen-0.0.1/colav_unsafe_set_gen/unsafe_set/__init__.py
@@ -0,0 +1,5 @@
+from unsafe_set_gen.risk_assesment import *
+from unsafe_set_gen import objects
+from .unsafe_set import create_unsafe_set_polyshape
+
+__all__ = ['create_unsafe_set_polyshape', 'objects']
