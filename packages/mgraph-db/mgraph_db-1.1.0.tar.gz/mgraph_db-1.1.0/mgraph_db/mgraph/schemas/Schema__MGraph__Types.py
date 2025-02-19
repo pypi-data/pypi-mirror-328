bcf11b99a8ca5281c9cff55c03ffb491929741f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:30 +0000

    Add package mgraph-db/1.1.0/mgraph_db-1.1.0.tar.gz

---
+++ packages/mgraph-db/mgraph_db-1.1.0.tar.gz/mgraph_db-1.1.0/mgraph_db/mgraph/schemas/Schema__MGraph__Types.py
@@ -0,0 +1,14 @@
+from typing                                                 import Type
+from osbot_utils.type_safe.Type_Safe                        import Type_Safe
+from mgraph_db.mgraph.schemas.Schema__MGraph__Edge          import Schema__MGraph__Edge
+from mgraph_db.mgraph.schemas.Schema__MGraph__Edge__Config  import Schema__MGraph__Edge__Config
+from mgraph_db.mgraph.schemas.Schema__MGraph__Graph__Data   import Schema__MGraph__Graph__Data
+from mgraph_db.mgraph.schemas.Schema__MGraph__Node          import Schema__MGraph__Node
+from mgraph_db.mgraph.schemas.Schema__MGraph__Node__Data    import Schema__MGraph__Node__Data
+
+class Schema__MGraph__Types(Type_Safe):
+    edge_type        : Type[Schema__MGraph__Edge         ]
+    edge_config_type : Type[Schema__MGraph__Edge__Config ]
+    graph_data_type  : Type[Schema__MGraph__Graph__Data]
+    node_type        : Type[Schema__MGraph__Node         ]
+    node_data_type   : Type[Schema__MGraph__Node__Data   ]
