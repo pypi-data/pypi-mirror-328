646cf5e9fffeb744d47071cd62fac34a9efeffdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:06:53 +0000

    Add package deepvisiontools/0.0.4/deepvisiontools-0.0.4-py3-none-any.whl

---
+++ packages/deepvisiontools/deepvisiontools-0.0.4-py3-none-any.whl/deepvisiontools/inference/__init__.py
@@ -0,0 +1,4 @@
+from deepvisiontools.inference.patchifier import BasePatchifier, SemanticPatchifier, DetectPatchifier, PostProcesser
+from deepvisiontools.inference.predictor import Predictor, PredictorDataLoader
+
+__all__ = ("Predictor", "BasePatchifier", "DetectPatchifier", "SemanticPatchifier", "PostProcesser", "PredictorDataLoader")
