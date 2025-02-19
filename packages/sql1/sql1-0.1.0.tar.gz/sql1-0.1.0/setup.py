f181c3df8b523514ec810255a030ac0d0198bd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:57 +0000

    Add package sql1/0.1.0/sql1-0.1.0.tar.gz

---
+++ packages/sql1/sql1-0.1.0.tar.gz/sql1-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sql1',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT',
+    description="An asynchronous Python ORM that supports MySQL, SQLite, and PostgreSQL with robust performance and easy-to-use interface.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='NuoQTe',
+    author_email='nuoqte@gmail.com',
+    url='https://github.com/NuoQte/sql1',
+)
