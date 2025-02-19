73b0a4f8f05aa45830199e4d55113d3e8cb7095d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:16:01 +0000

    Add package nextline-alert/0.9.2/nextline_alert-0.9.2-py3-none-any.whl

---
+++ packages/nextline-alert/nextline_alert-0.9.2-py3-none-any.whl/nextline_alert/emitter.py
@@ -0,0 +1,57 @@
+from logging import getLogger
+
+import httpx
+
+from nextline.plugin.spec import Context, hookimpl
+
+
+class Emitter:
+    def __init__(self, url: str, platform: str):
+        self._url = url
+        self._platform = platform
+        self._logger = getLogger(__name__)
+        self._logger.info(f'Campana endpoint: {url}')
+        self._logger.debug(f'Platform: {platform!r}')
+
+    @hookimpl
+    async def on_end_run(self, context: Context) -> None:
+        nextline = context.nextline
+
+        if not (fmt_exc := nextline.format_exception()):
+            return
+
+        # TODO: Quick implementation to ignore KeyboardInterrupt
+        #       Instead of parsing fmt_exc, store the exception type as string
+        #       in RunResult in the spawned process.
+        #       https://github.com/simonsobs/nextline/blob/v0.7.4/nextline/spawned/types.py#L35-L58
+        if fmt_exc.rstrip().endswith('KeyboardInterrupt'):
+            self._logger.info('Ignoring KeyboardInterrupt')
+            return
+
+        run_arg = context.run_arg
+        run_no_str = 'unknown' if run_arg is None else f'{run_arg.run_no}'
+        alertname = f'Run {run_no_str} failed'
+        self._logger.info(f"Emitting alert: '{alertname}'")
+        labels = {'alertname': alertname, 'platform': self._platform}
+        try:
+            await emit(url=self._url, labels=labels, description=fmt_exc)
+        except BaseException:
+            self._logger.exception(f"Failed to emit alert: '{alertname}'")
+            self._logger.debug(f'Alert description: {fmt_exc!r}')
+
+
+async def emit(url: str, labels: dict[str, str], description: str) -> None:
+    data = {
+        'status': 'firing',
+        'alerts': [
+            {
+                'status': 'firing',
+                'labels': labels,
+                'annotations': {'description': description, 'groups': 'nextline'},
+            }
+        ],
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=data)
+        response.raise_for_status()
---
+++ packages/nextline-alert/nextline_alert-0.9.2-py3-none-any.whl/nextline_alert/plugin.py
@@ -0,0 +1,59 @@
+from collections.abc import Mapping
+from contextlib import asynccontextmanager
+from logging import getLogger
+from pathlib import Path
+from typing import Optional, cast
+
+from dynaconf import Dynaconf, Validator
+
+from nextline import Nextline
+from nextlinegraphql.hook import spec
+
+from .__about__ import __version__
+from .emitter import Emitter
+from .schema import Mutation, Query, Subscription
+
+HERE = Path(__file__).resolve().parent
+DEFAULT_CONFIG_PATH = HERE / 'default.toml'
+
+assert DEFAULT_CONFIG_PATH.is_file()
+
+PRELOAD = (str(DEFAULT_CONFIG_PATH),)
+SETTINGS = ()
+VALIDATORS = (
+    Validator('ALERT.CAMPANA_URL', must_exist=True, is_type_of=str),
+    Validator('ALERT.PLATFORM', must_exist=True, is_type_of=str),
+)
+
+
+class Plugin:
+    @spec.hookimpl
+    def dynaconf_preload(self) -> Optional[tuple[str, ...]]:
+        return PRELOAD
+
+    @spec.hookimpl
+    def dynaconf_settings_files(self) -> Optional[tuple[str, ...]]:
+        return SETTINGS
+
+    @spec.hookimpl
+    def dynaconf_validators(self) -> Optional[tuple[Validator, ...]]:
+        return VALIDATORS
+
+    @spec.hookimpl
+    def configure(self, settings: Dynaconf):
+        logger = getLogger(__name__)
+        logger.info(f'{__package__} version: {__version__}')
+        url = settings.alert.campana_url
+        platform = settings.alert.platform
+        self._emitter = Emitter(url=url, platform=platform)
+
+    @spec.hookimpl
+    def schema(self) -> tuple[type, type | None, type | None]:
+        return (Query, Mutation, Subscription)
+
+    @spec.hookimpl
+    @asynccontextmanager
+    async def lifespan(self, context: Mapping):
+        nextline = cast(Nextline, context['nextline'])
+        nextline.register(self._emitter)
+        yield
