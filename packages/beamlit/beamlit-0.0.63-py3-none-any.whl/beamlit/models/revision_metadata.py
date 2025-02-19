4bd8ac7ead9c0ffa77405fd3dc761f8f922a8362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:27:52 +0000

    Add package beamlit/0.0.63/beamlit-0.0.63-py3-none-any.whl

---
+++ packages/beamlit/beamlit-0.0.63-py3-none-any.whl/beamlit/models/revision_metadata.py
@@ -0,0 +1,70 @@
+from typing import Any, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="RevisionMetadata")
+
+
+@_attrs_define
+class RevisionMetadata:
+    """Revision metadata
+
+    Attributes:
+        created_at (Union[Unset, str]): Revision created at
+        id (Union[Unset, str]): Revision ID
+    """
+
+    created_at: Union[Unset, str] = UNSET
+    id: Union[Unset, str] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        created_at = self.created_at
+
+        id = self.id
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if created_at is not UNSET:
+            field_dict["createdAt"] = created_at
+        if id is not UNSET:
+            field_dict["id"] = id
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        if not src_dict:
+            return None
+        d = src_dict.copy()
+        created_at = d.pop("createdAt", UNSET)
+
+        id = d.pop("id", UNSET)
+
+        revision_metadata = cls(
+            created_at=created_at,
+            id=id,
+        )
+
+        revision_metadata.additional_properties = d
+        return revision_metadata
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
