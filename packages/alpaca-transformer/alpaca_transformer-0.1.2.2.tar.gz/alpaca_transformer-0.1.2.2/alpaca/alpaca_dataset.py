04dade9ef96a00db7e830b6cfc126c7b4c472f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 11:06:31 +0000

    Add package alpaca-transformer/0.1.2.2/alpaca_transformer-0.1.2.2.tar.gz

---
+++ packages/alpaca-transformer/alpaca_transformer-0.1.2.2.tar.gz/alpaca_transformer-0.1.2.2/alpaca/alpaca_dataset.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from .tokenizer import Tokenizer
+
+class AlpacaDataset(Dataset):
+    def __init__(self, txt_file, tokenizer:Tokenizer, max_seq_len=512, num_merges=1000):
+        self.tokenizer = tokenizer
+        self.max_seq_len = max_seq_len
+        
+       
+        with open(txt_file, 'r') as f:
+            whole_text = f.read()  
+        
+        
+        self.vocab = tokenizer.create_vocab(whole_text, num_merges=num_merges)
+        
+        
+        with open(txt_file, 'r') as f:
+            self.sentences = f.readlines()
+
+    def __getitem__(self, index):
+        sentence = self.sentences[index]
+        
+        tokens = self.tokenizer.tokenize(sentence, vocab=self.vocab)
+        
+       
+        tokens = [t if 0 <= t < len(self.vocab) else 0 for t in tokens]  
+        
+        
+        tokens = [t if t >= 0 else 0 for t in tokens]
+        
+        
+        if len(tokens) < self.max_seq_len:
+            padding = [0] * (self.max_seq_len - len(tokens))
+            tokens = tokens + padding
+        else:
+            tokens = tokens[:self.max_seq_len]
+        
+        return torch.tensor(tokens)
+
+    
+    def __len__(self):
+        return len(self.sentences)
