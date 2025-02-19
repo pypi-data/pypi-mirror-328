5ee1a688a3b20ffc10acd61eac0cfc9a00e793a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:18:20 +0000

    Add package oarepo-model-builder-rdm/1.0.10/oarepo_model_builder_rdm-1.0.10.tar.gz

---
+++ packages/oarepo-model-builder-rdm/oarepo_model_builder_rdm-1.0.10.tar.gz/oarepo_model_builder_rdm-1.0.10/oarepo_model_builder_rdm/invenio/__init__.py
@@ -0,0 +1,6 @@
+TEMPLATES = {
+    "rdm-record-metadata": "templates/rdm_record_metadata.py.jinja2",
+    "rdm-record": "templates/rdm_record.py.jinja2",
+    "rdm-draft-record": "templates/rdm_draft_record.py.jinja2",
+    "rdm-record-search-options": "templates/rdm_record_search_options.py.jinja2"
+}
