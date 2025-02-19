03bdb106cb089000403559d1b53a48ed0564ba5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:04:45 +0000

    Add package mandarin-tamer/0.0.1/mandarin_tamer-0.0.1-py3-none-any.whl

---
+++ packages/mandarin-tamer/mandarin_tamer-0.0.1-py3-none-any.whl/mandarinTamer/__init__.py
@@ -0,0 +1,7 @@
+"""src/mandarinTamer/__init__.py - Package initialization"""
+
+__version__ = "0.0.1"
+
+from mandarinTamer.mandarin_tamer import convert_mandarin_script
+
+__all__ = ["convert_mandarin_script"]
