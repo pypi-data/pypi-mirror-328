810fdf810b24f3276ad6ed105ce89a5bd71c697b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:25:52 +0000

    Add package versionhq/1.2.1.16/versionhq-1.2.1.16.tar.gz

---
+++ packages/versionhq/versionhq-1.2.1.16.tar.gz/versionhq-1.2.1.16/tests/formation_test.py
@@ -0,0 +1,42 @@
+def test_minimum_inputs():
+    import versionhq as vhq
+
+    network = vhq.form_agent_network(
+        task="Describe the color of the sky by weather condition",
+        expected_outcome="Dict that pairs color of the sky with specific weather condition"
+    )
+    assert isinstance(network, vhq.AgentNetwork)
+    assert network.members
+    assert network.tasks
+    assert isinstance(network.formation, vhq.Formation)
+    assert network.managers if network.formation in (vhq.Formation.SUPERVISING, vhq.Formation.HYBRID) else not network.managers
+
+
+def _test_specific_formation():
+    import versionhq as vhq
+
+    formations_to_test = [
+        vhq.Formation.NETWORK,
+        vhq.Formation.RANDOM,
+        vhq.Formation.SOLO,
+        vhq.Formation.SUPERVISING,
+        vhq.Formation.UNDEFINED,
+        "network",
+        1,
+        "dummy",
+        1000,
+        None
+    ]
+
+    for item in formations_to_test:
+        network = vhq.form_agent_network(
+            task="Describe the color of the sky by weather condition",
+            expected_outcome="Dict that pairs color of the sky with specific weather condition",
+            formation=item,
+        )
+
+        assert isinstance(network, vhq.AgentNetwork)
+        assert network.formation == item if isinstance(item, vhq.Formation) and item != vhq.Formation.UNDEFINED else isinstance(network.formation, vhq.Formation)
+        assert network.members
+        assert network.tasks
+        assert network.managers is not None if network.formation == vhq.Formation.SUPERVISING else not network.manager_tasks
