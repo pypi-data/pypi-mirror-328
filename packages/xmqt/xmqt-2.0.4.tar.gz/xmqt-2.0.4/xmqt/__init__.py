83bb62c36d059af8887ad468612a260e5f56e03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:06:11 +0000

    Add package xmqt/2.0.4/xmqt-2.0.4.tar.gz

---
+++ packages/xmqt/xmqt-2.0.4.tar.gz/xmqt-2.0.4/xmqt/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+from xmqt.quantify_xoenmap import quantify_xoenmap as quantify_xoenmap
+from xmqt.sarindicator import sarindicator as sarindicator
+#from xmqt.dingtalkboot import dingtalkboot as dingtalkboot
