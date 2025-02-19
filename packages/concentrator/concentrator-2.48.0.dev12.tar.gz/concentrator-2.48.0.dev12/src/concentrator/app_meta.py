d12565f66e6daac9b1ca3a8b20cf7bbb55e23387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 03:07:41 +0000

    Add package concentrator/2.48.0.dev12/concentrator-2.48.0.dev12.tar.gz

---
+++ packages/concentrator/concentrator-2.48.0.dev12.tar.gz/concentrator-2.48.0.dev12/src/concentrator/app_meta.py
@@ -0,0 +1,19 @@
+from kinder.controllers import (
+    dict_controller,
+)
+
+from concentrator.changes.actions import (
+    ChangesDetailPack,
+    ChangesPack,
+)
+from concentrator.dict.actions import (
+    ServicesPack,
+)
+from concentrator.update_conf.actions import (
+    UpdateConfigPack,
+)
+
+
+def register_actions():
+    """Метод регистрации Action'ов для приложения в котором описан."""
+    dict_controller.packs.extend([UpdateConfigPack(), ChangesDetailPack(), ChangesPack(), ServicesPack()])
