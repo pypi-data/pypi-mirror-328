542acf5bb520d759ec32ff6abb99c2217623dc95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:06:56 +0000

    Add package humiris-ai/1.0.7/humiris_ai-1.0.7.tar.gz

---
+++ packages/humiris-ai/humiris_ai-1.0.7.tar.gz/humiris_ai-1.0.7/humiris_ai/basic_mixtuning.py
@@ -0,0 +1,40 @@
+import requests
+
+class MessageType:
+    def __init__(self, role: str, content: str):
+        self.role = role
+        self.content = content
+
+class BasicPayload:
+    def __init__(self, mix_tuning_id: str, messages: list, temperature: float, max_tokens: int):
+        self.mix_tuning_id = mix_tuning_id
+        self.messages = messages
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+
+class BasicMoAi:
+    def __init__(self, api_key: str):
+        self.api_url = 'https://moai-service-app.humiris.ai/api/api-key-operators/use-basic-mixtuning'
+        self.api_key = api_key
+
+    def use_basic_mixtuning(self, payload: BasicPayload):
+        headers = {
+            'moai-api-key': self.api_key,
+            'Content-Type': 'application/json'
+        }
+
+         # Convert payload and MessageType objects to dictionaries
+        payload_dict = {
+            "mix_tuning_id": payload.mix_tuning_id,
+            "messages": [{"role": msg.role, "content": msg.content} for msg in payload.messages],
+            "temperature": payload.temperature,
+            "max_tokens": payload.max_tokens
+        }
+
+        try:
+            response = requests.post(self.api_url, json=vars(payload_dict), headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            raise Exception(f"Failed to send request: {error}")
+
---
+++ packages/humiris-ai/humiris_ai-1.0.7.tar.gz/humiris_ai-1.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='humiris_ai',
+    version='1.0.7',
+    author='Hilario Houmey',
+    author_email='h.hilario@humiris.ai',
+    description='Humiris AI is a library that allows developers to seamlessly integrate Mixture of AI Basic and Advanced into their applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/joeai-tech-org/humiris-pip-package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # list your package dependencies here
+    ],
+)
