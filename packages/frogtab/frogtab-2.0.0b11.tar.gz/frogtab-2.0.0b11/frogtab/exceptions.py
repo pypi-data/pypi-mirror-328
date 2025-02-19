16992256cecb4fdac519d601834c3737d9042ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:17:56 +0000

    Add package frogtab/2.0.0b11/frogtab-2.0.0b11.tar.gz

---
+++ packages/frogtab/frogtab-2.0.0b11.tar.gz/frogtab-2.0.0b11/frogtab/exceptions.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+class Read(PermissionError):
+    def __init__(self, path: Path):
+        super().__init__(f"unable to read '{path.absolute()}'")
+        self.path = path
+
+class Write(PermissionError):
+    def __init__(self, path: Path):
+        super().__init__(f"unable to write '{path.absolute()}'")
+        self.path = path
+
+class WrongVersion(Exception):
+    def __init__(self, port: int, version: str):
+        super().__init__(f"version {version} of Frogtab Local is running on port {port}")
+        self.port = port
+        self.version = version
+
+class WrongApp(Exception):
+    def __init__(self, port: int):
+        super().__init__(f"a different app is using port {port}")
+        self.port = port
+
+class Running(Exception):
+    def __init__(self, port: int):
+        super().__init__(f"Frogtab Local is running on port {port}")
+        self.port = port
+
+class NotRunning(Exception):
+    def __init__(self, port: int):
+        super().__init__(f"Frogtab Local is not running on port {port}")
+        self.port = port
