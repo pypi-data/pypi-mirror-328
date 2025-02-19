81c54a6a1ff8a92b87d57b86624c5de473bc23f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:15:45 +0000

    Add package comfy-installer/0.1.12/comfy_installer-0.1.12.tar.gz

---
+++ packages/comfy-installer/comfy_installer-0.1.12.tar.gz/comfy_installer-0.1.12/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+def load_requirements():
+    req_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    if os.path.exists(req_path):
+        with open(req_path, "r") as f:
+            return f.read().splitlines()
+    return []
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="comfy-installer",
+    version="0.1.12",
+    description="A CLI tool to install custom nodes for ComfyUI using YAML configuration.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="khengyun",
+    author_email="khaangnguyeen@gmail.com",
+    packages=find_packages(),
+    install_requires=load_requirements(),
+    entry_points={
+        "console_scripts": [
+            "comfy-installer=comfy_installer.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
