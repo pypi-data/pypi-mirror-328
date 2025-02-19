80b29529c312ae2217923f998d5550aeef65b055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:38 +0000

    Add package agentevals/0.0.1rc5/agentevals-0.0.1rc5-py3-none-any.whl

---
+++ packages/agentevals/agentevals-0.0.1rc5-py3-none-any.whl/agentevals/evaluators/types.py
@@ -0,0 +1,11 @@
+from openevals.evaluators.types import (
+    ChatCompletionMessage,
+    EvaluatorResult,
+    FewShotExample,
+)
+
+__all__ = [
+    "ChatCompletionMessage",
+    "EvaluatorResult",
+    "FewShotExample",
+]
