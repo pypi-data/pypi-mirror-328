4d954dd789a31ed9dd589896e314866bf9521050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:06:39 +0000

    Add package ai71/0.0.23/ai71-0.0.23.tar.gz

---
+++ packages/ai71/ai71-0.0.23.tar.gz/ai71-0.0.23/ai71/types/model.py
@@ -0,0 +1,24 @@
+import pydantic
+from typing_extensions import Annotated
+
+_UUID_PATTERN = (
+    r"[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}"
+)
+_MODELS = [
+    "tiiuae/falcon-180B-chat",
+    "tiiuae/falcon-mamba-7B-instruct",
+    "tiiuae/falcon3-7B-instruct",
+    "tiiuae/falcon3-10B-instruct",
+]
+_FT_MODELS = (
+    model.replace("tiiuae/", "").replace("B", b)
+    for model in _MODELS
+    for b in ("B", "b")
+)
+FINETUNE_MODEL_PATTERN = rf"ft:(?P<model>{'|'.join(_FT_MODELS)}):(?<org_id>{_UUID_PATTERN})(?::(?P<suffix>[^:]*))?:(?<uuid>{_UUID_PATTERN})"
+_BASE_MODELS = (
+    model.replace("/", r"\/").replace("B", b) for model in _MODELS for b in ("B", "b")
+)
+BASE_MODEL_PATTERN = rf"{'|'.join(_BASE_MODELS)}"
+MODEL_PATTERN = rf"^(?:{BASE_MODEL_PATTERN})|(?:{FINETUNE_MODEL_PATTERN})$"
+Model = Annotated[str, pydantic.StringConstraints(to_lower=True, pattern=MODEL_PATTERN)]
