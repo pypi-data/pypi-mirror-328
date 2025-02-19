876d065b4aa6fd2939b31d248f1ba8b2e9978e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:16:05 +0000

    Add package llmbo-bedrock/0.1.6/llmbo_bedrock-0.1.6-py3-none-any.whl

---
+++ packages/llmbo-bedrock/llmbo_bedrock-0.1.6-py3-none-any.whl/llmbo/__init__.py
@@ -0,0 +1,15 @@
+from .llmbo import (
+    BatchInferer,
+    Manifest,
+    ModelInput,
+    StructuredBatchInferer,
+    ToolChoice,
+)
+
+__all__ = [
+    "Manifest",
+    "ToolChoice",
+    "ModelInput",
+    "BatchInferer",
+    "StructuredBatchInferer",
+]
