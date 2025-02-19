922d46dcf5ba9c543d28ed3c9ac9e85ed1cb28cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:16:31 +0000

    Add package dissect.eventlog/3.10.dev1/dissect.eventlog-3.10.dev1-py3-none-any.whl

---
+++ packages/dissect.eventlog/dissect.eventlog-3.10.dev1-py3-none-any.whl/dissect/eventlog/utils.py
@@ -0,0 +1,15 @@
+class KeyValueCollection(dict):
+    """Eventlog key-value pair collection."""
+
+    def __init__(self):
+        super().__init__()
+        self.idx = {}
+
+    def __setitem__(self, key, value):
+        if key in self.idx:
+            self.idx[key] += 1
+            key = f"{key}_{self.idx[key]}"
+        else:
+            self.idx[key] = 0
+
+        dict.__setitem__(self, key, value)
