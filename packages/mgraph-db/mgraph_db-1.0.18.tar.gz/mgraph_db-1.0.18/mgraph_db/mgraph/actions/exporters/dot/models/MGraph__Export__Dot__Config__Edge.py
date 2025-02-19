b1fcc04fd5ece8276c423f855ebb47deb4c2256f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:06:04 +0000

    Add package mgraph-db/1.0.18/mgraph_db-1.0.18.tar.gz

---
+++ packages/mgraph-db/mgraph_db-1.0.18.tar.gz/mgraph_db-1.0.18/mgraph_db/mgraph/actions/exporters/dot/models/MGraph__Export__Dot__Config__Edge.py
@@ -0,0 +1,3 @@
+from mgraph_db.mgraph.actions.exporters.dot.models.MGraph__Export__Dot__Config__Style import MGraph__Export__Dot__Config__Style
+
+class MGraph__Export__Dot__Config__Edge(MGraph__Export__Dot__Config__Style): pass
