abd7d23115e4d9f4c69281fbacf50057fce28e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:05:37 +0000

    Add package quantum-evolution-kernel/0.2.4/quantum_evolution_kernel-0.2.4-py3-none-any.whl

---
+++ packages/quantum-evolution-kernel/quantum_evolution_kernel-0.2.4-py3-none-any.whl/qek/kernel/__init__.py
@@ -0,0 +1,3 @@
+from .kernel import QuantumEvolutionKernel
+
+__all__ = ["QuantumEvolutionKernel"]
---
+++ packages/quantum-evolution-kernel/quantum_evolution_kernel-0.2.4-py3-none-any.whl/qek/shared/error.py
@@ -0,0 +1,6 @@
+class CompilationError(Exception):
+    """
+    An error raised when attempting to compile a graph for an architecture
+    that does not support it, e.g. because it requires too many qubits or
+    because the physical constraints on the geometry are not satisfied.
+    """
