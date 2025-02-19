2d324f4029291840215aa898ca9e3eb88f4af485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:04:15 +0000

    Add package roofai/6.203.1/roofai-6.203.1.tar.gz

---
+++ packages/roofai/roofai-6.203.1.tar.gz/roofai-6.203.1/roofai/help/google_maps/__init__.py
@@ -0,0 +1,7 @@
+from roofai.help.google_maps.geocode import help_geocode
+from roofai.help.google_maps.static_api import help_get_static_image
+
+help_functions = {
+    "geocode": help_geocode,
+    "get_static_image": help_get_static_image,
+}
---
+++ packages/roofai/roofai-6.203.1.tar.gz/roofai-6.203.1/roofai/tests/test_google_maps_get_static_image.py
@@ -0,0 +1,40 @@
+import pytest
+import numpy as np
+
+from blue_options import string
+from blue_objects import objects
+
+from roofai import env
+from roofai.google_maps.api.static import get as get_static_image
+
+
+@pytest.mark.parametrize(
+    ["lat"],
+    [[env.ROOFAI_TEST_GOOGLE_MAPS_LAT]],
+)
+@pytest.mark.parametrize(
+    ["lon"],
+    [[env.ROOFAI_TEST_GOOGLE_MAPS_LON]],
+)
+def test_google_maps_get_static_image(
+    lat: float,
+    lon: float,
+):
+    object_name = objects.unique_object("test_google_maps_get_static_image")
+
+    success, image, metadata = get_static_image(
+        lat=lat,
+        lon=lon,
+        filename=objects.path_of(
+            object_name=object_name,
+            filename=f"{string.timestamp()}.png",
+        ),
+    )
+
+    assert success
+    assert isinstance(image, np.ndarray)
+
+    assert "gsd" in metadata
+    assert isinstance(metadata["gsd"], float)
+
+    assert "size" in metadata
