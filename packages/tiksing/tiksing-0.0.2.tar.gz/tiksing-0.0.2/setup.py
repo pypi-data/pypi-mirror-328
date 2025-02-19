f6fa4ebaeb883fd228f052549279503354f735f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:56 +0000

    Add package tiksing/0.0.2/tiksing-0.0.2.tar.gz

---
+++ packages/tiksing/tiksing-0.0.2.tar.gz/tiksing-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'tiksing'
+LONG_DESCRIPTION = 'tiktok sing.'
+
+# Setting up
+setup(
+    name="tiksing",
+    version=VERSION,
+    author="tiksing (singtok singtok)",
+    author_email="<singtok@tiksing.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=['tiktok', 'tiktok', 'tiktok', 'tiktok', 'tiktok', 'tiktok'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
