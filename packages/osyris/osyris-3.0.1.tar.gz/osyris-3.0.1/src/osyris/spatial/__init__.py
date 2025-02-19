e11d7a9de083b5baad1772b2bbc16ade715082b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:18:04 +0000

    Add package osyris/3.0.1/osyris-3.0.1.tar.gz

---
+++ packages/osyris/osyris-3.0.1.tar.gz/osyris-3.0.1/src/osyris/spatial/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Osyris contributors (https://github.com/osyris-project/osyris)
+
+from .subdomain import extract_box, extract_sphere
+
+__all__ = [
+    "angular_momentum_vector",
+    "extract_box",
+    "extract_sphere",
+    "side_view",
+    "top_view",
+]
