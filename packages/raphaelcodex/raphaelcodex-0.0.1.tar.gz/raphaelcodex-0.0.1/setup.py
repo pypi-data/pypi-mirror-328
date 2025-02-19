8979afa96278f69fdfdfd80864236875ca5cc8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:04:58 +0000

    Add package raphaelcodex/0.0.1/raphaelcodex-0.0.1.tar.gz

---
+++ packages/raphaelcodex/raphaelcodex-0.0.1.tar.gz/raphaelcodex-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="raphaelcodex",
+    version="0.0.1",
+    packages=find_packages(),
+    description="ChioneRaphael",
+    author="Chione Raphael",
+    author_email="chioneraphael@gmail.com",
+    license="MIT",
+    install_requires=[
+        "pyperclip",
+        "numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
