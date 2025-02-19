fbfc6cfb3021b1ef9bbaa72c4036424a2dfd2908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:56 +0000

    Add package grandlantern/0.0.16/grandlantern-0.0.16-py3-none-any.whl

---
+++ packages/grandlantern/grandlantern-0.0.16-py3-none-any.whl/grandlantern/layers/__init__.py
@@ -0,0 +1,3 @@
+from .Activation import ActivationFunction, Linear, Sigmoid, Tanh, ReLU, LReLU, SoftMax
+from .Regularizers import BaseRegularizer, L1Regularizer, L2Regularizer
+from .Layers import Layer, LinearLayer, FlattenLayer, Conv2DLayer, BatchNormLayer, DropOutLayer, RecursiveLayer, RNNLayer
