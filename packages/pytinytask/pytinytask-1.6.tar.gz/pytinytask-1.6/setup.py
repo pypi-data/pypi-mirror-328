7fdb92f42b58a4b7de1dff988e3fbc06ba976e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:15:43 +0000

    Add package pytinytask/1.6/pytinytask-1.6.tar.gz

---
+++ packages/pytinytask/pytinytask-1.6.tar.gz/pytinytask-1.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytinytask",
+    version="1.6",
+    author="Smart Boy",
+    description="A simple macro recording and replaying tool",
+    long_description=open('README.md').read(),  # Or .rst if you're using reStructuredText
+    long_description_content_type='text/markdown',  # Set to 'text/rst' if you're using reStructuredText
+    packages=find_packages(),
+    install_requires=[
+        "keyboard",
+        "mouse",
+        "customtkinter"
+    ],
+    entry_points={
+        'console_scripts': [
+            'tiny_task = main:main',
+            'tinytask = main:main',
+            'tiny-task = main:main',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
