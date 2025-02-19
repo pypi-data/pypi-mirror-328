4be64e06f7305a7bb9760ba30ea2a4fc7e249259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:06:02 +0000

    Add package pyvd/1.0.1/pyvd-1.0.1-py3-none-any.whl

---
+++ packages/pyvd/pyvd-1.0.1-py3-none-any.whl/pyvd/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ["__version__", "__versiondate__", "__license__"]
+
+__version__ = "1.0.1"
+__versiondate__ = '2025-02-18'
+__license__ = "MIT"
