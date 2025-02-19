5ee07c8a796414e7ef36aa896a7c410f70253a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:06:56 +0000

    Add package lamindb/1.1.0/lamindb-1.1.0.tar.gz

---
+++ packages/lamindb/lamindb-1.1.0.tar.gz/lamindb-1.1.0/lamindb/core/storage/_pyarrow_dataset.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pyarrow.dataset
+from lamindb_setup.core.upath import LocalPathClasses
+
+if TYPE_CHECKING:
+    from pyarrow.dataset import Dataset as PyArrowDataset
+    from upath import UPath
+
+
+PYARROW_SUFFIXES = (".parquet", ".csv", ".json", ".orc", ".arrow", ".feather", ".ipc")
+
+
+def _is_pyarrow_dataset(paths: UPath | list[UPath]) -> bool:
+    # it is assumed here that the paths exist
+    # we don't check here that the filesystem is the same
+    # but this is a requirement for pyarrow.dataset.dataset
+    if isinstance(paths, list):
+        suffixes = {path.suffix for path in paths}
+    elif paths.is_file():
+        suffixes = {paths.suffix}
+    else:
+        suffixes = {path.suffix for path in paths.rglob("*") if path.suffix != ""}
+    return len(suffixes) == 1 and suffixes.pop() in PYARROW_SUFFIXES
+
+
+def _open_pyarrow_dataset(paths: UPath | list[UPath]) -> PyArrowDataset:
+    if isinstance(paths, list):
+        path0 = paths[0]
+        if isinstance(path0, LocalPathClasses):
+            paths_str, filesystem = [path.as_posix() for path in paths], None
+        else:
+            paths_str, filesystem = [path.path for path in paths], path0.fs
+    elif isinstance(paths, LocalPathClasses):
+        paths_str, filesystem = paths.as_posix(), None
+    else:
+        paths_str, filesystem = paths.path, paths.fs
+
+    return pyarrow.dataset.dataset(paths_str, filesystem=filesystem)
---
+++ packages/lamindb/lamindb-1.1.0.tar.gz/lamindb-1.1.0/sub/bionty/bionty/base/entities/_ethnicity.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from bionty.base._public_ontology import PublicOntology
+
+from ._shared_docstrings import _doc_params, doc_entites
+
+
+@_doc_params(doc_entities=doc_entites)
+class Ethnicity(PublicOntology):
+    """Ethnicity.
+
+    1. Human Ancestry Ontology
+    https://github.com/EBISPOT/hancestro
+
+    Args:
+        {doc_entities}
+    """
+
+    def __init__(
+        self,
+        organism: Literal["human"] | None = None,
+        source: Literal["hancestro"] | None = None,
+        version: Literal["3.0"] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            source=source,
+            version=version,
+            organism=organism,
+            include_id_prefixes={"hancestro": ["HANCESTRO"]},
+            **kwargs,
+        )
