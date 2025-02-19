b2a7f204d646ebf730c9c0e0e613edc8d2b987ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:36:33 +0000

    Add package mapclient/0.23.0/mapclient-0.23.0-py3-none-any.whl

---
+++ packages/mapclient/mapclient-0.23.0-py3-none-any.whl/mapclient/application.py
@@ -0,0 +1,367 @@
+#!/usr/bin/env python
+"""
+MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
+    Copyright (C) 2012  University of Auckland
+
+This file is part of MAP Client. (http://launchpad.net/mapclient)
+
+    MAP Client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MAP Client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
+"""
+import os
+import sys
+import ctypes
+import argparse
+
+import locale
+
+import logging
+import zipfile
+from logging import handlers
+
+from mapclient.core.exitcodes import HEADLESS_MODE_WITH_NO_WORKFLOW, INVALID_WORKFLOW_LOCATION_GIVEN
+from mapclient.core.utils import is_frozen, find_file
+from mapclient.settings.definitions import INTERNAL_WORKFLOWS_ZIP, INTERNAL_WORKFLOWS_AVAILABLE, INTERNAL_WORKFLOW_DIR, UNSET_FLAG, PREVIOUS_WORKFLOW, AUTOLOAD_PREVIOUS_WORKFLOW
+from mapclient.settings.info import DEFAULT_WORKFLOW_PROJECT_FILENAME, APPLICATION_ENVIRONMENT_CONFIG_DIR_VARIABLE
+
+os.environ['ETS_TOOLKIT'] = 'qt'
+# With PEP366 we need to conditionally import the settings module based on
+# whether we are executing the file directly of indirectly.  This is my
+# workaround.
+if __package__:
+    from .settings import info
+    from .settings.general import get_log_location, get_default_internal_workflow_dir
+else:
+    from mapclient.settings import info
+    from mapclient.settings.general import get_log_location, get_default_internal_workflow_dir
+
+logger = logging.getLogger('mapclient.application')
+
+
+def initialise_logger(log_path):
+    """
+    Initialise logger settings and information formatting
+    """
+
+    logging.basicConfig(format='%(asctime)s.%(msecs).03d - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
+                        datefmt='%d/%m/%Y - %H:%M:%S')
+    logging.addLevelName(29, 'PLUGIN')
+
+    rotating_fh = handlers.RotatingFileHandler(log_path, mode='a', maxBytes=5000000, backupCount=9)
+    rotating_fh.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s.%(msecs).03d - %(name)s - %(levelname)s - %(message)s',
+                                       datefmt='%d/%m/%Y - %H:%M:%S')
+    rotating_fh.setFormatter(file_formatter)
+    logging.getLogger().addHandler(rotating_fh)
+    rotating_fh.doRollover()
+
+
+def program_header():
+    """
+    Display program header
+    """
+    program_header_string = '   {0} (version {1})   '.format(info.APPLICATION_NAME, info.ABOUT['version'])
+    logger.info('-' * len(program_header_string))
+    logger.info(program_header_string)
+    logger.info('-' * len(program_header_string))
+
+
+# This method starts MAP Client
+def windows_main(workflow, execute_now):
+    """
+    Initialise common settings and check the operating environment before starting the application.
+    """
+    if sys.platform == 'win32':
+        my_app_id = 'MusculoSkeletal.MAPClient'  # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
+
+    # import the locale, and set the locale. This is used for
+    # locale-aware number to string formatting
+    locale.setlocale(locale.LC_ALL, '')
+
+    from PySide6 import QtWidgets
+    from mapclient.splashscreen import SplashScreen
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    splash = SplashScreen()
+    splash.show()
+    splash.showMessage("Loading settings ...", 5)
+    info.set_applications_settings(app)
+
+    log_path = get_log_location()
+    initialise_logger(log_path)
+    program_header()
+
+    logger.info('Setting toolbox settings for matplotlib and enthought to: qt')
+
+    splash.showMessage('Loading cmlibs.zinc ...', 10)
+    try:
+        import cmlibs.zinc.context
+        logger.info('Zinc is available.')
+    except ImportError:
+        logger.warning(' *** Zinc is not available ***')
+
+    splash.showMessage('Creating application ...', 20)
+    from mapclient.core.mainapplication import MainApplication
+    model = MainApplication()
+
+    splash.showMessage('Creating main window ...', 30)
+    from mapclient.view.mainwindow import MainWindow
+    window = MainWindow(model)
+
+    # Run Checks
+    if not window.check_application_setup():
+        window.setup_application()
+
+        splash.showMessage('Check application setup ...', 40)
+        if not window.check_application_setup():
+            window.show_options_dialog(current_tab=1)
+
+    splash.showMessage('Loading packages ...', 50)
+    window.load_packages()
+    splash.showMessage('Loading plugins ...', 60)
+    window.load_plugins()
+
+    splash.showMessage('Loading internal workflow ...', 70)
+    om = model.optionsManager()
+    _prepare_internal_workflows(om)
+    if om.getOption(AUTOLOAD_PREVIOUS_WORKFLOW):
+        workflow = _load_previous_workflow(workflow, om)
+
+    window.show()
+    wm = model.workflowManager()
+    if workflow and not wm.is_restricted(workflow):
+        splash.showMessage('Opening workflow ...', 80)
+        logger.info(f"Opening workflow: {workflow}")
+        window.open_workflow(workflow)
+    elif workflow:
+        logger.info(f"Not opening workflow '{workflow}', this workflow is already in use.")
+
+    window.start_metrics()
+    if execute_now:
+        splash.showMessage('Executing workflow ...', 90)
+        if wm.canExecute() == 0:
+            window.execute()
+        else:
+            logger.error(f'Could not execute workflow, reason: "{wm.execute_status_message()}"')
+
+    splash.showMessage('Ready ...', 100)
+    splash.finish(window)
+    return app.exec()
+
+
+def _get_default_internal_workflow(om):
+    internal_workflow_dir = om.getOption(INTERNAL_WORKFLOW_DIR)
+    default_workflow = os.path.join(internal_workflow_dir, "default_workflow.txt")
+    if os.path.isfile(default_workflow):
+        with open(default_workflow) as f:
+            lines = f.readlines()
+
+        return find_file(DEFAULT_WORKFLOW_PROJECT_FILENAME, os.path.join(internal_workflow_dir, lines[0].rstrip()))
+
+    return find_file(DEFAULT_WORKFLOW_PROJECT_FILENAME, internal_workflow_dir)
+
+
+def _load_previous_workflow(workflow, om):
+    previous_workflow_dir = om.getOption(PREVIOUS_WORKFLOW)
+    if previous_workflow_dir != UNSET_FLAG:
+        workflow_file = find_file(DEFAULT_WORKFLOW_PROJECT_FILENAME, previous_workflow_dir)
+    else:
+        if not om.getOption(INTERNAL_WORKFLOWS_AVAILABLE):
+            return
+
+        workflow_file = _get_default_internal_workflow(om)
+
+    # Set workflow to internal workflow if None is currently present.
+    if workflow is None and workflow_file is not None:
+        # Should definitely have a workflow now.
+        workflow = os.path.dirname(workflow_file)
+
+    return workflow
+
+
+def _prepare_internal_workflows(om):
+    # Determine if we have an internal workflow.
+    if is_frozen():
+        internal_workflows_zip = os.path.join(sys._MEIPASS, INTERNAL_WORKFLOWS_ZIP)
+    else:
+        file_dir = os.path.dirname(os.path.abspath(__file__))
+        internal_workflows_zip = os.path.realpath(os.path.join(file_dir, '..', INTERNAL_WORKFLOWS_ZIP))
+
+    if os.path.isfile(internal_workflows_zip):
+        # We have an internal workflow set the option as active.
+        om.setOption(INTERNAL_WORKFLOWS_AVAILABLE, True)
+
+        # Work out internal workflow directory and create if it doesn't exist.
+        internal_workflow_dir = om.getOption(INTERNAL_WORKFLOW_DIR)
+        if internal_workflow_dir == UNSET_FLAG or not os.path.isdir(internal_workflow_dir):
+            internal_workflow_dir = get_default_internal_workflow_dir()
+            if not os.path.isdir(internal_workflow_dir):
+                logger.info(f"Creating internal workflow(s) directory '{internal_workflow_dir}'")
+                os.mkdir(internal_workflow_dir)
+
+        om.setOption(INTERNAL_WORKFLOW_DIR, internal_workflow_dir)
+
+        # Test if a workflow is present.
+        workflow_file = find_file(DEFAULT_WORKFLOW_PROJECT_FILENAME, internal_workflow_dir)
+        if workflow_file is None:
+            # No workflow exists in the workflow directory so we will
+            # unzip the stored workflow(s) into this location.
+            logger.info("Decompressing internal workflow(s) ...")
+            archive = zipfile.ZipFile(internal_workflows_zip)
+            archive.extractall(f"{internal_workflow_dir}")
+
+    else:
+        om.setOption(INTERNAL_WORKFLOWS_AVAILABLE, False)
+
+
+class ConsumeOutput(object):
+    def __init__(self):
+        self.messages = list()
+
+    def write(self, message):
+        self.messages.append(message)
+
+
+def _prepare_sans_gui_app(app):
+    logging.basicConfig(level='INFO')
+
+    info.set_applications_settings(app)
+
+    old_stdout = sys.stdout
+    sys.stdout = ConsumeOutput()
+    #     sys.stdout = redirectstdout = ConsumeOutput()
+
+    log_path = get_log_location()
+    initialise_logger(log_path)
+    program_header()
+    sys.stdout = old_stdout
+
+    from mapclient.core.mainapplication import MainApplication
+    model = MainApplication()
+    model.readSettings()
+
+    return model
+
+
+def sans_gui_main(workflow):
+    locale.setlocale(locale.LC_ALL, '')
+
+    from PySide6 import QtWidgets
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    model = _prepare_sans_gui_app(app)
+
+    wm = model.workflowManager()
+    pm = model.pluginManager()
+    pam = model.package_manager()
+    om = model.optionsManager()
+
+    pam.load()
+    pm.load()
+
+    _prepare_internal_workflows(om)
+
+    class FacadeMainWindow:
+
+        def __init__(self, _model):
+            self._model = _model
+
+        def model(self):
+            return self._model
+
+    try:
+        wm.scene().setMainWindow(FacadeMainWindow(model))
+        wm.load(workflow)
+    except:
+        logger.error('Not a valid workflow location: "{0}"'.format(workflow))
+        sys.exit(INVALID_WORKFLOW_LOCATION_GIVEN)
+
+    wm.registerDoneExecutionForAll(wm.execute)
+
+    if wm.canExecute() == 0:
+        wm.execute()
+    else:
+        logger.error(f'Could not execute workflow, reason: "{wm.execute_status_message()}"')
+
+    # Possibly don't need to run app.exec_()
+    return app.quit()
+
+
+def _parse_prepare_user_specified_environment_args():
+    parser = argparse.ArgumentParser(prog=f"{info.APPLICATION_NAME}_use".lower(),
+                                     description="An application to create and setup a separate configuration location for running MAP Client.")
+    parser.add_argument("base_dir", help="Sets the base directory for the setup, must exist.")
+    parser.add_argument("-d", "--directory", action='append', help="Specify a plugin directory, can be used multiple times.")
+
+    return parser.parse_args()
+
+
+def user_specified_environment_main():
+    locale.setlocale(locale.LC_ALL, '')
+
+    from PySide6 import QtWidgets
+
+    app = QtWidgets.QApplication(sys.argv)
+    logging.basicConfig(level='INFO')
+
+    info.set_applications_settings(app)
+
+    args = _parse_prepare_user_specified_environment_args()
+    if not os.path.isdir(args.base_dir):
+        sys.exit(1)
+
+    config_dir = os.path.join(args.base_dir, ".config")
+    os.environ[APPLICATION_ENVIRONMENT_CONFIG_DIR_VARIABLE] = config_dir
+
+    if args.directory is not None:
+        model = _prepare_sans_gui_app(app)
+        model.readSettings()
+        pm = model.pluginManager()
+        directories = pm.directories()
+        for d in args.directory:
+            if os.path.isdir(d) and d not in directories:
+                directories.append(d)
+
+        pm.setDirectories(directories)
+        model.writeSettings()
+
+    print(f"Set environment variable '{APPLICATION_ENVIRONMENT_CONFIG_DIR_VARIABLE}' to '{config_dir}' to use application with these settings.")
+    if sys.platform == "win32":
+        print(f'set {APPLICATION_ENVIRONMENT_CONFIG_DIR_VARIABLE}="{config_dir}"')
+    else:
+        print(f'export {APPLICATION_ENVIRONMENT_CONFIG_DIR_VARIABLE}="{config_dir}"')
+
+
+def main():
+    parser = argparse.ArgumentParser(prog=info.APPLICATION_NAME)
+    parser.add_argument("-x", "--execute", action="store_true", help="execute a workflow")
+    parser.add_argument("-s", "--headless", action="store_true",
+                        help="operate in headless mode, without a gui.  Requires a location of a workflow to be set")
+    parser.add_argument("-w", "--workflow", help="location of workflow")
+    args = parser.parse_args(sys.argv[1:])
+
+    if args.headless and args.workflow is None:
+        parser.print_help()
+        sys.exit(HEADLESS_MODE_WITH_NO_WORKFLOW)
+
+    if args.headless and args.workflow:
+        sys.exit(sans_gui_main(args.workflow))
+    else:
+        sys.exit(windows_main(args.workflow, args.execute))
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/mapclient/mapclient-0.23.0-py3-none-any.whl/mapclient/mountpoints/workflowstep.py
@@ -0,0 +1,330 @@
+"""
+MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
+    Copyright (C) 2012  University of Auckland
+    
+This file is part of MAP Client. (http://launchpad.net/mapclient)
+
+    MAP Client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MAP Client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
+"""
+import os
+import sys
+
+from mapclient.core import pluginframework
+from mapclient.core.annotations import USES_ANNOTATIONS, PROVIDES_ANNOTATIONS, PORT_ANNOTATION, USES_ANNOTATION, PROVIDES_ANNOTATION, PROVIDES_LIST_OF_ANNOTATION, \
+    USES_LIST_OF_ANNOTATION
+
+
+class WorkflowStepPort(object):
+    """
+    Describes the location and properties of a port for a workflow step.
+    """
+    def __init__(self):
+        self.subj = {}
+        self.pred = {}
+        self.obj = {}
+
+    def addProperty(self, rdftriple):
+        if rdftriple[0] in self.subj:
+            self.subj[rdftriple[0]].append(rdftriple)
+        else:
+            self.subj[rdftriple[0]] = [rdftriple]
+
+        if rdftriple[1] in self.pred:
+            self.pred[rdftriple[1]].append(rdftriple)
+        else:
+            self.pred[rdftriple[1]] = [rdftriple]
+
+        if rdftriple[2] in self.obj:
+            self.obj[rdftriple[2]].append(rdftriple)
+        else:
+            self.obj[rdftriple[2]] = [rdftriple]
+
+    def getTriplesForObj(self, obj):
+        if obj in self.obj:
+            return self.obj[obj]
+
+        return []
+#        return [triple[2] for triple in self.obj[obj]]
+
+    def hasUses(self):
+        for uses_annotation in USES_ANNOTATIONS:
+            if uses_annotation in self.pred:
+                return True
+
+        return False
+
+    def hasProvides(self):
+        for provides_annotation in PROVIDES_ANNOTATIONS:
+            if provides_annotation in self.pred:
+                return True
+
+        return False
+
+    def getTriplesForPred(self, pred):
+        if pred in self.pred:
+            return self.pred[pred]
+
+        return []
+
+    def index(self):
+        index = -1
+        if 'http://physiomeproject.org/workflow/1.0/rdf-schema#index' in self.pred:
+            indexList = self.pred['http://physiomeproject.org/workflow/1.0/rdf-schema#index']
+            index = indexList[0][2]
+
+        return index
+
+    def _test_for_possible_connection(self, mineProvides, theirsUses):
+        for mine in mineProvides:
+            for theirs in theirsUses:
+                if mine[2] == theirs[2]:
+                    return True
+
+        return False
+
+    def canConnect(self, other):
+        if PORT_ANNOTATION in self.subj and PORT_ANNOTATION in other.subj:
+            myPorts = self.subj[PORT_ANNOTATION]
+            theirPorts = other.subj[PORT_ANNOTATION]
+
+            mineProvides = [triple for triple in myPorts if PROVIDES_ANNOTATION == triple[1]]
+            theirsUses = [triple for triple in theirPorts if USES_ANNOTATION == triple[1]]
+            if self._test_for_possible_connection(mineProvides, theirsUses):
+                return True
+
+            mineProvides = [triple for triple in myPorts if PROVIDES_LIST_OF_ANNOTATION == triple[1]]
+            theirsUses = [triple for triple in theirPorts if USES_LIST_OF_ANNOTATION == triple[1]]
+            if self._test_for_possible_connection(mineProvides, theirsUses):
+                return True
+
+        return False
+
+
+"""
+Plugins can inherit this mount point to add a workflow step.
+
+A plugin that registers this mount point must:
+  - Pass the name of the step into the base class on init.
+  - Implement a function 'configure(self)'
+  - Implement a function 'setIdentifier(self, identifier)'
+  - Implement a function 'getIdentifier(self)'
+  - Implement a function 'string serialize(self)'
+  - Implement a function 'deserialize(self, string)'
+
+
+A plugin that registers this mount point could have:
+  - An attribute _icon that is a QImage icon for a visual representation of the step
+  - An attribute _category that is a string representation of the step's category
+  
+"""
+
+
+def _workflow_step_init(self, name, location, parent=None):
+    self._parent = parent
+    self._name = name
+    self._location = location
+    self._main_window = None
+    self._category = 'General'
+    self._ports = []
+    self._icon = None
+    self._configured = False
+    self._configuredObserver = None
+    self._doneExecution = None
+    self._setCurrentWidget = None
+    self._identifierOccursCount = None
+
+
+def _workflow_step_setLocation(self, location):
+    self._location = location
+
+
+def _workflow_step_getLocation(self):
+    return self._location
+
+
+def _workflow_step_execute(self, dataIn=None):
+    self._doneExecution()
+
+
+def _workflow_step_getPortData(self, index):
+    return None
+
+
+def _workflow_step_setPortData(self, index, dataIn):
+    pass
+
+
+def _workflow_step_setMainWindow(self, main_window):
+    self._main_window = main_window
+
+
+def _workflow_step_get_source_uri(self):
+    if hasattr(self, '__module__'):
+        module = self.__module__
+        module_sep = module.split('.')
+        module_sep = module_sep[:2]
+        package = '.'.join(module_sep)
+        import importlib
+        p = importlib.import_module(package)
+        if hasattr(p, '__location__'):
+            return p.__location__
+
+    return None
+
+
+def _workflow_step_registerDoneExecution(self, observer):
+    self._doneExecution = observer
+
+
+def _workflow_step_registerOnExecuteEntry(self, observer, setCurrentUndoRedoStackObserver=None):
+    self._setCurrentWidget = observer
+    self._setCurrentUndoRedoStack = setCurrentUndoRedoStackObserver
+
+
+def _workflow_step_registerConfiguredObserver(self, observer):
+    self._configuredObserver = observer
+
+
+def _workflow_step_registerIdentifierOccursCount(self, observer):
+    self._identifierOccursCount = observer
+
+
+def _workflow_step_configure(self, location):
+    raise NotImplementedError
+
+
+def _workflow_step_getIdentifier(self):
+    raise NotImplementedError
+
+
+def _workflow_step_setIdentifier(self):
+    raise NotImplementedError
+
+
+def _workflow_step_serialize(self):
+    raise NotImplementedError
+
+
+def _workflow_step_deserialize(self, string):
+    raise NotImplementedError
+
+
+def _workflow_step_isConfigured(self):
+    return self._configured
+
+
+def _workflow_step_addPort(self, triple):
+    port = WorkflowStepPort()
+    if isinstance(triple, list):
+        for t in triple:
+            port.addProperty(t)
+    else:
+        port.addProperty(triple)
+    port.addProperty(('http://physiomeproject.org/workflow/1.0/rdf-schema#port', 'http://physiomeproject.org/workflow/1.0/rdf-schema#index', len(self._ports)))
+    self._ports.append(port)
+
+
+def _workflow_step_getName(self):
+    if hasattr(self, '_name'):
+        return self._name
+
+    return self.__class__.__name__
+
+
+def _workflow_step_get_icon(self):
+    return self._icon
+
+  
+def _workflow_step_get_category(self):
+    return self._category
+
+
+def _workflow_step_git_include(self):
+    return []
+
+
+def _workflow_step_create_git_ignore(self):
+    step_location = os.path.join(self._location, self.getIdentifier())
+    if os.path.exists(step_location):
+        step_gitignore = os.path.join(step_location, ".gitignore")
+        workflow_gitignore = os.path.join(self._location, ".gitignore")
+
+        if os.path.isfile(workflow_gitignore):
+            if os.path.isfile(step_gitignore):
+                os.remove(step_gitignore)
+            return
+
+        with open(step_gitignore, "w") as file:
+            file.write("# This file was automatically generated by the MAP-Client.\n")
+            file.write("# Any changes made here will be overwritten when saving the workflow.\n")
+            file.write("# You can override this file by adding a .gitignore in the workflow root directory.\n\n")
+            file.write("*\n")
+            for expression in self.gitInclude():
+                file.write("!" + expression + "\n")
+
+
+def _get_additional_config_files(self):
+    return []
+
+
+attr_dict = {}
+attr_dict['__init__'] = _workflow_step_init
+attr_dict['setLocation'] = _workflow_step_setLocation
+attr_dict['getLocation'] = _workflow_step_getLocation
+attr_dict['execute'] = _workflow_step_execute
+attr_dict['getPortData'] = _workflow_step_getPortData
+attr_dict['setPortData'] = _workflow_step_setPortData
+attr_dict['setMainWindow'] = _workflow_step_setMainWindow
+attr_dict['registerDoneExecution'] = _workflow_step_registerDoneExecution
+attr_dict['registerOnExecuteEntry'] = _workflow_step_registerOnExecuteEntry
+attr_dict['configure'] = _workflow_step_configure
+attr_dict['isConfigured'] = _workflow_step_isConfigured
+attr_dict['registerIdentifierOccursCount'] = _workflow_step_registerIdentifierOccursCount
+attr_dict['registerConfiguredObserver'] = _workflow_step_registerConfiguredObserver
+attr_dict['addPort'] = _workflow_step_addPort
+attr_dict['getName'] = _workflow_step_getName
+attr_dict['deserialize'] = _workflow_step_deserialize
+attr_dict['serialize'] = _workflow_step_serialize
+attr_dict['getSourceURI'] = _workflow_step_get_source_uri
+attr_dict['getIcon'] = _workflow_step_get_icon
+attr_dict['getCategory'] = _workflow_step_get_category
+attr_dict['gitInclude'] = _workflow_step_git_include
+attr_dict['createGitIgnore'] = _workflow_step_create_git_ignore
+attr_dict['getAdditionalConfigFiles'] = _get_additional_config_files
+
+WorkflowStepMountPoint = pluginframework.MetaPluginMountPoint('WorkflowStepMountPoint', (object,), attr_dict)
+
+
+def workflowStepFactory(step_name, location):
+    for step in WorkflowStepMountPoint.getPlugins(location):
+        if step_name == step.getName():
+            return step
+
+    raise ValueError('Failed to find/create a step named: ' + step_name)
+
+
+def removeWorkflowStep(step_module):
+    """
+    takes a module name (as a string) and removes all references
+     - from sys.modules
+     - from WorkflowStepMountPoint class
+    """
+    for key in list(sys.modules.keys()):
+        if step_module in key:
+            del sys.modules[key]
+
+    for cls in WorkflowStepMountPoint.plugins[:]:
+        if cls and step_module in cls.__module__:
+            index = WorkflowStepMountPoint.plugins.index(cls)
+            WorkflowStepMountPoint.plugins.pop(index)
