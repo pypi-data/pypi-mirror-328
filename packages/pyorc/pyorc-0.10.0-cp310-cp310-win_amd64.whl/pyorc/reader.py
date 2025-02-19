ee723abc165758ba4d19a43caad0060d7723b5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:00 +0000

    Add package pyorc/0.10.0/pyorc-0.10.0-cp310-cp310-win_amd64.whl

---
+++ packages/pyorc/pyorc-0.10.0-cp310-cp310-win_amd64.whl/pyorc/reader.py
@@ -0,0 +1,123 @@
+from collections import defaultdict
+from typing import Any, BinaryIO, Dict, Iterator, List, Optional, Type, Union
+
+from pyorc._pyorc import reader, stripe
+
+from .converters import DEFAULT_CONVERTERS, ORCConverter
+from .enums import CompressionKind, StructRepr, TypeKind, WriterVersion
+from .predicates import Predicate
+
+try:
+    import zoneinfo
+except ImportError:
+    from backports import zoneinfo
+
+
+class Column:
+    def __init__(self, stream: Union["Reader", "Stripe"], index: int):
+        self.index = index
+        self.stream = stream
+        self._stats = self.stream._statistics(self.index)
+
+    @property
+    def statistics(self) -> Dict[str, Any]:
+        result = {}
+        result_list = defaultdict(list)
+        for stat in self._stats:
+            for key, val in stat.items():
+                result_list[key].append(val)
+        for key, values in result_list.items():
+            if key in (
+                "number_of_values",
+                "sum",
+                "false_count",
+                "true_count",
+                "total_length",
+            ):
+                result[key] = sum(values)
+            elif key in ("minimum", "lower_bound"):
+                result[key] = min(values)
+            elif key in ("maximum", "upper_bound"):
+                result[key] = max(values)
+            elif key == "has_null":
+                result[key] = any(values)
+        result["kind"] = TypeKind(result_list["kind"][0])
+        return result
+
+
+class Stripe(stripe):
+    def __getitem__(self, col_idx: int) -> "Column":
+        return Column(self, col_idx)
+
+
+class Reader(reader):
+    def __init__(
+        self,
+        fileo: BinaryIO,
+        batch_size: int = 1024,
+        column_indices: Optional[List[int]] = None,
+        column_names: Optional[List[str]] = None,
+        timezone: zoneinfo.ZoneInfo = zoneinfo.ZoneInfo("UTC"),
+        struct_repr: StructRepr = StructRepr.TUPLE,
+        converters: Optional[Dict[TypeKind, Type[ORCConverter]]] = None,
+        predicate: Optional[Predicate] = None,
+        null_value: Any = None,
+    ) -> None:
+        if column_indices is None:
+            column_indices = []
+        if column_names is None:
+            column_names = []
+        struct_repr = StructRepr(struct_repr)
+        conv = None
+        if converters:
+            conv = DEFAULT_CONVERTERS.copy()
+            conv.update(converters)
+        else:
+            conv = converters
+        super().__init__(
+            fileo,
+            batch_size,
+            column_indices,
+            column_names,
+            timezone,
+            struct_repr,
+            conv,
+            predicate,
+            null_value,
+        )
+
+    def __getitem__(self, col_idx: int) -> Column:
+        return Column(self, col_idx)
+
+    def read_stripe(self, stripe_idx: int) -> Stripe:
+        return Stripe(self, stripe_idx)
+
+    def iter_stripes(self) -> Iterator[Stripe]:
+        for num in range(self.num_of_stripes):
+            yield self.read_stripe(num)
+
+    @property
+    def compression(self) -> CompressionKind:
+        return CompressionKind(super().compression)
+
+    @property
+    def writer_id(self) -> str:
+        wid = super().writer_id
+        if wid == 0:
+            return "ORC_JAVA_WRITER"
+        elif wid == 1:
+            return "ORC_CPP_WRITER"
+        elif wid == 2:
+            return "PRESTO_WRITER"
+        elif wid == 3:
+            return "SCRITCHLEY_GO"
+        elif wid == 4:
+            return "TRINO_WRITER"
+        elif wid == 5:
+            return "CUDF_WRITER"
+        else:
+            return "UNKNOWN_WRITER"
+
+    @property
+    def writer_version(self) -> WriterVersion:
+        return WriterVersion(super().writer_version)
---
+++ packages/pyorc/pyorc-0.10.0-cp310-cp310-win_amd64.whl/pyorc/writer.py
@@ -0,0 +1,105 @@
+import copy
+from typing import Any, BinaryIO, Dict, List, Optional, Type, Union
+
+from pyorc._pyorc import writer
+
+from .converters import DEFAULT_CONVERTERS, ORCConverter
+from .enums import CompressionKind, CompressionStrategy, StructRepr, TypeKind
+from .typedescription import TypeDescription
+
+try:
+    import zoneinfo
+except ImportError:
+    from backports import zoneinfo
+
+
+class Writer(writer):
+    def __init__(
+        self,
+        fileo: BinaryIO,
+        schema: Union[str, TypeDescription],
+        batch_size: int = 1024,
+        stripe_size: int = 67108864,
+        row_index_stride: int = 10000,
+        compression: CompressionKind = CompressionKind.ZLIB,
+        compression_strategy: CompressionStrategy = CompressionStrategy.SPEED,
+        compression_block_size: int = 65536,
+        bloom_filter_columns: Optional[List[Union[str, int]]] = None,
+        bloom_filter_fpp: float = 0.05,
+        timezone: zoneinfo.ZoneInfo = zoneinfo.ZoneInfo("UTC"),
+        struct_repr: StructRepr = StructRepr.TUPLE,
+        converters: Optional[Dict[TypeKind, Type[ORCConverter]]] = None,
+        padding_tolerance: float = 0.0,
+        dict_key_size_threshold: float = 0.0,
+        null_value: Any = None,
+        memory_block_size = 65536,
+    ) -> None:
+        if isinstance(schema, str):
+            schema = TypeDescription.from_string(schema)
+        elif not isinstance(schema, TypeDescription):
+            raise TypeError("Invalid `schema` type, must be string or TypeDescription")
+        if 0.0 >= bloom_filter_fpp or bloom_filter_fpp >= 1.0:
+            raise ValueError("False positive probability should be > 0.0 & < 1.0")
+        self.__schema = schema
+        self.__user_metadata: Dict[str, bytes] = {}
+        comp = CompressionKind(compression)
+        comp_strat = CompressionStrategy(compression_strategy)
+        bf_set = set()
+        if bloom_filter_columns:
+            if any(not isinstance(item, (int, str)) for item in bloom_filter_columns):
+                raise ValueError(
+                    "All items in `bloom_filter_columns` mut be string or int"
+                )
+            for item in bloom_filter_columns:
+                if isinstance(item, int):
+                    bf_set.add(item)
+                elif isinstance(item, str):
+                    bf_set.add(self.__schema.find_column_id(item))
+        conv = None
+        if converters:
+            conv = DEFAULT_CONVERTERS.copy()
+            conv.update(converters)
+        else:
+            conv = converters
+        super().__init__(
+            fileo,
+            self.__schema,
+            batch_size,
+            stripe_size,
+            row_index_stride,
+            comp,
+            comp_strat,
+            compression_block_size,
+            bf_set,
+            bloom_filter_fpp,
+            timezone,
+            struct_repr,
+            conv,
+            padding_tolerance,
+            dict_key_size_threshold,
+            null_value,
+            memory_block_size,
+        )
+
+    def __enter__(self) -> "Writer":
+        return self
+
+    def __exit__(self, *exc: Any) -> None:
+        self.close()
+
+    def close(self) -> None:
+        for key, val in self.__user_metadata.items():
+            super()._add_user_metadata(key, val)
+        super().close()
+
+    @property
+    def schema(self) -> TypeDescription:
+        return copy.deepcopy(self.__schema)
+
+    def set_user_metadata(self, **kwargs: bytes) -> None:
+        for key, val in kwargs.items():
+            if not isinstance(val, bytes):
+                raise TypeError(
+                    "All values must be bytes, key '{0}' is {1}".format(key, type(val))
+                )
+            self.__user_metadata[key] = val
