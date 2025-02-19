b4af590af075a42024e4a1499d847972f3f3c3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:53 +0000

    Add package plain.tailwind/0.5.0/plain_tailwind-0.5.0.tar.gz

---
+++ packages/plain.tailwind/plain_tailwind-0.5.0.tar.gz/plain_tailwind-0.5.0/.pytype/pyi/tailwind/cli.pyi
@@ -0,0 +1,14 @@
+# (generated with --quick)
+
+import click
+import os
+import tailwind.core
+from typing import Any
+
+APP_PATH: Any
+Tailwind: type[tailwind.core.Tailwind]
+cli: Any
+compile: Any
+init: Any
+packages: Any
+update: Any
---
+++ packages/plain.tailwind/plain_tailwind-0.5.0.tar.gz/plain_tailwind-0.5.0/.pytype/pyi/tailwind/templates.pyi
@@ -0,0 +1,9 @@
+# (generated with --quick)
+
+from typing import Any
+
+APP_ASSETS_DIR: Any
+InclusionTagExtension: Any
+TailwindCSSExtension: Any
+register_template_extension: Any
+settings: Any
