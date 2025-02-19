5a85c92dbdccb8e9fd37f5ca4aa37437cafe0bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:56 +0000

    Add package plain.tunnel/0.3.0/plain_tunnel-0.3.0.tar.gz

---
+++ packages/plain.tunnel/plain_tunnel-0.3.0.tar.gz/plain_tunnel-0.3.0/.pytype/pyi/tunnel/cli.pyi
@@ -0,0 +1,11 @@
+# (generated with --quick)
+
+import click
+import getpass
+import random
+import string
+import tunnel.client
+from typing import Any
+
+TunnelClient: type[tunnel.client.TunnelClient]
+cli: Any
