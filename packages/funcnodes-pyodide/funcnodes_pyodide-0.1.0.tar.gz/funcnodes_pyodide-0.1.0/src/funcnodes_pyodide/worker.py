f2881911530c79634bbb9b4242b24aa2659585d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:17:51 +0000

    Add package funcnodes-pyodide/0.1.0/funcnodes_pyodide-0.1.0.tar.gz

---
+++ packages/funcnodes-pyodide/funcnodes_pyodide-0.1.0.tar.gz/funcnodes_pyodide-0.1.0/src/funcnodes_pyodide/worker.py
@@ -0,0 +1,22 @@
+from funcnodes_worker import RemoteWorker
+import json
+
+
+class PyodideWorker(RemoteWorker):
+    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._receiver = None
+
+    async def receivejs(self, msg):
+        await self.receive_message(json.loads(msg))
+
+    async def sendmessage(self, msg: str, **kwargs):
+        if self._receiver:
+            self._receiver.receivepy(msg)
+
+    def set_receiver(self, res):
+        self._receiver = res
+
+
---
+++ packages/funcnodes-pyodide/funcnodes_pyodide-0.1.0.tar.gz/funcnodes_pyodide-0.1.0/tests/test_import.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.imports
+def test_import():
+    from funcnodes_pyodide import PyodideWorker, new_worker
