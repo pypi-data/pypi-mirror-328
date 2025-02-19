2eaa41dc3b16fc0743389dd776ceab9e5bb0c38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:22 +0000

    Add package xml2arr/0.1.3/xml2arr-0.1.3.tar.gz

---
+++ packages/xml2arr/xml2arr-0.1.3.tar.gz/xml2arr-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.3'
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+  name="xml2arr", 
+  version=VERSION,
+  author="Blazej Turczynowicz",
+  author_email="blazejturczynowicz@gmail.com",
+  description="Extract bounding boxes from XML format to list",
+  packages=find_packages(),
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  keywords=['bounding box', 'xml', 'xml2arr'],
+)
