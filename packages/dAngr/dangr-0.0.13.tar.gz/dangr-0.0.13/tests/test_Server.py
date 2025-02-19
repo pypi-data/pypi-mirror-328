9f41142a5ebbae92b38e95fe465389a117cb512a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:05:03 +0000

    Add package dAngr/0.0.13/dangr-0.0.13.tar.gz

---
+++ packages/dAngr/dangr-0.0.13.tar.gz/dangr-0.0.13/tests/test_Server.py
@@ -0,0 +1,67 @@
+
+import asyncio
+from unittest.mock import Mock, MagicMock, patch
+from prompt_toolkit import PromptSession
+import pytest
+
+from dAngr.cli.command_line_debugger import DEBUGGER_COMMANDS, CommandLineDebugger
+from dAngr.cli.cli_connection import CliConnection
+from dAngr.cli.server import Server
+
+
+@pytest.fixture
+def server():
+    server = Server()
+    server.start_server = Mock()
+    # server.loop = Mock()
+    return server
+
+
+@patch('asyncio.run',new_callable=Mock)
+def test_start_server(asyncio_run,server):
+
+        def run_coroutine_sync(coro):
+            loop = asyncio.get_event_loop()
+            return loop.run_until_complete(coro)
+
+        asyncio.run(server.start_server())
+
+
+        server.start_server()
+        asyncio_run.assert_called_once()
+
+
+@patch.object(CliConnection,'send_info')
+@patch.object(CommandLineDebugger,'handle', side_effect=[False])
+@patch.object(PromptSession ,'prompt', side_effect=["help"])
+def test_loop(prompt, handle, send_info, server):
+    
+    server.loop()
+    prompt.assert_called_once()
+    send_info.assert_called_once_with("Welcome to dAngr, the symbolic debugger. Type help or ? to list commands.")
+    handle.assert_called_once_with('help', False)
+
+
+def test_check_shortnames(server):
+    conn = CliConnection()
+    dbg = CommandLineDebugger(conn)
+
+            # get duplicate short commands and print short and fullname
+    for cmd in DEBUGGER_COMMANDS.values():
+        
+        if cmd.name in ["start","continue"]:
+            break
+        assert not any([o.short_name and o.short_name == cmd.short_name for o in DEBUGGER_COMMANDS.values() if o != cmd]), f"Duplicate short command {cmd.short_name}"
+
+
+def test_check_cmdnames(server):
+    conn = CliConnection()
+    dbg = CommandLineDebugger(conn)
+
+            # get duplicate short commands and print short and fullname
+    for cmd in DEBUGGER_COMMANDS.values():
+        
+        if cmd.name in ["start","continue"]:
+            break
+        assert not any([o.name and o.name == cmd.name for o in DEBUGGER_COMMANDS.values() if o != cmd]), f"Duplicate short command {cmd.name}"
+
---
+++ packages/dAngr/dangr-0.0.13.tar.gz/dangr-0.0.13/tests/test_debug_info.py
@@ -0,0 +1,87 @@
+from dAngr.cli.command_line_debugger import CommandLineDebugger
+from dAngr.cli.cli_connection import CliConnection
+
+
+import pytest
+
+
+import os
+from unittest.mock import Mock
+
+
+class TestDebugInfoCommands:
+    old_dir = os.getcwd()
+
+    def setup_method(self):
+        os.chdir(os.path.dirname(__file__))
+
+    def teardown_method(self):
+        os.chdir(self.old_dir)
+
+
+    @pytest.fixture
+    def conn(self):
+        c = CliConnection()
+        c.send_result = Mock()
+        c.send_info = Mock()
+        c.send_error = Mock()
+        return c
+
+    @pytest.fixture
+    def dbg(self,conn):
+        dbg = CommandLineDebugger(conn)
+        assert dbg.handle("load example")
+        assert dbg.handle("add_breakpoint 0x400566")
+        assert dbg.handle("run")
+        return dbg
+
+    # 
+    # def test_get_cfg(self, dbg, conn):
+    #     assert dbg.handle("get_cfg")
+    #     assert r == True
+    #     assert "{\'graph\': \'digraph " in str(conn.send_info.call_args[0][0])
+    
+    
+    def test_get_basicblocks(self, dbg, conn):
+        assert dbg.handle("get_basicblocks")
+        assert "Address: 0x40059b" in str(conn.send_result.call_args[0][0])
+
+    
+    def test_get_current_block(self, dbg, conn):
+        assert dbg.handle("get_current_block")
+        assert "Address: 0x400566:" in str(conn.send_result.call_args[0][0])
+    
+    
+    def test_list_active_paths(self, dbg, conn):
+        assert dbg.handle("list_states 'active'")
+        assert "[<SimState @ 0x400566>]" == str(conn.send_result.call_args[0][0])    
+    
+    def test_list_active_paths2(self, dbg, conn):
+        assert dbg.handle("list_states")
+        assert "[<SimState @ 0x400566>]" == str(conn.send_result.call_args[0][0])    
+    
+    def test_list_deadended_paths(self, dbg, conn):
+        assert dbg.handle("list_states 'deadended'")
+        assert "[]" == str(conn.send_result.call_args[0][0])    
+
+    
+    def test_list_binary_symbols(self, dbg, conn):
+        assert dbg.handle("list_binary_symbols")
+        assert "Binary Symbols:" in str(conn.send_result.call_args[0][0])
+
+    
+    def test_list_constraints(self, dbg, conn):
+        assert dbg.handle("list_constraints")
+        assert "Constraints:" in str(conn.send_result.call_args[0][0])
+    
+    
+    def test_list_path_history(self, dbg, conn):
+        assert dbg.handle("list_path_history")
+        assert "Path History:" in str(conn.send_result.call_args[0][0])
+    
+    
+    def test_list_breakpoints(self, dbg, conn):
+        assert dbg.handle("list_breakpoints")
+        assert "Breakpoint(s): [0] Address Filter: 0x400566" == str(conn.send_result.call_args[0][0])
+    
+    
