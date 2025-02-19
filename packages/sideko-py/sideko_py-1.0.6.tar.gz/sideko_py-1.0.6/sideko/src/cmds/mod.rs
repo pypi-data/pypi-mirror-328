1d08c820729ce813af8982303bd74b52abe2a869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 11:06:04 +0000

    Add package sideko-py/1.0.6/sideko_py-1.0.6.tar.gz

---
+++ packages/sideko-py/sideko_py-1.0.6.tar.gz/sideko_py-1.0.6/sideko/src/cmds/mod.rs
@@ -0,0 +1,24 @@
+mod login;
+pub(crate) use login::LoginCommand;
+
+mod logout;
+pub(crate) use logout::LogoutCommand;
+
+mod api;
+pub(crate) use api::ApiSubcommand;
+
+mod sdk;
+pub(crate) use sdk::SdkSubcommand;
+
+mod doc;
+pub(crate) use doc::DocSubcommand;
+
+mod config;
+pub(crate) use config::ConfigSubcommand;
+
+#[derive(clap::ValueEnum, serde::Serialize, Default, Debug, Clone)]
+pub enum DisplayOutput {
+    #[default]
+    Raw,
+    Pretty,
+}
