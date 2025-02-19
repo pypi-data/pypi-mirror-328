395a8bdd72d64c3eba595667729e17f4932d796a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:25:49 +0000

    Add package osi-dump/0.1.3.3.4.2/osi_dump-0.1.3.3.4.2.tar.gz

---
+++ packages/osi-dump/osi_dump-0.1.3.3.4.2.tar.gz/osi_dump-0.1.3.3.4.2/src/osi_dump/model/role_assignment.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, ValidationError
+
+
+class RoleAssignment(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    user_id: Optional[str]
+
+    user_name: Optional[str]
+
+    role_id: Optional[str]
+
+    role_name: Optional[str]
+
+    options: Optional[dict]
+
+    password_expires_at: Optional[str]
+
+    scope: Optional[dict]
