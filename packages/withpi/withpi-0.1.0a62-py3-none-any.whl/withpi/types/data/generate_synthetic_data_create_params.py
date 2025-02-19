0595b7a461ada974e5249379407caf5d2cc526dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:37 +0000

    Add package withpi/0.1.0a62/withpi-0.1.0a62-py3-none-any.whl

---
+++ packages/withpi/withpi-0.1.0a62-py3-none-any.whl/withpi/types/data/generate_synthetic_data_create_params.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Literal, Required, TypedDict
+
+from ..shared_params.example import Example
+
+__all__ = ["GenerateSyntheticDataCreateParams"]
+
+
+class GenerateSyntheticDataCreateParams(TypedDict, total=False):
+    application_description: Required[str]
+    """The application description for which the synthetic data would be applicable."""
+
+    num_examples_to_generate: Required[int]
+    """The number of new LLM examples to generate"""
+
+    seeds: Required[Iterable[Example]]
+    """The list of LLM examples (inputs + outputs) to be used as seeds"""
+
+    batch_size: int
+    """Number of examples to generate in one LLM call.
+
+    Must be <=10. Generally it could be same as `num_shots`.
+    """
+
+    exploration_mode: Literal["CONSERVATIVE", "BALANCED", "CREATIVE", "ADVENTUROUS"]
+    """The exloration mode for examples generation. Defaults to `BALANCED`"""
+
+    num_shots: int
+    """Number of examples to be included in the prompt for generation.
+
+    Generally it could be same as `batch_size`.
+    """
