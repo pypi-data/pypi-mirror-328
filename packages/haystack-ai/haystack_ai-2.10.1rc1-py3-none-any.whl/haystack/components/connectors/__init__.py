4bc836cd83f95c42b6ce00260e2b1a33baa6f020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:18:16 +0000

    Add package haystack-ai/2.10.1rc1/haystack_ai-2.10.1rc1-py3-none-any.whl

---
+++ packages/haystack-ai/haystack_ai-2.10.1rc1-py3-none-any.whl/haystack/components/connectors/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from haystack.components.connectors.openapi import OpenAPIConnector
+from haystack.components.connectors.openapi_service import OpenAPIServiceConnector
+
+__all__ = ["OpenAPIServiceConnector", "OpenAPIConnector"]
