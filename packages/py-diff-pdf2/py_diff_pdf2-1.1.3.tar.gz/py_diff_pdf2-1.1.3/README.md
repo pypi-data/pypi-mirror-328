3ffe09433a371301efeb818b1b796d1bb7b139ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:26:00 +0000

    Add package py-diff-pdf2/1.1.3/py_diff_pdf2-1.1.3.tar.gz

---
+++ packages/py-diff-pdf2/py_diff_pdf2-1.1.3.tar.gz/py_diff_pdf2-1.1.3/README.md
@@ -0,0 +1,28 @@
+# Example Package
+build:
+```shell
+python  -m build 
+```
+upload
+```shell
+twine upload dist/* 
+```
+
+安装：
+```shell
+pip install py_diff_pdf2
+```
+
+使用：
+
+```python
+from py_diff_pdf2 import compare_pdfs
+
+result = compare_pdfs("file1.pdf","file2.pdf", output_path="diff.pdf")
+
+ # result = {
+ #        "success": success,
+ #        "message": '',
+ #        "output_path": output_path
+ # }
+```
---
+++ packages/py-diff-pdf2/py_diff_pdf2-1.1.3.tar.gz/py_diff_pdf2-1.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_diff_pdf2",
+    version="1.1.3",
+    author="lwinhong",
+    url='https://github.com/lwinhong/py-diff-pdf',
+    author_email="",
+    description="pdf文档比对",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(
+        where="src",
+        include=["py_diff_pdf2", "py_diff_pdf2.libs.*", "py_diff_pdf2.*"],
+        exclude=["py_diff_pdf2.*.pdf"]),
+    python_requires=">=3.6",
+    include_package_data=True
+)
