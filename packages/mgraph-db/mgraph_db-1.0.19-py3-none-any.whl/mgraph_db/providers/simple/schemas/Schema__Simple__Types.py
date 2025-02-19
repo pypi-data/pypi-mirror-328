86ea1f324ae136c5375fce4a164596b991b967dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:34 +0000

    Add package mgraph-db/1.0.19/mgraph_db-1.0.19-py3-none-any.whl

---
+++ packages/mgraph-db/mgraph_db-1.0.19-py3-none-any.whl/mgraph_db/providers/simple/schemas/Schema__Simple__Types.py
@@ -0,0 +1,15 @@
+from typing                                                        import Type
+from mgraph_db.mgraph.schemas.Schema__MGraph__Edge                 import Schema__MGraph__Edge
+from mgraph_db.mgraph.schemas.Schema__MGraph__Edge__Config         import Schema__MGraph__Edge__Config
+from mgraph_db.mgraph.schemas.Schema__MGraph__Graph__Data          import Schema__MGraph__Graph__Data
+from mgraph_db.mgraph.schemas.Schema__MGraph__Types                import Schema__MGraph__Types
+from mgraph_db.providers.simple.schemas.Schema__Simple__Node       import Schema__Simple__Node
+from mgraph_db.providers.simple.schemas.Schema__Simple__Node__Data import Schema__Simple__Node__Data
+
+
+class Schema__Simple__Types(Schema__MGraph__Types):
+    node_type        : Type[Schema__Simple__Node        ]           # only change from Schema__MGraph__Types
+    node_data_type   : Type[Schema__Simple__Node__Data  ]
+    edge_type        : Type[Schema__MGraph__Edge        ]
+    edge_config_type : Type[Schema__MGraph__Edge__Config]
+    graph_data_type  : Type[Schema__MGraph__Graph__Data ]
