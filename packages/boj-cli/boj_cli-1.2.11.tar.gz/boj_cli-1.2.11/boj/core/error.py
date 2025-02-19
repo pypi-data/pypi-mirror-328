c9cb087bae651ff5f036c0b550aaebb21178a171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 11:06:08 +0000

    Add package boj-cli/1.2.11/boj_cli-1.2.11.tar.gz

---
+++ packages/boj-cli/boj_cli-1.2.11.tar.gz/boj_cli-1.2.11/boj/core/error.py
@@ -0,0 +1,58 @@
+class BojError(BaseException):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class FatalError(BojError):
+    def __init__(self, msg):
+        super().__init__("fatal: " + msg)
+
+
+class AuthenticationError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class DeprecatedError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class HttpError(BojError):
+    def __init__(self, url):
+        super().__init__(f"Error while calling remote service: {url}")
+
+
+class ParsingHtmlError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class IllegalStatementError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class ResourceNotFoundError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class ParsingConfigError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class FileIOError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class RunCodeError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class WebSocketError(BojError):
+    def __init__(self, msg):
+        super().__init__(msg)
