55fcd3c38488d595d86d0b7294884e628b704379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:17:01 +0000

    Add package rimetool/0.1.6.2/rimetool-0.1.6.2-py3-none-any.whl

---
+++ packages/rimetool/rimetool-0.1.6.2-py3-none-any.whl/rimetool/main.py
@@ -0,0 +1,44 @@
+import os
+from rimetool.utils import vcf
+from rimetool.utils import singleword
+from rimetool.utils import singlechinese
+from rimetool.utils import tosougou
+import argparse
+
+# import utils.singleword as singleword
+
+def get_args_parser(add_help=True):
+
+	parser = argparse.ArgumentParser(description='rime输入法相关工具', add_help=add_help)
+	parser.add_argument('--input-path', '-i', required=True, type=str, help='需要处理的文件路径')
+	parser.add_argument('--output-path', '-o',default='./rimetool_output', type=str, help='输出文件路径')
+	parser.add_argument('--tool', '-t', required=True,choices=['vcf','singleword','singlechinese','tosougou','hello'],type=str, help='选择工具')
+
+	
+	return parser
+
+def main():
+	parser = get_args_parser()
+	args = parser.parse_args()
+	
+	# if not os.path.exists('./rimetool_cache'):
+		# os.makedirs('./rimetool_cache')
+	# if not os.path.exists('./rimetool_output'):
+		# os.makedirs('./rimetool_output')
+	os.makedirs(args.output_path, exist_ok=True)
+	if args.tool == 'vcf':
+		vcf.main(args.input_path, args.output_path)
+	elif args.tool == 'singleword':
+		singleword.main(args.input_path, args.output_path)
+	elif args.tool == 'singlechinese':
+		singlechinese.main(args.input_path, args.output_path)
+	elif args.tool == 'tosougou':
+		tosougou.main(args.input_path, args.output_path)
+	else:
+		print('这里有问题')
+	
+	
+
+
+if __name__ == "__main__":
+	main()
