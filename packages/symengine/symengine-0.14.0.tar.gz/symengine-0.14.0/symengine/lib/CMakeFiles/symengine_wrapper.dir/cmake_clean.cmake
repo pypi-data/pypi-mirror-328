5a8d2e90b92aab3b6e510c0aa812c38e9f06d9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 04:36:34 +0000

    Add package symengine/0.14.0/symengine-0.14.0.tar.gz

---
+++ packages/symengine/symengine-0.14.0.tar.gz/symengine-0.14.0/symengine/lib/CMakeFiles/symengine_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/symengine_wrapper.dir/pywrapper.cpp.o"
+  "CMakeFiles/symengine_wrapper.dir/pywrapper.cpp.o.d"
+  "CMakeFiles/symengine_wrapper.dir/symengine_wrapper.cpp.o"
+  "CMakeFiles/symengine_wrapper.dir/symengine_wrapper.cpp.o.d"
+  "symengine_wrapper.cpp"
+  "symengine_wrapper.cpython-310-x86_64-linux-gnu.pdb"
+  "symengine_wrapper.cpython-310-x86_64-linux-gnu.so"
+  "symengine_wrapper.pxd"
+  "symengine_wrapper.pyx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/symengine_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
