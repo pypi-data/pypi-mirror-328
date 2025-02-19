715b7ab68c0f0e3127ac30cd80932620396cd37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:07:31 +0000

    Add package dataprep-ml/25.2.3.0/dataprep_ml-25.2.3.0.tar.gz

---
+++ packages/dataprep-ml/dataprep_ml-25.2.3.0.tar.gz/dataprep_ml-25.2.3.0/dataprep_ml/__init__.py
@@ -0,0 +1,7 @@
+from dataprep_ml.base import StatisticalAnalysis, DataAnalysis
+
+__version__ = '25.2.3.0'
+__name__ = "dataprep_ml"
+
+
+__all__ = ['__version__', '__name__', 'StatisticalAnalysis', 'DataAnalysis']
