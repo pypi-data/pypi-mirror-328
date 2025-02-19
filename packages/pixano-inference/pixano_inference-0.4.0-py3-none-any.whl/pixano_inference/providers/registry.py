1102cd98e3ae6012969bf1fc1450a47b2d06b167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:05:54 +0000

    Add package pixano-inference/0.4.0/pixano_inference-0.4.0-py3-none-any.whl

---
+++ packages/pixano-inference/pixano_inference-0.4.0-py3-none-any.whl/pixano_inference/providers/registry.py
@@ -0,0 +1,64 @@
+# =================================
+# Copyright: CEA-LIST/DIASI/SIALV
+# Author : pixano@cea.fr
+# License: CECILL-C
+# =================================
+
+"""Registry for providers."""
+
+from pixano_inference.providers.base import BaseProvider
+
+
+PROVIDERS_REGISTRY: dict[str, type[BaseProvider]] = {}
+
+
+def register_provider(provider: str):
+    """Return a decorator to register a provider in the registry.
+
+    Args:
+        provider: Name of the provider.
+
+    Returns:
+        The decorator.
+    """
+
+    def decorator(cls):
+        """Register the provider in the registry.
+
+        Args:
+            cls: Class to register.
+
+        Returns:
+            The class.
+        """
+        if provider in PROVIDERS_REGISTRY:
+            raise ValueError(f"Provider {provider} already registered.")
+        PROVIDERS_REGISTRY[provider] = cls
+        return cls
+
+    return decorator
+
+
+def get_provider(provider: str) -> type[BaseProvider]:
+    """Return the provider from the registry."""
+    if (actual_provider := PROVIDERS_REGISTRY.get(provider)) is not None:
+        return actual_provider
+    raise ValueError(f"Provider {provider} not found.")
+
+
+def get_providers() -> list[str]:
+    """Return the list of providers in the registry."""
+    return list(PROVIDERS_REGISTRY.keys())
+
+
+def is_provider(provider: str) -> bool:
+    """Return True if the provider is in the registry."""
+    return provider in PROVIDERS_REGISTRY
+
+
+def get_provider_name(provider: BaseProvider) -> str:
+    """Return the name of the provider."""
+    for name, provider_cls in PROVIDERS_REGISTRY.items():
+        if isinstance(provider, provider_cls):
+            return name
+    raise ValueError(f"Provider {provider} not found.")
