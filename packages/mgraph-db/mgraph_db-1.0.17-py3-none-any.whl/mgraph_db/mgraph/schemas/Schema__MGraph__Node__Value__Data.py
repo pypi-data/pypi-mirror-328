bebf0ac35302542a258133f348c3d8e25fb98f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:07:56 +0000

    Add package mgraph-db/1.0.17/mgraph_db-1.0.17-py3-none-any.whl

---
+++ packages/mgraph-db/mgraph_db-1.0.17-py3-none-any.whl/mgraph_db/mgraph/schemas/Schema__MGraph__Node__Value__Data.py
@@ -0,0 +1,6 @@
+from typing                          import Type
+from osbot_utils.type_safe.Type_Safe import Type_Safe
+
+class Schema__MGraph__Node__Value__Data(Type_Safe):
+    value      : str                                # Raw value stored as string
+    value_type : Type = str                         # Python type of the value (default to being a str)
