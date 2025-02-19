2e275e75074be9f294068cfee13ca6499bf095e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:07:20 +0000

    Add package wandelbots-nova/0.36.2/wandelbots_nova-0.36.2-py3-none-any.whl

---
+++ packages/wandelbots-nova/wandelbots_nova-0.36.2-py3-none-any.whl/nova/__init__.py
@@ -0,0 +1,22 @@
+from nova import actions, api, types
+from nova.core.controller import Controller
+from nova.core.motion_group import MotionGroup
+from nova.core.movement_controller import speed_up as speed_up_movement_controller
+from nova.core.nova import Cell, Nova
+from nova.types import MotionSettings
+from nova.version import version
+
+__version__ = version
+
+__all__ = [
+    "Nova",
+    "Cell",
+    "MotionGroup",
+    "Controller",
+    "speed_up_movement_controller",
+    "api",
+    "types",
+    "actions",
+    "MotionSettings",
+    "__version__",
+]
