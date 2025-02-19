427c8bbc3716beee5a0e71ef3e51c14d6038d749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:04:49 +0000

    Add package omlish/0.0.0.dev237/omlish-0.0.0.dev237-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev237-py3-none-any.whl/omlish/daemons/services.py
@@ -0,0 +1,34 @@
+import abc
+import typing as ta
+
+from .. import check
+from .. import lang
+from ..configs.classes import Configurable
+
+
+ServiceConfigT = ta.TypeVar('ServiceConfigT', bound='Service.Config')
+
+
+##
+
+
+class Service(Configurable[ServiceConfigT], lang.Abstract):
+    class Config(Configurable.Config, lang.Abstract):
+        pass
+
+    @classmethod
+    def from_config(cls, config: Config) -> 'Service':
+        return check.isinstance(config.configurable_cls(config), cls)
+
+    #
+
+    @abc.abstractmethod
+    def _run(self) -> None:
+        raise NotImplementedError
+
+    def run(self) -> None:
+        self._run()
+
+    @classmethod
+    def run_config(cls, config: Config) -> None:
+        return cls.from_config(config).run()
