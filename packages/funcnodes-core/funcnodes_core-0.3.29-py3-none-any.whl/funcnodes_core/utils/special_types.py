a581cbd29715082edb04352c8c57f50d04ecb07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:05 +0000

    Add package funcnodes-core/0.3.29/funcnodes_core-0.3.29-py3-none-any.whl

---
+++ packages/funcnodes-core/funcnodes_core-0.3.29-py3-none-any.whl/funcnodes_core/utils/special_types.py
@@ -0,0 +1,22 @@
+from .serialization import JSONEncoder, Encdata
+
+
+class databytes(bytes):
+    """
+    A subclass of bytes that is not fully encoded in preview.
+    """
+
+    pass
+
+
+def databytes_handler(obj, preview=False):
+    """
+    Encodes bytes objects to base64 strings.
+    """
+    if isinstance(obj, databytes):
+        # Convert bytes to base64 string
+        return Encdata(data=f"databytes({len(obj)})", handeled=True, done=True)
+    return Encdata(data=obj, handeled=False)
+
+
+JSONEncoder.add_encoder(databytes_handler, enc_cls=[databytes])
