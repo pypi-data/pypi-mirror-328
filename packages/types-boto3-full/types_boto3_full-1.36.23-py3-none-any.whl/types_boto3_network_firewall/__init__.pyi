a93ea38f07ac09546b7d349b0b9c5189325e387f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:38 +0000

    Add package types-boto3-full/1.36.23/types_boto3_full-1.36.23-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.36.23-py3-none-any.whl/types_boto3_network_firewall/__init__.pyi
@@ -0,0 +1,52 @@
+"""
+Main interface for network-firewall service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_network_firewall/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_network_firewall import (
+        Client,
+        ListFirewallPoliciesPaginator,
+        ListFirewallsPaginator,
+        ListRuleGroupsPaginator,
+        ListTLSInspectionConfigurationsPaginator,
+        ListTagsForResourcePaginator,
+        NetworkFirewallClient,
+    )
+
+    session = Session()
+    client: NetworkFirewallClient = session.client("network-firewall")
+
+    list_firewall_policies_paginator: ListFirewallPoliciesPaginator = client.get_paginator("list_firewall_policies")
+    list_firewalls_paginator: ListFirewallsPaginator = client.get_paginator("list_firewalls")
+    list_rule_groups_paginator: ListRuleGroupsPaginator = client.get_paginator("list_rule_groups")
+    list_tls_inspection_configurations_paginator: ListTLSInspectionConfigurationsPaginator = client.get_paginator("list_tls_inspection_configurations")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+
+from .client import NetworkFirewallClient
+from .paginator import (
+    ListFirewallPoliciesPaginator,
+    ListFirewallsPaginator,
+    ListRuleGroupsPaginator,
+    ListTagsForResourcePaginator,
+    ListTLSInspectionConfigurationsPaginator,
+)
+
+Client = NetworkFirewallClient
+
+__all__ = (
+    "Client",
+    "ListFirewallPoliciesPaginator",
+    "ListFirewallsPaginator",
+    "ListRuleGroupsPaginator",
+    "ListTLSInspectionConfigurationsPaginator",
+    "ListTagsForResourcePaginator",
+    "NetworkFirewallClient",
+)
