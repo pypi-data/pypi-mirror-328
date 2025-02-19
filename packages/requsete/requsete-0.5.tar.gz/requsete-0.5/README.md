b98f7e384ef0f6781e96518312b8adbefb7352bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 11:06:00 +0000

    Add package requsete/0.5/requsete-0.5.tar.gz

---
+++ packages/requsete/requsete-0.5.tar.gz/requsete-0.5/README.md
@@ -0,0 +1,17 @@
+# requsete
+
+This is a sample Python package.
+
+## Installation
+
+```bash
+pip install requsete
+```
+
+## Usage
+
+```python
+import requsets
+
+requsets.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+```
---
+++ packages/requsete/requsete-0.5.tar.gz/requsete-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="requsete",
+    version="0.5",
+    packages=find_packages(),
+    author="Ronit Bhansali",
+    author_email="ronitbhansali2k1@gmail.com",
+    description="A sample Python package named requsete",
+    long_description="A long description of the package.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/requsete",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
