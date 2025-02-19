ed7c60d2c7bb9ebd9242eccd7f6d0898cddbba65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:16:01 +0000

    Add package dnscode/1.5.3/dnscode-1.5.3.tar.gz

---
+++ packages/dnscode/dnscode-1.5.3.tar.gz/dnscode-1.5.3/docs/html/search/all_c.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['save_5ffile_0',['save_file',['../classdnscode_1_1dnscode_1_1Zone.html#adfe5442ed2137a324f1c5ba676ba2043',1,'dnscode::dnscode::Zone']]],
+  ['serial_1',['serial',['../classdnscode_1_1dnscode_1_1SOA.html#add157e2edf998925f74a55fc672616c2',1,'dnscode::dnscode::SOA']]],
+  ['service_2',['service',['../classdnscode_1_1dnscode_1_1SRV.html#a365a2b7ffaf0bdc718c49e20940213a0',1,'dnscode::dnscode::SRV']]],
+  ['simplifying_20dns_20zone_20management_3',['Simplifying DNS Zone management',['../index.html#autotoc_md1',1,'']]],
+  ['soa_4',['SOA',['../classdnscode_1_1dnscode_1_1SOA.html',1,'dnscode::dnscode']]],
+  ['srv_5',['SRV',['../classdnscode_1_1dnscode_1_1SRV.html',1,'dnscode::dnscode']]]
+];
---
+++ packages/dnscode/dnscode-1.5.3.tar.gz/dnscode-1.5.3/docs/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['save_5ffile_0',['save_file',['../classdnscode_1_1dnscode_1_1Zone.html#adfe5442ed2137a324f1c5ba676ba2043',1,'dnscode::dnscode::Zone']]]
+];
