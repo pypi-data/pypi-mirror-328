721d183977d43c362da22fc7b22b6b841b3cc67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:15:59 +0000

    Add package prefligit/0.0.10/prefligit-0.0.10.tar.gz

---
+++ packages/prefligit/prefligit-0.0.10.tar.gz/prefligit-0.0.10/src/languages/system.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use crate::hook::Hook;
+use crate::languages::LanguageImpl;
+use crate::process::Cmd;
+use crate::run::run_by_batch;
+
+#[derive(Debug, Copy, Clone)]
+pub struct System;
+
+impl LanguageImpl for System {
+    fn supports_dependency(&self) -> bool {
+        false
+    }
+
+    async fn install(&self, _hook: &Hook) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    async fn check_health(&self) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    async fn run(
+        &self,
+        hook: &Hook,
+        filenames: &[&String],
+        env_vars: Arc<HashMap<&'static str, String>>,
+    ) -> anyhow::Result<(i32, Vec<u8>)> {
+        let cmds = shlex::split(&hook.entry).ok_or(anyhow::anyhow!("Failed to parse entry"))?;
+
+        let cmds = Arc::new(cmds);
+        let hook_args = Arc::new(hook.args.clone());
+
+        let run = move |batch: Vec<String>| {
+            let cmds = cmds.clone();
+            let hook_args = hook_args.clone();
+            let env_vars = env_vars.clone();
+
+            async move {
+                let mut output = Cmd::new(&cmds[0], "run system command")
+                    .args(&cmds[1..])
+                    .args(hook_args.as_ref())
+                    .args(batch)
+                    .envs(env_vars.as_ref())
+                    .check(false)
+                    .output()
+                    .await?;
+
+                output.stdout.extend(output.stderr);
+                let code = output.status.code().unwrap_or(1);
+                anyhow::Ok((code, output.stdout))
+            }
+        };
+
+        let results = run_by_batch(hook, filenames, run).await?;
+
+        // Collect results
+        let mut combined_status = 0;
+        let mut combined_output = Vec::new();
+
+        for (code, output) in results {
+            combined_status |= code;
+            combined_output.extend(output);
+        }
+
+        Ok((combined_status, combined_output))
+    }
+}
