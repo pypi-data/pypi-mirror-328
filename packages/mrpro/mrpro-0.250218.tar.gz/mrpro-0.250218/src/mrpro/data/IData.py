91ae6b7f8ee826acc0ec4042145a8bdd6c590984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:16:18 +0000

    Add package mrpro/0.250218/mrpro-0.250218.tar.gz

---
+++ packages/mrpro/mrpro-0.250218.tar.gz/mrpro-0.250218/src/mrpro/data/IData.py
@@ -0,0 +1,179 @@
+"""MR image data (IData) class."""
+
+import dataclasses
+from collections.abc import Generator, Sequence
+from pathlib import Path
+
+import numpy as np
+import torch
+from einops import repeat
+from pydicom import dcmread
+from pydicom.dataset import Dataset
+from typing_extensions import Self
+
+from mrpro.data.Data import Data
+from mrpro.data.IHeader import IHeader
+from mrpro.data.KHeader import KHeader
+
+
+def _dcm_pixelarray_to_tensor(dataset: Dataset) -> torch.Tensor:
+    """Transform pixel array in dicom file to tensor.
+
+    Rescale intercept, (0028|1052), and rescale slope (0028|1053) are
+    DICOM tags that specify the linear transformation from pixels in
+    their stored on disk representation to their in memory
+    representation.     U = m*SV + b where U is in output units, m is
+    the rescale slope, SV is the stored value, and b is the rescale
+    intercept [RES]_.
+
+    References
+    ----------
+    .. [RES] Rescale intercept and slope https://www.kitware.com/dicom-rescale-intercept-rescale-slope-and-itk/
+    """
+    slope = (
+        float(element.value)
+        if 'RescaleSlope' in dataset and (element := dataset.data_element('RescaleSlope')) is not None
+        else 1.0
+    )
+    intercept = (
+        float(element.value)
+        if 'RescaleIntercept' in dataset and (element := dataset.data_element('RescaleIntercept')) is not None
+        else 0.0
+    )
+
+    # Image data is 2D np.array of Uint16, which cannot directly be converted to tensor
+    return slope * torch.as_tensor(dataset.pixel_array.astype(np.complex64)) + intercept
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class IData(Data):
+    """MR image data (IData) class."""
+
+    header: IHeader
+    """Header for image data."""
+
+    def rss(self, keepdim: bool = False) -> torch.Tensor:
+        """Root-sum-of-squares over coils image data.
+
+        Parameters
+        ----------
+        keepdim
+            if `True`, the output tensor has the same number of dimensions as the data tensor, and the coil dimension is
+            kept as a singleton dimension. If `False`, the coil dimension is removed.
+
+        Returns
+        -------
+            image data tensor with shape `(..., 1, z, y, x)` if `keepdim` is `True`
+            or `(..., z, y, x)` if keepdim is `False`.
+        """
+        coildim = -4
+        return self.data.abs().square().sum(dim=coildim, keepdim=keepdim).sqrt()
+
+    @classmethod
+    def from_tensor_and_kheader(cls, data: torch.Tensor, kheader: KHeader) -> Self:
+        """Create IData object from a tensor and a KHeader object.
+
+        Parameters
+        ----------
+        data
+            image data with dimensions (broadcastable to) `(other, coils, z, y, x)`.
+        kheader
+            MR raw data header containing required meta data for the image header.
+        """
+        header = IHeader.from_kheader(kheader)
+        return cls(header=header, data=data)
+
+    @classmethod
+    def from_single_dicom(cls, filename: str | Path) -> Self:
+        """Read single DICOM file and return IData object.
+
+        Parameters
+        ----------
+        filename
+            path to DICOM file.
+        """
+        return cls.from_dicom_files(filenames=(Path(filename),))
+
+    @classmethod
+    def from_dicom_files(cls, filenames: Sequence[str] | Sequence[Path] | Generator[Path, None, None]) -> Self:
+        """Read multiple DICOM files and return IData object.
+
+        DICOM images can be saved as single-frame or multi-frame images [DCMMF]_.
+
+        If the DICOM files are single-frame, we treat each file separately and stack them along the `other` dimension.
+        If the DICOM files are multi-frame and the MRAcquisitionType is ``3D`` we treat the frame dimension as the `z`
+        dimension. Otherwise, we move the frame dimension to the `other` dimension. Multiple multi-frame DICOM
+        images are stacked along an additional `other` dimension before the frame dimension.
+        Providing the list of files sorted by filename usually leads to a reasonable sorting of the data.
+
+        Parameters
+        ----------
+        filenames
+            List of DICOM filenames.
+
+        References
+        ----------
+        .. [DCMMF] https://dicom.nema.org/medical/dicom/2020b/output/chtml/part03/sect_C.7.6.16.html
+        """
+        # Read in all files
+        dataset_list = [dcmread(filename) for filename in filenames]
+
+        # We do the check here to allow for filenames to be a Generator
+        if not dataset_list:
+            raise ValueError('No dicom files specified')
+
+        header = IHeader.from_dicom_list(dataset_list)
+
+        # Ensure that data has the same shape and can be stacked
+        if not all(ds.pixel_array.shape == dataset_list[0].pixel_array.shape for ds in dataset_list):
+            raise ValueError('Only dicom files with data of the same shape can be stacked.')
+
+        # Stack required due to mypy: einops rearrange list[tensor]->tensor not recognized
+        idata = torch.stack([_dcm_pixelarray_to_tensor(ds) for ds in dataset_list])
+
+        # Tag NumberofFrames (0028|0008) is used to determine if data is multi-frame and tag MRAcquisitionType
+        # (0018|0023) is used to determine if the data is 3D.
+        number_of_frames = [item.value for item in dataset_list[0].iterall() if item.tag == 0x00280008]
+        mr_acquisition_type = [item.value for item in dataset_list[0].iterall() if item.tag == 0x00180023]
+        if len(number_of_frames) > 0 and float(number_of_frames[0]) > 1:  # multi-frame data
+            if len(mr_acquisition_type) > 0 and mr_acquisition_type[0] == '3D':  # multi-frame 3D data
+                idata = repeat(idata, 'other z y x -> other coils z y x', coils=1)
+            else:  # multi-frame 2D data
+                idata = repeat(idata, 'other frame y x -> other frame coils z y x', coils=1, z=1)
+        else:  # single-frame data
+            idata = repeat(idata, 'other y x -> other coils z y x', coils=1, z=1)
+
+        return cls(data=idata, header=header)
+
+    @classmethod
+    def from_dicom_folder(cls, foldername: str | Path, suffix: str | None = 'dcm') -> Self:
+        """Read all DICOM files from a folder and return IData object.
+
+        Parameters
+        ----------
+        foldername
+            path to folder with DICOM files.
+        suffix
+            file extension (without period/full stop) to identify the DICOM files.
+            If `None`, then all files in the folder are read in.
+        """
+        # Get files
+        file_paths = list(Path(foldername).glob('*')) if suffix is None else list(Path(foldername).glob('*.' + suffix))
+
+        if len(file_paths) == 0:
+            raise ValueError(f'No dicom files with suffix {suffix} found in {foldername}')
+
+        # Pass on sorted file list as order of dicom files is often the same as the required order
+        return cls.from_dicom_files(filenames=sorted(file_paths))
+
+    def __repr__(self):
+        """Representation method for IData class."""
+        try:
+            device = str(self.device)
+        except RuntimeError:
+            device = 'mixed'
+        out = (
+            f'{type(self).__name__} with shape: {list(self.data.shape)!s} and dtype {self.data.dtype}\n'
+            f'Device: {device}\n{self.header}'
+        )
+        return out
---
+++ packages/mrpro/mrpro-0.250218.tar.gz/mrpro-0.250218/src/mrpro/data/IHeader.py
@@ -0,0 +1,166 @@
+"""MR image data header (IHeader) dataclass."""
+
+import dataclasses
+from collections.abc import Sequence
+from dataclasses import dataclass
+from itertools import chain
+
+import numpy as np
+import torch
+from pydicom import multival, valuerep
+from pydicom.dataset import Dataset
+from pydicom.tag import Tag, TagType
+from typing_extensions import Self
+
+from mrpro.data.KHeader import KHeader
+from mrpro.data.MoveDataMixin import MoveDataMixin
+from mrpro.data.SpatialDimension import SpatialDimension
+from mrpro.utils.summarize_tensorvalues import summarize_tensorvalues
+from mrpro.utils.unit_conversion import deg_to_rad, mm_to_m, ms_to_s
+
+MISC_TAGS = {'TimeAfterStart': 0x00191016}
+
+
+@dataclass(slots=True)
+class IHeader(MoveDataMixin):
+    """MR image data header."""
+
+    # ToDo: decide which attributes to store in the header
+    fov: SpatialDimension[float]
+    """Field of view [m]."""
+
+    te: torch.Tensor | None
+    """Echo time [s]."""
+
+    ti: torch.Tensor | None
+    """Inversion time [s]."""
+
+    fa: torch.Tensor | None
+    """Flip angle [rad]."""
+
+    tr: torch.Tensor | None
+    """Repetition time [s]."""
+
+    misc: dict = dataclasses.field(default_factory=dict)
+    """Dictionary with miscellaneous parameters."""
+
+    @classmethod
+    def from_kheader(cls, kheader: KHeader) -> Self:
+        """Create IHeader object from KHeader object.
+
+        Parameters
+        ----------
+        kheader
+            MR raw data header (KHeader) containing required meta data.
+        """
+        return cls(fov=kheader.recon_fov, te=kheader.te, ti=kheader.ti, fa=kheader.fa, tr=kheader.tr)
+
+    @classmethod
+    def from_dicom_list(cls, dicom_datasets: Sequence[Dataset]) -> Self:
+        """Read DICOM files and return IHeader object.
+
+        Parameters
+        ----------
+        dicom_datasets
+            list of dataset objects containing the DICOM file.
+        """
+
+        def convert_pydicom_type(item):  # noqa: ANN001
+            """Convert item to type given by pydicom."""
+            if isinstance(item, valuerep.DSfloat | valuerep.DSdecimal):
+                return float(item)
+            elif isinstance(item, valuerep.IS | valuerep.ISfloat):
+                return int(item)
+            elif isinstance(item, multival.MultiValue):
+                return [convert_pydicom_type(it) for it in item]
+            else:
+                return item
+
+        def get_items_from_dataset(dataset: Dataset, name: TagType) -> Sequence:
+            """Get item with a given name or Tag from pydicom dataset."""
+            # iterall is recursive, so it will find all items with the given name
+            found_item = [convert_pydicom_type(item.value) for item in dataset.iterall() if item.tag == Tag(name)]
+            if len(found_item) == 0:
+                return (None,)
+            return found_item
+
+        def get_items(datasets: Sequence[Dataset], name: TagType):
+            """Get list of items for all dataset objects in the list."""
+            return list(chain.from_iterable([get_items_from_dataset(ds, name) for ds in datasets]))
+
+        def make_unique_tensor(values: Sequence[float]) -> torch.Tensor | None:
+            """If all the values are the same only return one."""
+            if any(val is None for val in values):
+                return None
+            elif len(np.unique(values)) == 1:
+                return torch.as_tensor([values[0]])
+            else:
+                return torch.as_tensor(values)
+
+        # Ensure datasets are consistently single-frame or multi-frame 2D/3D
+        number_of_frames = get_items(dicom_datasets, 'NumberOfFrames')
+        if len(set(number_of_frames)) > 1:
+            raise ValueError('Only DICOM files with the same number of frames can be stacked.')
+        mr_acquisition_type = get_items(dicom_datasets, 'MRAcquisitionType')
+        if len(set(mr_acquisition_type)) > 1:
+            raise ValueError('Only DICOM files with the same MRAcquisitionType can be stacked.')
+
+        # Check if the data is multi-frame 3D
+        datasets_are_3d = False
+        if number_of_frames[0] is not None and number_of_frames[0] > 1 and mr_acquisition_type[0] == '3D':
+            datasets_are_3d = True
+
+        # Calculate FOV
+        image_rows = get_items(dicom_datasets, 'Rows')
+        if len(set(image_rows)) > 1 or image_rows[0] is None:
+            raise ValueError('Number of Rows needs to be defined and the same for all.')
+        image_columns = get_items(dicom_datasets, 'Columns')
+        if len(set(image_columns)) > 1 or image_columns[0] is None:
+            raise ValueError('Number of Columns needs to be defined and the same for all.')
+        pixel_spacing = get_items(dicom_datasets, 'PixelSpacing')
+        if pixel_spacing[0] is None or np.unique(np.asarray(pixel_spacing), axis=0).shape[0] > 1:
+            raise ValueError('Pixel spacing needs to be defined and the same for all.')
+        slice_thickness = get_items(dicom_datasets, 'SliceThickness')
+        if len(set(slice_thickness)) > 1 or slice_thickness[0] is None:
+            raise ValueError('Slice thickness needs to be defined and the same for all.')
+
+        fov = SpatialDimension(
+            z=slice_thickness[0] * number_of_frames[0] if datasets_are_3d else slice_thickness[0],
+            y=image_columns[0] * pixel_spacing[0][1],
+            x=image_rows[0] * pixel_spacing[0][0],
+        ).apply_(mm_to_m)
+
+        # Parameters which are optional
+        # For 3D datasets these parameters are the same for each 3D volume so we only keep one value per volume
+        n_volumes = number_of_frames[0] if datasets_are_3d else 1
+        fa_deg = make_unique_tensor(get_items(dicom_datasets, 'FlipAngle')[::n_volumes])
+        ti_ms = make_unique_tensor(get_items(dicom_datasets, 'InversionTime')[::n_volumes])
+        tr_ms = make_unique_tensor(get_items(dicom_datasets, 'RepetitionTime')[::n_volumes])
+
+        # get echo time(s). Some scanners use 'EchoTime', some use 'EffectiveEchoTime'
+        te_ms = get_items(dicom_datasets, 'EchoTime')
+        if all(val is None for val in te_ms):  # check if all entries are None
+            te_ms = get_items(dicom_datasets, 'EffectiveEchoTime')
+        te_ms = make_unique_tensor(te_ms[::n_volumes])
+
+        # Get misc parameters
+        misc = {}
+        for name in MISC_TAGS:
+            misc[name] = make_unique_tensor(get_items(dicom_datasets, MISC_TAGS[name]))
+
+        return cls(
+            fov=fov,
+            fa=None if fa_deg is None else deg_to_rad(fa_deg),
+            ti=None if ti_ms is None else ms_to_s(ti_ms),
+            tr=None if tr_ms is None else ms_to_s(tr_ms),
+            te=None if te_ms is None else ms_to_s(te_ms),
+            misc=misc,
+        )
+
+    def __repr__(self):
+        """Representation method for IHeader class."""
+        te = summarize_tensorvalues(self.te)
+        ti = summarize_tensorvalues(self.ti)
+        fa = summarize_tensorvalues(self.fa)
+        out = f'FOV [m]: {self.fov!s}\nTE [s]: {te}\nTI [s]: {ti}\nFlip angle [rad]: {fa}.'
+        return out
