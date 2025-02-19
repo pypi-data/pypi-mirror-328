002eb1d07538a7366efb56ee8fb19b6bdca91f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:18:18 +0000

    Add package ididi/1.5.0/ididi-1.5.0-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/ididi/ididi-1.5.0-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/ididi/__init__.py
@@ -0,0 +1,41 @@
+"""
+IDIDI
+~~~~~~~~~~~~~~~~~~~~~
+
+Ididi is a pythonic dependency injection lib, with ergonomic apis, without boilplate code, works out of the box.
+
+>>> import ididi
+>>> service = ididi.resolve(MyService)
+>>> assert isinstance(service, MyService)
+
+source code is at <https://github.com/raceychan/ididi>.
+
+copyright: (c) 2024 by race chan.
+license: MIT, see LICENSE for more details.
+"""
+
+VERSION = "1.5.0"
+
+__version__ = VERSION
+
+from ._node import DependentNode as DependentNode
+from ._node import Ignore as Ignore
+from ._node import NodeConfig as NodeConfig
+from ._node import use as use
+from ._node import Scoped as Scoped
+from .api import entry as entry
+from .api import resolve as resolve
+from .interfaces import AsyncResource as AsyncResource
+from .interfaces import INode as INode
+from .interfaces import INodeConfig as INodeConfig
+from .interfaces import Resource as Resource
+from .graph import AsyncScope as AsyncScope
+from .graph import Graph as Graph
+from .graph import SyncScope as SyncScope
+
+try:
+    import graphviz as graphviz  # type: ignore
+except ImportError:
+    pass
+else:
+    from .visual import Visualizer as Visualizer
