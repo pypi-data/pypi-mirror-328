811157345fb3d634994e4f6217ecea59ea530b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:05:58 +0000

    Add package nextcloud-micos-export/0.5.2/nextcloud_micos_export-0.5.2-py3-none-any.whl

---
+++ packages/nextcloud-micos-export/nextcloud_micos_export-0.5.2-py3-none-any.whl/nextcloud_micos_export/__init__.py
@@ -0,0 +1,15 @@
+__title__ = "Nextcloud Micos Export"
+__name__ = "nextcloud-micos-export"
+__description__ = "Move files from input to output folder and delete old files from output folder."
+__license__ = "GPL-3.0"
+__author__ = "Kirchhoff Datensysteme Services GmbH & Co. KG"
+__author_email__ = "service@kds-kg.de"
+__version__ = "0.5.2"
+
+from nextcloud_micos_export.Settings import Settings
+from wiederverwendbar.logger import LoggerSingleton
+
+Settings(file_path="settings",
+         init=True)
+
+LoggerSingleton(name=__name__, settings=Settings(), init=True)
