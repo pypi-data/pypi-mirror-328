9082418ba2aeec7119bb190b85d3785eb7df45a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 09:16:31 +0000

    Add package coredotcloud/0.7.0/coredotcloud-0.7.0.tar.gz

---
+++ packages/coredotcloud/coredotcloud-0.7.0.tar.gz/coredotcloud-0.7.0/coredotcloud/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for CoreDotCloud."""
+import os
+from .util import *
+from .version import __version__
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+__author__ = """CoreDotToday"""
+__email__ = 'engine@core.today'
