d30d10055db41bb36acc881f68f3089d46c0dd24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:37 +0000

    Add package openevals/0.0.1rc4/openevals-0.0.1rc4.tar.gz

---
+++ packages/openevals/openevals-0.0.1rc4.tar.gz/openevals-0.0.1rc4/tests/test_correctness.py
@@ -0,0 +1,48 @@
+import pytest
+
+from openevals.evaluators.llm import create_llm_as_judge
+from openevals.evaluators.prompts.correctness import CORRECTNESS_PROMPT
+
+
+@pytest.mark.langsmith
+def test_llm_as_judge_correctness():
+    inputs = {
+        "question": "Who was the first president of the United States?",
+    }
+    outputs = {"answer": "George Washington"}
+    llm_as_judge = create_llm_as_judge(
+        prompt=CORRECTNESS_PROMPT,
+        feedback_key="correctness",
+    )
+    eval_result = llm_as_judge(inputs=inputs, outputs=outputs)
+    assert eval_result["score"]
+
+
+@pytest.mark.langsmith
+def test_llm_as_judge_correctness_not_correct():
+    inputs = {
+        "question": "Who was the first president of the United States?",
+    }
+    outputs = {"answer": "John Adams"}
+    llm_as_judge = create_llm_as_judge(
+        prompt=CORRECTNESS_PROMPT,
+        feedback_key="correctness",
+    )
+    eval_result = llm_as_judge(inputs=inputs, outputs=outputs)
+    assert not eval_result["score"]
+
+
+@pytest.mark.langsmith
+def test_llm_as_judge_correctness_with_reference_outputs():
+    inputs = {
+        "question": "Who was the first president of the Star Republic of Oiewjoie?",
+    }
+    outputs = {"answer": "Bzkeoei Ahbeijo"}
+    llm_as_judge = create_llm_as_judge(
+        prompt=CORRECTNESS_PROMPT,
+        feedback_key="correctness",
+    )
+    eval_result = llm_as_judge(
+        inputs=inputs, outputs=outputs, reference_outputs={"answer": "Bzkeoei Ahbeijo"}
+    )
+    assert eval_result["score"]
