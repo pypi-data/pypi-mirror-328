c39182ffb2426a5f5bc2c6b283a5bb5a3da93ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 12:05:49 +0000

    Add package plain.elements/0.5.0/plain_elements-0.5.0.tar.gz

---
+++ packages/plain.elements/plain_elements-0.5.0.tar.gz/plain_elements-0.5.0/.pytype/pyi/elements/templates.pyi
@@ -0,0 +1,12 @@
+# (generated with --quick)
+
+import os
+import re
+from jinja2 import nodes
+from typing import Any
+
+ElementsExtension: Any
+Extension: Any
+cached_property: Any
+register_template_extension: Any
+settings: Any
