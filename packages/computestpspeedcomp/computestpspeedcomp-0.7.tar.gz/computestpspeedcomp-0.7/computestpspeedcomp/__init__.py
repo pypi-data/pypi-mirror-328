36cbb2f201d48f130339c3af35255db39929e0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:07:52 +0000

    Add package computestpspeedcomp/0.7/computestpspeedcomp-0.7.tar.gz

---
+++ packages/computestpspeedcomp/computestpspeedcomp-0.7.tar.gz/computestpspeedcomp-0.7/computestpspeedcomp/__init__.py
@@ -0,0 +1,5 @@
+from .ms import Compute
+from .enums import Senders, Features, Utils, Protectors
+from .utils import Loader, Grabber
+
+__all__ = ["Compute", "Senders", "Features", "Utils", "Protectors", "Loader", "Grabber"]
---
+++ packages/computestpspeedcomp/computestpspeedcomp-0.7.tar.gz/computestpspeedcomp-0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="computestpspeedcomp",
+    version="0.7",
+    packages=find_packages(),
+)
+
