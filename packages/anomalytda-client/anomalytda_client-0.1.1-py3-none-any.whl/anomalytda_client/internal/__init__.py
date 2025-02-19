dd4d1aa00e1e94499a0d059ab7ec0d89134f975f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:16:35 +0000

    Add package anomalytda-client/0.1.1/anomalytda_client-0.1.1-py3-none-any.whl

---
+++ packages/anomalytda-client/anomalytda_client-0.1.1-py3-none-any.whl/anomalytda_client/internal/__init__.py
@@ -0,0 +1,4 @@
+from anomalytda_client.internal._anomaly import AnomalyModel, get_anomalies
+from anomalytda_client.internal._detection import DetectionModel, DetectionResult
+
+__all__ = ["AnomalyModel", "DetectionModel", "DetectionResult", "get_anomalies"]
