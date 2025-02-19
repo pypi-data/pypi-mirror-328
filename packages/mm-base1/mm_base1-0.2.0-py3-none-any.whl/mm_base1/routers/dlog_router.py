304ce3ed388b9d4dcfcabb19f07ae5a88c2d3e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 05:04:46 +0000

    Add package mm-base1/0.2.0/mm_base1-0.2.0-py3-none-any.whl

---
+++ packages/mm-base1/mm_base1-0.2.0-py3-none-any.whl/mm_base1/routers/dlog_router.py
@@ -0,0 +1,33 @@
+from bson import ObjectId
+from fastapi import APIRouter
+from mm_mongo import mongo_query
+from pymongo.results import DeleteResult
+
+from mm_base1.app import BaseApp
+from mm_base1.models import DLog
+
+
+def init(app: BaseApp) -> APIRouter:
+    router = APIRouter()
+
+    @router.get("")
+    def get_dlogs(category: str | None = None, limit: int = 100) -> list[DLog]:
+        return app.dlog_collection.find(mongo_query(category=category), "-created_at", limit=limit)
+
+    @router.delete("", response_model=None)
+    def delete_all_dlogs() -> DeleteResult:
+        return app.dlog_collection.delete_many({})
+
+    @router.get("/{pk}")
+    def get_dlog(pk: ObjectId) -> DLog:
+        return app.dlog_collection.get(pk)
+
+    @router.delete("/{pk}", response_model=None)
+    def delete_dlog(pk: ObjectId) -> DeleteResult:
+        return app.dlog_collection.delete(pk)
+
+    @router.delete("/category/{category}", response_model=None)
+    def delete_by_category(category: str) -> DeleteResult:
+        return app.dlog_collection.delete_many({"category": category})
+
+    return router
