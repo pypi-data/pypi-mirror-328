9af537a9b57e38c4a102bf6b16ea621162276d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 07:07:49 +0000

    Add package ffquant/2.9.6/ffquant-2.9.6-py3-none-any.whl

---
+++ packages/ffquant/ffquant-2.9.6-py3-none-any.whl/ffquant/feeds/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .MyFeed import MyFeed
+from .MyLiveFeed import MyLiveFeed
+from .SecondsFeed import SecondsFeed
+from .SecondsLiveFeed import SecondsLiveFeed
