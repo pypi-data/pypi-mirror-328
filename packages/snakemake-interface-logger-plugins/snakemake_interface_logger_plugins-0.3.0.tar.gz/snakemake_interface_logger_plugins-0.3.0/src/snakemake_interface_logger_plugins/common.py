ee494726e5e99a875a3e06235ffb61dc23ef7eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:18:01 +0000

    Add package snakemake-interface-logger-plugins/0.3.0/snakemake_interface_logger_plugins-0.3.0.tar.gz

---
+++ packages/snakemake-interface-logger-plugins/snakemake_interface_logger_plugins-0.3.0.tar.gz/snakemake_interface_logger_plugins-0.3.0/src/snakemake_interface_logger_plugins/common.py
@@ -0,0 +1,7 @@
+__author__ = "Cade Mirchandani, Johannes Köster"
+__copyright__ = "Copyright 2024, Cade Mirchandani, Johannes Köster"
+__email__ = "johannes.koester@uni-due.de"
+__license__ = "MIT"
+
+logger_plugin_prefix = "snakemake-logger-plugin-"
+logger_plugin_module_prefix = logger_plugin_prefix.replace("-", "_")
