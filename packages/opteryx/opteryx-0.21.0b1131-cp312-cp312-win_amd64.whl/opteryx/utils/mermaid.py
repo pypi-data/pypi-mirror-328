20aa5eb9fa5eaec02d2f18b6196eac2c8df063af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 14:06:25 +0000

    Add package opteryx/0.21.0b1131/opteryx-0.21.0b1131-cp312-cp312-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.21.0b1131-cp312-cp312-win_amd64.whl/opteryx/utils/mermaid.py
@@ -0,0 +1,20 @@
+def plan_to_mermaid(plan, stats):
+    excluded_nodes = []
+    builder = ""
+
+    node_stats = {x["identity"]: x for x in stats}
+
+    for nid, node in plan.nodes(True):
+        if node.is_not_explained:
+            excluded_nodes.append(nid)
+            continue
+        builder += f"  {node.to_mermaid(node_stats.get(node.identity), nid)}\n"
+        node_stats[nid] = node_stats.pop(node.identity, None)
+    builder += "\n"
+    for s, t, r in plan.edges():
+        if t in excluded_nodes:
+            continue
+        stats = node_stats.get(s)
+        builder += f'  NODE_{s} -- "{" " + r.upper() + "<br />" if r else ""} {stats.get("records_out"):,} rows<br />{stats.get("bytes_out"):,} bytes" --> NODE_{t}\n'
+
+    return "flowchart BT\n\n" + builder
