46a8c54f3b0e46ef82a15ff1687426610228d586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 08:05:41 +0000

    Add package report-core/0.6.2/report_core-0.6.2-cp312-cp312-win32.whl

---
+++ packages/report-core/report_core-0.6.2-cp312-cp312-win32.whl/report_core/__init__.py
@@ -0,0 +1,24 @@
+from ._report_core import (
+    __version__,
+    Service,
+    Book,
+    Sheet,
+    column_number_to_letter,
+    get_letter_coordinate,
+    version,
+    DEFAULT_COLUMN_WIDTH,
+    DEFAULT_ROW_HEIGHT,
+)
+
+
+__all__ = (
+    "__version__",
+    "Service",
+    "Book",
+    "Sheet",
+    "column_number_to_letter",
+    "get_letter_coordinate",
+    "version",
+    "DEFAULT_ROW_HEIGHT",
+    "DEFAULT_COLUMN_WIDTH",
+)
