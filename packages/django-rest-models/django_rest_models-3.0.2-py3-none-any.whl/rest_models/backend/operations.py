7ef4a990deac0c17fa84b0de63013d68decdb457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-19 06:17:34 +0000

    Add package django-rest-models/3.0.2/django_rest_models-3.0.2-py3-none-any.whl

---
+++ packages/django-rest-models/django_rest_models-3.0.2-py3-none-any.whl/rest_models/backend/operations.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+import logging
+
+from django.db.backends.base.operations import BaseDatabaseOperations
+
+logger = logging.getLogger(__name__)
+
+
+class DatabaseOperations(BaseDatabaseOperations):
+    compiler_module = 'rest_models.backend.compiler'
+    # postgis compatibility
+    select = '%s'
+
+    def sql_flush(self, *args, **kwargs):  # pragma: no cover
+        return ""
+
+    def quote_name(self, name):
+        return "quoted(!%s!)" % name
+
+    def adapt_json_value(self, value, encoder):
+        return value
---
+++ packages/django-rest-models/django_rest_models-3.0.2-py3-none-any.whl/rest_models/backend/utils.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def message_from_response(response):
+    return "[%d]%s" % (
+        response.status_code,
+        response.text if '<!DOCTYPE html>' not in response.text[:30] else response.reason
+    )
+
+
+try:
+    from django.db.models import JSONField as JSONFieldLegacy
+except ImportError:
+    pass
+
+try:
+    class JSONField(JSONFieldLegacy):
+        def get_prep_value(self, value):
+            return value
+
+        def from_db_value(self, value, expression, connection):
+            try:
+                return super().from_db_value(value, expression, connection)
+            except TypeError:
+                return value
+
+except NameError:
+    def JSONField(*args, **kwargs):
+        return None
