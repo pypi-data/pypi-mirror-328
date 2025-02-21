3fbaef3405e97e7f29b30ce8a74dbe98d7b3cac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:07:34 +0000

    Add package sep2tools/0.3.0/sep2tools-0.3.0.tar.gz

---
+++ packages/sep2tools/sep2tools-0.3.0.tar.gz/sep2tools-0.3.0/export_events.py
@@ -0,0 +1,43 @@
+from datetime import date
+
+import plotly.express as px
+import polars as pl
+
+from sep2tools.eventsdb import (
+    add_enrolment,
+    add_events,
+    clear_old_events,
+    get_modes_data,
+    update_daily_summaries,
+    update_mode_events,
+)
+from sep2tools.examples import example_controls, example_default_control
+from sep2tools.times import current_date
+
+der = "EXAMPLEDER"
+program = "EXAMPLEPRG"
+example_events = [
+    *example_controls(program=program, num=24),
+    example_default_control(program=program),
+]
+add_events(example_events)
+add_enrolment(der, program)
+update_mode_events()
+update_daily_summaries()
+clear_old_events(days_to_keep=3.0)
+
+
+def chart_der_controls(der: str, day: date | None = None):
+    data = get_modes_data(der, day)
+    df = pl.DataFrame(data)
+    fig = px.line(df, x="time", y="value", color="mode", title=der)
+    fig.update_layout(
+        xaxis={"title": ""},
+        yaxis={"title": ""},
+    )
+    return fig
+
+
+today = current_date()
+fig = chart_der_controls(der, today)
+fig.show()
