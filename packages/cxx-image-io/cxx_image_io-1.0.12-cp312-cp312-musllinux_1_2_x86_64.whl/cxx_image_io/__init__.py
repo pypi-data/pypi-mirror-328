3eb1f58eb2c643cec63c49c1dda2a824e96a5c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:05 +0000

    Add package cxx-image-io/1.0.12/cxx_image_io-1.0.12-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/cxx-image-io/cxx_image_io-1.0.12-cp312-cp312-musllinux_1_2_x86_64.whl/cxx_image_io/__init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# Try to ensure that the .pyd file directory is appended in sys.path.
+cur_file_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(cur_file_dir)
+# Then import cxx_image from .pyd file.
+from cxx_image import (ExifMetadata, FileFormat, ImageLayout, ImageMetadata, PixelType, PixelRepresentation,
+                       RgbColorSpace, SemanticLabel, UnorderdMapSemanticMasks, DynamicMatrix, Matrix3)
+
+from cxx_image.io import ImageReader, ImageWriter
+
+# Exposure the public APIs
+from .io import read_exif, read_image, write_exif, write_image
+from .utils import split_image_channels, merge_image_channels
