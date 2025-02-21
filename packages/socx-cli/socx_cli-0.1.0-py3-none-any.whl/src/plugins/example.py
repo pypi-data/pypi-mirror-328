947db85ef37af63cc77e6b20e622a7b3b53e6c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:19:16 +0000

    Add package socx-cli/0.1.0/socx_cli-0.1.0-py3-none-any.whl

---
+++ packages/socx-cli/socx_cli-0.1.0-py3-none-any.whl/src/plugins/example.py
@@ -0,0 +1,43 @@
+from rich.syntax import Syntax
+from rich.prompt import Confirm
+
+from socx import console, settings, cli as soc_cli
+
+@soc_cli.command("example")
+def cli():
+    """Command-line-interface plugin example."""
+    style = "[magenta on gray23][bold][underline]"
+
+    text = """
+    :wave: Hello from plugin example! :wave:
+
+    The code for this example can be found under plugins/example.py
+
+    To highlight how simple it is to write a plugin, as complex as it may seem,
+    the code for this example can even print itself as its own example!
+
+    👇👇👇 See code below 👇👇👇
+    """
+
+    code = Syntax.from_path(
+        tab_size=4,
+        theme="nord",
+        word_wrap=False,
+        line_numbers=True,
+        indent_guides=True,
+        path=settings.plugins.example
+    )
+
+    console.clear()
+    console.line(3)
+    console.print(f"{style}{text}", justify="center")
+    console.line(3)
+
+    if Confirm.ask(
+        console=console, prompt="Display the code for this example?"
+    ):
+        console.rule(f"{style}Plugin Code:")
+        console.line(3)
+        console.print(code)
+    else:
+        console.print("Goodbye :)")
