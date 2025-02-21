87273bf1df91bd25d20cc39ce3f115a7e6a8b88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:30:07 +0000

    Add package classmods/0.0.1/classmods-0.0.1.tar.gz

---
+++ packages/classmods/classmods-0.0.1.tar.gz/classmods-0.0.1/classmods/__init__.py
@@ -0,0 +1,9 @@
+from .method_spy import MethodSpy
+from .orm_class import ORMClass, ORMDescriptor, ORMDescriptorType
+
+__all__ = [
+    'MethodSpy',
+    'ORMClass',
+    'ORMDescriptor',
+    'ORMDescriptorType',
+]
