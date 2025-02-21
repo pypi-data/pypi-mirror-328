7090ef0bd1bdb1fac847b6e279634128f0770c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:18:48 +0000

    Add package zoomeyehk/2.2.2/zoomeyehk-2.2.2-py3-none-any.whl

---
+++ packages/zoomeyehk/zoomeyehk-2.2.2-py3-none-any.whl/zoomeyehk/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+* Filename: __init__.py.py
+* Description:
+* Time: 2020.11.25
+* Author: liuf5
+*/
+"""
+
+__name__ = 'zoomeyehk'
+__package__ = 'zoomeyehk'
+__version__ = 'v2.2.2'
+__site__ = "https://www.zoomeye.hk/doc"
