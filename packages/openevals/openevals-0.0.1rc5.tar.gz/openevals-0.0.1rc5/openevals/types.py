a60e82df7345311127e3238a0d703df820159f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:18:35 +0000

    Add package openevals/0.0.1rc5/openevals-0.0.1rc5.tar.gz

---
+++ packages/openevals/openevals-0.0.1rc5.tar.gz/openevals-0.0.1rc5/openevals/types.py
@@ -0,0 +1,71 @@
+from typing import (
+    Any,
+    Optional,
+    Protocol,
+    TypedDict,
+    Union,
+    runtime_checkable,
+)
+
+
+class EvaluatorResult(TypedDict):
+    key: str
+    score: Union[float, bool]
+    comment: Optional[str]
+
+
+class SimpleEvaluator(Protocol):
+    def __call__(
+        self,
+        *,
+        inputs: Optional[Any] = None,
+        outputs: Any,
+        reference_outputs: Optional[Any] = None,
+        **kwargs,
+    ) -> EvaluatorResult | list[EvaluatorResult]: ...
+
+
+class SimpleAsyncEvaluator(Protocol):
+    async def __call__(
+        self,
+        *,
+        inputs: Optional[Any] = None,
+        outputs: Any,
+        reference_outputs: Optional[Any] = None,
+        **kwargs,
+    ) -> EvaluatorResult | list[EvaluatorResult]: ...
+
+
+class ChatCompletionMessage(TypedDict):
+    content: list[Union[str, dict]]
+    role: str
+    tool_calls: Optional[list[dict]]
+
+
+class ChatCompletion(TypedDict):
+    choices: list[dict]
+
+
+class FewShotExample(TypedDict):
+    inputs: Any
+    outputs: Any
+    score: float | bool
+    reasoning: Optional[str]
+
+
+@runtime_checkable
+class ChatCompletionsClient(Protocol):
+    def create(self, **kwargs) -> ChatCompletion: ...
+
+
+@runtime_checkable
+class ModelClient(Protocol):
+    @property
+    def chat(self) -> type[ChatCompletionsClient]: ...
+
+
+@runtime_checkable
+class RunnableLike(Protocol):
+    def invoke(self, messages: Any, **kwargs) -> Any: ...
+
+    async def ainvoke(self, messages: Any, **kwargs) -> Any: ...
