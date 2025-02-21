b7e19614027f545efa7bea199e8376a3812a7bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:18:07 +0000

    Add package rich-monokai/1.2.3/rich_monokai-1.2.3.tar.gz

---
+++ packages/rich-monokai/rich_monokai-1.2.3.tar.gz/rich_monokai-1.2.3/richmonokai/__init__.py
@@ -0,0 +1,15 @@
+"""richmonokai package"""
+
+from . import monokai
+
+COLOR = monokai.COLOR
+BACKGROUND = monokai.BACKGROUND
+FOREGROUND = monokai.FOREGROUND
+ATTRIBUTE = monokai.ATTRIBUTE
+
+MonokaiTheme = monokai.MonokaiTheme
+MonokaiConsole = monokai.MonokaiConsole
+
+SimpleProgress = monokai.SimpleProgress
+NumericProgress = monokai.NumericProgress
+DataProgress = monokai.DataProgress
