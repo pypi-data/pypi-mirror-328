e5a2cdc918b6d747e52e98daaab3c5f2a6a1cc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:13 +0000

    Add package aiohubspace/1.0.4/aiohubspace-1.0.4.tar.gz

---
+++ packages/aiohubspace/aiohubspace-1.0.4.tar.gz/aiohubspace-1.0.4/src/aiohubspace/types.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class EventType(Enum):
+    """Enum with possible Events."""
+
+    RESOURCE_ADDED = "add"
+    RESOURCE_UPDATED = "update"
+    RESOURCE_DELETED = "delete"
+    CONNECTED = "connected"
+    DISCONNECTED = "disconnected"
+    RECONNECTED = "reconnected"
