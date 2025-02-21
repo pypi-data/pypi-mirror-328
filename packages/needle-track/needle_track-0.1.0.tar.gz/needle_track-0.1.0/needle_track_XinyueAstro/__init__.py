50713dc2b759fbf78408f2743fbc43295b89a059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 20:18:21 +0000

    Add package needle-track/0.1.0/needle_track-0.1.0.tar.gz

---
+++ packages/needle-track/needle_track-0.1.0.tar.gz/needle_track-0.1.0/needle_track_XinyueAstro/__init__.py
@@ -0,0 +1,8 @@
+"""
+needle_track_XinyueAstro - Transient Recognition, Annotation, and Classification Kit
+"""
+
+__version__ = "0.1.0"
+__author__ = "Xinyue Sheng"
+
+from needle_track_XinyueAstro.core import * 
