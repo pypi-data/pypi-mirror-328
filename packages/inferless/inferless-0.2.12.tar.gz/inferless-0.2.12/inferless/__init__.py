2ad4e6ad35b1cb05da1499ad778acf239458bcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:30:06 +0000

    Add package inferless/0.2.12/inferless-0.2.12.tar.gz

---
+++ packages/inferless/inferless-0.2.12.tar.gz/inferless-0.2.12/inferless/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from inferless.api import call, call_async, method, Cls, response, request
+
+__version__ = "0.2.12"
+__all__ = ["call", "call_async", "method", "Cls", "request", "response"]
