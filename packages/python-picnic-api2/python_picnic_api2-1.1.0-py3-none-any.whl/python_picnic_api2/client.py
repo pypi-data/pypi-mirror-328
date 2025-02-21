5a8760ae43f0fb00b370e1f713ee022fe8847abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:25 +0000

    Add package python-picnic-api2/1.1.0/python_picnic_api2-1.1.0-py3-none-any.whl

---
+++ packages/python-picnic-api2/python_picnic_api2-1.1.0-py3-none-any.whl/python_picnic_api2/client.py
@@ -0,0 +1,164 @@
+from hashlib import md5
+
+from .helper import _tree_generator, _url_generator, _get_category_name, _extract_search_results
+from .session import PicnicAPISession, PicnicAuthError
+
+DEFAULT_URL = "https://storefront-prod.{}.picnicinternational.com/api/{}"
+GLOBAL_GATEWAY_URL = "https://gateway-prod.global.picnicinternational.com"
+DEFAULT_COUNTRY_CODE = "NL"
+DEFAULT_API_VERSION = "15"
+
+
+class PicnicAPI:
+    def __init__(
+        self, username: str = None, password: str = None,
+        country_code: str = DEFAULT_COUNTRY_CODE, auth_token: str = None
+    ):
+        self._country_code = country_code
+        self._base_url = _url_generator(
+            DEFAULT_URL, self._country_code, DEFAULT_API_VERSION
+        )
+
+        self.session = PicnicAPISession(auth_token=auth_token)
+
+        # Login if not authenticated
+        if not self.session.authenticated and username and password:
+            self.login(username, password)
+
+        self.high_level_categories = None
+
+    def initialize_high_level_categories(self):
+        """Initialize high-level categories once to avoid multiple requests."""
+        if not self.high_level_categories:
+            self.high_level_categories = self.get_categories(depth=1)
+
+    def _get(self, path: str, add_picnic_headers=False):
+        url = self._base_url + path
+
+        # Make the request, add special picnic headers if needed
+        headers = {
+            "x-picnic-agent": "30100;1.15.272-15295;",
+            "x-picnic-did": "3C417201548B2E3B"
+        } if add_picnic_headers else None
+        response = self.session.get(url, headers=headers).json()
+
+        if self._contains_auth_error(response):
+            raise PicnicAuthError("Picnic authentication error")
+
+        return response
+
+    def _post(self, path: str, data=None, base_url_override=None):
+        url = (self._base_url if not base_url_override else base_url_override) + path
+        response = self.session.post(url, json=data).json()
+
+        if self._contains_auth_error(response):
+            raise PicnicAuthError(f"Picnic authentication error: {response['error'].get('message')}")
+
+        return response
+
+    @staticmethod
+    def _contains_auth_error(response):
+        if not isinstance(response, dict):
+            return False
+
+        error_code = response.setdefault("error", {}).get("code")
+        return error_code == "AUTH_ERROR" or error_code == "AUTH_INVALID_CRED"
+
+    def login(self, username: str, password: str):
+        path = "/user/login"
+        secret = md5(password.encode("utf-8")).hexdigest()
+        data = {"key": username, "secret": secret, "client_id": 30100}
+
+        return self._post(path, data)
+
+    def logged_in(self):
+        return self.session.authenticated
+
+    def get_user(self):
+        return self._get("/user")
+
+    def search(self, term: str):
+        path = f"/pages/search-page-results?search_term={term}"
+        raw_results = self._get(path, add_picnic_headers=True)
+        return _extract_search_results(raw_results)
+
+    def get_lists(self, list_id: str = None):
+        if list_id:
+            path = "/lists/" + list_id
+        else:
+            path = "/lists"
+        return self._get(path)
+
+    def get_sublist(self, list_id: str, sublist_id: str) -> list:
+        """Get sublist.
+
+        Args:
+            list_id (str): ID of list, corresponding to requested sublist.
+            sublist_id (str): ID of sublist.
+
+        Returns:
+            list: Sublist result.
+        """
+        return self._get(f"/lists/{list_id}?sublist={sublist_id}")
+
+    def get_cart(self):
+        return self._get("/cart")
+
+    def get_article(self, article_id: str, add_category_name=False):
+        path = "/articles/" + article_id
+        article = self._get(path)
+        if add_category_name and "category_link" in article:
+            self.initialize_high_level_categories()
+            article.update(
+                category_name=_get_category_name(article['category_link'], self.high_level_categories)
+            )
+        return article
+
+    def get_article_category(self, article_id: str):
+        path = "/articles/" + article_id + "/category"
+        return self._get(path)
+
+    def add_product(self, product_id: str, count: int = 1):
+        data = {"product_id": product_id, "count": count}
+        return self._post("/cart/add_product", data)
+
+    def remove_product(self, product_id: str, count: int = 1):
+        data = {"product_id": product_id, "count": count}
+        return self._post("/cart/remove_product", data)
+
+    def clear_cart(self):
+        return self._post("/cart/clear")
+
+    def get_delivery_slots(self):
+        return self._get("/cart/delivery_slots")
+
+    def get_delivery(self, delivery_id: str):
+        path = "/deliveries/" + delivery_id
+        return self._get(path)
+
+    def get_delivery_scenario(self, delivery_id: str):
+        path = "/deliveries/" + delivery_id + "/scenario"
+        return self._get(path, add_picnic_headers=True)
+
+    def get_delivery_position(self, delivery_id: str):
+        path = "/deliveries/" + delivery_id + "/position"
+        return self._get(path, add_picnic_headers=True)
+
+    def get_deliveries(self, summary: bool = False, data=None):
+        data = [] if data is None else data
+        if summary:
+            return self._post("/deliveries/summary", data=data)
+        return self._post("/deliveries", data=data)
+
+    def get_current_deliveries(self):
+        return self.get_deliveries(data=["CURRENT"])
+
+    def get_categories(self, depth: int = 0):
+        return self._get(f"/my_store?depth={depth}")["catalog"]
+
+    def print_categories(self, depth: int = 0):
+        tree = "\n".join(_tree_generator(self.get_categories(depth=depth)))
+        print(tree)
+
+
+__all__ = ["PicnicAPI"]
