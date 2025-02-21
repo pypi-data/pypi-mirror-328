5faf28453f7ee80fd30e6882d43fc78676630102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:30:02 +0000

    Add package sws_api_client/1.4.5/sws_api_client-1.4.5-py3-none-any.whl

---
+++ packages/sws_api_client/sws_api_client-1.4.5-py3-none-any.whl/sws_api_client/__init__.py
@@ -0,0 +1,16 @@
+from .sws_api_client import SwsApiClient
+from .discover import Discover
+from .datasets import Datasets
+from .datatable import Datatables
+from .tasks import (
+    TaskManager,
+    PluginPayload,
+    TaskDataset,
+    TaskInfo,
+    TaskResponse
+)
+from .tags import Tags
+from .plugins import Plugins
+from .codelist import Codelists
+
+__version__ = "1.0.12-beta.1"
---
+++ packages/sws_api_client/sws_api_client-1.4.5-py3-none-any.whl/sws_api_client/generic_models.py
@@ -0,0 +1,16 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class Multilanguage(BaseModel):
+    en: str
+    fr: Optional[str] = None
+    es: Optional[str] = None
+    ar: Optional[str] = None
+    ru: Optional[str] = None
+    zh: Optional[str] = None
+
+class Code(BaseModel, extra="allow"):
+    id: str
+    label: Multilanguage
+    children: Optional[List[str]] = []
