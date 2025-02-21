2362ee6f297ba9f3eecc0c4edef299bbe900f915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:19:24 +0000

    Add package git-authorship/0.3.0/git_authorship-0.3.0-py3-none-any.whl

---
+++ packages/git-authorship/git_authorship-0.3.0-py3-none-any.whl/git_authorship/_types.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2024 Joseph Hale
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from pathlib import Path
+from typing import Dict
+from typing import TypedDict
+
+from typing_extensions import NotRequired
+
+
+FilePath = Path
+Author = str
+LineCount = int
+License = str
+
+
+class AuthorshipInfo(TypedDict):
+    lines: LineCount
+    license: NotRequired[License]
+
+
+Authorship = Dict[Author, AuthorshipInfo]
+RepoAuthorship = Dict[FilePath, Authorship]
+
+__all__ = ["FilePath", "Author", "LineCount", "Authorship", "RepoAuthorship"]
