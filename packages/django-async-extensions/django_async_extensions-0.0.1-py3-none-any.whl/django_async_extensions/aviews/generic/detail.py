1d4c17f2a9cf6ec089bac9d6c96f90e7ed38e7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:07:28 +0000

    Add package django-async-extensions/0.0.1/django_async_extensions-0.0.1-py3-none-any.whl

---
+++ packages/django-async-extensions/django_async_extensions-0.0.1-py3-none-any.whl/django_async_extensions/aviews/generic/detail.py
@@ -0,0 +1,129 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+from django.http import Http404
+from django.views.generic.detail import (
+    SingleObjectTemplateResponseMixin,
+)
+from django.utils.translation import gettext as _
+
+from django_async_extensions.aviews.generic.base import AsyncView, AsyncContextMixin
+
+
+class AsyncSingleObjectMixin(AsyncContextMixin):
+    """
+    Provide the ability to retrieve a single object for further manipulation.
+    """
+
+    model = None
+    queryset = None
+    slug_field = "slug"
+    context_object_name = None
+    slug_url_kwarg = "slug"
+    pk_url_kwarg = "pk"
+    query_pk_and_slug = False
+
+    async def get_object(self, queryset=None):
+        """
+        Return the object the view is displaying.
+
+        Require `self.queryset` and a `pk` or `slug` argument in the URLconf.
+        Subclasses can override this to return any object.
+        """
+        # Use a custom queryset if provided; this is required for subclasses
+        # like DateDetailView
+        if queryset is None:
+            queryset = await self.get_queryset()
+
+        # Next, try looking up by primary key.
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+
+        # Next, try looking up by slug.
+        if slug is not None and (pk is None or self.query_pk_and_slug):
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})
+
+        # If none of those are defined, it's an error.
+        if pk is None and slug is None:
+            raise AttributeError(
+                "Generic detail view %s must be called with either an object "
+                "pk or a slug in the URLconf." % self.__class__.__name__
+            )
+
+        try:
+            # Get the single item from the filtered queryset
+            obj = await queryset.aget()
+        except queryset.model.DoesNotExist:
+            raise Http404(
+                _("No %(verbose_name)s found matching the query")
+                % {"verbose_name": queryset.model._meta.verbose_name}
+            )
+        return obj
+
+    async def get_queryset(self):
+        """
+        Return the `QuerySet` that will be used to look up the object.
+
+        This method is called by the default implementation of get_object() and
+        may not be called if get_object() is overridden.
+        """
+        # this is implemented as an async function so user can overwrite it
+        # and `await` on an ORM call without needing to rewrite `get_object`
+        if self.queryset is None:
+            if self.model:
+                return self.model._default_manager.all()
+            else:
+                raise ImproperlyConfigured(
+                    "%(cls)s is missing a QuerySet. Define "
+                    "%(cls)s.model, %(cls)s.queryset, or override "
+                    "%(cls)s.get_queryset()." % {"cls": self.__class__.__name__}
+                )
+        return self.queryset.all()
+
+    def get_slug_field(self):
+        """Get the name of a slug field to be used to look up by slug."""
+        return self.slug_field
+
+    def get_context_object_name(self, obj):
+        """Get the name to use for the object."""
+        if self.context_object_name:
+            return self.context_object_name
+        elif isinstance(obj, models.Model):
+            return obj._meta.model_name
+        else:
+            return None
+
+    async def get_context_data(self, **kwargs):
+        """Insert the single object into the context dict."""
+        context = {}
+        if self.object:
+            context["object"] = self.object
+            context_object_name = self.get_context_object_name(self.object)
+            if context_object_name:
+                context[context_object_name] = self.object
+        context.update(kwargs)
+        return await super().get_context_data(**context)
+
+
+class AsyncBaseDetailView(AsyncSingleObjectMixin, AsyncView):
+    """
+    Base view for displaying a single object.
+
+    This requires subclassing to provide a response mixin.
+    """
+
+    async def get(self, request, *args, **kwargs):
+        self.object = await self.get_object()
+        context = await self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class AsyncDetailView(SingleObjectTemplateResponseMixin, AsyncBaseDetailView):
+    """
+    Render a "detail" view of an object.
+
+    By default this is a model instance looked up from `self.queryset`, but the
+    view will support display of *any* object by overriding `self.get_object()`.
+    """
