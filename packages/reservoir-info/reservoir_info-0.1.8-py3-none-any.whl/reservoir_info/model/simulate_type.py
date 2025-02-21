74eb00bc29eda21fc95b32db1cde77ac04184374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:18:50 +0000

    Add package reservoir-info/0.1.8/reservoir_info-0.1.8-py3-none-any.whl

---
+++ packages/reservoir-info/reservoir_info-0.1.8-py3-none-any.whl/reservoir_info/model/simulate_type.py
@@ -0,0 +1,10 @@
+
+from mag_tools.model.base_enum import BaseEnum
+
+class SimulateType(BaseEnum):
+    """
+    模型模拟方式枚举
+    """
+    BLACKOIL = ('BLK', '黑油模型')  # 黑油模型
+    COMP = ('Comp', '组分模型')  # 组分模型
+    FRAC = ('Frac', '裂缝模型')  # 黑油模型
