13aaba083e5e26a1aeb823c4a5ebcafc3d75ccb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:41:16 +0000

    Add package dStats/0.1.0/dstats-0.1.0.tar.gz

---
+++ packages/dStats/dstats-0.1.0.tar.gz/dstats-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dStats",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A real-time web-based monitoring tool that provides performance stats for Docker containers and visualizes their network connectivity graph",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Abdullah Al Arif",
+    author_email="arifcse21@gmail.com",
+    url="https://github.com/Arifcse21/dStats",  # Optional: URL to your repository
+    install_requires=[
+        "Django>=5.1.4",
+        "graphviz>=0.20.3",
+        "daphne>=4.1.2",
+        "requests>=2.32.3",
+        "black>=24.10.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12.3",
+)
