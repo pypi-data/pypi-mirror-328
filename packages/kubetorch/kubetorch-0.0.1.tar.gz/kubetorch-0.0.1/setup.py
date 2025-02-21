5e074180ed666c7d1345a55014599006dc953b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:29:55 +0000

    Add package kubetorch/0.0.1/kubetorch-0.0.1.tar.gz

---
+++ packages/kubetorch/kubetorch-0.0.1.tar.gz/kubetorch-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kubetorch",
+    version="0.0.1",
+    author="Name",
+    description="KubeTorch",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
