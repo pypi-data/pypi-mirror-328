d2b9e0ac8262f4281da96c89b47124558551ac2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:18:36 +0000

    Add package workbench/0.8.106/workbench-0.8.106.tar.gz

---
+++ packages/workbench/workbench-0.8.106.tar.gz/workbench-0.8.106/tests/create_test_artifacts/delete_test_artifacts.py
@@ -0,0 +1,67 @@
+"""This Script Deletes the Workbench Artifacts used for the tests"""
+
+import time
+from workbench.api.data_source import DataSource
+from workbench.api.feature_set import FeatureSet
+from workbench.api.model import Model
+from workbench.api.endpoint import Endpoint
+
+
+if __name__ == "__main__":
+
+    # Delete the test_data Artifacts
+    DataSource.managed_delete("test_data")
+    DataSource.managed_delete("abc")
+    DataSource.managed_delete("abc_2")
+    FeatureSet.managed_delete("test_features")
+    Endpoint.managed_delete("abc")
+    Endpoint.managed_delete("abc-2")
+
+    # Delete the abalone_data Artifacts
+    DataSource.managed_delete("abalone_data")
+    DataSource.managed_delete("abalone_data_copy")
+    FeatureSet.managed_delete("abalone_features")
+    Model.managed_delete("abalone-regression")
+    Endpoint.managed_delete("abalone-regression-end")
+    Endpoint.managed_delete("abalone-regression-end-rt")
+
+    # Delete abalone_classification Artifacts
+    FeatureSet.managed_delete("abalone_classification")
+    Model.managed_delete("abalone-classification")
+    Endpoint.managed_delete("abalone-classification-end")
+
+    # Delete the wine_data Artifacts
+    DataSource.managed_delete("wine_data")
+    FeatureSet.managed_delete("wine_features")
+    Model.managed_delete("wine-classification")
+    Endpoint.managed_delete("wine-classification-end")
+
+    # AQSol Artifacts
+    DataSource.managed_delete("aqsol_data")
+    FeatureSet.managed_delete("aqsol_features")
+    Model.managed_delete("aqsol-regression")
+    Endpoint.managed_delete("aqsol-regression-end")
+    FeatureSet.managed_delete("aqsol_mol_descriptors")
+    Model.managed_delete("aqsol-mol-regression")
+    Endpoint.managed_delete("aqsol-mol-regression-end")
+
+    # AQSol Artifacts (Classification)
+    Model.managed_delete("aqsol-mol-class")
+    Endpoint.managed_delete("aqsol-mol-class-end")
+
+    # Quantile Regressors
+    Model.managed_delete("abalone-quantile-reg")
+    Endpoint.managed_delete("abalone-qr-end")
+    Model.managed_delete("aqsol-quantile-reg")
+    Endpoint.managed_delete("aqsol-qr-end")
+
+    # Scikit Learn Models
+    Model.managed_delete("abalone-knn-reg")
+    Endpoint.managed_delete("abalone-knn-end")
+    Model.managed_delete("abalone-clusters")
+    Endpoint.managed_delete("abalone-clusters-end")
+    Model.managed_delete("wine-rfc-class")
+    Model.managed_delete("aqsol-knn-reg")
+
+    time.sleep(5)
+    print("All test artifacts should now be deleted!")
---
+++ packages/workbench/workbench-0.8.106.tar.gz/workbench-0.8.106/tests/web_components/graph_plot_test.py
@@ -0,0 +1,27 @@
+"""Tests for graph_plot web component"""
+
+# Workbench Imports
+from workbench.api.graph_store import GraphStore
+from workbench.web_interface.components.plugins.graph_plot import GraphPlot
+
+
+def test_graph_plot():
+    """Test the GraphPlot class"""
+
+    # Instantiate a Graph
+    graph = GraphStore().get("test/karate_club")
+
+    # Instantiate the GraphPlot class
+    graph_plot = GraphPlot()
+
+    # Generate the figure
+    properties = graph_plot.update_properties(graph, label="club")
+
+    # The first property should be the figure
+    figure = properties[0]
+    figure.show()
+
+
+if __name__ == "__main__":
+    # Run the tests
+    test_graph_plot()
