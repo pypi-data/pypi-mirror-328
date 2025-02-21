0980f42746cc2af58db2d8a302ddb47a591a5bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 20:18:35 +0000

    Add package recursion2loop/0.2.2/recursion2loop-0.2.2.tar.gz

---
+++ packages/recursion2loop/recursion2loop-0.2.2.tar.gz/recursion2loop-0.2.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="recursion2loop",
+    version="0.2.2",
+    packages=find_packages(include=["recursion2loop", "recursion2loop.*"]),
+    description="conversion of recursive functions to iterative implementations. Optimize your Python code by transforming stack-heavy recursion into efficient loops.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Adil Köken',
+    author_email='mail@adilkoken.com',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    url='https://github.com/AdilKoken/recursion2loop',
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
