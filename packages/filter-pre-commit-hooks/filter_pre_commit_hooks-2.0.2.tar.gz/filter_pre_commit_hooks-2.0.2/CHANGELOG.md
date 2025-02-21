095756b5e21b2a3be069d9fe6e6bba9313cd6880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:10 +0000

    Add package filter-pre-commit-hooks/2.0.2/filter_pre_commit_hooks-2.0.2.tar.gz

---
+++ packages/filter-pre-commit-hooks/filter_pre_commit_hooks-2.0.2.tar.gz/filter_pre_commit_hooks-2.0.2/CHANGELOG.md
@@ -0,0 +1,54 @@
+# Changelog
+
+All notable changes to this project are documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0),
+and adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0).
+
+## Unreleased
+
+Nothing.
+
+## [2.0.2](https://github.com/trallnag/filter-pre-commit-hooks/compare/v2.0.1...v2.0.2) / 2025-02-19
+
+### Changed
+
+- Program is now also available as a package on PyPI. Installed from PyPI, the
+  program can be executed with `filter-pre-commit-hooks`.
+
+## [2.0.1](https://github.com/trallnag/filter-pre-commit-hooks/compare/v2.0.0...v2.0.1) / 2025-02-14
+
+### Changed
+
+- Added info about uv and shebang to script help message.
+
+## [2.0.0](https://github.com/trallnag/filter-pre-commit-hooks/compare/v1.1.2...v2.0.0) / 2025-02-11
+
+### Changed
+
+- **BREAKING:** Complete rewrite of the script. It is best to check out the
+  README and the help message of the script for more information. The script now
+  comes with inline script metadata and with that dependencies on a third-party
+  package.
+
+## [1.1.2](https://github.com/trallnag/filter-pre-commit-hooks/compare/v1.1.1...v1.1.2) / 2025-01-22
+
+### Changed
+
+- Simplified code based on feedback from Ruff.
+
+## [1.1.1](https://github.com/trallnag/filter-pre-commit-hooks/compare/v1.1.0...v1.1.1) / 2025-01-05
+
+### Fixed
+
+- Switched minium required version of Python from 3.10 to 3.9.
+
+## [1.1.0](https://github.com/trallnag/filter-pre-commit-hooks/compare/v1.0.0...v1.1.0) / 2025-01-02
+
+### Added
+
+- Ensure that at least one filter is provided to script (in any mode).
+
+## [1.0.0](https://github.com/trallnag/filter-pre-commit-hooks/compare/6014a859fec1b8842ea1dc573e096609e61ceecd...v1.0.0) / 2025-01-01
+
+Initial release.
---
+++ packages/filter-pre-commit-hooks/filter_pre_commit_hooks-2.0.2.tar.gz/filter_pre_commit_hooks-2.0.2/tests/test_integration.py
@@ -0,0 +1,110 @@
+"""
+Integration tests for the `filter_pre_commit_hooks` module.
+"""
+
+import click
+import pytest
+from click.testing import CliRunner
+
+from src.filter_pre_commit_hooks import Command, filter_pre_commit_hooks
+
+
+def test_cli_help() -> None:
+    """Test the CLI help message."""
+
+    result = CliRunner().invoke(filter_pre_commit_hooks, ["--help"])
+
+    assert result.exit_code == 0
+
+
+def test_command() -> None:
+    """
+    Test that the custom command handles missing epilog.
+    """
+
+    @click.command(
+        cls=Command,
+    )
+    def command() -> None:
+        pass
+
+    CliRunner().invoke(command, ["--help"])
+
+
+@pytest.mark.parametrize(
+    ("extra_args", "expected"),
+    [
+        pytest.param(
+            id="default",
+            *[
+                ["check", "task"],
+                ", ".join(  # noqa: FLY002
+                    [
+                        "end-of-file-fixer",
+                        "fix-byte-order-marker",
+                        "shellcheck",
+                        "shfmt",
+                        "yamlfmt",
+                    ]
+                )
+                + "\n",
+            ],
+        ),
+        pytest.param(
+            id="no_invert",
+            *[
+                ["--orient=no-invert", "check", "task"],
+                ", ".join(  # noqa: FLY002
+                    [
+                        "check-added-large-files",
+                        "check-merge-conflict",
+                    ]
+                )
+                + "\n",
+            ],
+        ),
+        pytest.param(
+            id="id_all_of",
+            *[
+                ["--target=id", "check-added-large-files", "shfmt", "shfmt"],
+                ", ".join(  # noqa: FLY002
+                    [
+                        "check-added-large-files",
+                        "check-merge-conflict",
+                        "end-of-file-fixer",
+                        "fix-byte-order-marker",
+                        "shellcheck",
+                        "shfmt",
+                        "yamlfmt",
+                    ]
+                )
+                + "\n",
+            ],
+        ),
+        pytest.param(
+            id="id_realistic",
+            *[
+                ["--target=id", "--mode=any-of", "shfmt", "shellcheck", "yamlfmt"],
+                ", ".join(  # noqa: FLY002
+                    [
+                        "check-added-large-files",
+                        "check-merge-conflict",
+                        "end-of-file-fixer",
+                        "fix-byte-order-marker",
+                    ]
+                )
+                + "\n",
+            ],
+        ),
+    ],
+)
+def test_cli(extra_args: list[str], expected: str) -> None:
+    """Test the CLI."""
+
+    result = CliRunner().invoke(
+        filter_pre_commit_hooks,
+        ["--config", "tests/resources/pre-commit-config.yaml", *extra_args],
+    )
+
+    assert result.output == expected
+    assert result.exit_code == 0
