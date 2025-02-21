1bab1ea0d600d9a27591892473c3b080c9bfa220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:12 +0000

    Add package pipatch/0.1.0/pipatch-0.1.0-py3-none-any.whl

---
+++ packages/pipatch/pipatch-0.1.0-py3-none-any.whl/pipatch/__init__.py
@@ -0,0 +1,13 @@
+"""pipatch
+
+Minimal patching tool for Python packages.
+"""
+
+__title__ = "pipatch"
+__description__ = "Minimal patching tool for Python packages."
+__version__ = "0.1.0"
+__license__ = "MIT"
+__author__ = "Teddy van Jerry (Wuqiong Zhao)"
+__author_email__ = "me@teddy-van-jerry.org"
+
+from .pipatch import Pipatch
