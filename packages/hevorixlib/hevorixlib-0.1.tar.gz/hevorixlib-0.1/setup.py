356a3b86d669c44311a53e8dcc8c585011f5850b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:41:19 +0000

    Add package hevorixlib/0.1/hevorixlib-0.1.tar.gz

---
+++ packages/hevorixlib/hevorixlib-0.1.tar.gz/hevorixlib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="hevorixlib",  # O'zingizga xos nom tanlang
+    version="0.1",
+    py_modules=["library"],  # Kutubxonadagi asosiy modul
+    author="Hevorix",
+    description="Oddiy kutubxona",
+)
