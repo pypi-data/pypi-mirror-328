72535de4e080c6e4422227979818301ef6188ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:07 +0000

    Add package maicos/0.9/maicos-0.9-cp311-cp311-win_amd64.whl

---
+++ packages/maicos/maicos-0.9-cp311-cp311-win_amd64.whl/maicos/lib/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2024 Authors and contributors
+# (see the AUTHORS.rst file for the full list of names)
+#
+# Released under the GNU Public Licence, v3 or any higher version
+# SPDX-License-Identifier: GPL-3.0-or-later
+r"""Library modules of MAICoS.
+
+This library contains additional modules, like general and mathematical helper
+functions, which are used in the other MAICoS modules.
+"""
+
+__all__ = [
+    "util",
+    "math",
+    "weights",
+]
