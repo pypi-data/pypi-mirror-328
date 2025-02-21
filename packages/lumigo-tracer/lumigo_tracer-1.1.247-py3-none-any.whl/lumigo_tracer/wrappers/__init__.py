30fdd37c554ea1ddb51570f057efb46b650f79e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:07:41 +0000

    Add package lumigo-tracer/1.1.247/lumigo_tracer-1.1.247-py3-none-any.whl

---
+++ packages/lumigo-tracer/lumigo_tracer-1.1.247-py3-none-any.whl/lumigo_tracer/wrappers/__init__.py
@@ -0,0 +1,23 @@
+from ..lumigo_utils import is_aws_environment
+from .aiohttp.aiohttp_wrapper import wrap_aiohttp
+from .http.sync_http_wrappers import wrap_http_calls
+from .pymongo.pymongo_wrapper import wrap_pymongo
+from .redis.redis_wrapper import wrap_redis
+from .sql.sqlalchemy_wrapper import wrap_sqlalchemy
+
+already_wrapped = False
+
+
+def wrap(force: bool = False) -> None:
+    if not is_aws_environment():
+        return
+    global already_wrapped
+    if not already_wrapped:
+        # Never wrap http calls twice - it will create duplicate body
+        wrap_http_calls()
+    if force or not already_wrapped:
+        wrap_pymongo()
+        wrap_redis()
+        wrap_sqlalchemy()
+        wrap_aiohttp()
+        already_wrapped = True
