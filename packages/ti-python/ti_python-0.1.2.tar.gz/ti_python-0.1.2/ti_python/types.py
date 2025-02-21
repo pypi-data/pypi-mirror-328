85fbec310c096dbac57739a5a4b518a03771632a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:19:17 +0000

    Add package ti-python/0.1.2/ti_python-0.1.2.tar.gz

---
+++ packages/ti-python/ti_python-0.1.2.tar.gz/ti_python-0.1.2/ti_python/types.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+import dataclasses
+
+
+# ---- Types
+@dataclasses.dataclass
+class Type:
+    value: str
+
+
+class Number(Type):
+    pass
+
+
+class Str(Type):
+    pass
+
+
+class Var(Type):
+    pass
+
+
+class Name(Type):
+    pass
+
+
+# --- Calls, Assigns, and Control Flow
+@dataclasses.dataclass
+class Call:
+    value: str
+    args: list[Type]
+
+
+@dataclasses.dataclass
+class Assign:
+    target: Type
+    value: Type
+
+
+@dataclasses.dataclass
+class Block:
+    head: Op | Type
+    body: list
+
+
+class If(Block):
+    pass
+
+class While(Block):
+    pass
+
+# ---- Operations
+@dataclasses.dataclass
+class Op:
+    left: Type
+    right: Type
+
+
+class Add(Op):
+    pass
+
+
+class Sub(Op):
+    pass
+
+
+class Mul(Op):
+    pass
+
+
+class Div(Op):
+    pass
+
+
+class Comp(Op):
+    pass
+
+
+class LessThan(Comp):
+    pass
+
+
+class GreaterThan(Comp):
+    pass
+
+class EqualTo(Comp):
+    pass
+
+class NotEqualTo(Comp):
+    pass
+
+class LessThanEqualTo(Comp):
+    pass
+
+class GreaterThanEqualTo(Comp):
+    pass
