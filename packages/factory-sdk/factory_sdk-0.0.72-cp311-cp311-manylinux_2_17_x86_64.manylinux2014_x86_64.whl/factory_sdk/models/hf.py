9f6c39745f959b71f6bc5694c9b2bc5ae4a2d8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 20:07:36 +0000

    Add package factory-sdk/0.0.72/factory_sdk-0.0.72-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/factory-sdk/factory_sdk-0.0.72-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/factory_sdk/models/hf.py
@@ -0,0 +1,62 @@
+from huggingface_hub import HfApi
+from rich import print
+from rich.progress import Progress
+
+def fingerprint(name, token):
+    print("[bold yellow]🔍 Retrieving dataset fingerprint from HuggingFace Hub...[/bold yellow]")
+    api = HfApi(token=token)
+    info = api.model_info(name)
+    print(f"[bold yellow]✔ Fingerprint (SHA) retrieved: {info.sha}[/bold yellow]")
+    return info.sha
+
+
+FILE_TYPES = [
+    ".py",
+    ".md",
+    "LICENSE",
+    ".json",
+    ".yaml",
+    ".yml",
+    ".txt",
+    ".safetensors",
+    ".model",
+    ".bin",
+]
+
+SKIP_FILES = [
+    ("mistralai/Mistral-7B-Instruct-v0.3","consolidated.safetensors")
+]
+
+
+def load(name, token, directory):
+    api = HfApi(token=token)
+    info = api.repo_info(name, repo_type="model")
+    siblings = info.siblings
+
+    # Filter siblings by file type
+    siblings = [
+        sibling
+        for sibling in siblings
+        if any(file_type in sibling.rfilename for file_type in FILE_TYPES)
+    ]
+
+    # filter skip files
+    siblings = [
+        sibling
+        for sibling in siblings
+        if not any(skip_file[1] in sibling.rfilename and skip_file[0] in name for skip_file in SKIP_FILES)
+    ]
+
+    # Download files sequentially with a Rich progress bar
+    with Progress() as progress:
+        task = progress.add_task("[green]Downloading files...", total=len(siblings))
+        for sibling in siblings:
+            api.hf_hub_download(
+                repo_id=name,
+                filename=sibling.rfilename,
+                repo_type="model",
+                token=token,
+                local_dir=directory,
+            )
+            progress.advance(task)
+    return info.sha
