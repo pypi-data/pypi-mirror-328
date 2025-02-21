1c80f072f79099ea3497c361f8d2fe57ff6d755f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:04 +0000

    Add package mgraph-db/1.1.2/mgraph_db-1.1.2-py3-none-any.whl

---
+++ packages/mgraph-db/mgraph_db-1.1.2-py3-none-any.whl/mgraph_db/mgraph/schemas/Schema__MGraph__Edge__Config.py
@@ -0,0 +1,5 @@
+from osbot_utils.helpers.Obj_Id                     import Obj_Id
+from osbot_utils.type_safe.Type_Safe                import Type_Safe
+
+class Schema__MGraph__Edge__Config(Type_Safe):
+    edge_id       : Obj_Id                              # todo: move this from here and into the main Schema__MGraph__Edge
