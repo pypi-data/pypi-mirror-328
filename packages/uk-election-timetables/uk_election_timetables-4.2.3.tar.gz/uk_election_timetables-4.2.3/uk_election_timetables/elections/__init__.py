00b0bc8518abec1ed7085dcf4f9eb2ba8cac1cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:41 +0000

    Add package uk-election-timetables/4.2.3/uk_election_timetables-4.2.3.tar.gz

---
+++ packages/uk-election-timetables/uk_election_timetables-4.2.3.tar.gz/uk_election_timetables-4.2.3/uk_election_timetables/elections/__init__.py
@@ -0,0 +1,31 @@
+from uk_election_timetables.elections.city_of_london_local import (
+    CityOfLondonLocalElection,
+)
+from uk_election_timetables.elections.greater_london_assembly import (
+    GreaterLondonAssemblyElection,
+)
+from uk_election_timetables.elections.local import LocalElection
+from uk_election_timetables.elections.mayor import MayoralElection
+from uk_election_timetables.elections.northern_ireland_assembly import (
+    NorthernIrelandAssemblyElection,
+)
+from uk_election_timetables.elections.police_and_crime_commissioner import (
+    PoliceAndCrimeCommissionerElection,
+)
+from uk_election_timetables.elections.scottish_parliament import (
+    ScottishParliamentElection,
+)
+from uk_election_timetables.elections.senedd_cymru import SeneddCymruElection
+from uk_election_timetables.elections.uk_parliament import UKParliamentElection
+
+__ALL__ = (
+    ScottishParliamentElection,
+    SeneddCymruElection,
+    GreaterLondonAssemblyElection,
+    NorthernIrelandAssemblyElection,
+    LocalElection,
+    CityOfLondonLocalElection,
+    UKParliamentElection,
+    PoliceAndCrimeCommissionerElection,
+    MayoralElection,
+)
