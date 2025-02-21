afbebe96df70b5c69f1a07aa45a66f1813977b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:18:17 +0000

    Add package geoproject510/0.0.6/geoproject510-0.0.6.tar.gz

---
+++ packages/geoproject510/geoproject510-0.0.6.tar.gz/geoproject510-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="geoproject510",
+    version="0.0.6",
+    description="A package for Geog 510",
+    author="Casey Shea",
+    author_email="shea96@gmail.com",
+    packages=find_packages(),  # Automatically find packages in your directory
+    install_requires=[],  # Add your dependencies here
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
