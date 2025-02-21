b921d46f27916a87a9fc4bc85b5d0c806ace2174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:07:38 +0000

    Add package hdl-registers/7.0.4/hdl_registers-7.0.4.tar.gz

---
+++ packages/hdl-registers/hdl_registers-7.0.4.tar.gz/hdl_registers-7.0.4/hdl_registers/generator/html/page.py
@@ -0,0 +1,174 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl-registers project, an HDL register generator fast enough to run
+# in real time.
+# https://hdl-registers.com
+# https://github.com/hdl-registers/hdl-registers
+# --------------------------------------------------------------------------------------------------
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from hdl_registers.register_modes import REGISTER_MODES
+
+from .constant_table import HtmlConstantTableGenerator
+from .html_generator_common import HtmlGeneratorCommon
+from .register_table import HtmlRegisterTableGenerator
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+class HtmlPageGenerator(HtmlGeneratorCommon):
+    """
+    Generate a HTML page with register and constant information.
+    See the :ref:`generator_html` article for usage details.
+    """
+
+    __version__ = "1.0.0"
+
+    SHORT_DESCRIPTION = "HTML page"
+
+    @property
+    def output_file(self) -> Path:
+        """
+        Result will be placed in this file.
+        """
+        return self.output_folder / f"{self.name}_regs.html"
+
+    def get_code(
+        self,
+        **kwargs: Any,  # noqa: ANN401, ARG002
+    ) -> str:
+        """
+        Get a complete HTML page with register and constant information.
+        """
+        title = f"Documentation of {self.name} registers"
+        html = f"""\
+<!DOCTYPE html>
+<html>
+<head>
+  <title>{title}</title>
+    <!-- Include the style both inline and as a link to a separate CSS file. -->
+    <!-- Some tools, e.g. Jenkins, will not render with an inline stylesheet. -->
+    <!-- For other tools, e.g. page inclusion in sphinx, the style must be in the file. -->
+    <link rel="stylesheet" href="regs_style.css">
+    <style>
+      {self.get_page_style()}
+    </style>
+</head>
+<body>
+  <h1>{title}</h1>
+  <p>This document is a specification for the register interface of the FPGA module \
+<b>{self.name}</b>.</p>
+  <p>{" ".join(self.generated_source_info)}</p>
+  <h2>Register modes</h2>
+  <p>The following register modes are available.</p>
+{self._get_mode_descriptions()}
+"""
+
+        html += "  <h2>Registers</h2>\n"
+        if self.register_list.register_objects:
+            register_table_generator = HtmlRegisterTableGenerator(
+                register_list=self.register_list, output_folder=self.output_folder
+            )
+            html += f"""
+  <p>The following registers make up the register list.</p>
+{register_table_generator.get_code()}
+"""
+        else:
+            html += "  <p>This module does not have any registers.</p>"
+
+        html += "  <h2>Constants</h2>\n"
+        if self.register_list.constants:
+            constant_table_generator = HtmlConstantTableGenerator(
+                register_list=self.register_list, output_folder=self.output_folder
+            )
+            html += f"""
+  <p>The following constants are part of the register interface.</p>
+{constant_table_generator.get_code()}"""
+        else:
+            html += "  <p>This module does not have any constants.</p>"
+
+        html += """
+</body>
+</html>"""
+
+        return html
+
+    @staticmethod
+    def get_page_style(
+        table_style: str | None = None, font_style: str | None = None, extra_style: str = ""
+    ) -> str:
+        """
+        Get a CSS style for the register pages. Shall be saved to a file called ``regs_style.css``.
+
+        Return:
+            str: CSS code.
+        """
+        if font_style is None:
+            font_style = """
+html * {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+}"""
+
+        if table_style is None:
+            table_style = """
+table {
+  border-collapse: collapse;
+}
+td, th {
+  border-width: 1px;
+  border-style: solid;
+  border-color: #ddd;
+  padding: 8px;
+}
+td.array_header {
+  border-top-width: 10px;
+  border-top-color: #4cacaf;
+}
+td.array_footer {
+  border-bottom-width: 10px;
+  border-bottom-color: #4cacaf;
+}
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}"""
+
+        return f"""
+{font_style}
+{table_style}
+{extra_style}"""
+
+    @staticmethod
+    def _get_mode_descriptions() -> str:
+        html = """
+<table>
+<thead>
+  <tr>
+    <th>Mode</th>
+    <th>Description</th>
+  </tr>
+</thead>
+<tbody>"""
+
+        for mode in REGISTER_MODES.values():
+            html += f"""
+<tr>
+  <td>{mode.name}</td>
+  <td>{mode.description}</td>
+</tr>
+"""
+        html += """
+</tbody>
+</table>"""
+        return html
---
+++ packages/hdl-registers/hdl_registers-7.0.4.tar.gz/hdl_registers-7.0.4/hdl_registers/generator/html/register_table.py
@@ -0,0 +1,221 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl-registers project, an HDL register generator fast enough to run
+# in real time.
+# https://hdl-registers.com
+# https://github.com/hdl-registers/hdl-registers
+# --------------------------------------------------------------------------------------------------
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from hdl_registers.field.bit import Bit
+from hdl_registers.field.bit_vector import BitVector
+from hdl_registers.field.enumeration import Enumeration
+from hdl_registers.field.integer import Integer
+from hdl_registers.register import Register
+
+from .html_generator_common import HtmlGeneratorCommon
+from .html_translator import HtmlTranslator
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from hdl_registers.field.register_field import RegisterField
+    from hdl_registers.register_array import RegisterArray
+    from hdl_registers.register_list import RegisterList
+
+
+class HtmlRegisterTableGenerator(HtmlGeneratorCommon):
+    """
+    Generate HTML code with register information in a table.
+    See the :ref:`generator_html` article for usage details.
+    """
+
+    __version__ = "1.0.0"
+
+    SHORT_DESCRIPTION = "HTML register table"
+
+    @property
+    def output_file(self) -> Path:
+        """
+        Result will be placed in this file.
+        """
+        return self.output_folder / f"{self.name}_register_table.html"
+
+    def __init__(self, register_list: RegisterList, output_folder: Path) -> None:
+        super().__init__(register_list=register_list, output_folder=output_folder)
+
+        self._html_translator = HtmlTranslator()
+
+    def get_code(
+        self,
+        **kwargs: Any,  # noqa: ANN401, ARG002
+    ) -> str:
+        if not self.register_list.register_objects:
+            return ""
+
+        html = """\
+<table>
+<thead>
+  <tr>
+    <th>Name</th>
+    <th>Index</th>
+    <th>Address</th>
+    <th>Mode</th>
+    <th>Default value</th>
+    <th>Description</th>
+  </tr>
+</thead>
+<tbody>"""
+
+        for register_object in self.iterate_register_objects():
+            if isinstance(register_object, Register):
+                html += self._annotate_register(register_object)
+            else:
+                html += self._annotate_register_array(register_object)
+
+        html += """
+</tbody>
+</table>"""
+
+        return html
+
+    @staticmethod
+    def _to_hex_string(value: int, num_nibbles: int = 4) -> str:
+        """
+        Convert an integer value to a hexadecimal string. E.g. "0x1000".
+        """
+        if value < 0:
+            return "-"
+
+        formatting_string = f"0x{{:0{num_nibbles}X}}"
+        return formatting_string.format(value)
+
+    def _annotate_register_array(self, register_object: RegisterArray) -> str:
+        description = self._html_translator.translate(register_object.description)
+        html = f"""
+  <tr>
+    <td class="array_header" colspan=5>
+      Register array <strong>{register_object.name}</strong>, \
+repeated {register_object.length} times.
+      Iterator <i>i &isin; [0, {register_object.length - 1}].</i>
+    </td>
+    <td class="array_header">{description}</td>
+  </tr>"""
+        array_index_increment = len(register_object.registers)
+        for register in register_object.registers:
+            register_index = register_object.base_index + register.index
+            html += self._annotate_register(register, register_index, array_index_increment)
+
+        html += f"""
+  <tr>
+    <td colspan="6" class="array_footer">
+      End register array <strong>{register_object.name}.</strong>
+    </td>
+  </tr>"""
+        return html
+
+    def _annotate_register(
+        self,
+        register: Register,
+        register_array_index: int | None = None,
+        array_index_increment: int | None = None,
+    ) -> str:
+        if register_array_index is None:
+            address_readable = self._to_hex_string(register.address)
+            index = str(register.address // 4)
+        else:
+            register_address = self._to_hex_string(4 * register_array_index)
+            address_increment = self._to_hex_string(4 * array_index_increment)
+            address_readable = f"{register_address} + i &times; {address_increment}"
+
+            index = f"{register_array_index} + i &times; {array_index_increment}"
+
+        description = self._html_translator.translate(register.description)
+        html = f"""
+  <tr>
+    <td><strong>{register.name}</strong></td>
+    <td>{index}</td>
+    <td>{address_readable}</td>
+    <td>{register.mode.name}</td>
+    <td>{self._to_hex_string(register.default_value, num_nibbles=1)}</td>
+    <td>{description}</td>
+  </tr>"""
+
+        for field in register.fields:
+            html += self._annotate_field(field)
+
+        return html
+
+    def _annotate_field(self, field: RegisterField) -> str:
+        description = self._html_translator.translate(field.description)
+
+        if isinstance(field, Enumeration):
+            description += """\
+      <br />
+      <br />
+      Can be set to the following values:
+
+      <dl>
+"""
+
+            for element in field.elements:
+                description += f"""\
+        <dt style="display: list-item; margin-left:1em">
+          <em>{element.name} ({element.value})</em>:
+        </dt>
+"""
+
+                element_html = self._html_translator.translate(element.description)
+                description += f"        <dd>{element_html}</dd>\n"
+
+            description += "      </dl>\n"
+
+        if isinstance(field, Integer):
+            description += f"""\
+      <br />
+      <br />
+      Valid numeric range: [{field.min_value} &ndash; {field.max_value}].
+"""
+
+        return f"""
+  <tr>
+    <td>&nbsp;&nbsp;<em>{field.name}</em></td>
+    <td>&nbsp;&nbsp;{self._field_range(field=field)}</td>
+    <td></td>
+    <td></td>
+    <td>{self._field_default_value(field=field)}</td>
+    <td>
+      {description}
+    </td>
+  </tr>"""
+
+    @staticmethod
+    def _field_range(field: RegisterField) -> str:
+        """
+        Return the bits that this field occupies in a readable format.
+        The way it shall appear in documentation.
+        """
+        if field.width == 1:
+            return f"{field.base_index}"
+
+        return f"{field.base_index + field.width - 1}:{field.base_index}"
+
+    @staticmethod
+    def _field_default_value(field: RegisterField) -> str:
+        """
+        A human-readable string representation of the default value.
+        """
+        if isinstance(field, (Bit, BitVector)):
+            return f"0b{field.default_value}"
+
+        if isinstance(field, Enumeration):
+            return field.default_value.name
+
+        if isinstance(field, Integer):
+            return str(field.default_value)
+
+        raise ValueError(f"Unknown field: {field}")
