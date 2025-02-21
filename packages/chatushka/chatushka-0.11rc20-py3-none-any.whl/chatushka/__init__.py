3f162011bf94f297daa856de064a12f4c045c4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:18:57 +0000

    Add package chatushka/0.11rc20/chatushka-0.11rc20-py3-none-any.whl

---
+++ packages/chatushka/chatushka-0.11rc20-py3-none-any.whl/chatushka/__init__.py
@@ -0,0 +1,27 @@
+from chatushka._chatushka import Chatushka
+from chatushka._errors import ChatushkaError, ChatushkaResponseError
+from chatushka._matchers import BaseMatcher, CommandMatcher, EventMatcher, RegExMatcher
+from chatushka._models import Chat, ChatPermissions, Events, Message, Update, User
+from chatushka._transport import TelegramBotAPI
+
+__version__ = "0.11rc20"
+__all__ = [
+    # matchers
+    "BaseMatcher",
+    "Chat",
+    "ChatPermissions",
+    "Chatushka",
+    # errors
+    "ChatushkaError",
+    "ChatushkaResponseError",
+    "CommandMatcher",
+    "EventMatcher",
+    # models
+    "Events",
+    "Message",
+    "RegExMatcher",
+    # transport
+    "TelegramBotAPI",
+    "Update",
+    "User",
+]
