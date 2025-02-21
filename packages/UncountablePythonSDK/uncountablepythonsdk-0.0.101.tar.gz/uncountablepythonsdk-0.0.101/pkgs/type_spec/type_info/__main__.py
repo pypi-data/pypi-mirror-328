eb09f5b9f65f7a07fecbb6675485ded2d6180c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:19 +0000

    Add package UncountablePythonSDK/0.0.101/uncountablepythonsdk-0.0.101.tar.gz

---
+++ packages/UncountablePythonSDK/uncountablepythonsdk-0.0.101.tar.gz/uncountablepythonsdk-0.0.101/pkgs/type_spec/type_info/__main__.py
@@ -0,0 +1,33 @@
+"""
+This emittier relies on the output of the type_spec type generation.
+The justfile calls them in the expected order.
+This allows the processing at this layer to use the generated types for ext_info.
+"""
+
+import argparse
+import sys
+
+from ..config import parse_yaml_config
+from ..load_types import load_types
+from .emit_type_info import emit_type_info
+
+
+def main() -> bool:
+    parser = argparse.ArgumentParser(
+        "type_spec", usage="python -m type_spec.type_info config_file.yaml"
+    )
+    parser.add_argument("config_file", type=str)
+    args = parser.parse_args()
+
+    config = parse_yaml_config(args.config_file)
+    builder = load_types(config)
+    if builder is None:
+        return False
+
+    assert config.typescript is not None
+    emit_type_info(builder, config.typescript.type_info_output)
+
+    return True
+
+
+sys.exit(0 if main() else 1)
