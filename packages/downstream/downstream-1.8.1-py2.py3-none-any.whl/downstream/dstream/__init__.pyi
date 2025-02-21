c17f4b5a303dd3a875a85e08ffe5c5b906170d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 14:06:26 +0000

    Add package downstream/1.8.1/downstream-1.8.1-py2.py3-none-any.whl

---
+++ packages/downstream/downstream-1.8.1-py2.py3-none-any.whl/downstream/dstream/__init__.pyi
@@ -0,0 +1,16 @@
+from . import compressing_algo, steady_algo, stretched_algo, tilted_algo
+from ._hybrid_algo import hybrid_algo
+from ._hybrid_algo_prefab import (
+    hybrid_0_steady_1_stretched_2_algo,
+    hybrid_0_steady_1_tilted_2_algo,
+)
+
+__all__ = [
+    "compressing_algo",
+    "hybrid_algo",
+    "hybrid_0_steady_1_stretched_2_algo",
+    "hybrid_0_steady_1_tilted_2_algo",
+    "steady_algo",
+    "stretched_algo",
+    "tilted_algo",
+]
