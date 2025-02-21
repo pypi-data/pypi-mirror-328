709e7aaee681ba5c45395e73ada0b4d1e7c0e82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:25 +0000

    Add package liffile/2025.2.20/liffile-2025.2.20.tar.gz

---
+++ packages/liffile/liffile-2025.2.20.tar.gz/liffile-2025.2.20/liffile/__init__.py
@@ -0,0 +1,10 @@
+# liffile/__init__.py
+# pylint: skip-file
+
+from .liffile import *
+from .liffile import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Liffile version string."""
