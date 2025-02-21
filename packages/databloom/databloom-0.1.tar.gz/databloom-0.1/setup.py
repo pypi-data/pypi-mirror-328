0fecfc108a656362d126c84a4eae19a96aaa9fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:29:43 +0000

    Add package databloom/0.1/databloom-0.1.tar.gz

---
+++ packages/databloom/databloom-0.1.tar.gz/databloom-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="databloom",
+    version="0.1",
+    author="gada121982",
+    author_email="gada121982@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[]
+)
