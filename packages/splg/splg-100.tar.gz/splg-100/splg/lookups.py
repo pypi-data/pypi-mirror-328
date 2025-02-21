a7d3e5d3f6b29b5c0eb65c2b6232c7469c1a2349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 20:18:52 +0000

    Add package splg/100/splg-100.tar.gz

---
+++ packages/splg/splg-100.tar.gz/splg-100/splg/lookups.py
@@ -0,0 +1,30 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "splg.modules.irc",
+    "dne": "splg.modules.tdo",
+    "dpl": "splg.modules.rss",
+    "err": "splg.modules.err",
+    "exp": "splg.modules.rss",
+    "flt": "splg.modules.flt",
+    "fnd": "splg.modules.fnd",
+    "imp": "splg.modules.rss",
+    "log": "splg.modules.log",
+    "mod": "splg.modules.mod",
+    "mre": "splg.modules.irc",
+    "nme": "splg.modules.rss",
+    "pwd": "splg.modules.irc",
+    "req": "splg.modules.req",
+    "res": "splg.modules.rss",
+    "rss": "splg.modules.rss",
+    "slg": "splg.modules.slg",
+    "syn": "splg.modules.rss",
+    "tdo": "splg.modules.tdo",
+    "thr": "splg.modules.thr",
+    "upt": "splg.modules.upt",
+    "ver": "splg.modules.upt"
+}
---
+++ packages/splg/splg-100.tar.gz/splg-100/splg/modules/slg.py
@@ -0,0 +1,11 @@
+# This file is placed in the Public Domain.
+
+
+"slogan"
+
+
+TXT = "By law using poison destructing in whole or in part elderly handicapped criminals wicked, @KarimKhanQC reconsider OTP-CR-117/19 4 March 2019 http://bit.ly/gcid3"
+
+
+def slg(event):
+    event.reply(TXT)
