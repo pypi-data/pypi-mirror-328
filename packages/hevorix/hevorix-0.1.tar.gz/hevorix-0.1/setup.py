28ee6a3eabc93894a73bb1d3f2cc065b5aa7691a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:41:18 +0000

    Add package hevorix/0.1/hevorix-0.1.tar.gz

---
+++ packages/hevorix/hevorix-0.1.tar.gz/hevorix-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="hevorix",  # Kutubxona nomi
+    version="0.1",
+    py_modules=["library"],  # Ichidagi modullar
+    author="hevorix",
+    description="Oddiy kutubxona",
+)
