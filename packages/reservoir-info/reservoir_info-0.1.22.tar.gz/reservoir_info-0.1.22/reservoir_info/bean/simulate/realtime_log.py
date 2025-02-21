3ff2a59c45163bb4d5aa04d9b5f1d3c92ba3195f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:18:45 +0000

    Add package reservoir-info/0.1.22/reservoir_info-0.1.22.tar.gz

---
+++ packages/reservoir-info/reservoir_info-0.1.22.tar.gz/reservoir_info-0.1.22/reservoir_info/bean/simulate/realtime_log.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from mag_tools.bean.common.base_data import BaseData
+
+from reservoir_info.model.simulate_type import SimulateType
+
+
+@dataclass
+class RealTimeLog(BaseData):
+    user_sn: Optional[int] = field(default=None, metadata={"description": "用户序号"})
+    simulate_type: SimulateType = field(default=None, metadata={"description": "模拟方式"})
+    simulate_id: Optional[str] = field(default=None, metadata={"description": "模拟标识"})
+    case_file: Optional[int] = field(default=None, metadata={"description": "用户序号"})
+    message: Optional[str] = field(default=None, metadata={"description": "日志消息"})
