07686926a80b799a0f716595a3933da272cbd693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:19:23 +0000

    Add package ry/0.0.30/ry-0.0.30.tar.gz

---
+++ packages/ry/ry-0.0.30.tar.gz/ry-0.0.30/crates/ryo3-fnv/src/lib.rs
@@ -0,0 +1,74 @@
+#![doc = include_str!("../README.md")]
+use std::hash::Hasher;
+
+use ::fnv as fnv_rs;
+use pyo3::types::PyModule;
+use pyo3::{intern, prelude::*};
+use pyo3::{wrap_pyfunction, PyResult};
+
+#[pyclass(name = "FnvHasher", module = "ryo3")]
+pub struct PyFnvHasher {
+    pub hasher: fnv_rs::FnvHasher,
+}
+
+#[pymethods]
+impl PyFnvHasher {
+    #[new]
+    #[pyo3(signature = (s = None))]
+    fn py_new(s: Option<&[u8]>) -> Self {
+        match s {
+            Some(s) => {
+                let mut hasher = fnv_rs::FnvHasher::default();
+                hasher.write(s);
+                Self { hasher }
+            }
+            None => Self {
+                hasher: fnv_rs::FnvHasher::default(),
+            },
+        }
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("fnv1a<{:x}>", self.hasher.finish()))
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("fnv1a<{:x}>", self.hasher.finish()))
+    }
+
+    #[classattr]
+    fn name(py: Python<'_>) -> String {
+        intern!(py, "fnv1a").to_string()
+    }
+
+    fn digest(&self) -> PyResult<u64> {
+        Ok(self.hasher.finish())
+    }
+
+    fn hexdigest(&self) -> PyResult<String> {
+        // format hex string lowercase
+        Ok(format!("{:x}", self.hasher.finish()))
+    }
+
+    fn update(&mut self, s: &[u8]) -> PyResult<()> {
+        self.hasher.write(s);
+        Ok(())
+    }
+
+    fn copy(&self) -> PyResult<Self> {
+        Ok(Self {
+            hasher: fnv_rs::FnvHasher::with_key(self.hasher.finish()),
+        })
+    }
+}
+
+#[pyfunction]
+pub fn fnv1a(s: &[u8]) -> PyResult<PyFnvHasher> {
+    Ok(PyFnvHasher::py_new(Some(s)))
+}
+
+pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<PyFnvHasher>()?;
+    m.add_function(wrap_pyfunction!(self::fnv1a, m)?)?;
+    Ok(())
+}
---
+++ packages/ry/ry-0.0.30.tar.gz/ry-0.0.30/tests/test_fs.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+import ry
+
+
+def test_read_string(tmp_path: Path) -> None:
+    p = tmp_path / "test.txt"
+    p.write_text("hello")
+    ry.cd(tmp_path)
+    assert ry.read_text("test.txt") == "hello"
+
+
+def test_read_string_invalid_utf8(tmp_path: Path) -> None:
+    p = tmp_path / "test.txt"
+    p.write_bytes(b"\x80")
+    ry.cd(tmp_path)
+    with open("test.txt", "rb") as f:
+        assert f.read() == b"\x80"
+    # with python open and get error type
+    with pytest.raises(UnicodeDecodeError):
+        with open("test.txt", encoding="utf-8") as f:
+            f.read()
+    with pytest.raises(UnicodeDecodeError):
+        ry.read_text("test.txt")
+
+
+def test_read_bytes(tmp_path: Path) -> None:
+    p = tmp_path / "test.txt"
+    p.write_bytes(b"hello")
+    ry.cd(tmp_path)
+    assert ry.read_bytes("test.txt") == b"hello"
+
+
+def test_read_file_missing(tmp_path: Path) -> None:
+    p = tmp_path / "test.txt"
+    ry.cd(tmp_path)
+    with pytest.raises(FileNotFoundError):
+        ry.read_bytes(str(p))
+    with pytest.raises(FileNotFoundError):
+        ry.read_text(str(p))
+
+
+# @pytest.mark.skip(reason="TODO: pathlike not implemented")
+def test_read_file_missing_pathlike(tmp_path: Path) -> None:
+    p = tmp_path / "test.txt"
+    ry.cd(tmp_path)
+    with pytest.raises(FileNotFoundError):
+        ry.read_bytes(p)
+    with pytest.raises(FileNotFoundError):
+        ry.read_text(p)
