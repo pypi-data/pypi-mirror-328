54750e00c6e46053affaae29329639346c6a78e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:29:11 +0000

    Add package increase/0.192.0/increase-0.192.0.tar.gz

---
+++ packages/increase/increase-0.192.0.tar.gz/increase-0.192.0/src/increase/types/digital_wallet_token.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["DigitalWalletToken"]
+
+
+class DigitalWalletToken(BaseModel):
+    id: str
+    """The Digital Wallet Token identifier."""
+
+    card_id: str
+    """The identifier for the Card this Digital Wallet Token belongs to."""
+
+    created_at: datetime
+    """
+    The [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) date and time at which
+    the Card was created.
+    """
+
+    status: Literal["active", "inactive", "suspended", "deactivated"]
+    """This indicates if payments can be made with the Digital Wallet Token.
+
+    - `active` - The digital wallet token is active.
+    - `inactive` - The digital wallet token has been created but not successfully
+      activated via two-factor authentication yet.
+    - `suspended` - The digital wallet token has been temporarily paused.
+    - `deactivated` - The digital wallet token has been permanently canceled.
+    """
+
+    token_requestor: Literal["apple_pay", "google_pay", "samsung_pay", "unknown"]
+    """The digital wallet app being used.
+
+    - `apple_pay` - Apple Pay
+    - `google_pay` - Google Pay
+    - `samsung_pay` - Samsung Pay
+    - `unknown` - Unknown
+    """
+
+    type: Literal["digital_wallet_token"]
+    """A constant representing the object's type.
+
+    For this resource it will always be `digital_wallet_token`.
+    """
