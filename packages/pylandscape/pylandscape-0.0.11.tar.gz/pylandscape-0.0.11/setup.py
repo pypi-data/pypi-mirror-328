b4ecffe967e077b8aec559f0463fbe5745146493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:23 +0000

    Add package pylandscape/0.0.11/pylandscape-0.0.11.tar.gz

---
+++ packages/pylandscape/pylandscape-0.0.11.tar.gz/pylandscape-0.0.11/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+    
+    
+setup(
+    name="pylandscape",
+    version="0.0.11",
+    description="Python package to explore the loss landscape of Machine Learning models",
+    package_dir={"": "pylandscape"},
+    packages=find_packages(where="pyhessian"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/balditommaso/PyLandscape",
+    download_url="https://github.com/balditommaso/PyLandscape/archive/refs/tags/main.tar.gz",
+    author="Tommaso Baldi",
+    author_email="tommaso.baldi@santannapisa.it",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 3 - Alpha"
+    ],
+    install_requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "scipy",
+        "tqdm",
+        "PyHessian"
+    ],
+    extras_require={
+        "dev": ["twine>=4.0.2"]
+    },
+    python_requires=">=3.8"
+)
