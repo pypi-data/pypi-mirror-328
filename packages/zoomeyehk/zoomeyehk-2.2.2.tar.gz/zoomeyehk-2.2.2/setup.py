0a8a8e51d88f8da3aac69b692d2588a0bc21cdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:18:48 +0000

    Add package zoomeyehk/2.2.2/zoomeyehk-2.2.2.tar.gz

---
+++ packages/zoomeyehk/zoomeyehk-2.2.2.tar.gz/zoomeyehk-2.2.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+* Filename: setup.py
+* Description:
+* Time: 2020.11.27
+* Author: liuf5
+*/
+"""
+from setuptools import setup
+
+from zoomeyehk import __version__
+
+
+README = open('README.md', 'r', encoding='utf-8').read()
+
+setup(
+    name='zoomeyehk',
+    version=__version__,
+    description='Python library and command-line tool for ZoomEye.HK (https://www.zoomeye.hk/doc)',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='ZoomEye.HK',
+    url='https://github.com/zoomeye-hk/zoomeye-python',  #
+    packages=['zoomeyehk'],
+    entry_points={'console_scripts': ['zoomeyehk=zoomeyehk.cli:main']},
+    keywords=['security tool', 'zoomeye', 'zoomeyehk', 'command tool'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
---
+++ packages/zoomeyehk/zoomeyehk-2.2.2.tar.gz/zoomeyehk-2.2.2/zoomeyehk/cli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+DEPRECATION_NOTICE = """
+This project has been deprecated. Please use the new project at: https://github.com/zoomeye-ai/ZoomEye-python
+"""
+
+def main():
+    print(DEPRECATION_NOTICE)
+    sys.exit(1)
+
+if __name__ == '__main__':
+    main()
