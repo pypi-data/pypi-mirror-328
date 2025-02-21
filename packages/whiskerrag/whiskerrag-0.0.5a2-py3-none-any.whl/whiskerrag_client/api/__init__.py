247df3b0515483bb6a00a92935d0c0d46076625b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:30:10 +0000

    Add package whiskerrag/0.0.5a2/whiskerrag-0.0.5a2-py3-none-any.whl

---
+++ packages/whiskerrag/whiskerrag-0.0.5a2-py3-none-any.whl/whiskerrag_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from whiskerrag_client.api.chunk_api import ChunkApi
+from whiskerrag_client.api.default_api import DefaultApi
+from whiskerrag_client.api.knowledge_api import KnowledgeApi
+from whiskerrag_client.api.retrieval_api import RetrievalApi
+from whiskerrag_client.api.task_api import TaskApi
