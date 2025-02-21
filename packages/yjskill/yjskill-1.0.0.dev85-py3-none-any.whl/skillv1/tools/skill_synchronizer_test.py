ceeff5bf5b671d8583a18847ee0c2f080b5f893b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:07:39 +0000

    Add package yjskill/1.0.0.dev85/yjskill-1.0.0.dev85-py3-none-any.whl

---
+++ packages/yjskill/yjskill-1.0.0.dev85-py3-none-any.whl/skillv1/tools/skill_synchronizer_test.py
@@ -0,0 +1,42 @@
+"""
+distribute_skill.py
+Authors: leibin01(leibin01@baidu.com)
+Date: 2024/12/10
+"""
+
+import unittest
+
+from .skill_synchronizer import SyncSkillKind
+
+
+class TestSkillSynchronizer(unittest.TestCase):
+    """
+    Test Skill Synchronizer
+    """
+
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName=methodName)
+
+    def test_sync_kind(self):
+        """
+        test sync kind
+        """
+
+        kind = "Edge"
+        sync_kind = SyncSkillKind[kind]
+        print(sync_kind == SyncSkillKind.Edge)
+
+
+def suite():
+    """
+    test suite
+    """
+
+    suite = unittest.TestSuite()
+    suite.addTest(TestSkillSynchronizer('test_sync_kind'))
+    return suite
+
+
+if __name__ == '__main__':
+    print('starting tests...')
+    unittest.main(defaultTest='suite')
