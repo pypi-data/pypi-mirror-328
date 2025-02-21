1bc4465c7b83f8447a45607548b40bf6997a9e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:18:53 +0000

    Add package pybaselib/0.1.0/pybaselib-0.1.0-py3-none-any.whl

---
+++ packages/pybaselib/pybaselib-0.1.0-py3-none-any.whl/pybaselib/utils/pythrow/exceptions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: maoyongfan
+# @email: maoyongfan@163.com
+# @Date: 2025/1/6 17:32
+
+class GenErr(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class BadValue(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ParameterError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
