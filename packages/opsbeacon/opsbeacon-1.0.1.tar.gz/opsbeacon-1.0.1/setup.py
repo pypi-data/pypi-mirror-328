9f9c57b00af2a6cb13917fea9dd07ce36d1ae526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:43 +0000

    Add package opsbeacon/1.0.1/opsbeacon-1.0.1.tar.gz

---
+++ packages/opsbeacon/opsbeacon-1.0.1.tar.gz/opsbeacon-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="opsbeacon",
+    version="1.0.1",
+    author="Cihan Sahin",
+    author_email="cihan@opsbeacon.com",
+    description="OpsBeacon python client library to interact with the OpsBeacon API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ob2ai/ob-python-sdk",  # Replace with your GitHub URL
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
