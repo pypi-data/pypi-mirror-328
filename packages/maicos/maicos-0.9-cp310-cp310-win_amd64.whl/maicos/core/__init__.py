2a86cd7c4fd2c569775002fce59345f4aabf1583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:06 +0000

    Add package maicos/0.9/maicos-0.9-cp310-cp310-win_amd64.whl

---
+++ packages/maicos/maicos-0.9-cp310-cp310-win_amd64.whl/maicos/core/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2024 Authors and contributors
+# (see the AUTHORS.rst file for the full list of names)
+#
+# Released under the GNU Public Licence, v3 or any higher version
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""These Modules build the core of other MAICoS modules."""
+
+__all__ = [
+    "AnalysisBase",
+    "AnalysisCollection",
+    "ProfileBase",
+    "CylinderBase",
+    "ProfileCylinderBase",
+    "PlanarBase",
+    "ProfilePlanarBase",
+    "SphereBase",
+    "ProfileSphereBase",
+]
+
+from .base import AnalysisBase, AnalysisCollection, ProfileBase
+from .cylinder import CylinderBase, ProfileCylinderBase
+from .planar import PlanarBase, ProfilePlanarBase
+from .sphere import ProfileSphereBase, SphereBase
