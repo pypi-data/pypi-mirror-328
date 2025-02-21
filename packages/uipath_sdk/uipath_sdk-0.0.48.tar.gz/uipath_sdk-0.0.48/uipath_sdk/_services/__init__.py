0422a24b1b1fb974ae9e91c582257d7e838a36f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 20:18:31 +0000

    Add package uipath_sdk/0.0.48/uipath_sdk-0.0.48.tar.gz

---
+++ packages/uipath_sdk/uipath_sdk-0.0.48.tar.gz/uipath_sdk-0.0.48/uipath_sdk/_services/__init__.py
@@ -0,0 +1,5 @@
+from .actions_service import ActionsService
+from .assets_service import AssetsService
+from .processes_service import ProcessesService
+
+__all__ = ["ActionsService", "AssetsService", "ProcessesService"]
