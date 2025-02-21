f0942fddac1bff11fe82c38adee4b60a9c9ab218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:29:58 +0000

    Add package ropt/0.15.2/ropt-0.15.2-py3-none-any.whl

---
+++ packages/ropt/ropt-0.15.2-py3-none-any.whl/ropt/plugins/plan/_metadata.py
@@ -0,0 +1,93 @@
+"""This module implements the default meta data handler."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from pydantic import BaseModel, ConfigDict
+
+from ropt.config.validated_types import ItemOrSet  # noqa: TC001
+from ropt.enums import EventType
+from ropt.plugins.plan.base import ResultHandler
+
+if TYPE_CHECKING:
+    from ropt.config.plan import ResultHandlerConfig
+    from ropt.plan import Event, Plan
+
+
+class DefaultMetadataHandler(ResultHandler):
+    """The default metadata results handler.
+
+    This handler adds arbitrary metadata to results produced by steps by
+    merging them into the `metadata` field of the
+    [`Results`][ropt.results.Results] objects that it receives. It is configured
+    using a dictionary that maps keys to the data to store. These data entries
+    can be of any type; however, if they are strings, they will be evaluated
+    using the [`eval`][ropt.plan.Plan.eval] method of the executing
+    [`Plan`][ropt.plan.Plan] object. This evaluation occurs when the metadata
+    handler processes an event, enabling dynamic data insertion based on the
+    current content of plan variables.
+
+    The metadata step uses the [`DefaultMetadataHandlerWith`]
+    [ropt.plugins.plan._metadata.DefaultMetadataHandler.DefaultMetadataHandlerWith]
+    configuration class to parse the `with` field of the
+    [`ResultHandler`][ropt.config.plan.ResultHandlerConfig] used to specify this
+    handler in a plan configuration.
+    """
+
+    class DefaultMetadataHandlerWith(BaseModel):
+        """Parameters used by the default metadata results handler.
+
+        The data to merge into the metadata of a result are required.
+
+        The `tags` field allows optional labels to be attached to each result,
+        which can assist result handlers in filtering relevant results.
+
+        Attributes:
+            data: Data to merge into the metadata of the results.
+            tags: Optional tags specifying which result sources to modify.
+        """
+
+        data: dict[str, Any]
+        tags: ItemOrSet[str]
+
+        model_config = ConfigDict(
+            extra="forbid",
+            validate_default=True,
+            arbitrary_types_allowed=True,
+            frozen=True,
+        )
+
+    def __init__(self, config: ResultHandlerConfig, plan: Plan) -> None:
+        """Initialize a default metadata results handler.
+
+        Args:
+            config: The configuration of the step.
+            plan:   The plan that runs this step.
+        """
+        super().__init__(config, plan)
+
+        self._with = self.DefaultMetadataHandlerWith.model_validate(config.with_)
+
+    def handle_event(self, event: Event) -> Event:
+        """Handle an event.
+
+        Args:
+            event: The event to handle.
+
+        Returns:
+            The (possibly modified) event.
+        """
+        if (
+            event.event_type
+            in {
+                EventType.FINISHED_EVALUATION,
+                EventType.FINISHED_EVALUATOR_STEP,
+            }
+            and "results" in event.data
+            and (event.tags & self._with.tags)
+        ):
+            for results in event.data["results"]:
+                for key, expr in self._with.data.items():
+                    results.metadata[key] = self._plan.eval(expr)
+        return event
---
+++ packages/ropt/ropt-0.15.2-py3-none-any.whl/ropt/plugins/plan/evaluator.py
@@ -0,0 +1,204 @@
+"""This module implements the default evaluator step."""
+
+from __future__ import annotations
+
+from copy import deepcopy
+from typing import TYPE_CHECKING, Any
+
+import numpy as np
+from pydantic import BaseModel, ConfigDict
+
+from ropt.config.enopt import EnOptConfig
+from ropt.config.validated_types import Array2D, ItemOrSet  # noqa: TC001
+from ropt.ensemble_evaluator import EnsembleEvaluator
+from ropt.enums import EventType, OptimizerExitCode
+from ropt.exceptions import OptimizationAborted
+from ropt.plan import Event
+from ropt.plugins.plan.base import PlanStep
+from ropt.results import FunctionResults
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    from ropt.config.plan import PlanStepConfig
+    from ropt.plan import Plan
+    from ropt.transforms import OptModelTransforms
+
+
+class DefaultEvaluatorStep(PlanStep):
+    """The default evaluator step.
+
+    This step performs a single ensemble evaluation, yielding one or more
+    [`FunctionResults`][ropt.results.FunctionResults] objects. The evaluation
+    can process multiple variable vectors, each of which is evaluated
+    separately, producing an individual results object for each vector.
+
+    Before executing the evaluator step, a
+    [`START_EVALUATOR_STEP`][ropt.enums.EventType.START_EVALUATOR_STEP] event is
+    emitted. After the evaluator step finishes, an
+    [`FINISHED_EVALUATOR_STEP`][ropt.enums.EventType.FINISHED_EVALUATOR_STEP]
+    event is emitted. Result handlers should respond to the latter event to
+    process the generated results.
+
+    The evaluator step uses the [`DefaultEvaluatorStepWith`]
+    [ropt.plugins.plan.evaluator.DefaultEvaluatorStep.DefaultEvaluatorStepWith]
+    configuration class to parse the `with` field of the
+    [`PlanStepConfig`][ropt.config.plan.PlanStepConfig] used to specify this step
+    in a plan configuration.
+    """
+
+    class DefaultEvaluatorStepWith(BaseModel):
+        """Parameters used by the default evaluator step.
+
+        The
+        [`DefaultEvaluatorStep`][ropt.plugins.plan.evaluator.DefaultEvaluatorStep]
+        requires an optimizer configuration; the `tags` and `values` parameters
+        are optional. The configuration  object must be an
+        [`EnOptConfig`][ropt.config.enopt.EnOptConfig] object, or a dictionary
+        that can be parsed into such an object. If no `values` are provided, the
+        initial values specified by the optimizer configuration are used. If
+        `values` is given, it may be a single vector or a two-dimensional array.
+        In the latter case, each row of the matrix is treated as a separate set
+        of values to be evaluated.
+
+        The `tags` field allows optional labels to be attached to each result,
+        which can assist result handlers in filtering relevant results.
+
+        The `data` field can be used to pass additional information via the `data`
+        field of the events emitted by the optimizer step. Avoid the use of `results`
+        and `exit_code` in this field, as these are already passed in the event data.
+
+        Attributes:
+            config: The optimizer configuration.
+            tags:   Tags to add to the emitted events.
+            values: Values to evaluate at.
+            data:   Data to pass via events.
+        """
+
+        config: str
+        tags: ItemOrSet[str] = set()
+        values: str | Array2D | None = None
+        data: dict[str, Any] = {}
+
+        model_config = ConfigDict(
+            extra="forbid",
+            validate_default=True,
+            arbitrary_types_allowed=True,
+            frozen=True,
+        )
+
+    def __init__(self, config: PlanStepConfig, plan: Plan) -> None:
+        """Initialize a default evaluator step.
+
+        Args:
+            config: The configuration of the step.
+            plan:   The optimization plan that runs this step.
+        """
+        super().__init__(config, plan)
+
+        self._with = self.DefaultEvaluatorStepWith.model_validate(config.with_)
+
+    def run(self) -> None:
+        """Run the evaluator step."""
+        variables = self._get_variables()
+        config = self.plan.eval(self._with.config)
+        match config:
+            case dict():
+                enopt_config, transforms = EnOptConfig.model_validate(config), None
+            case EnOptConfig():
+                enopt_config, transforms = config, None
+            case list():
+                enopt_config, transforms = config
+            case _:
+                msg = "No valid EnOpt configuration provided"
+                raise TypeError(msg)
+        enopt_config = EnOptConfig.model_validate(config)
+        if variables is None:
+            variables = enopt_config.variables.initial_values
+        self._run(enopt_config, transforms, variables)
+
+    def _run(
+        self,
+        enopt_config: EnOptConfig,
+        transforms: OptModelTransforms | None,
+        variables: NDArray[np.float64],
+    ) -> None:
+        for event_type in (EventType.START_EVALUATOR_STEP, EventType.START_EVALUATION):
+            self.emit_event(
+                Event(
+                    event_type=event_type,
+                    config=enopt_config,
+                    tags=self._with.tags,
+                    data=deepcopy(self._with.data),
+                )
+            )
+
+        ensemble_evaluator = EnsembleEvaluator(
+            enopt_config,
+            transforms,
+            self.plan.optimizer_context.evaluator,
+            self.plan.plan_id,
+            self.plan.optimizer_context.plugin_manager,
+        )
+
+        exit_code = OptimizerExitCode.EVALUATION_STEP_FINISHED
+
+        try:
+            results = ensemble_evaluator.calculate(
+                variables, compute_functions=True, compute_gradients=False
+            )
+        except OptimizationAborted as exc:
+            exit_code = exc.exit_code
+
+        assert results
+        assert isinstance(results[0], FunctionResults)
+        if results[0].functions is None:
+            exit_code = OptimizerExitCode.TOO_FEW_REALIZATIONS
+
+        data = deepcopy(self._with.data)
+        data["exit_code"] = exit_code
+        if transforms is not None:
+            data["transformed_results"] = results
+            data["results"] = [
+                item.transform_from_optimizer(transforms) for item in results
+            ]
+        else:
+            data["results"] = results
+        for event_type in (
+            EventType.FINISHED_EVALUATION,
+            EventType.FINISHED_EVALUATOR_STEP,
+        ):
+            self.emit_event(
+                Event(
+                    event_type=event_type,
+                    config=enopt_config,
+                    tags=self._with.tags,
+                    data=data,
+                )
+            )
+
+        if exit_code == OptimizerExitCode.USER_ABORT:
+            self.plan.abort()
+
+    def emit_event(self, event: Event) -> None:
+        """Emit an event.
+
+        Args:
+            event: The event to emit.
+        """
+        self.plan.emit_event(event)
+
+    def _get_variables(self) -> NDArray[np.float64] | None:
+        if self._with.values is not None:
+            parsed_variables = self.plan.eval(self._with.values)
+            match parsed_variables:
+                case FunctionResults():
+                    return parsed_variables.evaluations.variables
+                case np.ndarray() | list():
+                    return np.asarray(parsed_variables, dtype=np.float64)
+                case None:
+                    return None
+                case _:
+                    msg = f"`{self._with.values}` does not contain variables."
+                    raise ValueError(msg)
+        return None
