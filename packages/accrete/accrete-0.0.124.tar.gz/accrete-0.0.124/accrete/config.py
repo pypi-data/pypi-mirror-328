7f78c428826983796205b3a95bd2ea438fa0e41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 22:07:35 +0000

    Add package accrete/0.0.124/accrete-0.0.124.tar.gz

---
+++ packages/accrete/accrete-0.0.124.tar.gz/accrete-0.0.124/accrete/config.py
@@ -0,0 +1,14 @@
+import logging
+from django.conf import settings
+
+_logger = logging.getLogger(__name__)
+
+ACCRETE_TENANT_NOT_SET_URL = getattr(
+    settings, 'ACCRETE_TENANT_NOT_SET_URL',
+    False
+)
+
+if not ACCRETE_TENANT_NOT_SET_URL:
+    _logger.warning(
+        'Setting ACCRETE_TENANT_NOT_SET_URL missing.'
+    )
