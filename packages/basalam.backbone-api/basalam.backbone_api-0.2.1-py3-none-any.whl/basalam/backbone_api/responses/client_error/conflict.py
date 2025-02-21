80ce4d60871b9f50bacf6ff2162baf9cc57e5374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 19:18:20 +0000

    Add package basalam.backbone-api/0.2.1/basalam.backbone_api-0.2.1-py3-none-any.whl

---
+++ packages/basalam.backbone-api/basalam.backbone_api-0.2.1-py3-none-any.whl/basalam/backbone_api/responses/client_error/conflict.py
@@ -0,0 +1,22 @@
+from typing import List, Generic, TypeVar
+
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_409_CONFLICT
+
+from basalam.backbone_api.responses.client_error.base import Base400Response, Error
+from basalam.backbone_api.responses.response_model_abstract import T
+
+
+class ExtendedError(Error):
+    fields: List[str]
+
+
+class ConflictResponse(Base400Response, Generic[T]):
+    errors: List[ExtendedError]
+    data: List[T]
+
+    async def as_json_response(self) -> JSONResponse:
+        return JSONResponse(
+            content=self.model_dump(),
+            status_code=HTTP_409_CONFLICT
+        )
