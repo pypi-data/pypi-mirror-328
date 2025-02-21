4d561be1066db3ad3cbb411cffe6e188f97c9931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:29:51 +0000

    Add package pyxelart-detector/0.1.1/pyxelart_detector-0.1.1.tar.gz

---
+++ packages/pyxelart-detector/pyxelart_detector-0.1.1.tar.gz/pyxelart_detector-0.1.1/__main__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+
+from .pyxelart_detector import is_pixel_art
+
+def usage():
+    print("Usage:  python -m pyxelart_detector <path/to/file.{png,jpg,gif,...}>")
+    exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+
+        filepath = Path(sys.argv[1])
+        if not filepath.is_file():
+            print(f"Error: {filepath} is not a valid file.")
+            usage()
+
+        print(is_pixel_art(filepath))
+
+if __name__ == '__main__':
+    main()
