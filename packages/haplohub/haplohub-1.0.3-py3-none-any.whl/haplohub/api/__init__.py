2e1f1e1581bce43d2a5b30783f7884282af90dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 21:18:18 +0000

    Add package haplohub/1.0.3/haplohub-1.0.3-py3-none-any.whl

---
+++ packages/haplohub/haplohub-1.0.3-py3-none-any.whl/haplohub/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from haplohub.api.metadata_api import MetadataApi
+from haplohub.api.predictions_api import PredictionsApi
+from haplohub.api.sample_api import SampleApi
+from haplohub.api.default_api import DefaultApi
