8d2643d9473f10a4709e67bc2345b8a511520d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:30:04 +0000

    Add package cumulusci/4.3.0.dev0/cumulusci-4.3.0.dev0.tar.gz

---
+++ packages/cumulusci/cumulusci-4.3.0.dev0.tar.gz/cumulusci-4.3.0.dev0/cumulusci/tasks/preflight/tests/test_licenses.py
@@ -0,0 +1,95 @@
+from unittest.mock import Mock
+
+from cumulusci.tasks.preflight.licenses import (
+    GetAvailableLicenses,
+    GetAvailablePermissionSetLicenses,
+    GetAvailablePermissionSets,
+    GetPermissionLicenseSetAssignments,
+)
+from cumulusci.tasks.salesforce.tests.util import create_task
+
+
+class TestLicensePreflights:
+    def test_license_preflight(self):
+        task = create_task(GetAvailableLicenses, {})
+        task._init_api = Mock()
+        task._init_api.return_value.query.return_value = {
+            "totalSize": 2,
+            "records": [
+                {"LicenseDefinitionKey": "TEST1"},
+                {"LicenseDefinitionKey": "TEST2"},
+            ],
+        }
+        task()
+
+        task._init_api.return_value.query.assert_called_once_with(
+            "SELECT LicenseDefinitionKey FROM UserLicense"
+        )
+        assert task.return_values == ["TEST1", "TEST2"]
+
+    def test_psl_preflight(self):
+        task = create_task(GetAvailablePermissionSetLicenses, {})
+        task._init_api = Mock()
+
+        task._init_api.return_value.query.return_value = {
+            "totalSize": 2,
+            "records": [
+                {"PermissionSetLicenseKey": "TEST1"},
+                {"PermissionSetLicenseKey": "TEST2"},
+            ],
+        }
+        task()
+
+        task._init_api.return_value.query.assert_called_once_with(
+            "SELECT PermissionSetLicenseKey FROM PermissionSetLicense"
+        )
+        assert task.return_values == ["TEST1", "TEST2"]
+
+    def test_assigned_permsetlicense_preflight(self):
+        task = create_task(GetPermissionLicenseSetAssignments, {})
+        task._init_api = Mock()
+        task._init_api.return_value.query_all.return_value = {
+            "totalSize": 2,
+            "done": True,
+            "records": [
+                {
+                    "PermissionSetLicense": {
+                        "MasterLabel": "Document Checklist",
+                        "DeveloperName": "DocumentChecklist",
+                    },
+                },
+                {
+                    "PermissionSetLicense": {
+                        "MasterLabel": "Einstein Analytics Plus Admin",
+                        "DeveloperName": "EinsteinAnalyticsPlusAdmin",
+                    },
+                },
+            ],
+        }
+        task()
+
+        task._init_api.return_value.query_all.assert_called_once_with(
+            "SELECT PermissionSetLicense.DeveloperName FROM PermissionSetLicenseAssign WHERE AssigneeId = 'USER_ID'"
+        )
+        assert task.return_values == [
+            "DocumentChecklist",
+            "EinsteinAnalyticsPlusAdmin",
+        ]
+
+    def test_permsets_preflight(self):
+        task = create_task(GetAvailablePermissionSets, {})
+        task._init_api = Mock()
+
+        task._init_api.return_value.query_all.return_value = {
+            "totalSize": 2,
+            "records": [
+                {"Name": "TEST1"},
+                {"Name": "TEST2"},
+            ],
+        }
+        task()
+
+        task._init_api.return_value.query_all.assert_called_once_with(
+            "SELECT Name FROM PermissionSet"
+        )
+        assert task.return_values == ["TEST1", "TEST2"]
