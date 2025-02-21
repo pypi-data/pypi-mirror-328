3d5c170fd7a1187386a7e51c67e8242777cf4d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:19:21 +0000

    Add package backend-client/1.11.21/backend_client-1.11.21-py3-none-any.whl

---
+++ packages/backend-client/backend_client-1.11.21-py3-none-any.whl/playbookmedia_backend_client_sdk/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from playbookmedia_backend_client_sdk.api.lead_scraper_service_api import LeadScraperServiceApi
+from playbookmedia_backend_client_sdk.api.workspace_service_api import WorkspaceServiceApi
+
