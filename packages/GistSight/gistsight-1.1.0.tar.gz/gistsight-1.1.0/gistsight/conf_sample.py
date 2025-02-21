07cbd4399b07e429b28d1d859a59897804101625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:30:10 +0000

    Add package GistSight/1.1.0/gistsight-1.1.0.tar.gz

---
+++ packages/GistSight/gistsight-1.1.0.tar.gz/gistsight-1.1.0/gistsight/conf_sample.py
@@ -0,0 +1,16 @@
+# GitHub
+github_api_url = "https://api.github.com/gists/public"
+github_token = ""
+
+# For PyVulnerabilityLookup
+vulnerability_lookup_base_url = "http://127.0.0.1:5000/"
+vulnerability_auth_token = ""
+
+# Maximum number of sightings to create from a single Gist
+max_bulk_sighting = 10
+
+# Hearbeat mechanism
+heartbeat_enabled = True
+valkey_host = "127.0.0.1"
+valkey_port = 10002
+expiration_period = 3600
---
+++ packages/GistSight/gistsight-1.1.0.tar.gz/gistsight-1.1.0/gistsight/config.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+"""This module is responsible for loading the configuration variables.
+"""
+
+import importlib.util
+import os
+
+
+def load_config(path):
+    spec = importlib.util.spec_from_file_location("config", path)
+    if spec:
+        config = importlib.util.module_from_spec(spec)
+        if spec.loader:
+            spec.loader.exec_module(config)
+    return config
+
+
+conf = None
+try:
+    conf = load_config(os.environ.get("GISTSIGHT_CONFIG", "gistsight/conf_sample.py"))
+except Exception as exc:
+    raise Exception("No configuration file provided.") from exc
+finally:
+    if not conf:
+        raise Exception("No configuration file provided.")
+
+try:
+    # For PyVulnerabilityLookup
+    vulnerability_lookup_base_url = conf.vulnerability_lookup_base_url
+    vulnerability_auth_token = conf.vulnerability_auth_token
+
+    github_api_url = conf.github_api_url
+    github_token = conf.github_token
+except AttributeError:
+    raise Exception("Missing configuration variable.")
+
+try:
+    max_bulk_sighting = conf.max_bulk_sighting
+except Exception:
+    max_bulk_sighting = 10
+
+try:
+    heartbeat_enabled = True
+    valkey_host = conf.valkey_host
+    valkey_port = conf.valkey_port
+    expiration_period = conf.expiration_period
+except Exception:
+    heartbeat_enabled = False
