cfc7f44fee48f1388ac0b43939fd70753db6fa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:07:37 +0000

    Add package hdl-registers/7.0.4/hdl_registers-7.0.4-py3-none-any.whl

---
+++ packages/hdl-registers/hdl_registers-7.0.4-py3-none-any.whl/hdl_registers/generator/html/html_translator.py
@@ -0,0 +1,82 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl-registers project, an HDL register generator fast enough to run
+# in real time.
+# https://hdl-registers.com
+# https://github.com/hdl-registers/hdl-registers
+# --------------------------------------------------------------------------------------------------
+
+import re
+
+
+class HtmlTranslator:
+    r"""
+    Translate a raw text with markdown/rst annotations into HTML code.
+
+    Supports:
+
+    * Strong: **double asterisks**
+    * Emphasis: *single asterisks*
+
+    Literal asterisks should be escaped: \*
+    """
+
+    _not_escaped = r"(?<!\\)"
+    _double_asterisks = r"\*\*"
+    _single_asterisk = r"\*"
+    _match_text = r"(.*?)"
+
+    # These patterns match asterisks only if they are not preceded by \escape
+    _re_strong_pattern = re.compile(
+        _not_escaped + _double_asterisks + _match_text + _not_escaped + _double_asterisks
+    )
+    _re_emphasis_pattern = re.compile(
+        _not_escaped + _single_asterisk + _match_text + _not_escaped + _single_asterisk
+    )
+
+    # This pattern matches escaped asterisks
+    _re_escaped_literal_pattern = re.compile(r"\\(\*)")
+
+    # Consecutive newlines is a paragraph separator
+    _re_paragraph_separator = re.compile(r"\n{2,}")
+
+    def translate(self, text: str) -> str:
+        """
+        Translate the text to have HTML tags where appropriate.
+        """
+        result = self._translate_angle_brackets(text)
+        result = self._annotate(result)
+        return self._insert_line_breaks(result)
+
+    def _annotate(self, text: str) -> str:
+        """
+        Replace markdown/rst syntax with HTML tags.
+        """
+        result = re.sub(self._re_strong_pattern, r"<strong>\g<1></strong>", text)
+        result = re.sub(self._re_emphasis_pattern, r"<em>\g<1></em>", result)
+
+        # Remove the escape sign
+        return re.sub(self._re_escaped_literal_pattern, r"\g<1>", result)
+
+    def _insert_line_breaks(self, text: str) -> str:
+        """
+        Insert HTML line break tag instead of consecutive newlines.
+        """
+        # Two line breaks to get new paragraph.
+        result = re.sub(self._re_paragraph_separator, "<br /><br />", text)
+
+        # A single newline in Markdown should be a space
+        result = result.replace("\n", " ")
+
+        # Split to get nicer HTML file formatting
+        return result.replace("<br />", "<br />\n")
+
+    @staticmethod
+    def _translate_angle_brackets(text: str) -> str:
+        """
+        The HTML may not contain raw angle brackets ("<", ">") since they will be interpreted as
+        HTML tags by the web browse.
+        """
+        result = text.replace("<", "&lt;")
+        return result.replace(">", "&gt;")
