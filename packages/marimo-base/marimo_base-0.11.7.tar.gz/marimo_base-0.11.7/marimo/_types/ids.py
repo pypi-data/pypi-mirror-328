ddaf19eb80e7fbe34559da482d84958b5705604f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 22:07:31 +0000

    Add package marimo-base/0.11.7/marimo_base-0.11.7.tar.gz

---
+++ packages/marimo-base/marimo_base-0.11.7.tar.gz/marimo_base-0.11.7/marimo/_types/ids.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import NewType
+
+CellId_t = NewType("CellId_t", str)
+
+UIElementId = NewType("UIElementId", str)
+
+SessionId = NewType("SessionId", str)
+
+ConsumerId = NewType("ConsumerId", str)
+
+VariableName = NewType("VariableName", str)
+
+RequestId = NewType("RequestId", str)
