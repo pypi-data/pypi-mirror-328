cae03c902677263d617fe408dc11bdb6a3ca7d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 23:08:12 +0000

    Add package pipatch/0.1.0/pipatch-0.1.0.tar.gz

---
+++ packages/pipatch/pipatch-0.1.0.tar.gz/pipatch-0.1.0/README.md
@@ -0,0 +1,15 @@
+# `pipatch` = `pip` + `patch`
+Minimal patching tool for Python packages.
+
+## Installation
+```sh
+pip install pipatch
+```
+
+## Usage
+Check out the `Pipatch` class.
+Documentation is coming soon.
+
+## License
+Copyright ©️ 2025 Teddy van Jerry ([Wuqiong Zhao](https://wqzhao.org)).
+This project is licensed under the [MIT License](LICENSE).
---
+++ packages/pipatch/pipatch-0.1.0.tar.gz/pipatch-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pipatch",
+    version="0.1.0",
+    description="Minimal patching tool for Python packages.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Teddy van Jerry (Wuqiong Zhao)",
+    author_email="me@teddy-van-jerry.org",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["requests"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development",
+    ],
+)
