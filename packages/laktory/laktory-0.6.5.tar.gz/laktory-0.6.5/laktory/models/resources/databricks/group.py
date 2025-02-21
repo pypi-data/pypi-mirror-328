1a3c7a4ae32149dafa759382ee5c88534e57b1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 22:28:28 +0000

    Add package laktory/0.6.5/laktory-0.6.5.tar.gz

---
+++ packages/laktory/laktory-0.6.5.tar.gz/laktory-0.6.5/laktory/models/resources/databricks/group.py
@@ -0,0 +1,110 @@
+from typing import Union
+
+from pydantic import Field
+
+from laktory.models.basemodel import BaseModel
+from laktory.models.resources.baseresource import ResourceLookup
+from laktory.models.resources.databricks.mwspermissionassignment import (
+    MwsPermissionAssignment,
+)
+from laktory.models.resources.pulumiresource import PulumiResource
+from laktory.models.resources.terraformresource import TerraformResource
+
+
+class GroupLookup(ResourceLookup):
+    """
+    Attributes
+    ----------
+    id:
+        Id of the group. Only supported when using Pulumi backend.
+    display_name:
+        Display name of the group. Only support when using Terraform backend
+    """
+
+    id: str = None
+    display_name: str = None
+
+
+class Group(BaseModel, PulumiResource, TerraformResource):
+    """
+    Databricks group
+
+    Attributes
+    ----------
+    allow_cluster_create:
+        When `True`, the group is allowed to have cluster create permissions
+    display_name:
+        Display name for the group.
+    lookup_existing:
+        Specifications for looking up existing resource. Other attributes will
+        be ignored.
+    workspace_access
+        When `True`, the group is allowed to have workspace access
+
+    Examples
+    --------
+    ```py
+    from laktory import models
+
+    d = models.resources.databricks.Group(display_name="role-engineers")
+    ```
+    """
+
+    allow_cluster_create: bool = False
+    display_name: str
+    lookup_existing: GroupLookup = Field(None, exclude=True)
+    workspace_access: bool = None
+    workspace_permission_assignments: list[MwsPermissionAssignment] = None
+
+    # ----------------------------------------------------------------------- #
+    # Resource Properties                                                     #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def resource_key(self) -> str:
+        """display name"""
+        return self.display_name
+
+    # ----------------------------------------------------------------------- #
+    # Pulumi Properties                                                       #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def pulumi_resource_type(self) -> str:
+        return "databricks:Group"
+
+    @property
+    def pulumi_excludes(self) -> Union[list[str], dict[str, bool]]:
+        return ["workspace_permission_assignments"]
+
+    @property
+    def additional_core_resources(self) -> list[PulumiResource]:
+        """
+        - workspace permission assignments
+        """
+        resources = []
+        if self.workspace_permission_assignments:
+            for a in self.workspace_permission_assignments:
+                if a.principal_id is None:
+                    a.principal_id = f"${{resources.{self.resource_name}.id}}"
+                resources += [a]
+        return resources
+
+    # TODO:
+    # if group.id is None:
+    #     self.group = databricks.Group(name, opts=opts, **group.model_pulumi_dump())
+    #     group.id = self.group.id
+    # else:
+    #     self.group = databricks.Group.get(name, id=group.id)
+
+    # ----------------------------------------------------------------------- #
+    # Terraform Properties                                                    #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def terraform_resource_type(self) -> str:
+        return "databricks_group"
+
+    @property
+    def terraform_excludes(self) -> Union[list[str], dict[str, bool]]:
+        return self.pulumi_excludes
---
+++ packages/laktory/laktory-0.6.5.tar.gz/laktory-0.6.5/laktory/models/resources/databricks/user.py
@@ -0,0 +1,125 @@
+from typing import Union
+
+from pydantic import Field
+
+from laktory.models.basemodel import BaseModel
+from laktory.models.resources.baseresource import ResourceLookup
+from laktory.models.resources.databricks.groupmember import GroupMember
+from laktory.models.resources.databricks.userrole import UserRole
+from laktory.models.resources.pulumiresource import PulumiResource
+from laktory.models.resources.terraformresource import TerraformResource
+
+
+class UserLookup(ResourceLookup):
+    """
+    Attributes
+    ----------
+    user_id:
+         ID of the user
+    """
+
+    user_id: str = Field(serialization_alias="id")
+
+
+class User(BaseModel, PulumiResource, TerraformResource):
+    """
+    Databricks user
+
+    Attributes
+    ----------
+    disable_as_user_deletion:
+        If `True` user is disabled instead of delete when the resource is deleted
+    display_name:
+        Display name for the user
+    group_ids:
+        List of the group ids that the user should be member of.
+    lookup_existing:
+        Specifications for looking up existing resource. Other attributes will
+        be ignored.
+    roles:
+        List of roles assigned to the user e.g. ("account_admin")
+    workspace_access
+        When `True`, the user is allowed to have workspace access
+
+    Examples
+    --------
+    ```py
+    from laktory import models
+
+    u = models.resources.databricks.User(
+        user_name="john.doe@okube.ai",
+        display_name="John Doe",
+        group_ids=[
+            "${resources.group-role-engineer.id}",
+            "${resources.group-domain-finance.id}",
+        ],
+        roles=["account_admin"],
+    )
+    ```
+    """
+
+    disable_as_user_deletion: bool = False
+    display_name: str = None
+    group_ids: list[str] = []
+    lookup_existing: UserLookup = Field(None, exclude=True)
+    roles: list[str] = []
+    user_name: str
+    workspace_access: bool = None
+
+    # ----------------------------------------------------------------------- #
+    # Resource Properties                                                     #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def resource_key(self) -> str:
+        return self.user_name
+
+    @property
+    def additional_core_resources(self) -> list[PulumiResource]:
+        """
+        - user roles
+        - user group members
+        """
+        resources = []
+        for role in self.roles:
+            resources += [
+                UserRole(
+                    user_id=f"${{resources.{self.resource_name}.id}}",
+                    role=role,
+                )
+            ]
+
+        # Group Member
+        for group_id in self.group_ids:
+            resources += [
+                GroupMember(
+                    group_id=group_id,
+                    member_id=f"${{resources.{self.resource_name}.id}}",
+                )
+            ]
+
+        return resources
+
+    # ----------------------------------------------------------------------- #
+    # Pulumi Properties                                                       #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def pulumi_resource_type(self) -> str:
+        return "databricks:User"
+
+    @property
+    def pulumi_excludes(self) -> Union[list[str], dict[str, bool]]:
+        return ["groups", "roles", "group_ids"]
+
+    # ----------------------------------------------------------------------- #
+    # Terraform Properties                                                    #
+    # ----------------------------------------------------------------------- #
+
+    @property
+    def terraform_resource_type(self) -> str:
+        return "databricks_user"
+
+    @property
+    def terraform_excludes(self) -> Union[list[str], dict[str, bool]]:
+        return self.pulumi_excludes
