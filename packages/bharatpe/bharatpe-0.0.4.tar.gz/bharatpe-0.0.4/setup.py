e8cac6d14d8529dc6dc2e432308b1e75d98d089e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 17:08:32 +0000

    Add package bharatpe/0.0.4/bharatpe-0.0.4.tar.gz

---
+++ packages/bharatpe/bharatpe-0.0.4.tar.gz/bharatpe-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="bharatpe",
+    version="0.0.4",
+    description="Hmm, Payments API.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://t.me/North_Yankton",
+    author="Alpha",
+    author_email="imr@outlook.in",
+    license="MIT",
+    keywords="payments api bharatpe",
+    project_urls={
+        "Tracker": "https://github.com/Alpha-Like/bharatpe/issues",
+        "Community": "https://t.me/SpLBots",
+        "Source": "https://github.com/Alpha-Like/bharatpe",
+        "Documentation": "https://t.me/SpLBots",
+    },
+    python_requires="~=3.7",
+    packages=find_packages(),
+    test_suite="tests",
+    zip_safe=False,
+    install_requires = [
+        'requests',
+        'pymongo'
+    ]
+)
