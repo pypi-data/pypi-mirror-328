cfe4509df001730ca04d8614423806e98924f614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-21 18:07:36 +0000

    Add package Fnai-deepseek/0.1/fnai_deepseek-0.1.tar.gz

---
+++ packages/Fnai-deepseek/fnai_deepseek-0.1.tar.gz/fnai_deepseek-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Fnai_deepseek',  # 替换为你的库名
+    version='0.1',  # 替换为你的版本号
+    packages=find_packages(),  # 查找所有的包
+    install_requires=[  # 如果你的库有依赖，可以在这里列出
+        # 'requests',
+    ],
+    classifiers=[  # 可选的分类
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',  # 设置支持的Python版本
+)
