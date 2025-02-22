2feb84c8e29fc4a5796931f9045197df38ed7007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:11:45 +0000

    Add package oxenai/0.27.1/oxenai-0.27.1.tar.gz

---
+++ packages/oxenai/oxenai-0.27.1.tar.gz/oxenai-0.27.1/src/py_entry.rs
@@ -0,0 +1,64 @@
+use pyo3::prelude::*;
+
+use liboxen::model::MetadataEntry;
+
+#[pyclass]
+pub struct PyEntry {
+    _entry: MetadataEntry,
+}
+
+#[pymethods]
+impl PyEntry {
+    fn __repr__(&self) -> String {
+        format!("{:?}", self._entry)
+    }
+
+    fn __str__(&self) -> String {
+        self._entry.filename.to_owned()
+    }
+
+    #[getter]
+    pub fn hash(&self) -> &str {
+        &self._entry.hash
+    }
+
+    #[getter]
+    pub fn filename(&self) -> &str {
+        &self._entry.filename
+    }
+
+    #[getter]
+    pub fn data_type(&self) -> String {
+        self._entry.data_type.to_string()
+    }
+
+    #[getter]
+    pub fn mime_type(&self) -> &str {
+        &self._entry.mime_type
+    }
+
+    #[getter]
+    pub fn size(&self) -> u64 {
+        self._entry.size
+    }
+
+    #[getter]
+    pub fn is_dir(&self) -> bool {
+        self._entry.is_dir
+    }
+
+    #[getter]
+    pub fn path(&self) -> String {
+        if let Some(resource) = &self._entry.resource {
+            resource.path.to_string_lossy().to_string()
+        } else {
+            self._entry.filename.to_owned()
+        }
+    }
+}
+
+impl From<MetadataEntry> for PyEntry {
+    fn from(entry: MetadataEntry) -> PyEntry {
+        PyEntry { _entry: entry }
+    }
+}
