ea854637d47b3fbdad446fe09dd2f882b2b27a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:09:50 +0000

    Add package autocorrect-kh/0.2.0/autocorrect_kh-0.2.0.tar.gz

---
+++ packages/autocorrect-kh/autocorrect_kh-0.2.0.tar.gz/autocorrect_kh-0.2.0/autocorrect_kh/__init__.py
@@ -0,0 +1,9 @@
+# File: autocorrect_kh/__init__.py
+from .autocorrect import (
+    autocorrect_address_1,
+    autocorrect_address_2,
+    phum_dict,
+    khum_dict,
+    district_dict,
+    province_dict
+)
