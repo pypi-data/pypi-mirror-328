756323f84e41015ad1ab7ffba73f7ac031a2846f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 00:10:07 +0000

    Add package hello-py-1802/0.1.0/hello_py_1802-0.1.0.tar.gz

---
+++ packages/hello-py-1802/hello_py_1802-0.1.0.tar.gz/hello_py_1802-0.1.0/README.md
@@ -0,0 +1,8 @@
+# Hello Package
+
+This is a simple Python package that returns "hello".
+
+## Installation
+
+```sh
+pip install hello-package
---
+++ packages/hello-py-1802/hello_py_1802-0.1.0.tar.gz/hello_py_1802-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello-py-1802",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+)
