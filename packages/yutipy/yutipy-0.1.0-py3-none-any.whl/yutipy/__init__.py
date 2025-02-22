aba1d055fd7de9587f4c158cbb4854ef0eba6a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:09:52 +0000

    Add package yutipy/0.1.0/yutipy-0.1.0-py3-none-any.whl

---
+++ packages/yutipy/yutipy-0.1.0-py3-none-any.whl/yutipy/__init__.py
@@ -0,0 +1,13 @@
+from .deezer import Deezer
+from .itunes import Itunes
+from .models import MusicInfo
+from .musicyt import MusicYT
+from .spotify import Spotipy
+
+__all__ = [
+    "Deezer",
+    "Itunes",
+    "MusicInfo",
+    "MusicYT",
+    "Spotipy",
+]
