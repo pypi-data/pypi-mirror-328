a06ab197a0efb05c80c9cf604aa0ccc6dbcc99bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 09:09:00 +0000

    Add package catrxneng/1.0.0/catrxneng-1.0.0-py3-none-any.whl

---
+++ packages/catrxneng/catrxneng-1.0.0-py3-none-any.whl/catrxneng/co.py
@@ -0,0 +1,24 @@
+from .utils import dH, S
+
+
+class CO:
+    def __init__(self, T):
+        self.T = T
+        self.Hf_298 = -110530  # J/mol
+        self.S_298 = 197.66  # J/mol
+        self.thermo_params = {
+            "A": 25.56759,
+            "B": 6.096130,
+            "C": 4.054656,
+            "D": -2.671301,
+            "E": 0.131021,
+            "F": -118.0089,
+            "G": 227.3665,
+            "H": -110.5271,
+        }
+
+    def H(self):
+        return self.Hf_298 + dH(self)
+
+    def S(self):
+        return S(self)
---
+++ packages/catrxneng/catrxneng-1.0.0-py3-none-any.whl/catrxneng/utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def dH(species):
+    t = species.T / 1000
+    params = species.thermo_params
+    return (
+        params.A * t
+        + params.B * t**2 / 2
+        + params.C * t**3 / 3
+        + params.D * t**4 / 4
+        - params.E / t
+        + params.F
+        - params.H
+    )
+
+
+def S(species):
+    t = species.T / 1000
+    params = species.thermo_params
+    return (
+        params.A * np.log(t)
+        + params.B * t
+        + params.C * t**2 / 2
+        + params.D * t**3 / 3
+        - params.E / (2 * t**2)
+        + params.G
+    )
