2475f7cce72517e4c0764f57dce90426adf17ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:10 +0000

    Add package microfading/1.3.3/microfading-1.3.3-py3-none-any.whl

---
+++ packages/microfading/microfading-1.3.3-py3-none-any.whl/microfading/__init__.py
@@ -0,0 +1,3 @@
+from .microfading import MFT, DB, get_datasets, get_creators, get_DB, get_institutions, get_objects, get_light_dose_info, get_persons, process_rawdata, create_DB, get_DB_path, update_DB_objects, update_DB_projects, add_new_creator, add_new_institution, add_new_project, add_new_object, add_new_person, add_devices, add_references, get_devices, get_white_standards, set_colorimetry_info, set_DB, set_light_dose, set_exposure_conditions, get_exposure_conditions, get_colorimetry_info, get_DB_config
+
+__version__ = "1.3.3"
