fde003771b7ef67632db1890bf8d547216423df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:09:38 +0000

    Add package roboquant/0.9.10/roboquant-0.9.10.tar.gz

---
+++ packages/roboquant/roboquant-0.9.10.tar.gz/roboquant-0.9.10/roboquant/feeds/__init__.py
@@ -0,0 +1,27 @@
+from .util import AggregatorFeed, GroupingFeed
+from .csvfeed import CSVFeed
+from .eventchannel import EventChannel
+from .feed import Feed
+from .historic import HistoricFeed
+from .randomwalk import RandomWalk
+from .sql import SQLFeed
+
+# from .parquetfeed import ParquetFeed
+# from .avrofeed import AvroFeed
+
+try:
+    from .yahoo import YahooFeed
+except ImportError:
+    pass
+
+__all__ = [
+    "Feed",
+    "EventChannel",
+    "CSVFeed",
+    "HistoricFeed",
+    "RandomWalk",
+    "SQLFeed",
+    "AggregatorFeed",
+    "GroupingFeed",
+    "YahooFeed",
+]
---
+++ packages/roboquant/roboquant-0.9.10.tar.gz/roboquant-0.9.10/roboquant/strategies/multistrategy.py
@@ -0,0 +1,51 @@
+from itertools import groupby
+from statistics import mean
+from typing import Literal
+
+from roboquant.event import Event
+from roboquant.signal import Signal
+from roboquant.strategies.strategy import Strategy
+
+
+class MultiStrategy(Strategy):
+    """Combine one or more strategies. The MultiStrategy provides additional control on how to handle conflicting
+    signals for the same asset via the signal_filter:
+
+    - first: in case of multiple signals for the same asset, the first one wins
+    - last:  in case of multiple signals for the same asset, the last one wins.
+    - mean: return the mean of the signal ratings. All signals will be ENTRY and EXIT.
+    - none: return all signals. This is also the default.
+    """
+
+    def __init__(
+        self,
+        *strategies: Strategy,
+        signal_filter: Literal["last", "first", "none", "mean"] = "none"
+    ):
+        super().__init__()
+        self.strategies = list(strategies)
+        self.filter = signal_filter
+
+    def create_signals(self, event: Event) -> list[Signal]:
+        signals: list[Signal] = []
+        for strategy in self.strategies:
+            signals += strategy.create_signals(event)
+
+        match self.filter:
+            case "none":
+                return signals
+            case "last":
+                s = {s.asset: s for s in signals}
+                return list(s.values())
+            case "first":
+                s = {s.asset: s for s in reversed(signals)}
+                return list(s.values())
+            case "mean":
+                result = []
+                for key, group in groupby(signals, lambda signal: signal.asset):
+                    rating = mean(signal.rating for signal in group)
+                    if rating:
+                        result.append(Signal(key, rating))
+                return result
+
+        raise ValueError("unsupported signal filter")
