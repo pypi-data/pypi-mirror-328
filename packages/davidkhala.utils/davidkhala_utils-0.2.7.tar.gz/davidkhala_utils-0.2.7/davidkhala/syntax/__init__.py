013f308f6764b10fdb8421ebd4afe7bcdeaa89b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:25 +0000

    Add package davidkhala.utils/0.2.7/davidkhala_utils-0.2.7.tar.gz

---
+++ packages/davidkhala.utils/davidkhala_utils-0.2.7.tar.gz/davidkhala_utils-0.2.7/davidkhala/syntax/__init__.py
@@ -0,0 +1,33 @@
+import os
+from enum import Enum
+from typing import Callable, Iterable
+import platform
+
+
+class Package:
+    def __init__(self, name: str):
+        import re
+        assert re.match('^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$', name, re.IGNORECASE)
+
+
+class NameEnum(Enum):
+    @staticmethod
+    def _generate_next_value_(name, *args):
+        return name
+
+
+def for_each(content: Iterable, on_each: Callable[[int, str], None]):
+    for index, value in enumerate(content):
+        on_each(index, value)
+
+
+def is_windows() -> bool:
+    return os.name == 'nt' or platform.system() == 'Windows'
+
+
+def is_linux() -> bool:
+    return platform.system() == 'Linux'
+
+
+def is_mac() -> bool:
+    return platform.system() == 'Darwin'
