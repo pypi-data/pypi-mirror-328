fa7ba6fa9b4661141824fa10f33119a91d3e4838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:24 +0000

    Add package spherical-inr/0.1.4/spherical_inr-0.1.4.tar.gz

---
+++ packages/spherical-inr/spherical_inr-0.1.4.tar.gz/spherical_inr-0.1.4/spherical_inr/__init__.py
@@ -0,0 +1,20 @@
+"""
+spherical_inr package
+=====================
+
+This package provides modules for spherical implicit neural representations, including
+activations, positional encoding, and transforms.
+
+Modules:
+    activations: Custom activation functions.
+    inr: Implicit neural representations.
+    positional_encoding: Herglotz and other positional encoding utilities.
+    transforms: Coordinate transformation utilities.
+"""
+
+__version__ = "0.1.4"
+
+from .activations import *
+from .inr import *
+from .positional_encoding import *
+from .transforms import *
