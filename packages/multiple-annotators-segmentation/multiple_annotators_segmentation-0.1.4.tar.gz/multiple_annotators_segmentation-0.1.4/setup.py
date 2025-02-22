7772c2da9c29af43ac39c0f34725d2d98cb19379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:09:56 +0000

    Add package multiple-annotators-segmentation/0.1.4/multiple_annotators_segmentation-0.1.4.tar.gz

---
+++ packages/multiple-annotators-segmentation/multiple_annotators_segmentation-0.1.4.tar.gz/multiple_annotators_segmentation-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multiple_annotators_segmentation',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow==2.15.0',
+        'matplotlib',
+        'numpy'
+    ],
+    author='Lucas Iturriago',
+    author_email='liturriago@unal.edu.co',
+    description='Library for multiple annotators segmentation, using custom Losses and Keras models',
+    url='https://github.com/UN-GCPDS/python-gcpds.multiple_annotators_segmentation.git',
+)
