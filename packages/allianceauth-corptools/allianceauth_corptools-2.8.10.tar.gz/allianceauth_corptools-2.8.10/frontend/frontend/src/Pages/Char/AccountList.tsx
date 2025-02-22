42df07ea778951afe39e8f04cea697f5bd38754c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:10:27 +0000

    Add package allianceauth-corptools/2.8.10/allianceauth_corptools-2.8.10.tar.gz

---
+++ packages/allianceauth-corptools/allianceauth_corptools-2.8.10.tar.gz/allianceauth_corptools-2.8.10/frontend/frontend/src/Pages/Char/AccountList.tsx
@@ -0,0 +1,97 @@
+import { CharacterPortrait, CorporationLogo } from "../../Components/EveImages/EveImages";
+import TableWrapper from "../../Components/Tables/BaseTable/TableWrapper";
+import { components } from "../../api/CtApi";
+import { getCharacterList } from "../../api/character";
+import { createColumnHelper } from "@tanstack/react-table";
+import { Badge } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const AccountList = () => {
+  const { t } = useTranslation();
+
+  const { characterID } = useParams();
+
+  const { data, isFetching } = useQuery({
+    queryKey: ["contacts", characterID],
+    queryFn: () => getCharacterList(),
+    refetchOnWindowFocus: false,
+  });
+  const columnHelper = createColumnHelper<components["schemas"]["AccountStatus"]>();
+
+  const columns = [
+    columnHelper.accessor("main.character_id", {
+      header: "",
+      enableColumnFilter: false,
+      enableSorting: false,
+      cell: (cell) => <CharacterPortrait size={32} character_id={cell.getValue()} />,
+    }),
+    columnHelper.accessor("main.character_name", {
+      header: t("Character"),
+    }),
+    columnHelper.accessor("main.corporation_id", {
+      header: "",
+      enableColumnFilter: false,
+      enableSorting: false,
+      cell: (cell) => <CorporationLogo size={32} corporation_id={cell.getValue()} />,
+    }),
+    columnHelper.accessor("main.corporation_name", {
+      header: t("Main Corporation"),
+    }),
+    columnHelper.accessor("characters", {
+      header: t("Characters"),
+      cell: (props) =>
+        props.getValue() ? (
+          <div className="flex-container text-center">
+            {props?.getValue()?.map((char) => {
+              return (
+                <Badge className="padded-label" bg={char.active ? "primary" : "danger"}>
+                  {char.character.character_name}
+                </Badge>
+              );
+            })}
+          </div>
+        ) : (
+          <></>
+        ),
+      filterFn: (row, _, filterValue) => {
+        if (!filterValue) {
+          return true;
+        } else {
+          let rowValue = row.original?.characters?.reduce((p: any, c: any) => {
+            return p + "  " + c.character.character_name;
+          }, "");
+          return rowValue ? rowValue.toLowerCase().includes(filterValue.toLowerCase()) : false;
+        }
+      },
+    }),
+    columnHelper.accessor("main", {
+      header: "",
+      enableColumnFilter: false,
+      enableSorting: false,
+      cell: (cell) =>
+        cell.getValue() ? (
+          <Link
+            className="btn btn-primary"
+            to={{
+              pathname: `/audit/r/${cell.getValue().character_id}/`,
+            }}
+          >
+            <i className="fas fa-external-link" aria-hidden="true"></i>
+          </Link>
+        ) : (
+          <></>
+        ),
+    }),
+  ];
+
+  return (
+    <>
+      <TableWrapper {...{ data, isFetching, columns }} />
+    </>
+  );
+};
+
+export default AccountList;
---
+++ packages/allianceauth-corptools/allianceauth_corptools-2.8.10.tar.gz/allianceauth_corptools-2.8.10/frontend/frontend/vite.config.ts
@@ -0,0 +1,53 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3002,
+    proxy: {
+      "/audit/api/": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/static/corptools/i18n": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+    manifest: true,
+    outDir: "build/static/",
+    rollupOptions: {
+      output: {
+        // this is to make sure our static files are in the write place when we build
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          // fake the path, we copy them over properly later
+          return `static/corptools/bs5/static/${extType}/[name]-[hash][extname]`;
+        },
+        manualChunks(id) {
+          // creating a chunk to react routes deps. Reducing the vendor chunk size
+          if (id.includes("react-router-dom") || id.includes("react-router")) {
+            return "@react-router";
+          }
+          if (
+            id.includes("react-query") ||
+            id.includes("react-select") ||
+            id.includes("javascript-time-ago")
+          ) {
+            return "@libs";
+          }
+        },
+      },
+    },
+  },
+});
