f2dead53326c1fb3613869dbc47062e0a16ea40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 08:09:08 +0000

    Add package ahmethingram/0.0.7/ahmethingram-0.0.7.tar.gz

---
+++ packages/ahmethingram/ahmethingram-0.0.7.tar.gz/ahmethingram-0.0.7/ahmethingram/__init__.py
@@ -0,0 +1,12 @@
+def say_hello(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+def say(yi):
+    print(yi)
+
+
+def test(value):
+    print("value is good!")
---
+++ packages/ahmethingram/ahmethingram-0.0.7.tar.gz/ahmethingram-0.0.7/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ahmethingram",  # اسم المكتبة
+    version="0.0.7", 
+    packages=find_packages(),
+)
