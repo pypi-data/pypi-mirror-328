a2b3e010a38c4753724538e361c66cb09a9599b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 00:10:40 +0000

    Add package autocorrect-kh/0.1.0/autocorrect_kh-0.1.0.tar.gz

---
+++ packages/autocorrect-kh/autocorrect_kh-0.1.0.tar.gz/autocorrect_kh-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autocorrect_kh",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'autocorrect_kh': [
+            'data/phum/*.txt',
+            'data/khum/*.txt',
+            'data/*.txt'
+        ],
+    },
+    install_requires=[
+        "jellyfish",
+        "regex",
+    ],
+    author="Kim Ouddommony",
+    author_email="kimmony039@gmail.com",
+    description="An autocorrect library for Khmer text",
+    url="https://github.com/monykappa/autocorrect-kh-address.git",
+)
