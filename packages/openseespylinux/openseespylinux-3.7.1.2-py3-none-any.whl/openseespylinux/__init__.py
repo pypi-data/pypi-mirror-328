652fdd8326166657d37db2e7b4c427ec66a01fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 11:09:06 +0000

    Add package openseespylinux/3.7.1.2/openseespylinux-3.7.1.2-py3-none-any.whl

---
+++ packages/openseespylinux/openseespylinux-3.7.1.2-py3-none-any.whl/openseespylinux/__init__.py
@@ -0,0 +1,18 @@
+
+import sys
+
+# only work for 64 bit system
+if sys.maxsize < 2**31:
+    raise RuntimeError('64 bit system is required')
+
+# platform dependent
+if sys.platform.startswith('linux'):
+
+    try:
+        from openseespylinux.opensees import *
+    except:
+        raise RuntimeError('Failed to import openseespy on Linux.')
+
+else:
+
+    raise RuntimeError('This package is for Linux only.')
