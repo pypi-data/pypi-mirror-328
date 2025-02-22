f3c4f2233605d4613ada0a945433dedd4aee9eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:09:40 +0000

    Add package spatialformer/0.0.14/spatialformer-0.0.14.tar.gz

---
+++ packages/spatialformer/spatialformer-0.0.14.tar.gz/spatialformer-0.0.14/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open('./requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='spatialformer',  
+    version='0.0.14',
+    author='TerminatorJ',
+    author_email='wangjun19950708@gmail.com',
+    description='A single-cell foundation model focus on the spatial cell-cell colocalization',
+    url='https://github.com/TerminatorJ/Spatialformer/', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', 
+    ],
+    python_requires='>=3.9', 
+    install_requires=requirements,
+    include_package_data=True,
+    packages=find_packages(include=['spatialformer', 'spatialformer.tools', 'spatialformer.model', 
+                                    'spatialformer.model.submodules', 'spatialformer.config', 
+                                    'spatialformer.tokenizer']),
+   
+)
