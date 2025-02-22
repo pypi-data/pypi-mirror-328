a4e3737ef42be75d858c73b98e62183dcd836157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:03 +0000

    Add package gnukek-cli/1.0.0a1/gnukek_cli-1.0.0a1.tar.gz

---
+++ packages/gnukek-cli/gnukek_cli-1.0.0a1.tar.gz/gnukek_cli-1.0.0a1/gnukek_cli/commands/verify.py
@@ -0,0 +1,30 @@
+from io import FileIO
+
+import click
+from gnukek.constants import CHUNK_LENGTH
+
+from gnukek_cli.command_handlers.verify import VerifyContext, VerifyHandler
+
+
+@click.command()
+@click.argument("signature_file", type=click.File("rb"))
+@click.argument("original_file", type=click.File("rb"))
+@click.option("-k", "--key", help="key id to use")
+@click.option(
+    "--chunk-size",
+    type=int,
+    default=CHUNK_LENGTH,
+    show_default=True,
+    help="chunk size in bytes, use 0 to process file in one go",
+)
+def verify(signature_file: FileIO, original_file: FileIO, key, chunk_size):
+    """Verify signature."""
+    context = VerifyContext(
+        signature_file=signature_file,
+        original_file=original_file,
+        key_id=key,
+        chunk_length=chunk_size,
+    )
+
+    handler = VerifyHandler(context)
+    handler()
