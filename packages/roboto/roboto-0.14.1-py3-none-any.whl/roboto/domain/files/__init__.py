ca3550dad2d421de93f5c3fcec1076bdb6a5a601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 08:09:17 +0000

    Add package roboto/0.14.1/roboto-0.14.1-py3-none-any.whl

---
+++ packages/roboto/roboto-0.14.1-py3-none-any.whl/roboto/domain/files/__init__.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2024 Roboto Technologies, Inc.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from .file import File
+from .operations import (
+    AbortTransactionsRequest,
+    DeleteFileRequest,
+    DirectoryContentsPage,
+    FileRecordRequest,
+    ImportFileRequest,
+    QueryFilesRequest,
+    RenameFileRequest,
+    SignedUrlResponse,
+    UpdateFileRecordRequest,
+)
+from .record import (
+    CredentialProvider,
+    DirectoryRecord,
+    FileRecord,
+    FileStatus,
+    FileStorageType,
+    FileTag,
+    IngestionStatus,
+    S3Credentials,
+)
+
+__all__ = (
+    "AbortTransactionsRequest",
+    "CredentialProvider",
+    "DeleteFileRequest",
+    "File",
+    "DirectoryContentsPage",
+    "FileRecord",
+    "FileRecordRequest",
+    "FileStatus",
+    "FileStorageType",
+    "FileTag",
+    "ImportFileRequest",
+    "IngestionStatus",
+    "DirectoryRecord",
+    "NoopProgressMonitor",
+    "NoopProgressMonitorFactory",
+    "ProgressMonitor",
+    "ProgressMonitorFactory",
+    "QueryFilesRequest",
+    "RenameFileRequest",
+    "S3Credentials",
+    "SignedUrlResponse",
+    "UpdateFileRecordRequest",
+)
