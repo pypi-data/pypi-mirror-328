76c0b3f214e11366d00255ab853b5b751e903632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:09:16 +0000

    Add package devjoni-stllib/0.0.2/devjoni_stllib-0.0.2.tar.gz

---
+++ packages/devjoni-stllib/devjoni_stllib-0.0.2.tar.gz/devjoni_stllib-0.0.2/src/devjoni/stllib/stllib.py
@@ -0,0 +1,17 @@
+'''stllib - The 3D model parts by DEV Joni
+Copyright (C) 2024 DEV Joni / Joni Kemppainen 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+__version__ = "0.0.2"
