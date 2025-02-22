bb80fd387a9c0b25df3078201da1398c8122b617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:31 +0000

    Add package alembic-postgresql-enum/1.7.0/alembic_postgresql_enum-1.7.0-py3-none-any.whl

---
+++ packages/alembic-postgresql-enum/alembic_postgresql_enum-1.7.0-py3-none-any.whl/alembic_postgresql_enum/configuration.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Callable
+
+
+@dataclass
+class Config:
+    add_type_ignore: bool = False
+    include_name: Callable[[str], bool] = lambda _: True
+    drop_unused_enums: bool = True
+    detect_enum_values_changes: bool = True
+    force_dialect_support: bool = False
+
+
+_config = Config()
+
+
+def set_configuration(config: Config):
+    global _config
+    _config = config
+
+
+def get_configuration() -> Config:
+    return _config
