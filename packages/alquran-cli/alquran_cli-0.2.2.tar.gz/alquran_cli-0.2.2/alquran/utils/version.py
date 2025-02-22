4b9444135354a9ae91548b80701bea64eee81fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:35:10 +0000

    Add package alquran-cli/0.2.2/alquran_cli-0.2.2.tar.gz

---
+++ packages/alquran-cli/alquran_cli-0.2.2.tar.gz/alquran_cli-0.2.2/alquran/utils/version.py
@@ -0,0 +1,23 @@
+import importlib.metadata
+import httpx
+
+def check_package_update(package_name):
+    try:
+        # Get installed package version
+        installed_version = importlib.metadata.version(package_name)
+        
+        # Get latest version from PyPI
+        response = httpx.get(f'https://pypi.org/pypi/{package_name}/json')
+        response.raise_for_status()
+        latest_version = response.json()['info']['version']
+        
+        if installed_version != latest_version:
+            print(f"Package '{package_name}' is outdated: {installed_version} -> {latest_version}")
+        else:
+            print(f"Package '{package_name}' is up to date ({installed_version})")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"Package '{package_name}' is not installed.")
+    except httpx.RequestError as e:
+        print(f"Error checking latest version: {e}")
+
+
