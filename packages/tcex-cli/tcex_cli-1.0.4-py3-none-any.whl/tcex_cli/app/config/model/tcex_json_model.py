02f4d1d16eb94b0fde8d09119ed5b4b6bcc63c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:10:21 +0000

    Add package tcex-cli/1.0.4/tcex_cli-1.0.4-py3-none-any.whl

---
+++ packages/tcex-cli/tcex_cli-1.0.4-py3-none-any.whl/tcex_cli/app/config/model/tcex_json_model.py
@@ -0,0 +1,47 @@
+"""TcEx Framework Module"""
+
+# standard library
+from pathlib import PosixPath
+
+# third-party
+from pydantic import BaseModel, validator
+
+__all__ = ['TcexJsonModel']
+
+
+# pylint: disable=no-self-argument
+class PackageModel(BaseModel):
+    """Model definition for tcex_json.package"""
+
+    app_name: str
+    app_version: str | None
+    excludes: list
+    output_dir: str = 'target'
+
+    @validator('excludes')
+    def sorted(cls, v):
+        """Change value for excludes field."""
+        # the requirements.txt file is required for App Builder
+        v = [e for e in v if e != 'requirements.txt']
+        return list(sorted(set(v)))
+
+    class Config:
+        """DataModel Config"""
+
+        validate_assignment = True
+
+
+class TcexJsonModel(BaseModel):
+    """Model definition for tcex.json configuration file"""
+
+    package: PackageModel
+    template_name: str | None
+    template_repo_hash: str | None = None
+    template_type: str | None
+
+    class Config:
+        """DataModel Config"""
+
+        json_encoders = {PosixPath: lambda v: v.original_value}
+        use_enum_values = True
+        validate_assignment = True
---
+++ packages/tcex-cli/tcex_cli-1.0.4-py3-none-any.whl/tcex_cli/pleb/cached_property.py
@@ -0,0 +1,32 @@
+"""TcEx Framework Module"""
+
+# standard library
+from collections.abc import Callable
+from functools import cached_property as functool_cached_property
+from typing import Generic, TypeVar
+
+R = TypeVar('R')
+
+
+class cached_property(functool_cached_property, Generic[R]):
+    """Customized cached_property."""
+
+    # pylint: disable=useless-super-delegation
+    def __init__(self, func: Callable[..., R]):
+        """Initialize instance properties."""
+        super().__init__(func)
+
+    instances = []
+
+    def __get__(self, instance, owner=None) -> R:  # type: ignore
+        """Override method."""
+        self.instances.append(instance)
+        return super().__get__(instance, owner)  # type: ignore
+
+    @staticmethod
+    def _reset():
+        """Reset cache"""
+        for i in cached_property.instances:
+            for key, value in i.__dict__.items():
+                if isinstance(value, cached_property):
+                    del i.__dict__[key]
