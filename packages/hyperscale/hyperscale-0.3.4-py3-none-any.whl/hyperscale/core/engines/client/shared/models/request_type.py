69a7659d638353c4e8d9afeb31026912b2d8313e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 08:09:29 +0000

    Add package hyperscale/0.3.4/hyperscale-0.3.4-py3-none-any.whl

---
+++ packages/hyperscale/hyperscale-0.3.4-py3-none-any.whl/hyperscale/core/engines/client/shared/models/request_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class RequestType(Enum):
+    CUSTOM = 'CUSTOM'
+    HTTP = "HTTP"
+    HTTP2 = "HTTP2"
+    HTTP3 = "HTTP3"
+    WEBSOCKET = "WEBSOCKET"
+    GRAPHQL = "GRAPHQL"
+    GRAPHQL_HTTP2 = "GRAPHQL_HTTP2"
+    GRPC = "GRPC"
+    PLAYWRIGHT = "PLAYWRIGHT"
+    UDP = "UDP"
