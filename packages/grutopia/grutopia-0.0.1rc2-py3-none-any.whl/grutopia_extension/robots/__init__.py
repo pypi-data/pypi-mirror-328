4418d0f5c1d9eda665b9ea31fd413c6eeafa909e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:09:06 +0000

    Add package grutopia/0.0.1rc2/grutopia-0.0.1rc2-py3-none-any.whl

---
+++ packages/grutopia/grutopia-0.0.1rc2-py3-none-any.whl/grutopia_extension/robots/__init__.py
@@ -0,0 +1,11 @@
+from grutopia_extension.robots import (
+    aliengo,
+    camera_oracle,
+    franka,
+    g1,
+    gr1,
+    humanoid,
+    humanoid_with_hand,
+    mocap_controlled_franka,
+    npc,
+)
