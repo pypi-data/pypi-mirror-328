20987b56bdaada00f03728df161a6f5e42eb7080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:09:08 +0000

    Add package mkdocs-nav-async/0.23/mkdocs_nav_async-0.23.tar.gz

---
+++ packages/mkdocs-nav-async/mkdocs_nav_async-0.23.tar.gz/mkdocs_nav_async-0.23/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-nav-async',
+    version='0.23',
+    description='MkDocs plugin to extract navigation and load it asynchronously',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/0x10-z/mkdocs-nav-async",
+    author='Iker Ocio (0x10)',
+    install_requires=['mkdocs', 'lxml', 'htmlmin'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'mkdocs_nav_async': ['loading_icons/*.svg'],
+    },
+    entry_points={
+        'mkdocs.plugins': [
+            'nav_async = mkdocs_nav_async.plugin:NavAsync',
+        ]
+    }
+)
