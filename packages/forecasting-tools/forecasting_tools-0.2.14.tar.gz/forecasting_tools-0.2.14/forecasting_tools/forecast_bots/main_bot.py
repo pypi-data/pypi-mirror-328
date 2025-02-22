344869a8ac8ded8f100c35a0cd65936061344cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:35:02 +0000

    Add package forecasting-tools/0.2.14/forecasting_tools-0.2.14.tar.gz

---
+++ packages/forecasting-tools/forecasting_tools-0.2.14.tar.gz/forecasting_tools-0.2.14/forecasting_tools/forecast_bots/main_bot.py
@@ -0,0 +1,11 @@
+from forecasting_tools.forecast_bots.official_bots.q1_template_bot import (
+    Q1TemplateBot,
+)
+
+
+class MainBot(Q1TemplateBot):
+    """
+    Main bot is the template bot for now till we can confirm other bots are better.
+    """
+
+    pass
