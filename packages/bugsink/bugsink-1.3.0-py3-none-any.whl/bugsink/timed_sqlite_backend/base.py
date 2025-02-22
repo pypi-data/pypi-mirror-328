8449cb58c072d6144475eef16c7c84ff2d42a1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 08:09:33 +0000

    Add package bugsink/1.3.0/bugsink-1.3.0-py3-none-any.whl

---
+++ packages/bugsink/bugsink-1.3.0-py3-none-any.whl/bugsink/timed_sqlite_backend/base.py
@@ -0,0 +1,103 @@
+import time
+from contextlib import contextmanager
+from django.conf import settings
+
+from django.db.backends.sqlite3.base import (
+    DatabaseWrapper as UnpatchedDatabaseWrapper, SQLiteCursorWrapper as UnpatchedSQLiteCursorWrapper,
+)
+
+
+_allow_long_running_queries = False
+
+
+def allow_long_running_queries():
+    """Set a global flag to allow long-running queries. Useful for one-off commands, where slowness is expected."""
+    global _allow_long_running_queries
+    _allow_long_running_queries = True
+
+
+@contextmanager
+def limit_runtime(conn, seconds):
+    global _allow_long_running_queries
+
+    start = time.time()
+
+    def check_time():
+        if _allow_long_running_queries:  # check in-the-callback, to avoid
+            return 0
+
+        if time.time() > start + seconds:
+            return 1
+
+        return 0
+
+    # Set the progress handler to check the time; 10_000 is the number of SQLite VM instructions between invocations;
+    # Simon Willison's experiments in Datasette suggest to use 1_000 here; but I don't care about precision so much
+    # (it's just a final backstop) and I want to avoid the calls-into-Python (expensive!) as much as possible so I pick
+    # a higher value.
+    conn.set_progress_handler(check_time, 10_000)
+
+    yield
+
+    conn.set_progress_handler(None, 0)
+
+
+class PrintOnClose(object):
+    def __init__(self, conn):
+        self.conn = conn
+
+    def __setattr__(self, item, value):
+        if item == "conn":
+            return super().__setattr__(item, value)
+        return setattr(self.conn, item, value)
+
+    def __getattr__(self, item):
+        return getattr(self.conn, item)
+
+    def __hasattr__(self, item):
+        return hasattr(self.conn, item)
+
+    def close(self):
+        print("Connection closed", id(self.conn))
+        self.conn.close()
+
+
+class DatabaseWrapper(UnpatchedDatabaseWrapper):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # This makes the cursor act like a debug cursor (storing queries) even when DEBUG=False. We need this in
+        # PerformanceStatsMiddleware to provide a query-count. We expect the number of such-stored queries to be small
+        # (negligible memory impact) because Bugsink doesn't do many queries generally. But strictly speaking we could
+        # say that this should only be turned on when the result is actually used, i.e. when performance logging is
+        # enabled.
+        self.force_debug_cursor = True
+
+    # def get_new_connection(self, conn_params):
+    #     result = super().get_new_connection(conn_params)
+    #     import threading
+    #     print("Connection created", conn_params["database"], id(result), threading.current_thread().name)
+    #     return PrintOnClose(result)
+
+    def create_cursor(self, name=None):
+        return self.connection.cursor(factory=SQLiteCursorWrapper)
+
+
+class SQLiteCursorWrapper(UnpatchedSQLiteCursorWrapper):
+
+    def execute(self, query, params=None):
+        if settings.I_AM_RUNNING == "MIGRATE":
+            # migrations in Sqlite are often slow (drop/recreate tables, etc); so we don't want to limit them
+            return super().execute(query, params)
+
+        with limit_runtime(self.connection, 5.0):
+            return super().execute(query, params)
+
+    def executemany(self, query, param_list):
+        if settings.I_AM_RUNNING == "MIGRATE":
+            # migrations in Sqlite are often slow (drop/recreate tables, etc); so we don't want to limit them
+            return super().executemany(query, param_list)
+
+        with limit_runtime(self.connection, 5.0):
+            return super().executemany(query, param_list)
---
+++ packages/bugsink/bugsink-1.3.0-py3-none-any.whl/events/management/commands/make_consistent.py
@@ -0,0 +1,120 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+
+from releases.models import Release
+from issues.models import Issue, Grouping, TurningPoint
+from events.models import Event
+from projects.models import Project
+
+from bugsink.transaction import immediate_atomic
+from bugsink.timed_sqlite_backend.base import allow_long_running_queries
+from bugsink.moreiterutils import batched
+
+
+class DryRunException(Exception):
+    # transaction.rollback doesn't work in atomic blocks; a poor man's substitute is to just raise something specific.
+    pass
+
+
+def _delete_for_missing_fk(clazz, field_name):
+    """Delete all objects of class clazz of which the field field_name points to a non-existing object or null"""
+    BATCH_SIZE = 1_000
+
+    dangling_fks = set()
+
+    field = clazz._meta.get_field(field_name)
+    related_model = field.related_model
+
+    # We just load _all_ available PKs into memory; it's expected that environments with millions of records will
+    # have enough RAM to deal with this (millions would translate into ~Gigabytes of RAM).
+    available_keys = set(related_model.objects.values_list('pk', flat=True))
+
+    # construct "dangling_fks"
+    for batch in batched(clazz.objects.values_list(field.get_attname(), flat=True).distinct(), BATCH_SIZE):
+        for key in batch:
+            if key not in available_keys:
+                dangling_fks.add(key)
+
+    def _del(deletion_kwargs, msg_kind):
+        total_cnt, d_of_counts = clazz.objects.filter(**deletion_kwargs).delete()
+        count = d_of_counts.get(clazz._meta.label, 0)
+        if count == 0:
+            return
+        print("Deleted %d %ss, because their %s was %s" % (count, clazz.__name__, field_name, msg_kind))
+
+    _del({field.get_attname(): None}, "NULL")
+    for batch in batched(dangling_fks, BATCH_SIZE):
+        _del({field.get_attname() + '__in': batch}, "non-existing")
+
+
+def make_consistent():
+    # Delete all issues that have no events; they should not exist and in fact break various templates and views
+    for issue in Issue.objects.annotate(fresh_event_count=Count('event')).filter(fresh_event_count=0):
+        print("Deleting issue %s, because it has 0 events" % issue)
+        issue.delete()
+
+    # Various "dangling pointer" deletions:
+    _delete_for_missing_fk(Issue, 'project')
+
+    _delete_for_missing_fk(Grouping, 'project')
+    _delete_for_missing_fk(Grouping, 'issue')
+
+    _delete_for_missing_fk(TurningPoint, 'issue')
+
+    _delete_for_missing_fk(Release, 'project')
+
+    _delete_for_missing_fk(Event, 'project')
+    _delete_for_missing_fk(Event, 'issue')
+
+    for event in Event.objects.filter(turningpoint__isnull=False, never_evict=False).distinct():
+        print("Setting event %s to never_evict because it has a turningpoint" % event)
+        event.never_evict = True
+        event.save()
+
+    # Update counters
+    for issue in Issue.objects.all():
+        if issue.stored_event_count != issue.event_set.count():
+            print("Updating event count for issue %s from %d to %d" % (
+                issue, issue.stored_event_count, issue.event_set.count()))
+            issue.stored_event_count = issue.event_set.count()
+            issue.save()
+
+    for project in Project.objects.all():
+        if project.stored_event_count != project.event_set.count():
+            print("Updating event count for project %s from %d to %d" % (
+                project, project.stored_event_count, project.event_set.count()))
+            project.stored_event_count = project.event_set.count()
+            project.save()
+
+    # Alternatively (but does it work in MySQL?)
+    # Issue.objects.update(
+    #     stored_event_count=Subquery(
+    #         Issue.objects.filter(pk=OuterRef('pk')).annotate(recount=Count('event')).values('recount')[:1])
+    # )
+
+    # Project.objects.update(
+    #     stored_event_count=Subquery(
+    #         Project.objects.filter(pk=OuterRef('pk')).annotate(recount=Count('event')).values('recount')[:1])
+    # )
+
+
+class Command(BaseCommand):
+    help = """Make the database consistent by deleting dangling objects (issues, events, etc) and updating counters."""
+
+    # In theory, this command should not be required, because Bugsink _should_ leave itself in a consistent state after
+    # every operation. However, in practice Bugsink may not always do as promised, people reach into the database for
+    # whatever reason, or things go out of whack during development.
+
+    def add_arguments(self, parser):
+        parser.add_argument('--dry-run', action='store_true', help="Roll back all changes after making them.")
+
+    def handle(self, *args, **options):
+        allow_long_running_queries()
+
+        try:
+            with immediate_atomic():
+                make_consistent()
+                if options['dry_run']:
+                    raise DryRunException("Dry run requested; rolling back changes.")
+        except DryRunException:
+            print("Changes have been rolled back (dry-run)")
