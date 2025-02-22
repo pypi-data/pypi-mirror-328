dbc7ff4675b3bd696098e1d58b8bf84412c51c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:19 +0000

    Add package piwiPre/0.23.6/piwiPre-0.23.6-py3-none-any.whl

---
+++ packages/piwiPre/piwiPre-0.23.6-py3-none-any.whl/piwiPre/__main__.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------------------------------------------------------
+# piwiPre project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini(at)gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+import sys
+from piwiPre.pwpMain import pwp_main
+
+if __name__ == "__main__":
+    pwp_main(sys.argv[1:])
