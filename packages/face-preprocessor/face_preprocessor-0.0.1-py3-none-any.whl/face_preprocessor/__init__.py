276527b800b57ec386a19bdc573ca7bdaf9cf032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:02 +0000

    Add package face-preprocessor/0.0.1/face_preprocessor-0.0.1-py3-none-any.whl

---
+++ packages/face-preprocessor/face_preprocessor-0.0.1-py3-none-any.whl/face_preprocessor/__init__.py
@@ -0,0 +1,6 @@
+from .detector import FaceDetector
+from .processor import ImageProcessor
+from .config import FaceDetectionConfig
+
+__version__ = "0.0.1"
+__all__ = ["FaceDetector", "ImageProcessor", "FaceDetectionConfig"]
