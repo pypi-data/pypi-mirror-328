83209758bf0d26fe8b530f1ee9ed462787fe7478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:09:09 +0000

    Add package aos-keys/1.8.0b5/aos_keys-1.8.0b5-py3-none-any.whl

---
+++ packages/aos-keys/aos_keys-1.8.0b5-py3-none-any.whl/aos_keys/__init__.py
@@ -0,0 +1,10 @@
+#
+#  Copyright (c) 2018-2022 Renesas Inc.
+#  Copyright (c) 2018-2022 EPAM Systems Inc.
+#
+"""Main aos-keys module."""
+
+from aos_keys.main import main
+
+if __name__ == '__main__':
+    main()
