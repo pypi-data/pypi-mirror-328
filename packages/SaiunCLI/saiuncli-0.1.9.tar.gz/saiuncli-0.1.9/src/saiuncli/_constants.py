03479d84638e7922a8f417e485ec9cda697341c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:10:00 +0000

    Add package SaiunCLI/0.1.9/saiuncli-0.1.9.tar.gz

---
+++ packages/SaiunCLI/saiuncli-0.1.9.tar.gz/saiuncli-0.1.9/src/saiuncli/_constants.py
@@ -0,0 +1,8 @@
+_ROOT_COMMAND_NAME = "root"
+_HELP_NAME = "help"
+_VERSION_NAME = "version"
+_GLOBAL_FLAGS = {
+    _HELP_NAME: ["-h", "--help"],
+    _VERSION_NAME: ["-V", "--version"],
+}
+_DEFAULT_USAGE = "[SUBCOMMANDS][OPTIONS][ARGUMENTS]"
