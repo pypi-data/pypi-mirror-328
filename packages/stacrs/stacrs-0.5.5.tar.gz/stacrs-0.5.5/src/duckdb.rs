566ef772277c78472577ffcea8acdecc9a26575c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:12:56 +0000

    Add package stacrs/0.5.5/stacrs-0.5.5.tar.gz

---
+++ packages/stacrs/stacrs-0.5.5.tar.gz/stacrs-0.5.5/src/duckdb.rs
@@ -0,0 +1,81 @@
+use crate::Result;
+use pyo3::{
+    exceptions::PyException,
+    prelude::*,
+    types::{PyDict, PyList},
+};
+use stac_api::python::{StringOrDict, StringOrList};
+use stac_duckdb::{Client, Config};
+use std::sync::Mutex;
+
+#[pyclass(frozen)]
+pub struct DuckdbClient(Mutex<Client>);
+
+#[pymethods]
+impl DuckdbClient {
+    #[new]
+    #[pyo3(signature = (use_s3_credential_chain=true, use_hive_partitioning=false))]
+    fn new(use_s3_credential_chain: bool, use_hive_partitioning: bool) -> Result<DuckdbClient> {
+        let config = Config {
+            use_s3_credential_chain,
+            use_hive_partitioning,
+            convert_wkb: true,
+        };
+        let client = Client::with_config(config)?;
+        Ok(DuckdbClient(Mutex::new(client)))
+    }
+
+    #[pyo3(signature = (href, *, intersects=None, ids=None, collections=None, limit=None, bbox=None, datetime=None, include=None, exclude=None, sortby=None, filter=None, query=None, **kwargs))]
+    fn search<'py>(
+        &self,
+        py: Python<'py>,
+        href: String,
+        intersects: Option<StringOrDict>,
+        ids: Option<StringOrList>,
+        collections: Option<StringOrList>,
+        limit: Option<u64>,
+        bbox: Option<Vec<f64>>,
+        datetime: Option<String>,
+        include: Option<StringOrList>,
+        exclude: Option<StringOrList>,
+        sortby: Option<StringOrList>,
+        filter: Option<StringOrDict>,
+        query: Option<Bound<'py, PyDict>>,
+        kwargs: Option<Bound<'py, PyDict>>,
+    ) -> Result<Bound<'py, PyDict>> {
+        let search = stac_api::python::search(
+            intersects,
+            ids,
+            collections,
+            limit,
+            bbox,
+            datetime,
+            include,
+            exclude,
+            sortby,
+            filter,
+            query,
+            kwargs,
+        )?;
+        let item_collection = {
+            let client = self
+                .0
+                .lock()
+                .map_err(|err| PyException::new_err(err.to_string()))?;
+            client.search(&href, search)?
+        };
+        let dict = pythonize::pythonize(py, &item_collection)?;
+        let dict = dict.extract()?;
+        Ok(dict)
+    }
+
+    fn get_collections<'py>(&self, py: Python<'py>, href: String) -> Result<Bound<'py, PyList>> {
+        let client = self
+            .0
+            .lock()
+            .map_err(|err| PyException::new_err(err.to_string()))?;
+        let collections = client.collections(&href)?;
+        let collections = pythonize::pythonize(py, &collections)?.extract()?;
+        Ok(collections)
+    }
+}
---
+++ packages/stacrs/stacrs-0.5.5.tar.gz/stacrs-0.5.5/tests/test_duckdb.py
@@ -0,0 +1,39 @@
+import pytest
+from stacrs import DuckdbClient
+
+
+@pytest.fixture
+def client() -> DuckdbClient:
+    return DuckdbClient()
+
+
+def test_search(client: DuckdbClient) -> None:
+    item_collection = client.search("data/extended-item.parquet")
+    assert len(item_collection["features"]) == 1
+
+
+def test_search_offset(client: DuckdbClient) -> None:
+    item_collection = client.search(
+        "data/100-sentinel-2-items.parquet", offset=0, limit=1
+    )
+    assert (
+        item_collection["features"][0]["id"]
+        == "S2B_MSIL2A_20241203T174629_R098_T13TDE_20241203T211406"
+    )
+
+    item_collection = client.search(
+        "data/100-sentinel-2-items.parquet", offset=1, limit=1
+    )
+    assert (
+        item_collection["features"][0]["id"]
+        == "S2A_MSIL2A_20241201T175721_R141_T13TDE_20241201T213150"
+    )
+
+
+def test_get_collections(client: DuckdbClient) -> None:
+    collections = client.get_collections("data/100-sentinel-2-items.parquet")
+    assert len(collections) == 1
+
+
+def test_init_with_config() -> None:
+    DuckdbClient(use_s3_credential_chain=True, use_hive_partitioning=True)
