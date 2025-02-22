267f0340aa37fbf77993f2b80ee9f978a03b5f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:10:29 +0000

    Add package native-sparse-attention-pytorch/0.0.21/native_sparse_attention_pytorch-0.0.21-py3-none-any.whl

---
+++ packages/native-sparse-attention-pytorch/native_sparse_attention_pytorch-0.0.21-py3-none-any.whl/native_sparse_attention_pytorch/compress_networks.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+from torch.nn import Module, ModuleList
+
+from einops import einsum, rearrange
+
+# helpers
+
+def exists(v):
+    return v is not None
+
+def default(v, d):
+    return v if exists(v) else d
+
+# start accumulating some types of compression networks
+
+class ConvLinearCompress(Module):
+    """
+    used successfully in an old google brain paper, https://github.com/lucidrains/memory-efficient-attention-pytorch
+    grouped convolutions so each head get its own parameters
+    """
+
+    def __init__(
+        self,
+        heads,
+        dim_head,
+        compress_block_size
+    ):
+        super().__init__()
+        self.heads = heads
+        self.conv = nn.Conv1d(heads * dim_head, heads * dim_head, compress_block_size, stride = compress_block_size, groups = heads)
+
+    def forward(
+        self,
+        kv # Float['b h w n d']
+    ):
+
+        kv = rearrange(kv, 'b h w n d -> b (h d) (w n)')
+
+        compressed = self.conv(kv)
+
+        return rearrange(compressed, 'b (h d) n -> b h n d', h = self.heads)
+
+# attention pool used by enformer, deepmind's genetic attention network
+
+class AttentionPool(Module):
+    def __init__(
+        self,
+        dim_head,
+        compress_block_size
+    ):
+        super().__init__()
+        self.to_attn_logits = nn.Linear(dim_head, dim_head, bias = False)
+        self.to_attn_logits.weight.data.copy_(torch.eye(dim_head))
+
+    def forward(
+        self,
+        kv
+    ):
+
+        attn_logits = self.to_attn_logits(kv)
+
+        attn = attn_logits.softmax(dim = -2)
+
+        compressed = einsum(kv, attn, 'b h w n d, b h w n d -> b h w d')
+
+        return compressed
+
