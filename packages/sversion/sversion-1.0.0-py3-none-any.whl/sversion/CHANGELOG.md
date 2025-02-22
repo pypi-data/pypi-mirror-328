88d7d520959d99ac56fcb9ef4e5af195b5614f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:09:09 +0000

    Add package sversion/1.0.0/sversion-1.0.0-py3-none-any.whl

---
+++ packages/sversion/sversion-1.0.0-py3-none-any.whl/sversion/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+> The changelog **must** comply to the [keep a changelog](https://keepachangelog.com/en/1.1.0) standard.
+
+## 1.0.0 - 2025-02-20
+
+_*Added*_
+
+- First version of sversion
---
+++ packages/sversion/sversion-1.0.0-py3-none-any.whl/sversion/file_based.py
@@ -0,0 +1,24 @@
+import os
+
+from .contracts import Version, VersionRetriever
+from .error_handling import VersionNotFoundException
+
+__VERSION_NOT_FOUND = "version_not_found"
+__version_file_name = "VERSION.txt"
+__error_message = f"{__version_file_name} was not found in the module folder or one of the parent folders."
+
+def __get_version() -> Version:
+    current_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+
+    while current_path != os.path.dirname(current_path):
+        version_file_path = os.path.join(current_path, __version_file_name)
+        try:
+            if os.path.exists(version_file_path):
+                return open(version_file_path).read().strip()
+
+            current_path = os.path.dirname(current_path)
+        except PermissionError:
+            raise VersionNotFoundException(__error_message)
+
+    raise VersionNotFoundException(__error_message)
+get_version: VersionRetriever = __get_version
