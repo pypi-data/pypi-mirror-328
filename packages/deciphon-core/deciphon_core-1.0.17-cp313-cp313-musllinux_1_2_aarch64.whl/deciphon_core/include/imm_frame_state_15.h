6e1dbc8dd44a1d535bd5ed2cca47d0086d2dc445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:08:44 +0000

    Add package deciphon-core/1.0.17/deciphon_core-1.0.17-cp313-cp313-musllinux_1_2_aarch64.whl

---
+++ packages/deciphon-core/deciphon_core-1.0.17-cp313-cp313-musllinux_1_2_aarch64.whl/deciphon_core/include/imm_frame_state_15.h
@@ -0,0 +1,13 @@
+#ifndef IMM_FRAME_STATE_15_H
+#define IMM_FRAME_STATE_15_H
+
+struct imm_frame_state;
+struct imm_seq;
+
+float imm_joint_n1_15(struct imm_frame_state const *, struct imm_seq const *);
+float imm_joint_n2_15(struct imm_frame_state const *, struct imm_seq const *);
+float imm_joint_n3_15(struct imm_frame_state const *, struct imm_seq const *);
+float imm_joint_n4_15(struct imm_frame_state const *, struct imm_seq const *);
+float imm_joint_n5_15(struct imm_frame_state const *, struct imm_seq const *);
+
+#endif
