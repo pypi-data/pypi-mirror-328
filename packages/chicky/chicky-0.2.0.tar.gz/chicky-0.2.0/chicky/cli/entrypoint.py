6c54ec74b4c3e4579a40fdb3d2c35053da0d21d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 11:09:26 +0000

    Add package chicky/0.2.0/chicky-0.2.0.tar.gz

---
+++ packages/chicky/chicky-0.2.0.tar.gz/chicky-0.2.0/chicky/cli/entrypoint.py
@@ -0,0 +1,104 @@
+import argparse
+import sys
+from pathlib import Path
+
+from .. import __pkgname__, __version__
+from ..core import collect_files, formatter
+
+
+def argumentparser_init(parser_class):
+    """
+    Initialize Parser and define all arguments.
+    """
+    parser = parser_class(
+        description=(
+            "Build a Blake2b hash for every files recursively collected from a "
+            "directory. "
+            "({}=={})".format(__pkgname__, __version__)
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "source",
+        type=Path,
+        metavar="DIRECTORY_PATH",
+        help=(
+            "An existing path of a directory to traverse to find elligible files. "
+            "Eligibility is defined from allowed extensions."
+        )
+    )
+    parser.add_argument(
+        "--ext",
+        metavar="FILE_EXTENSION",
+        action="append",
+        help=(
+            "This is for allowed file extensions. Only filenames ending with one of "
+            "allowed extensions will be collected. You can use it multiple times to "
+            "allow multiple extensions. If no extension is given then all files are "
+            "collected. Do not start your extension pattern with their leading dot "
+            "because it is already appended from code."
+        )
+    )
+    parser.add_argument(
+        "--ignore-dir-lead",
+        metavar="PATTERN",
+        action="append",
+        help=(
+            "This is a leading pattern that will exclude paths starting with it. This "
+            "is applied on the relative (to the 'source' path) directory path. Use it "
+            "multiple time to define multiple pattern, a single match exclude a path."
+        )
+    )
+    parser.add_argument(
+        "--ignore-file-lead",
+        metavar="PATTERN",
+        action="append",
+        help=(
+            "This is a leading pattern that will exclude files starting with it. This "
+            "is applied on filename path. Use it multiple time to define multiple "
+            "pattern, a single match exclude a path."
+        )
+    )
+    parser.add_argument(
+        "--destination",
+        metavar="FILE_PATH",
+        type=Path,
+        help=(
+            "This is the path where to write file of collected files and their "
+            "checksum. If it is not given the data will be printed to standard output."
+        ),
+    )
+    parser.add_argument(
+        "--format",
+        default="json",
+        choices=["json", "text"],
+        help=(
+            "This is the format to serialize data. JSON format is the one with the "
+            "most informations."
+        ),
+    )
+
+    return parser
+
+
+def main(argv=sys.argv):
+    """
+    Command interface
+    """
+    parser = argumentparser_init(argparse.ArgumentParser)
+    args = parser.parse_args(argv[1:])
+
+    files = collect_files(
+        args.source,
+        extensions=args.ext,
+        dir_leads=args.ignore_dir_lead,
+        filename_leads=args.ignore_file_lead,
+    )
+
+    manifest = formatter(files, args)
+
+    if args.destination:
+        args.destination.write_text(manifest)
+        print("Written output to: {}".format(args.destination))
+    else:
+        print(manifest)
---
+++ packages/chicky/chicky-0.2.0.tar.gz/chicky-0.2.0/chicky/core.py
@@ -0,0 +1,102 @@
+import datetime
+import json
+import os
+from hashlib import blake2b
+from pathlib import Path
+
+
+def checksum(filepath, digest_size=20):
+    """
+    Checksum a file in an efficient way for large files with blake2b.
+
+    Borrowed from: https://stackoverflow.com/a/44873382
+
+    .. Todo::
+        Once minimal Python version support move to 3.11 we should be able to simplify
+        this code with ``hashlib.file_digest()`` usage instead.
+
+    Arguments:
+        filepath (pathlib.Path): File path to open and checksum.
+
+    Keyword Arguments:
+        digest_size (integer): Maximum size of hash digest. This is the size of hash in
+            bytes, the returned string will be longer since it is the hexadecimal
+            digest. Commonly if you want a string of 10 characters you should ask for a
+            digest of ``20``.
+
+    Returns:
+        string: The file checksum.
+    """
+    h = blake2b(digest_size=digest_size)
+    b = bytearray(128 * 1024)
+    mv = memoryview(b)
+
+    with open(filepath, "rb", buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
+            h.update(mv[:n])
+
+    return h.hexdigest()
+
+
+def collect_files(dirpath, extensions=None, dir_leads=None, filename_leads=None):
+    """
+    Recursively collect every file from a directory and compute their checksum.
+
+    Arguments:
+        dirpath (string or pathlib.Path):
+
+    Keyword Arguments:
+        extensions (list): List of allowed file extensions. When it is not empty, each
+            that does not match any of those extensions will be ignored.
+        dir_leads (list): A list of leading patterns to check on paths, each
+            path starting with one of those patterns will be ignored. Match is performed
+            against the relative file path (from the ``dirpath``).
+        filename_leads (list): A list of leading patterns to check on filenames,
+            each filename starting with one of those patterns will be ignored.
+
+    Returns:
+        Generator: Yield tuples of path + checksum string
+    """
+    extensions = tuple("." + v for v in extensions) if extensions else None
+    dir_leads = tuple(dir_leads) if dir_leads else None
+    filename_leads = tuple(filename_leads) if filename_leads else None
+
+    for root, dirs, files in os.walk(dirpath):
+        for item in files:
+            rel = os.path.relpath(root, start=dirpath)
+            # Only collect item that pass filters
+            if (
+                (not dir_leads or not rel.startswith(dir_leads)) and
+                (not filename_leads or not item.startswith(filename_leads)) and
+                (not extensions or item.endswith(extensions))
+            ):
+                path = Path(os.path.join(root, item))
+                yield (path.relative_to(dirpath), checksum(path))
+
+
+def formatter(files, args):
+    """
+    Format file data.
+
+    Arguments:
+        args (object):
+
+    Returns:
+        string:
+    """
+    if args.format == "text":
+        return "\n".join([
+            str(path) + "\t" + checksum
+            for path, checksum in files
+        ])
+    else:
+        store = {
+            "created": datetime.datetime.now().isoformat(timespec="seconds"),
+            "basedir": str(args.source),
+            "extensions": args.ext,
+            "files": {
+                str(path): checksum
+                for path, checksum in files
+            },
+        }
+        return json.dumps(store, indent=4)
