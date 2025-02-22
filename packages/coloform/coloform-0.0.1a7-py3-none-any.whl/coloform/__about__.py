d2c3694d5df6006c8ab5d63bc1eac7c954877116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:11:31 +0000

    Add package coloform/0.0.1a7/coloform-0.0.1a7-py3-none-any.whl

---
+++ packages/coloform/coloform-0.0.1a7-py3-none-any.whl/coloform/__about__.py
@@ -0,0 +1,4 @@
+__description__ = "Coloform colors terminal output, similar to colorama."
+__version__ = "0.0.1a7"
+__authors__ = {"name": "Fynn Freyer", "email": "fynn.freyer@googlemail.com"}
+__maintainers__ = __authors__
