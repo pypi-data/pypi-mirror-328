562623721e5801e7b78c501e7295794d7228667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:09:07 +0000

    Add package tach/0.26.1/tach-0.26.1.tar.gz

---
+++ packages/tach/tach-0.26.1.tar.gz/tach-0.26.1/src/parsing/error.rs
@@ -0,0 +1,21 @@
+use std::io;
+use thiserror::Error;
+
+use crate::exclusion::PathExclusionError;
+use crate::filesystem::FileSystemError;
+
+#[derive(Error, Debug)]
+pub enum ParsingError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+    #[error("Filesystem error: {0}")]
+    Filesystem(#[from] FileSystemError),
+    #[error("TOML parsing error: {0}")]
+    TomlParse(#[from] toml::de::Error),
+    #[error("Missing field in TOML: {0}")]
+    MissingField(String),
+    #[error("Module path error: {0}")]
+    ModulePath(String),
+    #[error("Path exclusion error: {0}")]
+    PathExclusion(#[from] PathExclusionError),
+}
