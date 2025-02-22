62b92d12750658e6477bf559150f1ad83d2025b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:06 +0000

    Add package ttrsscli/0.0.9/ttrsscli-0.0.9.tar.gz

---
+++ packages/ttrsscli/ttrsscli-0.0.9.tar.gz/ttrsscli-0.0.9/src/ttrsscli/__init__.py
@@ -0,0 +1,9 @@
+"""Init function."""
+
+from .ttrsscli import main, main_web
+
+if __name__ == "__main_web__":
+    main_web()  # pragma: no cover
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
