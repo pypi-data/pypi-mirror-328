8f8444e037e7cb98607d05f7e4e2d701e12b98bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:09:20 +0000

    Add package ruff/0.9.7/ruff-0.9.7.tar.gz

---
+++ packages/ruff/ruff-0.9.7.tar.gz/ruff-0.9.7/crates/red_knot_python_semantic/src/types/call.rs
@@ -0,0 +1,206 @@
+use super::context::InferContext;
+use super::{Signature, Type};
+use crate::types::UnionType;
+use crate::Db;
+
+mod arguments;
+mod bind;
+pub(super) use arguments::{Argument, CallArguments};
+pub(super) use bind::{bind_call, CallBinding};
+
+/// A successfully bound call where all arguments are valid.
+///
+/// It's guaranteed that the wrapped bindings have no errors.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(super) enum CallOutcome<'db> {
+    /// The call resolves to exactly one binding.
+    Single(CallBinding<'db>),
+
+    /// The call resolves to multiple bindings.
+    Union(Box<[CallBinding<'db>]>),
+}
+
+impl<'db> CallOutcome<'db> {
+    /// Calls each union element using the provided `call` function.
+    ///
+    /// Returns `Ok` if all variants can be called without error according to the callback and `Err` otherwise.
+    pub(super) fn try_call_union<F>(
+        db: &'db dyn Db,
+        union: UnionType<'db>,
+        call: F,
+    ) -> Result<Self, CallError<'db>>
+    where
+        F: Fn(Type<'db>) -> Result<Self, CallError<'db>>,
+    {
+        let elements = union.elements(db);
+        let mut bindings = Vec::with_capacity(elements.len());
+        let mut errors = Vec::new();
+        let mut not_callable = true;
+
+        for element in elements {
+            match call(*element) {
+                Ok(CallOutcome::Single(binding)) => bindings.push(binding),
+                Ok(CallOutcome::Union(inner_bindings)) => {
+                    bindings.extend(inner_bindings);
+                }
+                Err(error) => {
+                    not_callable |= error.is_not_callable();
+                    errors.push(error);
+                }
+            }
+        }
+
+        if errors.is_empty() {
+            Ok(CallOutcome::Union(bindings.into()))
+        } else if bindings.is_empty() && not_callable {
+            Err(CallError::NotCallable {
+                not_callable_ty: Type::Union(union),
+            })
+        } else {
+            Err(CallError::Union {
+                errors: errors.into(),
+                bindings: bindings.into(),
+                called_ty: Type::Union(union),
+            })
+        }
+    }
+
+    /// The type returned by this call.
+    pub(super) fn return_type(&self, db: &'db dyn Db) -> Type<'db> {
+        match self {
+            Self::Single(binding) => binding.return_type(),
+            Self::Union(bindings) => {
+                UnionType::from_elements(db, bindings.iter().map(bind::CallBinding::return_type))
+            }
+        }
+    }
+
+    pub(super) fn bindings(&self) -> &[CallBinding<'db>] {
+        match self {
+            Self::Single(binding) => std::slice::from_ref(binding),
+            Self::Union(bindings) => bindings,
+        }
+    }
+}
+
+/// The reason why calling a type failed.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(super) enum CallError<'db> {
+    /// The type is not callable.
+    NotCallable {
+        /// The type that can't be called.
+        not_callable_ty: Type<'db>,
+    },
+
+    /// A call to a union failed because at least one variant
+    /// can't be called with the given arguments.
+    ///
+    /// A union where all variants are not callable is represented as a `NotCallable` error.
+    Union {
+        /// The variants that can't be called with the given arguments.
+        errors: Box<[CallError<'db>]>,
+
+        /// The bindings for the callable variants (that have no binding errors).
+        bindings: Box<[CallBinding<'db>]>,
+
+        /// The union type that we tried calling.
+        called_ty: Type<'db>,
+    },
+
+    /// The type has a `__call__` method but it isn't always bound.
+    PossiblyUnboundDunderCall {
+        called_type: Type<'db>,
+        outcome: Box<CallOutcome<'db>>,
+    },
+
+    /// The type is callable but not with the given arguments.
+    BindingError { binding: CallBinding<'db> },
+}
+
+impl<'db> CallError<'db> {
+    /// Returns a fallback return type to use that best approximates the return type of the call.
+    ///
+    /// Returns `None` if the type isn't callable.
+    pub(super) fn return_type(&self, db: &'db dyn Db) -> Option<Type<'db>> {
+        match self {
+            CallError::NotCallable { .. } => None,
+            // If some variants are callable, and some are not, return the union of the return types of the callable variants
+            // combined with `Type::Unknown`
+            CallError::Union {
+                errors, bindings, ..
+            } => Some(UnionType::from_elements(
+                db,
+                bindings
+                    .iter()
+                    .map(CallBinding::return_type)
+                    .chain(errors.iter().map(|err| err.fallback_return_type(db))),
+            )),
+            Self::PossiblyUnboundDunderCall { outcome, .. } => Some(outcome.return_type(db)),
+            Self::BindingError { binding } => Some(binding.return_type()),
+        }
+    }
+
+    /// Returns the return type of the call or a fallback that
+    /// represents the best guess of the return type (e.g. the actual return type even if the
+    /// dunder is possibly unbound).
+    ///
+    /// If the type is not callable, returns `Type::Unknown`.
+    pub(super) fn fallback_return_type(&self, db: &'db dyn Db) -> Type<'db> {
+        self.return_type(db).unwrap_or(Type::unknown())
+    }
+
+    /// The resolved type that was not callable.
+    ///
+    /// For unions, returns the union type itself, which may contain a mix of callable and
+    /// non-callable types.
+    pub(super) fn called_type(&self) -> Type<'db> {
+        match self {
+            Self::NotCallable {
+                not_callable_ty, ..
+            } => *not_callable_ty,
+            Self::Union { called_ty, .. } => *called_ty,
+            Self::PossiblyUnboundDunderCall { called_type, .. } => *called_type,
+            Self::BindingError { binding } => binding.callable_type(),
+        }
+    }
+
+    pub(super) const fn is_not_callable(&self) -> bool {
+        matches!(self, Self::NotCallable { .. })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(super) enum CallDunderError<'db> {
+    /// The dunder attribute exists but it can't be called with the given arguments.
+    ///
+    /// This includes non-callable dunder attributes that are possibly unbound.
+    Call(CallError<'db>),
+
+    /// The type has the specified dunder method and it is callable
+    /// with the specified arguments without any binding errors
+    /// but it is possibly unbound.
+    PossiblyUnbound(CallOutcome<'db>),
+
+    /// The dunder method with the specified name is missing.
+    MethodNotAvailable,
+}
+
+impl<'db> CallDunderError<'db> {
+    pub(super) fn return_type(&self, db: &'db dyn Db) -> Option<Type<'db>> {
+        match self {
+            Self::Call(error) => error.return_type(db),
+            Self::PossiblyUnbound(_) => None,
+            Self::MethodNotAvailable => None,
+        }
+    }
+
+    pub(super) fn fallback_return_type(&self, db: &'db dyn Db) -> Type<'db> {
+        self.return_type(db).unwrap_or(Type::unknown())
+    }
+}
+
+impl<'db> From<CallError<'db>> for CallDunderError<'db> {
+    fn from(error: CallError<'db>) -> Self {
+        Self::Call(error)
+    }
+}
---
+++ packages/ruff/ruff-0.9.7.tar.gz/ruff-0.9.7/crates/ruff_linter/src/rules/flake8_self/rules/private_member_access.rs
@@ -0,0 +1,215 @@
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, ViolationMetadata};
+use ruff_python_ast::name::UnqualifiedName;
+use ruff_python_ast::{self as ast, Expr};
+use ruff_python_semantic::{BindingKind, ScopeKind};
+use ruff_text_size::Ranged;
+
+use crate::checkers::ast::Checker;
+
+/// ## What it does
+/// Checks for accesses on "private" class members.
+///
+/// ## Why is this bad?
+/// In Python, the convention is such that class members that are prefixed
+/// with a single underscore, or prefixed but not suffixed with a double
+/// underscore, are considered private and intended for internal use.
+///
+/// Using such "private" members is considered a misuse of the class, as
+/// there are no guarantees that the member will be present in future
+/// versions, that it will have the same type, or that it will have the same
+/// behavior. Instead, use the class's public interface.
+///
+/// This rule ignores accesses on dunder methods (e.g., `__init__`) and sunder
+/// methods (e.g., `_missing_`).
+///
+/// ## Example
+/// ```python
+/// class Class:
+///     def __init__(self):
+///         self._private_member = "..."
+///
+///
+/// var = Class()
+/// print(var._private_member)
+/// ```
+///
+/// Use instead:
+/// ```python
+/// class Class:
+///     def __init__(self):
+///         self.public_member = "..."
+///
+///
+/// var = Class()
+/// print(var.public_member)
+/// ```
+///
+/// ## Options
+/// - `lint.flake8-self.ignore-names`
+///
+/// ## References
+/// - [_What is the meaning of single or double underscores before an object name?_](https://stackoverflow.com/questions/1301346/what-is-the-meaning-of-single-and-double-underscore-before-an-object-name)
+#[derive(ViolationMetadata)]
+pub(crate) struct PrivateMemberAccess {
+    access: String,
+}
+
+impl Violation for PrivateMemberAccess {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let PrivateMemberAccess { access } = self;
+        format!("Private member accessed: `{access}`")
+    }
+}
+
+/// SLF001
+pub(crate) fn private_member_access(checker: &Checker, expr: &Expr) {
+    let Expr::Attribute(ast::ExprAttribute { value, attr, .. }) = expr else {
+        return;
+    };
+
+    if checker.semantic().in_annotation() {
+        return;
+    }
+
+    // Ignore non-private accesses.
+    if !attr.starts_with('_') {
+        return;
+    }
+
+    // Ignore dunder accesses.
+    let is_dunder = attr.starts_with("__") && attr.ends_with("__");
+    if is_dunder {
+        return;
+    }
+
+    // Ignore sunder accesses.
+    let is_sunder = attr.starts_with('_')
+        && attr.ends_with('_')
+        && !attr.starts_with("__")
+        && !attr.ends_with("__");
+    if is_sunder {
+        return;
+    }
+
+    if checker
+        .settings
+        .flake8_self
+        .ignore_names
+        .contains(attr.id())
+    {
+        return;
+    }
+
+    // Ignore accesses on instances within special methods (e.g., `__eq__`).
+    if let ScopeKind::Function(ast::StmtFunctionDef { name, .. }) =
+        checker.semantic().current_scope().kind
+    {
+        if matches!(
+            name.as_str(),
+            "__lt__"
+                | "__le__"
+                | "__eq__"
+                | "__ne__"
+                | "__gt__"
+                | "__ge__"
+                | "__add__"
+                | "__sub__"
+                | "__mul__"
+                | "__matmul__"
+                | "__truediv__"
+                | "__floordiv__"
+                | "__mod__"
+                | "__divmod__"
+                | "__pow__"
+                | "__lshift__"
+                | "__rshift__"
+                | "__and__"
+                | "__xor__"
+                | "__or__"
+                | "__radd__"
+                | "__rsub__"
+                | "__rmul__"
+                | "__rmatmul__"
+                | "__rtruediv__"
+                | "__rfloordiv__"
+                | "__rmod__"
+                | "__rdivmod__"
+                | "__rpow__"
+                | "__rlshift__"
+                | "__rrshift__"
+                | "__rand__"
+                | "__rxor__"
+                | "__ror__"
+                | "__iadd__"
+                | "__isub__"
+                | "__imul__"
+                | "__imatmul__"
+                | "__itruediv__"
+                | "__ifloordiv__"
+                | "__imod__"
+                | "__ipow__"
+                | "__ilshift__"
+                | "__irshift__"
+                | "__iand__"
+                | "__ixor__"
+                | "__ior__"
+        ) {
+            return;
+        }
+    }
+
+    // Allow some documented private methods, like `os._exit()`.
+    if let Some(qualified_name) = checker.semantic().resolve_qualified_name(expr) {
+        if matches!(qualified_name.segments(), ["os", "_exit"]) {
+            return;
+        }
+    }
+
+    if let Expr::Call(ast::ExprCall { func, .. }) = value.as_ref() {
+        // Ignore `super()` calls.
+        if let Some(name) = UnqualifiedName::from_expr(func) {
+            if matches!(name.segments(), ["super"]) {
+                return;
+            }
+        }
+    }
+
+    if let Some(name) = UnqualifiedName::from_expr(value) {
+        // Ignore `self` and `cls` accesses.
+        if matches!(name.segments(), ["self" | "cls" | "mcs"]) {
+            return;
+        }
+    }
+
+    if let Expr::Name(name) = value.as_ref() {
+        // Ignore accesses on class members from _within_ the class.
+        if checker
+            .semantic()
+            .resolve_name(name)
+            .and_then(|id| {
+                if let BindingKind::ClassDefinition(scope) = checker.semantic().binding(id).kind {
+                    Some(scope)
+                } else {
+                    None
+                }
+            })
+            .is_some_and(|scope| {
+                checker
+                    .semantic()
+                    .current_scope_ids()
+                    .any(|parent| scope == parent)
+            })
+        {
+            return;
+        }
+    }
+
+    checker.report_diagnostic(Diagnostic::new(
+        PrivateMemberAccess {
+            access: attr.to_string(),
+        },
+        expr.range(),
+    ));
+}
