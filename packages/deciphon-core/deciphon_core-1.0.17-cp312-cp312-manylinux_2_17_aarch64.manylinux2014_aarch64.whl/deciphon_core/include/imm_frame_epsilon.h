24f74765624998ae2e7dbc9ec4545fd11f9515a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:09:11 +0000

    Add package deciphon-core/1.0.17/deciphon_core-1.0.17-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/deciphon-core/deciphon_core-1.0.17-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/deciphon_core/include/imm_frame_epsilon.h
@@ -0,0 +1,12 @@
+#ifndef IMM_FRAME_EPSILON_H
+#define IMM_FRAME_EPSILON_H
+
+struct imm_frame_epsilon
+{
+  float loge;
+  float log1e;
+};
+
+struct imm_frame_epsilon imm_frame_epsilon(float epsilon);
+
+#endif
