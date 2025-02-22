9f82a8355452cb36fe7908323af29b76dcb6973d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:17 +0000

    Add package dagsonar/0.0.3/dagsonar-0.0.3.tar.gz

---
+++ packages/dagsonar/dagsonar-0.0.3.tar.gz/dagsonar-0.0.3/src/dagsonar/encoder.py
@@ -0,0 +1,19 @@
+import json
+from dataclasses import asdict
+from pathlib import Path
+
+from dagsonar.models import (DagReference, ExprReference, ShellScriptReference,
+                             TaskReference)
+
+
+class ReferenceEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(
+            o, (DagReference, TaskReference, ShellScriptReference, ExprReference)
+        ):
+            return asdict(o)
+
+        if isinstance(o, Path):
+            return str(o)
+
+        return super().default(o)
---
+++ packages/dagsonar/dagsonar-0.0.3.tar.gz/dagsonar-0.0.3/src/dagsonar/models.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, List, Optional, Set
+
+
+@dataclass
+class DagConfig:
+    path: Path
+    tasks: List[str]
+
+
+@dataclass
+class ShellScriptReference:
+    path: Path
+    content: str
+
+
+@dataclass
+class ExprReference:
+    name: str
+    content: str
+
+
+@dataclass
+class TaskReference:
+    task_id: Optional[str] = None
+    task_type: Optional[str] = None
+    content: Optional[str] = None
+    shell_scripts: List[ShellScriptReference] = field(
+        default_factory=list
+    )  # [(path, content)]
+    external_variables: List[ExprReference] = field(
+        default_factory=list
+    )  # [(name, content)]
+    called_functions: List[ExprReference] = field(
+        default_factory=list
+    )  # [(name, content)]
+    hash: Optional[str] = None
+    last_modified: Optional[str] = None
+
+
+@dataclass
+class DagReference:
+    dag_id: str
+    task_history: List[TaskReference]
