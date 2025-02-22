4f7dd6c7cc34aa88066eb320fc6e2b9a29f084a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:08:49 +0000

    Add package telegramTransfer/0.0.0.dev202502201203/telegramtransfer-0.0.0.dev202502201203-py3-none-any.whl

---
+++ packages/telegramTransfer/telegramtransfer-0.0.0.dev202502201203-py3-none-any.whl/telegramTransfer/__init__.py
@@ -0,0 +1,10 @@
+"""Telegram Transfer
+
+一个基于Telethon的Telegram文件传输工具，支持文件夹上传、下载和同步功能。
+"""
+
+from .client import TelegramTransferClient
+from .cli import cli
+
+__version__ = '0.0.0.dev202502201203'
+__all__ = ["TelegramTransferClient", "cli"]
