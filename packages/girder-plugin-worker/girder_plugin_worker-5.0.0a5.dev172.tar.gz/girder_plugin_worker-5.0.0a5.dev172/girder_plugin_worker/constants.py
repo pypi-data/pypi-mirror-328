69269c428d2fd3bb1ba604f5d677cc86743e76a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:11:20 +0000

    Add package girder-plugin-worker/5.0.0a5.dev172/girder_plugin_worker-5.0.0a5.dev172.tar.gz

---
+++ packages/girder-plugin-worker/girder_plugin_worker-5.0.0a5.dev172.tar.gz/girder_plugin_worker-5.0.0a5.dev172/girder_plugin_worker/constants.py
@@ -0,0 +1,13 @@
+# The path that will be mounted in docker containers for data IO
+DOCKER_DATA_VOLUME = '/mnt/girder_worker/data'
+
+# The path that will be mounted in docker containers for utility scripts
+DOCKER_SCRIPTS_VOLUME = '/mnt/girder_worker/scripts'
+
+
+# Settings where plugin information is stored
+class PluginSettings:
+    BROKER = 'worker.broker'
+    BACKEND = 'worker.backend'
+    API_URL = 'worker.api_url'
+    DIRECT_PATH = 'worker.direct_path'
