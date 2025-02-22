7ff89a1c25a535fdaa617f6a9d0627676e739cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 11:09:22 +0000

    Add package opsany/0.1.0/opsany-0.1.0.tar.gz

---
+++ packages/opsany/opsany-0.1.0.tar.gz/opsany-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opsany',
+    version='0.1.0',
+    author='OpsAny',
+    author_email='',
+    description="OpsAny's command-line tool!",
+    long_description=open('README.md').read(),
+    long_description_content_type='',
+    # url='https://gitee.com/unixhot/opsany-paas',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "typer",
+        "rich",
+        "pyyaml",
+        "requests",
+    ],
+    entry_points={
+        'console_scripts': [
+            'opsanyctl=opsanyctl.main:app',
+            'opsctl=opsanyctl.main:app',
+        ],
+    },
+)
