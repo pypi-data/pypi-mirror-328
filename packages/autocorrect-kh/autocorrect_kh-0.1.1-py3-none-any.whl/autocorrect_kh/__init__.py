9cc3b3d4205a970396aeb95f625e4e867b088f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 00:21:42 +0000

    Add package autocorrect-kh/0.1.1/autocorrect_kh-0.1.1-py3-none-any.whl

---
+++ packages/autocorrect-kh/autocorrect_kh-0.1.1-py3-none-any.whl/autocorrect_kh/__init__.py
@@ -0,0 +1,9 @@
+# File: autocorrect_kh/__init__.py
+from .autocorrect import (
+    normalize_text,
+    load_autocorrect_dicts,
+    load_autocorrect_dict_file,
+    autocorrect_word_in_part,
+    autocorrect_address_1,
+    autocorrect_address_2,
+)
