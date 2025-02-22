5bdcccb72e5fa0b23a18991a62dbff8c421d389f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:08:56 +0000

    Add package spatialformer/0.0.7/spatialformer-0.0.7.tar.gz

---
+++ packages/spatialformer/spatialformer-0.0.7.tar.gz/spatialformer-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('./requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='spatialformer',  
+    version='0.0.7',
+    author='TerminatorJ',
+    author_email='wangjun19950708@gmail.com',
+    description='A single-cell foundation model focus on the spatial cell-cell colocalization',
+    url='https://github.com/TerminatorJ/Spatialformer/', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', 
+    ],
+    python_requires='>=3.9', 
+    install_requires=requirements,
+    packages=find_packages(include=['spatialformer', 'spatialformer.tools']),
+   
+)
