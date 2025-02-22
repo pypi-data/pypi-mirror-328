f0e372fa821b02d17fe4ed9e49fc4a215c4cecd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:35:21 +0000

    Add package translib/0.1.0.1/translib-0.1.0.1.tar.gz

---
+++ packages/translib/translib-0.1.0.1.tar.gz/translib-0.1.0.1/README.md
@@ -0,0 +1,7 @@
+# TransLib
+
+A lightweight translation library
+
+## Installation
+```sh
+pip install translib
---
+++ packages/translib/translib-0.1.0.1.tar.gz/translib-0.1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="translib",
+    version="0.1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "googletrans==4.0.0-rc1"
+    ],
+    author="Phillip",
+    author_email="tranquanganhminh25313@gmail.com",
+    description="A simple translation library",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url=" ",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
