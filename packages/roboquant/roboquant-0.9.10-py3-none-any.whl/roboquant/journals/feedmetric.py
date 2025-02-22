d1c0848a1a9a1ae9016ac9a29a9281dde3910759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:09:38 +0000

    Add package roboquant/0.9.10/roboquant-0.9.10-py3-none-any.whl

---
+++ packages/roboquant/roboquant-0.9.10-py3-none-any.whl/roboquant/journals/feedmetric.py
@@ -0,0 +1,27 @@
+from roboquant.journals.metric import Metric
+
+
+class FeedMetric(Metric):
+    """Tracks the performance of the market, aka the price-items in the event"""
+
+    def __init__(self, price_type="DEFAULT"):
+        self._prev_prices = {}
+        self.price_type = price_type
+        self._last_total = 1.0
+
+    def calc(self, event, account, signals, orders):
+        mkt_return = 0.0
+        n = 0
+        for asset, price in event.get_prices(self.price_type).items():
+            if prev_price := self._prev_prices.get(asset):
+                mkt_return += price / prev_price - 1.0
+                n += 1
+
+            self._prev_prices[asset] = price
+
+        result = 0.0 if n == 0 else mkt_return / n
+
+        new_total = self._last_total * (1.0 + result)
+        self._last_total = new_total
+
+        return {"feed/pnl": result, "feed/total_pnl": new_total - 1.0}
---
+++ packages/roboquant/roboquant-0.9.10-py3-none-any.whl/roboquant/strategies/tastrategy.py
@@ -0,0 +1,41 @@
+from abc import abstractmethod
+
+from roboquant.asset import Asset
+from roboquant.event import Bar
+from roboquant.signal import Signal
+from roboquant.strategies.buffer import OHLCVBuffer
+from roboquant.strategies.strategy import Strategy
+
+
+class TaStrategy(Strategy):
+    """Abstract base class for other strategies that helps to implement trading solutions
+    based on technical indicators using a history of bars/candlesticks.
+
+    Subclasses should implement the `process_asset` method. This method is only invoked once
+    there is at least `size` history for an individual asset available.
+    """
+
+    def __init__(self, size: int) -> None:
+        super().__init__()
+        self._data: dict[Asset, OHLCVBuffer] = {}
+        self.size = size
+
+    def create_signals(self, event) -> list[Signal]:
+        result: list[Signal] = []
+        for item in event.items:
+            if isinstance(item, Bar):
+                asset = item.asset
+                if asset not in self._data:
+                    self._data[asset] = OHLCVBuffer(self.size)
+                ohlcv = self._data[asset]
+                if ohlcv.append(item.ohlcv):
+                    if signal := self.process_asset(asset, ohlcv):
+                        result.append(signal)
+        return result
+
+    @abstractmethod
+    def process_asset(self, asset: Asset, ohlcv: OHLCVBuffer) -> Signal | None:
+        """
+        Create zero or more orders for the provided asset
+        """
+        ...
