dac1d3bfd8b8937e74506f65909355f178584136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 11:09:34 +0000

    Add package par-infini-sweeper/0.2.9/par_infini_sweeper-0.2.9-py3-none-any.whl

---
+++ packages/par-infini-sweeper/par_infini_sweeper-0.2.9-py3-none-any.whl/par_infini_sweeper/__init__.py
@@ -0,0 +1,31 @@
+"""Par Infinite Minesweeper."""
+
+from __future__ import annotations
+
+import os
+
+__author__ = "Paul Robello"
+__credits__ = ["Paul Robello"]
+__maintainer__ = "Paul Robello"
+__email__ = "probello@gmail.com"
+__version__ = "0.2.9"
+__application_title__ = "Par Infinite Minesweeper"
+__application_binary__ = "pim"
+__licence__ = "MIT"
+
+from par_infini_sweeper.minesweeper_app import MinesweeperApp
+
+os.environ["USER_AGENT"] = f"{__application_title__} {__version__}"
+
+
+__all__: list[str] = [
+    "__author__",
+    "__credits__",
+    "__maintainer__",
+    "__email__",
+    "__version__",
+    "__application_binary__",
+    "__licence__",
+    "__application_title__",
+    "MinesweeperApp",
+]
