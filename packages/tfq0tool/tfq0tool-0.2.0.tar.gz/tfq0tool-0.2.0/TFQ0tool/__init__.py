49d5e8c1e1510318b5473d49ccfdc32540b1f028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 00:09:56 +0000

    Add package tfq0tool/0.2.0/tfq0tool-0.2.0.tar.gz

---
+++ packages/tfq0tool/tfq0tool-0.2.0.tar.gz/tfq0tool-0.2.0/TFQ0tool/__init__.py
@@ -0,0 +1,5 @@
+from .tfq0tool import main
+__version__ = "0.2.0"
+__author__ = "Talal"
+__description__ = "is a command-line utility for extracting text from various file formats, including text files, PDFs, Word documents, spreadsheets, and code files in popular programming languages."
+__all__ = ["main"]
