3f52e4dcfbf902f62a4cdc925ddf413a92b50411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 08:09:37 +0000

    Add package microgue/4.0.2/microgue-4.0.2-py3-none-any.whl

---
+++ packages/microgue/microgue-4.0.2-py3-none-any.whl/microgue/models/errors.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .base_table import (
+    DatabaseConnectionFailed,
+    DeleteFailed,
+    GetFailed,
+    InsertFailed,
+    KeyMissing,
+    UpdateFailed
+)
+from .model import RelatedModelsMissing, UniqueAttributesExist
+from .object import RequiredAttributesMissing, TypeCheckFailed
