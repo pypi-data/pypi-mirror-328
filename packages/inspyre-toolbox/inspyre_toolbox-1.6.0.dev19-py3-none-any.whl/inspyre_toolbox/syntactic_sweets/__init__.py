0520b43760877a20f937bd59200cf393738a5009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:09:21 +0000

    Add package inspyre-toolbox/1.6.0.dev19/inspyre_toolbox-1.6.0.dev19-py3-none-any.whl

---
+++ packages/inspyre-toolbox/inspyre_toolbox-1.6.0.dev19-py3-none-any.whl/inspyre_toolbox/syntactic_sweets/__init__.py
@@ -0,0 +1,71 @@
+﻿#  Copyright (c) 2021. Taylor-Jayde Blackstone <t.blackstone@inspyre.tech> https://inspyre.tech
+"""
+
+A package containing decorators that are a quick help in programming with Python
+
+"""
+import sys
+from contextlib import contextmanager
+from os import devnull
+
+SUPPRESSED = False
+
+
+@contextmanager
+def suppress_stderr():
+    """
+    The suppress_stderr function is a context manager that redirects stderr to
+    /dev/null.
+
+    This is useful for suppressing error messages from functions and methods,
+    especially when called in loops.  For example:
+
+        with suppress_stderr():
+            for i in range(10):
+                print(i) # this will not print anything
+
+    Args:
+        ``None``
+    """
+    global SUPPRESSED
+
+    if SUPPRESSED:
+        with open(devnull, "w") as dn:
+            old_stderr = sys.stderr
+            sys.stderr = dn
+            try:
+                yield
+            finally:
+                sys.stderr = old_stderr
+    else:
+        yield
+
+
+@contextmanager
+def suppress_stdout():
+    """
+    The suppress_stdout function is a context manager that redirects stdout to
+    /dev/null.
+
+    This is useful for suppressing output from functions and methods,
+    especially when called in loops.  For example:
+
+        with suppress_stdout():
+            for i in range(10):
+                print(i) # this will not print anything
+
+    Args:
+        ``None``
+    """
+    global SUPPRESSED
+
+    if SUPPRESSED:
+        with open(devnull, "w") as dn:
+            old_stdout = sys.stdout
+            sys.stdout = dn
+            try:
+                yield
+            finally:
+                sys.stdout = old_stdout
+    else:
+        yield
