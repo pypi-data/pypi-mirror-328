0f290ab2d6038cbd0e1ffb20832bdaca1a723f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:54 +0000

    Add package unofficial-tabdeal-api/0.1.2/unofficial_tabdeal_api-0.1.2.tar.gz

---
+++ packages/unofficial-tabdeal-api/unofficial_tabdeal_api-0.1.2.tar.gz/unofficial_tabdeal_api-0.1.2/src/unofficial_tabdeal_api/constants.py
@@ -0,0 +1,19 @@
+"""Constants storage"""
+
+GET_ACCOUNT_PREFERENCES_URL: str = "https://api.etctabdeal.org/r/preferences/"
+"""URL for getting account preferences. Used for checking authorization key validity"""
+
+# region Margin
+GET_MARGIN_ASSET_DETAILS_PRT1: str = (
+    "https://api.etctabdeal.org/margin/margin-account-v2/?pair_symbol="
+)
+"""First part the URL for getting margin asset details
+The isolated_symbol of the margin asset is added between the two parts"""
+GET_MARGIN_ASSET_DETAILS_PRT2: str = "&account_genre=IsolatedMargin"
+"""Seconds part of the URL for getting margin asset details
+The isolated_symbol of the margin asset is added between the two parts"""
+GET_ALL_MARGIN_OPEN_ORDERS_URL: str = (
+    "https://api.etctabdeal.org/r/treasury/isolated_positions/"
+)
+"""URL for getting all open margin orders."""
+# endregion Margin
