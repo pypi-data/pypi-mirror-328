cfd92ad730ed7ec4d8a519526ba7a6cceec5905d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:37 +0000

    Add package pardhu-simple-package/0.1.1/pardhu-simple-package-0.1.1.tar.gz

---
+++ packages/pardhu-simple-package/pardhu-simple-package-0.1.1.tar.gz/pardhu-simple-package-0.1.1/README.md
@@ -0,0 +1,7 @@
+# Pardhu Simple Package
+
+A simple Python package demonstrating basic functionality.
+
+## Installation
+
+You can install the package via pip:
---
+++ packages/pardhu-simple-package/pardhu-simple-package-0.1.1.tar.gz/pardhu-simple-package-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pardhu-simple-package",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["jwt>=1.3.1", "requests>=2.31.0", "urllib3>=2.0.7"],
+    author="Pardhu",
+    description="A simple package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
