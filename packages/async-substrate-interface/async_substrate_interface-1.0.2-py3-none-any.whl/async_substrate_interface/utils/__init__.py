ed5f230380bce55d1b5c1b6ce0917a0786bb5513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 00:10:12 +0000

    Add package async-substrate-interface/1.0.2/async_substrate_interface-1.0.2-py3-none-any.whl

---
+++ packages/async-substrate-interface/async_substrate_interface-1.0.2-py3-none-any.whl/async_substrate_interface/utils/__init__.py
@@ -0,0 +1,33 @@
+import importlib
+import hashlib
+
+
+def generate_unique_id(item: str, length=10):
+    hashed_value = hashlib.sha256(item.encode()).hexdigest()
+    return hashed_value[:length]
+
+
+def hex_to_bytes(hex_str: str) -> bytes:
+    """
+    Converts a hex-encoded string into bytes. Handles 0x-prefixed and non-prefixed hex-encoded strings.
+    """
+    if hex_str.startswith("0x"):
+        bytes_result = bytes.fromhex(hex_str[2:])
+    else:
+        bytes_result = bytes.fromhex(hex_str)
+    return bytes_result
+
+
+def import_json_lib():
+    libs = ["ujson", "orjson", "simplejson", "json"]
+
+    for lib in libs:
+        try:
+            return importlib.import_module(lib)
+        except ImportError:
+            continue
+
+    raise ImportError("None of the specified JSON libraries are installed.")
+
+
+json = import_json_lib()
