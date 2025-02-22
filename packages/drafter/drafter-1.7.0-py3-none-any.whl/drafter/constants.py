49db7f4a65f505272c32b9dbf1aefd0ed0c9d0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 08:09:16 +0000

    Add package drafter/1.7.0/drafter-1.7.0-py3-none-any.whl

---
+++ packages/drafter/drafter-1.7.0-py3-none-any.whl/drafter/constants.py
@@ -0,0 +1,5 @@
+RESTORABLE_STATE_KEY = "--restorable-state"
+SUBMIT_BUTTON_KEY = '--submit-button'
+PREVIOUSLY_PRESSED_BUTTON = "--last-button"
+LABEL_SEPARATOR = "$@~@$"
+JSON_DECODE_SYMBOL = "$@JSON~@$"
