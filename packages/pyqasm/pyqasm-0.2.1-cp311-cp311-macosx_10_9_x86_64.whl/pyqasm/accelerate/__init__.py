7e0d8de0832924897e3d01293e23c723fd1d9d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:31 +0000

    Add package pyqasm/0.2.1/pyqasm-0.2.1-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/pyqasm/pyqasm-0.2.1-cp311-cp311-macosx_10_9_x86_64.whl/pyqasm/accelerate/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2025 qBraid
+#
+# This file is part of PyQASM
+#
+# PyQASM is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for PyQASM, as per Section 15 of the GPL v3.
+
+"""
+Module providing Cython-based optimizations for computationally intensive functions.
+
+.. currentmodule:: pyqasm.accelerate
+
+"""
