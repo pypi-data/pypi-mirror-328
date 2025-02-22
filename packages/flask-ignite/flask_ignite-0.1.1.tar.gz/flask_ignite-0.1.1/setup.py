c7f0caf01f307c165dddb7de1007709026b30815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:08:53 +0000

    Add package flask-ignite/0.1.1/flask_ignite-0.1.1.tar.gz

---
+++ packages/flask-ignite/flask_ignite-0.1.1.tar.gz/flask_ignite-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flask_ignite",
+    version="0.1.1",
+    description="A simple Flask app creator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nahom D",
+    author_email="nahom@nahom.eu.org",
+    url="https://github.com/nahom-d54/flask_admin",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["flask", "flask-sqlalchemy", "flask-migrate", "python-dotenv"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "flask-admin=flask_admin.setup_flask_project:main",
+        ],
+    },
+)
