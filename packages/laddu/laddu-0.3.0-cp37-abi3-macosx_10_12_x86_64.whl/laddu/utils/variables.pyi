19848ac0078d1b1727edec3ed746a7b5db24502b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:10:25 +0000

    Add package laddu/0.3.0/laddu-0.3.0-cp37-abi3-macosx_10_12_x86_64.whl

---
+++ packages/laddu/laddu-0.3.0-cp37-abi3-macosx_10_12_x86_64.whl/laddu/utils/variables.pyi
@@ -0,0 +1,105 @@
+from typing import Literal
+
+import numpy as np
+import numpy.typing as npt
+
+from laddu.data import Dataset, Event
+
+class Mass:
+    def __init__(self, constituents: list[int]) -> None: ...
+    def value(self, event: Event) -> float: ...
+    def value_on(self, dataset: Dataset) -> npt.NDArray[np.float64]: ...
+
+class CosTheta:
+    def __init__(
+        self,
+        beam: int,
+        recoil: list[int],
+        daughter: list[int],
+        resonance: list[int],
+        frame: Literal[
+            'Helicity',
+            'HX',
+            'HEL',
+            'GottfriedJackson',
+            'Gottfried Jackson',
+            'GJ',
+            'Gottfried-Jackson',
+        ] = 'Helicity',
+    ) -> None: ...
+    def value(self, event: Event) -> float: ...
+    def value_on(self, dataset: Dataset) -> npt.NDArray[np.float64]: ...
+
+class Phi:
+    def __init__(
+        self,
+        beam: int,
+        recoil: list[int],
+        daughter: list[int],
+        resonance: list[int],
+        frame: Literal[
+            'Helicity',
+            'HX',
+            'HEL',
+            'GottfriedJackson',
+            'Gottfried Jackson',
+            'GJ',
+            'Gottfried-Jackson',
+        ] = 'Helicity',
+    ) -> None: ...
+    def value(self, event: Event) -> float: ...
+    def value_on(self, dataset: Dataset) -> npt.NDArray[np.float64]: ...
+
+class Angles:
+    costheta: CosTheta
+    phi: Phi
+    def __init__(
+        self,
+        beam: int,
+        recoil: list[int],
+        daughter: list[int],
+        resonance: list[int],
+        frame: Literal[
+            'Helicity',
+            'HX',
+            'HEL',
+            'GottfriedJackson',
+            'Gottfried Jackson',
+            'GJ',
+            'Gottfried-Jackson',
+        ] = 'Helicity',
+    ) -> None: ...
+
+class PolAngle:
+    def __init__(
+        self,
+        beam: int,
+        recoil: list[int],
+    ) -> None: ...
+    def value(self, event: Event) -> float: ...
+    def value_on(self, dataset: Dataset) -> npt.NDArray[np.float64]: ...
+
+class PolMagnitude:
+    def __init__(
+        self,
+        beam: int,
+    ) -> None: ...
+    def value(self, event: Event) -> float: ...
+    def value_on(self, dataset: Dataset) -> npt.NDArray[np.float64]: ...
+
+class Polarization:
+    pol_magnitude: PolMagnitude
+    pol_angle: PolAngle
+    def __init__(self, beam: int, recoil: list[int]) -> None: ...
+
+class Mandelstam:
+    def __init__(
+        self,
+        p1: list[int],
+        p2: list[int],
+        p3: list[int],
+        p4: list[int],
+        channel: Literal['s', 't', 'u'],
+    ) -> None: ...
+    def value(self, event: Event) -> float: ...
+    def value_on(self, dataset: Dataset) -> npt.NDArray[np.float64]: ...
