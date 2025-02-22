429274d86b2bb26d9f66adc991dc33ac685edda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:36:48 +0000

    Add package tbtamr/1.0.3/tbtamr-1.0.3.tar.gz

---
+++ packages/tbtamr/tbtamr-1.0.3.tar.gz/tbtamr-1.0.3/README.md
@@ -0,0 +1,23 @@
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/MDU-PHL/tbtamr/tree/master.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/MDU-PHL/tbtamr/tree/master)
+[![Python 3.9](https://img.shields.io/badge/python-3.10-blue.svg)](https://www.python.org/downloads/release/python-390/)
+
+
+# tbtAMR  <img src="https://github.com/MDU-PHL/tbtamr/blob/master/tbtamr_logo_transparent.png" width="100" height="70">
+
+`tbtAMR` implements TB-profiler and custom logic developed at MDU to identify mutations linked to AMR mechanisms in _M. tuberculosis_ and generate reports suitable for public health in Victoria. It may also be suitable for use in research settings.
+
+**`tbtAMR` is now accredited to ISO15189 standard by NATA for use in Victoria Australia.** 
+
+
+## tbtAMR installation
+
+In order to install `tbtAMR` conda is strongly recommended - installation instructions can be found [here](https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html). 
+
+```
+conda create -n tbtamr tbtamr
+```
+
+
+## Usage
+
+See our [wiki](https://github.com/MDU-PHL/tbtamr/wiki) page for further information on `tbtamr` usage.
---
+++ packages/tbtamr/tbtamr-1.0.3.tar.gz/tbtamr-1.0.3/setup.py
@@ -0,0 +1,56 @@
+"""
+tbTAMR --- Genotypic AMR for M. tuberculosis Public Health
+"""
+from sys import exit, version_info
+from setuptools import setup, find_packages
+from os import environ
+import logging
+import tbtamr
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tbtamr",
+    version="1.0.3",
+    description="Running TB-Profiler for MDU",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MDU-PHL/tbtamr",
+    author="Kristy Horan",
+    author_email="kristyhoran15@gmail.com",
+    maintainer="Kristy Horan",
+    maintainer_email="kristyhoran15@gmail.com",
+    python_requires=">=3.10, <4",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    zip_safe=False,
+    install_requires=[
+                        "pandas",
+                        "pytest",
+                        "tabulate",
+                        "unidecode",
+                        "pysam",
+                        'requests',
+                        "joblib",
+                        "pydantic",
+                        "tqdm"
+                        ],
+    test_suite="nose.collector",
+    tests_require=["nose", "pytest","psutil"],
+    entry_points={
+        "console_scripts": [
+            "tbtamr=tbtamr.tbtamr:main",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    package_data={"tbtamr": ["db/*","configs/*"]}
+)
