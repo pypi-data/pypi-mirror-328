e7b207737d95d32af6a9df282ba178e1666ee53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:10:11 +0000

    Add package datagrowth/0.19.10/datagrowth-0.19.10-py3-none-any.whl

---
+++ packages/datagrowth/datagrowth-0.19.10-py3-none-any.whl/datagrowth/processors/__init__.py
@@ -0,0 +1,2 @@
+from .base import Processor, QuerySetProcessor, ProcessorFactory
+from .input import ExtractProcessor
---
+++ packages/datagrowth/datagrowth-0.19.10-py3-none-any.whl/datagrowth/processors/input/transform.py
@@ -0,0 +1,2 @@
+# flake8: noqa: F401
+from datagrowth.processors.input.extraction import ExtractProcessor as TransformProcessor
