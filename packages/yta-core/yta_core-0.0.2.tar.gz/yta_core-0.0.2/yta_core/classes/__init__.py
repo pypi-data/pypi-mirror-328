c1b691303b3618b2af14a9212d546bbddba40c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 11:09:11 +0000

    Add package yta-core/0.0.2/yta_core-0.0.2.tar.gz

---
+++ packages/yta-core/yta_core-0.0.2.tar.gz/yta_core-0.0.2/yta_core/classes/__init__.py
@@ -0,0 +1,5 @@
+"""
+TODO: I made this file to create the different classes
+but I'm not sure if this is the way I should handle
+this... Feel free to move these classes
+"""
---
+++ packages/yta-core/yta_core-0.0.2.tar.gz/yta_core-0.0.2/yta_core/classes/project_for_database.py
@@ -0,0 +1,63 @@
+"""
+A project that can be stored in the database.
+"""
+from yta_core.classes.segment import Segment
+from dataclasses import dataclass
+
+import json
+
+
+@dataclass
+class ProjectForDatabase:
+    """
+    @dataclass
+    A project which information has been transformed
+    from the raw json to the information we need to
+    be stored in the database.
+    """
+
+    status: str
+    """
+    The current status of the project.
+    """
+    script: dict
+    """
+    The original script as it was read from the source
+    file.
+    """
+    segments: list[Segment]
+    """
+    The list of the segments once they've been read
+    from the database.
+    """
+
+    @property
+    def as_dict(
+        self
+    ):
+        """
+        Get the instance as a dictionary.
+        """
+        return {
+            'status': self.status,
+            'script': self.script,
+            'segments': [
+                segment.as_dict
+                for segment in self.segments
+            ]
+        }
+    
+    @property
+    def as_json(
+        self
+    ):
+        """
+        Get the instance as a json, transformed
+        from the dictionary version.
+        """
+        return json.dumps(
+            self.as_dict,
+            ensure_ascii = False
+        )
+    
+    #def init_from_database():
