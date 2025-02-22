6a0c19516186a7467eed6f1e34f6eb3a39f49fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 02:28:24 +0000

    Add package netkeiber/1.0.50/netkeiber-1.0.50-py3-none-any.whl

---
+++ packages/netkeiber/netkeiber-1.0.50-py3-none-any.whl/netkeiber/__init__.py
@@ -0,0 +1,7 @@
+
+__copyright__    = 'Copyright (C) 2021-2022 Unchain Works'
+__version__      = '1.0.50'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Unchain Works'
+__author_email__ = 'kajin0318@gmail.com'
+__url__          = 'https://github.com/kaioman/netkeiber.git'
