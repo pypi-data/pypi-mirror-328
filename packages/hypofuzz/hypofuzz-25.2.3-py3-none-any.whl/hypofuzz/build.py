947328177743aefce6d29e6a3482eb2f3e8530d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:36:21 +0000

    Add package hypofuzz/25.2.3/hypofuzz-25.2.3-py3-none-any.whl

---
+++ packages/hypofuzz/hypofuzz-25.2.3-py3-none-any.whl/hypofuzz/build.py
@@ -0,0 +1,33 @@
+import subprocess
+from pathlib import Path
+
+from setuptools.command.build_py import build_py
+from setuptools.command.editable_wheel import editable_wheel
+
+
+def build_frontend() -> None:
+    try:
+        subprocess.check_call(["npm", "--version"])
+    except Exception:
+        raise Exception(
+            "npm is required to build HypoFuzz, but was not found.\n"
+            "Install npm and add it to your path."
+        )
+
+    frontend_dir = Path(__file__).parent / "frontend"
+    subprocess.check_call(["npm", "install"], cwd=frontend_dir)
+    subprocess.check_call(["npm", "run", "build"], cwd=frontend_dir)
+
+
+# runs via pip install .
+class HypofuzzBuildPy(build_py):
+    def run(self) -> None:
+        super().run()
+        build_frontend()
+
+
+# runs via pip install -e . (iff PEP 660 behavior is enabled?)
+class HypofuzzEditableWheel(editable_wheel):
+    def run(self) -> None:
+        super().run()
+        build_frontend()
