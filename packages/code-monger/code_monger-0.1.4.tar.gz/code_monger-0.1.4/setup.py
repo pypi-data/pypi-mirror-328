80a14e3355a5e93b3f121f2111f5c3e54ea3db7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:01 +0000

    Add package code-monger/0.1.4/code_monger-0.1.4.tar.gz

---
+++ packages/code-monger/code_monger-0.1.4.tar.gz/code_monger-0.1.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name='code_monger',
+    version='0.1.4',
+    packages=['code_monger'],
+    install_requires=[]
+
+    )
+
+
+
+import setuptools
+from setuptools import setup, find_packages
+
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="code_monger",
+    version="0.1.3",
+    author="Victor Kipkemboi",
+    author_email="scriptilapia@gmail.com",
+    description="A package for on-the-fly code generation and validation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/victhepythonista/code_monger",
+    project_urls={
+        "Bug Tracker": "https://github.com/victhepythonista/code_monger/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    packages=["code_monger"],
+    python_requires=">=3.6",
+ 
+)
