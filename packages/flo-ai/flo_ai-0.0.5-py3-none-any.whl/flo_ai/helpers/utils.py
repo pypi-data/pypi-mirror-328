88b1b49e609ecbe278a95f3a40d8f43b91208d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:10:07 +0000

    Add package flo-ai/0.0.5/flo_ai-0.0.5-py3-none-any.whl

---
+++ packages/flo-ai/flo_ai-0.0.5-py3-none-any.whl/flo_ai/helpers/utils.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+
+def random_str(length: int = 5):
+    letters = string.ascii_letters + string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
