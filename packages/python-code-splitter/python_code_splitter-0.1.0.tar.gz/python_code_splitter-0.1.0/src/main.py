0a0523b381c8c82a6139345b118071df64bfa4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:15:32 +0000

    Add package python-code-splitter/0.1.0/python_code_splitter-0.1.0.tar.gz

---
+++ packages/python-code-splitter/python_code_splitter-0.1.0.tar.gz/python_code_splitter-0.1.0/src/main.py
@@ -0,0 +1,12 @@
+import argparse
+from pathlib import Path
+
+from src.code_splitter import CodeSplitter
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Python Code Splitter")
+    parser.add_argument("file_path", type=str, help="Path to the Python file to split")
+    parser.add_argument("--git", action="store_true", help="Enable git commit")
+    args = parser.parse_args()
+
+    CodeSplitter(original_file_path=Path(args.file_path), git_commit=args.git).execute()
