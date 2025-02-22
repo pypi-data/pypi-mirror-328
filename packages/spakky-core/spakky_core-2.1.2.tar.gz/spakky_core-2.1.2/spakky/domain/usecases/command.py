14aba52d596843477d347489dda6521ef99d1abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:35:56 +0000

    Add package spakky-core/2.1.2/spakky_core-2.1.2.tar.gz

---
+++ packages/spakky-core/spakky_core-2.1.2.tar.gz/spakky_core-2.1.2/spakky/domain/usecases/command.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Any, TypeVar, Protocol, runtime_checkable
+
+from spakky.core.mutability import immutable
+
+
+@immutable
+class Command(ABC): ...
+
+
+CommandT_contra = TypeVar("CommandT_contra", bound=Command, contravariant=True)
+ResultT_co = TypeVar("ResultT_co", bound=Any, covariant=True)
+
+
+@runtime_checkable
+class ICommandUseCase(Protocol[CommandT_contra, ResultT_co]):
+    @abstractmethod
+    def execute(self, command: CommandT_contra) -> ResultT_co: ...
+
+
+@runtime_checkable
+class IAsyncCommandUseCase(Protocol[CommandT_contra, ResultT_co]):
+    @abstractmethod
+    async def execute(self, command: CommandT_contra) -> ResultT_co: ...
