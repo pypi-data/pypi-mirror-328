81e89d343b4da1059070baab7a1440bd02b758ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 07:09:21 +0000

    Add package mdps-app-generator/1.0.0/mdps_app_generator-1.0.0-py3-none-any.whl

---
+++ packages/mdps-app-generator/mdps_app_generator-1.0.0-py3-none-any.whl/unity_app_generator/__main__.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+# 
+# *** Unity Application Package Generator ***
+#
+# Builds an OGC compliant appplication using the Unity application interface.
+# A Papermill parameterized Jupyter notebook is introspected to determine
+# input and output paramters that are connected through CWL. This software uses
+# the app-pack-generator as well as Unity.py.
+#
+# This software performs the following steps:
+# * Builds a Docker image using repo2docker 
+# * Pushes the Docker image into a container registry
+# * Creates CWL files from application metadata and Docker registry URL
+# * Register application and pushes CWL files into Dockstore
+
+STATE_DIRECTORY = ".unity_app_gen"
+
+import os
+import logging
+from argparse import ArgumentParser
+
+from unity_app_generator.generator import UnityApplicationGenerator, ApplicationGenerationError
+
+logger = logging.getLogger()
+
+class SubCommandError(Exception):
+    pass
+
+def state_directory_path(args):
+
+    if args.state_directory is not None:
+        return os.path.realpath(args.state_directory)
+
+    if hasattr(args, "destination_directory") and args.destination_directory is not None:
+        return os.path.realpath(os.path.join(args.destination_directory, STATE_DIRECTORY))
+
+    if hasattr(args, "source_repository") and os.path.isdir(args.source_repository):
+        return os.path.realpath(os.path.join(args.source_repository, STATE_DIRECTORY))
+
+    return os.path.realpath(os.path.join(os.curdir, STATE_DIRECTORY))
+
+def check_state_directory(state_dir):
+
+    if not os.path.exists(state_dir):
+        raise SubCommandError(f"Application state directory {state_dir} does not exist, please run init sub-command first")
+
+    return state_dir
+
+def init(args):
+    state_dir = state_directory_path(args)
+
+    app_gen = UnityApplicationGenerator(state_dir, args.source_repository, args.destination_directory, args.checkout)
+
+def build_docker(args):
+    state_dir = check_state_directory(state_directory_path(args))
+
+    app_gen = UnityApplicationGenerator(state_dir, use_owner=args.use_owner)
+
+    app_gen.create_docker_image()
+
+def push_docker(args):
+    state_dir = check_state_directory(state_directory_path(args))
+
+    app_gen = UnityApplicationGenerator(state_dir)
+    
+    app_gen.push_to_docker_registry(args.container_registry, image_tag=args.image_tag)
+
+def notebook_parameters(args):
+
+    state_dir = check_state_directory(state_directory_path(args))
+
+    app_gen = UnityApplicationGenerator(state_dir)
+
+    print()
+    print(app_gen.notebook_parameters())
+
+def build_cwl(args):
+    state_dir = check_state_directory(state_directory_path(args))
+
+    app_gen = UnityApplicationGenerator(state_dir)
+
+    app_gen.create_cwl(cwl_output_path=args.cwl_output_path, docker_url=args.image_url, monolithic=args.monolithic)
+
+def push_app_registry(args):
+    state_dir = check_state_directory(state_directory_path(args))
+
+    app_gen = UnityApplicationGenerator(state_dir)
+
+    app_gen.push_to_application_registry(args.dockstore_api_url, args.dockstore_token)
+
+def main():
+    parser = ArgumentParser(description="Unity Application Package Generator")
+
+    parser.add_argument("--state_directory",
+        help=f"An alternative location to store the application state other than {STATE_DIRECTORY}")
+
+    # init
+    subparsers = parser.add_subparsers(required=True)
+
+    parser_init = subparsers.add_parser('init',
+        help=f"Initialize a Git repository for use by this application. Creates a {STATE_DIRECTORY} directory in the destination directory")
+
+    parser_init.add_argument("source_repository", 
+        help="Directory or Git URL of application source files, default is current directory")
+
+    parser_init.add_argument("destination_directory", nargs="?",
+        help="Directory where to check out source repository, default is a directory under the current subdirectory with same basename as the source_repository")
+
+    parser_init.add_argument("-c", "--checkout", required=False,
+        help="Git hash, tag or branch to checkout from the source repository")
+
+    parser_init.set_defaults(func=init)
+
+    # build_docker
+
+    parser_build_docker = subparsers.add_parser('build_docker',
+        help=f"Build a Docker image from the initialized application directory")
+
+    parser_build_docker.add_argument("--no_owner", dest="use_owner", action="store_false", default=True,
+        help="Disable using the owner of the Git repository in the Docker image tag")
+
+    parser_build_docker.set_defaults(func=build_docker)
+
+    # push_docker
+
+    parser_push_docker = subparsers.add_parser('push_docker',
+        help=f"Push a Docker image from the initialized application directory to a remote registry")
+
+    parser_push_docker.add_argument("container_registry", 
+        help="URL or Dockerhub username of a Docker registry for pushing of the built image")
+
+    parser_push_docker.add_argument("-t", "--image_tag", 
+        help="Docker image tag to push into container registry if already built without using the build_docker subcommand")
+
+    parser_push_docker.set_defaults(func=push_docker)
+
+    # notebook_parameters
+
+    parser_parameters = subparsers.add_parser('parameters',
+        help=f"Display parsed notebook parameters")
+
+    parser_parameters.set_defaults(func=notebook_parameters)
+
+    # build_cwl
+
+    parser_build_cwl = subparsers.add_parser('build_cwl',
+        help=f"Create OGC compliant CWL files from the repository and Docker image")
+
+    parser_build_cwl.add_argument("-o", "--cwl_output_path", 
+        help="Alternate location to place CWL output files other than within application state directory")
+
+    parser_build_cwl.add_argument("-u", "--image_url", 
+        help="Docker image tag or remote registry URL to be included in the generated CWL files if not using the build_docker and/or push_docker subcommands") 
+
+    parser_build_cwl.add_argument("--monolithic", action="store_true",
+        help="Use the deprecated 'monolithic' approach to generating CWL where stage in and out are bundled inside the application")
+
+    parser_build_cwl.set_defaults(func=build_cwl)
+
+    # push_app_registry
+
+    parser_app_registry = subparsers.add_parser('push_app_registry',
+        help=f"Push CWL files to Dockstore application registry")
+
+    parser_app_registry.add_argument("--api_url", dest="dockstore_api_url", required=True,
+        help="Dockstore API URL including the trailing api/ portion of the URL") 
+
+    parser_app_registry.add_argument("--token", dest="dockstore_token", required=True,
+        help="Dockstore API token obtained from the My Services / Account page") 
+
+    parser_app_registry.set_defaults(func=push_app_registry)
+
+    # Process arguments
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    try:
+        args.func(args)
+    except (SubCommandError, ApplicationGenerationError) as err:
+        parser.error(err)
+
+
+    #app_gen.push_to_application_registry(None)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/mdps-app-generator/mdps_app_generator-1.0.0-py3-none-any.whl/unity_app_generator/generator.py
@@ -0,0 +1,166 @@
+import os
+import sys
+import tempfile
+import logging
+from glob import glob
+
+from .state import ApplicationState
+
+from app_pack_generator import GitManager, DockerUtil, ApplicationNotebook
+from app_pack_generator import ProcessCWL, DataStagingCWL, Descriptor
+
+from unity_sds_client.services.application_service import DockstoreAppCatalog
+
+logger = logging.getLogger(__name__)
+
+class ApplicationGenerationError(Exception):
+    pass
+
+class UnityApplicationGenerator(object):
+
+    def __init__(self, state_directory, source_repository=None, destination_directory=None, checkout=None, use_owner=True):
+
+        if not ApplicationState.exists(state_directory):
+            self.repo_info = self._localize_source(source_repository, destination_directory, checkout)
+            self.app_state = ApplicationState(state_directory, self.repo_info.directory, source_repository)
+        else:
+            self.app_state = ApplicationState(state_directory)
+            self.repo_info = self._localize_source(self.app_state.source_repository, self.app_state.app_base_path, checkout)
+
+        self.docker_util = DockerUtil(self.repo_info, do_prune=False, use_owner=use_owner)
+
+    def _localize_source(self, source, dest, checkout):
+
+        # Check out original repository
+        git_mgr = GitManager(source, dest)
+    
+        if checkout is not None:
+            logger.debug(f"Checking out {checkout} in {repo_dir}")
+            git_mgr.checkout(checkout)
+
+        return git_mgr
+
+    def create_docker_image(self):
+
+        # Create Docker image
+        self.app_state.docker_image_tag = self.docker_util.repo2docker()
+
+    def push_to_docker_registry(self, docker_registry, image_tag=None):
+
+        if image_tag is not None:
+            self.app_state.docker_image_tag = image_tag
+        
+        if self.app_state.docker_image_tag is None:
+            raise ApplicationGenerationError("Cannot push Docker image to registry without a valid tag. Run the Docker build command or supply an image tag as an argument.")
+
+        # Push to remote repository
+        self.app_state.docker_url = self.docker_util.push_image(docker_registry, self.app_state.docker_image_tag)
+
+    def _generate_dockstore_cwl(self, cwl_output_path, target_cwl_filename):
+
+        template = f"""
+cwlVersion: v1.0
+
+class: Workflow
+doc: |
+    Required Dockstore CWL file hosted workflows registered by unity_py.
+
+steps:
+step:
+    run: {target_cwl_filename}
+"""
+        
+        # Hard code file name because it is a hard coded re
+        dockstore_cwl_filename = os.path.join(cwl_output_path, "Dockstore.cwl")
+        with open(dockstore_cwl_filename, "w") as cwl_file:
+            cwl_file.write(template.lstrip())
+
+    def create_cwl(self, cwl_output_path=None, docker_url=None, monolithic=False):
+
+        # Fall through using docker_image_tag if docker_url does not exist because no push has occurred
+        # Or if docker_url is supplied as an argument use that
+        if docker_url is None and self.app_state.docker_url is not None:
+            docker_url = self.app_state.docker_url
+        elif docker_url is None and self.app_state.docker_image_tag is not None:
+            docker_url = self.app_state.docker_image_tag
+        elif docker_url is None:
+            raise ApplicationGenerationError("Cannot create CWL files when Docker image tag or URL has not yet been registered through building and/or pushing Docker image")
+        
+        # Use passed CWL output path and set to app state, or else
+        # use existing value from application state
+        if cwl_output_path is not None:
+            self.app_state.cwl_output_path = cwl_output_path
+        else:
+            cwl_output_path = self.app_state.cwl_output_path
+
+        if not os.path.exists(cwl_output_path):
+            os.makedirs(cwl_output_path)
+    
+        notebook_filename = os.path.join(self.repo_info.directory, "process.ipynb")
+
+        app = ApplicationNotebook(notebook_filename)
+
+        logger.info("Parameters:\n" + app.parameter_summary())
+            
+        # Create CWL files depending on the mode of production
+        cwl_generators = [ ProcessCWL(app) ]
+
+        if monolithic:
+            cwl_generators.append( DataStagingCWL(app) )
+
+        files_created = []
+        for cwl_gen in cwl_generators:
+            files_created += cwl_gen.generate_all(cwl_output_path, dockerurl=docker_url)
+        
+        # Add the JSON descriptor file
+        desc = Descriptor(app, self.repo_info)
+        files_created.append(desc.generate_descriptor(cwl_output_path, docker_url))
+
+        # Add Dockstore.cwl, point it to the appropriate entry point
+        if monolithic:
+            self._generate_dockstore_cwl(cwl_output_path, "workflow.cwl")
+        else:
+            self._generate_dockstore_cwl(cwl_output_path, "process.cwl")
+
+    def notebook_parameters(self):
+
+        nb = ApplicationNotebook(self.repo_info)
+
+        params_str = "Parsed Notebook Parameters:\n"
+        params_str += nb.parameter_summary()
+
+        return params_str
+
+    def _find_existing_app(self, app_catalog, app_name):
+
+        for app_info in app_catalog.application_list(for_user=True):
+            if app_info.dockstore_info['mode'] and app_info.name == app_name:
+                return app_info
+
+    def push_to_application_registry(self, dockstore_api_url, dockstore_token):
+
+        if self.app_state.cwl_output_path is None or not os.path.exists(self.app_state.cwl_output_path):
+            raise ApplicationGenerationError("Can not register into application registry before CWL generation step")
+
+        app_catalog = DockstoreAppCatalog(dockstore_api_url, dockstore_token) 
+
+        app_name = self.repo_info.name
+        cwl_param_files = glob(os.path.join(self.app_state.cwl_output_path, "*.cwl"))
+        json_param_files = glob(os.path.join(self.app_state.cwl_output_path, "*.json"))
+
+        print(json_param_files)
+
+        if len(cwl_param_files) == 0:
+            raise ApplicationGenerationError("No application package CWL files found")
+
+        if len(json_param_files) == 0:
+            raise ApplicationGenerationError("No JSON parameter file found")
+
+        if (reg_app := self._find_existing_app(app_catalog, app_name)) is not None:
+
+            # Upload updated JSON and CWL files
+            reg_app = app_catalog.upload_files(reg_app, cwl_files=cwl_param_files, json_files=json_param_files)
+
+        else:
+            # Register a new application with the CWL and JSON files
+            reg_app = app_catalog.register(app_name=app_name, cwl_files=cwl_param_files, json_files=json_param_files, publish=True)
