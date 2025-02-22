a5c3b99cb7134050cd10b5071f470ef39d6bde93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 06:09:13 +0000

    Add package bd-map-plotter/0.2/bd_map_plotter-0.2-py3-none-any.whl

---
+++ packages/bd-map-plotter/bd_map_plotter-0.2-py3-none-any.whl/bd_map_plotter/divisions_map.py
@@ -0,0 +1,25 @@
+import os
+
+try:
+    import geopandas as gpd
+    import matplotlib.pyplot as plt
+except ImportError as e:
+    print("Missing dependencies! Install with:\n  pip install geopandas matplotlib")
+    raise e
+
+class DivisionsMap:
+    def __init__(self):
+        """Initialize and load map data from a local file."""
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        json_path = os.path.join(base_dir, "data", "divisions_map.json")
+
+        if not os.path.exists(json_path):
+            raise FileNotFoundError(f"Map data file not found: {json_path}")
+
+        self.data = gpd.read_file(json_path)
+
+    def plot(self, figureSize=(5,5),title="Divisions of Bangladesh", edgeColor="black", fillColor="lightcyan", lineWidth=2):
+        fig, ax = plt.subplots(figsize=figureSize)
+        self.data.plot(ax=ax, edgecolor=edgeColor, color=fillColor, linewidth=lineWidth)
+        ax.set_title(title)
+        plt.show()
