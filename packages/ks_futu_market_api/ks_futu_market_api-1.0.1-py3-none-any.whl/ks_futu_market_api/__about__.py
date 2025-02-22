d2adbe8a536f02eb250bac5c836ccbdfc0afe818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 11:09:12 +0000

    Add package ks_futu_market_api/1.0.1/ks_futu_market_api-1.0.1-py3-none-any.whl

---
+++ packages/ks_futu_market_api/ks_futu_market_api-1.0.1-py3-none-any.whl/ks_futu_market_api/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+# __version__ = "0.1.66" # 周期增加月份支持
+# __version__ = "1.0.0" # 移除对老虎api依赖
+__version__ = "1.0.1" # query_hisotry_n由decimal改为返回float
