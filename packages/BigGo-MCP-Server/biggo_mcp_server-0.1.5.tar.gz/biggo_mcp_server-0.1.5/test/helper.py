928500ec4415eddbc6fbf663280fb8bccb1d4f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:22 +0000

    Add package BigGo-MCP-Server/0.1.5/biggo_mcp_server-0.1.5.tar.gz

---
+++ packages/BigGo-MCP-Server/biggo_mcp_server-0.1.5.tar.gz/biggo_mcp_server-0.1.5/test/helper.py
@@ -0,0 +1,20 @@
+import pytest
+from os import environ
+from biggo_mcp_server.types.setting import BigGoMCPSetting
+
+ES_PROXY_URL = environ.get("ES_PROXY_URL",
+                           "https://api.biggo.com/api/v1/mcp-es-proxy/")
+AUTH_TOKEN_URL = environ.get("AUTH_TOKEN_URL",
+                             "https://api.biggo.com/auth/v1/token")
+
+CLIENT_ID = environ.get("CLIENT_ID", None)
+CLIENT_SECRET = environ.get("CLIENT_SECRET", None)
+
+
+@pytest.fixture
+def setting():
+    setting = BigGoMCPSetting(client_id=CLIENT_ID,
+                              client_secret=CLIENT_SECRET,
+                              es_proxy_url=ES_PROXY_URL,
+                              auth_token_url=AUTH_TOKEN_URL)
+    return setting
