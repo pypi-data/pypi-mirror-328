d1710d5030f8f16738d01054c7314ecb62599931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:08 +0000

    Add package global-open-sdk-python/1.4.3/global_open_sdk_python-1.4.3-py3-none-any.whl

---
+++ packages/global-open-sdk-python/global_open_sdk_python-1.4.3-py3-none-any.whl/com/alipay/ams/api/model/payment_method_type.py
@@ -0,0 +1,13 @@
+
+from enum import Enum, unique
+
+
+@unique
+class PaymentMethodType(Enum):
+    DISCOUNT = "DISCOUNT"
+    INTEREST_FREE = "INTEREST_FREE"
+    BALANCE_ACCOUNT = "BALANCE_ACCOUNT"
+    SETTLEMENT_CARD = "SETTLEMENT_CARD"
+    APPLEPAY = "APPLEPAY"
+    def to_ams_dict(self):
+        return self.name
