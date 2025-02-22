6199536e1ef9ed119584edb8fe5bef8cfd0af470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:08:51 +0000

    Add package column-classifier/0.1.7/column_classifier-0.1.7.tar.gz

---
+++ packages/column-classifier/column_classifier-0.1.7.tar.gz/column_classifier-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='column_classifier',
+    version='0.1.7',
+    description='A column classifier using spaCy for entity recognition.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Roberto',
+    author_email='roberto.avogadro@sintef.no',
+    url='https://github.com/roby-avo/spacy-column-classifier',
+    license='Apache License 2.0',
+    packages=find_packages(),  # Automatically finds your 'column_classifier' package
+    install_requires=[
+        'spacy==3.7.5',  # Add other dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.7',
+)
