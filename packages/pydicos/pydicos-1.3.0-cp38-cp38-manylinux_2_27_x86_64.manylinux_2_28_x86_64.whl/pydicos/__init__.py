c846f80f22f225848287ab29cdaa97574ce20969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 03:11:02 +0000

    Add package pydicos/1.3.0/pydicos-1.3.0-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pydicos/pydicos-1.3.0-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pydicos/__init__.py
@@ -0,0 +1,11 @@
+from ._dicosio import dcsread, dcswrite
+from ._loaders import *
+from .utils.time import DicosDateTime
+from pydicos.version import _version as __version__
+
+
+__author__     = "Louis Combaldieu"
+__license__    = "MIT"
+__maintainer__ = "Louis Combaldieu"
+__email__      = "louis.combaldieu@auxilia-tech.com"
+__website__    = "https://github.com/Auxilia-tech/pyDICOS"
