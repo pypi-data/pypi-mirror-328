dfc0019caaadf65d09acae53ade8fd2400c7937c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 09:09:25 +0000

    Add package geoh5-interop/1.1.1/geoh5_interop-1.1.1.tar.gz

---
+++ packages/geoh5-interop/geoh5_interop-1.1.1.tar.gz/geoh5_interop-1.1.1/geoh5_interop/__init__.py
@@ -0,0 +1,10 @@
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2024-2025 Mira Geoscience Ltd.                                   '
+#                                                                                 '
+#  This file is part of geoh5-interop meta-package.                               '
+#                                                                                 '
+#  geoh5-interop is distributed under the terms and conditions of the MIT License '
+#  (see LICENSE file at the root of this source code package).                    '
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+__version__ = "1.1.1"
