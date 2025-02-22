34db6dcbf7c47266a7e0a7598e5a4868a7087d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 01:35:32 +0000

    Add package zhmiscellany/3.7.1/zhmiscellany-3.7.1-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-3.7.1-py3-none-any.whl/zhmiscellany/__init__.py
@@ -0,0 +1,45 @@
+from . import processing, misc, mousekb, discord, fileio, netio, string, math, image, list, dict, pipes, pastebin
+
+import importlib
+import inspect
+import sys
+
+# Explicit module list (better than parsing the file)
+_MODULES = [
+    'processing', 'misc', 'mousekb', 'discord', 'fileio',
+    'netio', 'string', 'math', 'image', 'list', 'dict',
+    'pipes', 'pastebin'
+]
+
+
+# Collect functions from all modules
+class Z:
+    """Aggregate of all package functions"""
+    pass
+
+
+def _collect_functions():
+    all_funcs = []
+    for mod_name in _MODULES:
+        module = importlib.import_module(f'.{mod_name}', __package__)
+        
+        # Get functions defined IN the module (not imported ones)
+        funcs = [
+            func for _, func in inspect.getmembers(module)
+            if inspect.isfunction(func) and func.__module__ == module.__name__
+        ]
+        
+        all_funcs.extend(funcs)
+    
+    # Add to class namespace
+    for func in all_funcs:
+        setattr(Z, func.__name__, func)
+
+
+_collect_functions()
+
+# Cleanup
+del _MODULES, _collect_functions
+
+if '__main__' in sys.modules:
+    sys.modules['__main__'].__dict__['z'] = z
