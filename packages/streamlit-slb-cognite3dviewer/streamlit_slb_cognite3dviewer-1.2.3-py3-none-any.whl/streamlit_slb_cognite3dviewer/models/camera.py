5162447132b58f780e12a8866f901a1306b80c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 05:10:37 +0000

    Add package streamlit-slb-cognite3dviewer/1.2.3/streamlit_slb_cognite3dviewer-1.2.3-py3-none-any.whl

---
+++ packages/streamlit-slb-cognite3dviewer/streamlit_slb_cognite3dviewer-1.2.3-py3-none-any.whl/streamlit_slb_cognite3dviewer/models/camera.py
@@ -0,0 +1,6 @@
+class Camera:
+    def __init__(self, id, external_id, name, deviceLocation):
+        self.id = id
+        self.external_id = external_id
+        self.name = name
+        self.deviceLocation = deviceLocation
