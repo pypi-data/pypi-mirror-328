1fdc2b6a0f8fe1c9cf63da7bf54d55a8b41768d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 04:09:13 +0000

    Add package stefan/0.1.14/stefan-0.1.14.tar.gz

---
+++ packages/stefan/stefan-0.1.14.tar.gz/stefan-0.1.14/stefan/code_search/llm/llm_tag.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class LLMTag(Enum):
+    # Agents
+    AGENT_PLANNER = "agent_planner"
+    AGENT_CODER = "agent_coder"
+    AGENT_KMP_CODER = "agent_kmp_coder"
+    AGENT_SEARCH_CODE = "agent_search_code"
+    AGENT_SAMPLES_PROVIDER = "agent_samples_provider"
+    AGENT_TEXTS_UPDATER = "agent_texts_updater"
+
+    # Code search
+    CODE_SEARCH_DIRECTORY_DESCRIPTION = "code_search_directory_description"
+    CODE_SEARCH_FILE_DESCRIPTION = "code_search_file_description"
+    CODE_SEARCH_FILE_RELEVANCY = "code_search_file_relevancy"
+
+    MONTE_CARLO_SOLUTION_EVALUATION = "monte_carlo_solution_evaluation"
---
+++ packages/stefan/stefan-0.1.14.tar.gz/stefan-0.1.14/stefan/execution_input_args.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+
+from stefan.code_search.llm.llm_model import LLM_MODEL
+
+class ExecutionInputArgs(BaseModel):
+    task: str
+    working_dir: Path
+    main_agent_model: LLM_MODEL
+    child_agent_model: LLM_MODEL
