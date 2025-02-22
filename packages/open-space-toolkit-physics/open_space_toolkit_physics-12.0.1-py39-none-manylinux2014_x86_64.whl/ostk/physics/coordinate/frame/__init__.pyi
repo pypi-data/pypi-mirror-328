1a001a93f93d40cf8d5a6411ba351317eb2dccde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 09:09:08 +0000

    Add package open-space-toolkit-physics/12.0.1/open_space_toolkit_physics-12.0.1-py39-none-manylinux2014_x86_64.whl

---
+++ packages/open-space-toolkit-physics/open_space_toolkit_physics-12.0.1-py39-none-manylinux2014_x86_64.whl/ostk/physics/coordinate/frame/__init__.pyi
@@ -0,0 +1,30 @@
+from __future__ import annotations
+import ostk.physics.time
+from . import provider
+__all__ = ['Provider', 'provider']
+class Provider:
+    """
+    
+                Frame provider.
+            
+    """
+    @staticmethod
+    def _pybind11_conduit_v1_(*args, **kwargs):
+        ...
+    def get_transform_at(self, instant: ostk.physics.time.Instant) -> ...:
+        """
+                        Get the transform at a given instant.
+        
+                        Args:
+                            instant (Instant): An instant
+        
+                        Returns:
+                            Transform: Transform
+        """
+    def is_defined(self) -> bool:
+        """
+                        Check if the Static provider is defined
+        
+                        Returns:
+                            bool: True if defined
+        """
