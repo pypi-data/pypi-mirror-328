d7748ebcb2f6cc0daae4e68766a22175f07bf130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-22 12:10:15 +0000

    Add package xxpop/0.0.1/xxpop-0.0.1.tar.gz

---
+++ packages/xxpop/xxpop-0.0.1.tar.gz/xxpop-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xxpop',
+    version='0.0.1',
+    description='function xxpop that λx. (x,x.pop()) define',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/xxpop',
+    packages=find_packages(exclude=[]),
+    keywords=['xxpop'],
+    python_requires='>=3.0',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
